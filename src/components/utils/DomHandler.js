9b96f31876070120c6b5ed0d784ef98b3d93917b
Çağatay Çivici <cagatay.civici@github.com>
2016-12-31 11:26:02 +0300

    Add more utils

--- src/components/utils/DomHandler.js
+++ src/components/utils/DomHandler.js
@@ -54,6 +54,20 @@ export default class DomHandler {
         }
     }
 
+    static addClass(element, className) {
+        if (element.classList)
+            element.classList.add(className);
+        else
+            element.className += ' ' + className;
+    }
+
+    static removeClass(element, className) {
+        if (element.classList)
+            element.classList.remove(className);
+        else
+            element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+
     static relativePosition(element, target) {
         var elementDimensions = element.offsetParent ? { width: element.outerWidth, height: element.outerHeight } : this.getHiddenElementDimensions(element);
         var targetHeight = target.offsetHeight;
