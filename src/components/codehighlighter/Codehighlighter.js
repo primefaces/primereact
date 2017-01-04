59ec3a1a724f60a1681884ecda59d9c319d19539
Çağatay Çivici <cagatay.civici@github.com>
2017-01-04 15:10:02 +0300

    Fixed codehighlighter

--- src/components/codehighlighter/Codehighlighter.js
+++ src/components/codehighlighter/Codehighlighter.js
@@ -1,32 +1,22 @@
 import React, {Component} from 'react';
-import prismjs from 'prismjs/prism.js';
 
 export class CodeHighlighter extends Component {
 
     componentDidMount() {
-        prismjs.highlightElement(this.code);
-        console.log(this.code);
-    }
-
-    componentDidUpdate() {
-        prismjs.highlightElement(this.code);
+        window.Prism.highlightElement(this.code);
     }
 
     render() {
-        console.log(this.props.children);
         return (
-            <div>
-                <pre>
-                    <code ref={(el) => this.code = el} className={this.props.className}>
-                        {this.props.children}
-                    </code>
-                </pre>
-            </div>
+            <pre>
+                <code ref={(el) => this.code = el} className={this.props.className}>
+                    {this.props.children}
+                </code>
+            </pre>
         );
     }
 }
 
 CodeHighlighter.defaultProps = {
-    children: null,
     className: null
 }
--- src/showcase/codehighlighter/CodehighlighterDemo.js
+++ src/showcase/codehighlighter/CodehighlighterDemo.js
@@ -13,33 +13,26 @@ export class CodeHighlighterDemo extends Component {
                     </div>
                 </div>
 
-                <div className="content-section implementation">
+                <div className="content-section implementation ui-helper-hidden" style={{display:'none'}}>
                     <CodeHighlighter className="language-css">
-                        .ui-datatable table &#123;
-                            border-collapse:collapse;
-                            width: 100%;
-                            table-layout: fixed;
-                        &#125;
+{`
+.ui-datatable table {
+    border-collapse:collapse;
+    width: 100%;
+    table-layout: fixed;
+}
+
+`}
                     </CodeHighlighter>
 
                     <CodeHighlighter className="language-markup">
-                        &lt;div id="pm" style="width:300px"&gt;
-                        &lt;div&gt;
-                        &lt;div&gt;&lt;a data-icon="fa-file-o"&gt;File&lt;/a&gt;&lt;/div&gt;
-                        &lt;div&gt;
-                            &lt;ul&gt;
-                                &lt;li&gt;&lt;a data-icon="fa-plus"&gt;New&lt;/a&gt;
-                                    &lt;ul&gt;
-                                        &lt;li&gt;&lt;a&gt;Project&lt;/a&gt;&lt;/li&gt;
-                                        &lt;li&gt;&lt;a&gt;Other&lt;/a&gt;&lt;/li&gt;
-                                    &lt;/ul&gt;
-                                &lt;/li&gt;
-                                &lt;li&gt;&lt;a&gt;Open&lt;/a&gt;&lt;/li&gt;
-                                &lt;li&gt;&lt;a&gt;Quit&lt;/a&gt;&lt;/li&gt;
-                            &lt;/ul&gt;
-                        &lt;/div&gt;
-                        &lt;/div&gt;
-                        &lt;/div&gt;
+{`
+<div className="feature-intro">
+    <h1>CodeHighlighter</h1>
+    <p>Chips is used to enter multiple values on an inputfield.</p>
+</div>
+
+`}
                     </CodeHighlighter>
                 </div>
             </div>
