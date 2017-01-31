18a273575d3a3bf592205e4fc6f6696bdc293824
mertsincan <sincan.mert@gmail.com>
2017-01-31 15:08:58 +0300

    Added car data and service

---
+++ src/showcase/service/CarService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+export class CarService {
+    
+    getCarsSmall(_this) {
+        axios.get('showcase/resources/demo/data/cars-small.json')
+            .then(res => res.data.data)
+            .then(data => { 
+                _this.setState({ cars: data });
+                return data; 
+            });
+    }
+
+    getCarsMedium(_this) {
+        axios.get('showcase/resources/demo/data/cars-medium.json')
+            .then(res => res.data.data)
+            .then(data => { 
+                _this.setState({ cars: data });
+                return data; 
+            });
+    }
+
+    getCarsLarge(_this) {
+        axios.get('showcase/resources/demo/data/cars-large.json')
+            .then(res => res.data.data)
+            .then(data => { 
+                _this.setState({ cars: data });
+                return data; 
+            });
+    }
+}
