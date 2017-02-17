ddcd7dfe4de3456b1fd0c120982e3e93e41dee0d
Mrtcndkn <mrtcndkn@gmail.com>
2017-02-17 16:10:14 +0300

    fixed warning

--- src/showcase/codehighlight/CodeHighlightDemo.js
+++ src/showcase/codehighlight/CodeHighlightDemo.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router';
 import {CodeHighlight} from '../../components/codehighlight/CodeHighlight';
 import {TabView,TabPanel} from '../../components/tabview/TabView';
 
