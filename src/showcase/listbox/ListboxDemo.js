b3ad3019bde6b161844bd997aa205842d146a807
Mrtcndkn <mrtcndkn@gmail.com>
2017-02-17 15:58:06 +0300

    Fixed doc for listbox

--- src/showcase/listbox/ListboxDemo.js
+++ src/showcase/listbox/ListboxDemo.js
@@ -259,42 +259,77 @@ carTemplate(option) {
         <TabPanel header="Source">
 <CodeHighlight className="language-javascript">
 {`
-export class ChipsDemo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {basicValues: [], advancedValues: []};
+export class ListboxDemo extends Component {
+        
+    constructor() {
+        super();
+        this.state = {};
+        this.onCityChange = this.onCityChange.bind(this);
+        this.onCarChange = this.onCarChange.bind(this);
     }
 
-    customTemplate(item) {
+    onCityChange(e) {
+        this.setState({city: e.value});
+    }
+
+    onCarChange(e) {
+        this.setState({car: e.value});
+    }
+
+    carTemplate(option) {
+        var logoPath = 'showcase/resources/demo/images/car/' + option.label + '.gif';
+
         return (
-            <div>
-                <span>{item} - (active) </span>
-                <i className="fa fa-user"></i>
+            <div className="ui-helper-clearfix">
+                <img alt={option.label} src={logoPath} style={{display:'inline-block',margin:'5px 0 0 5px'}} />
+                <span style={{fontSize:'1em',float:'right',margin:'1em .5em 0 0'}}>{option.label}</span>
             </div>
         );
     }
 
     render() {
+        var cities = [
+            {label: 'New York', value: 'New York'},
+            {label: 'Rome', value: 'Rome'},
+            {label: 'London', value: 'London'},
+            {label: 'Istanbul', value: 'Istanbul'},
+            {label: 'Paris', value: 'Paris'},
+        ];
+
+        var cars = [
+            {label: 'Audi', value: 'Audi'},
+            {label: 'BMW', value: 'BMW'},
+            {label: 'Fiat', value: 'Fiat'},
+            {label: 'Honda', value: 'Honda'},
+            {label: 'Jaguar', value: 'Jaguar'},
+            {label: 'Mercedes', value: 'Mercedes'},
+            {label: 'Renault', value: 'Renault'},
+            {label: 'VW', value: 'VW'},
+            {label: 'Volvo', value: 'Volvo'}
+        ];
+
         return (
             <div>
                 <div className="content-section">
                     <div className="feature-intro">
-                        <h1>Chips</h1>
-                        <p>Chips is used to enter multiple values on an inputfield.</p>
+                        <h1>Listbox</h1>
+                        <p>Listbox is used to select one or more values from a list of items.</p>
                     </div>
                 </div>
 
                 <div className="content-section implementation">
                     <h3>Basic</h3>
-                    <Chips value={this.state.basicValues}></Chips>
+                    <Listbox value={this.state.city} options={cities} onChange={this.onCityChange} />
+                    <div style={{marginTop: '.5em'}}>{this.state.city ? 'Selected City: ' + this.state.city : 'No city selected'}</div>
 
                     <h3>Advanced</h3>
-                    <Chips value={this.state.advancedValues} max={5} itemTemplate={this.customTemplate}></Chips>
+                    <Listbox value={this.state.car} options={cars} onChange={this.onCarChange} itemTemplate={this.carTemplate} style={{maxHeight: '250px'}}/>
+                    <div style={{marginTop: '.5em'}}>{this.state.car ? 'Selected Car: ' + this.state.car : 'No car selected'}</div>
                 </div>
-                
-                <ChipsDoc/>
+
+                <ListboxDoc></ListboxDoc>
             </div>
-        )
+        );
     }
 }
 
