7170882587d1c9770e938e388fbe8c8ec024c2b5
Mrtcndkn <mrtcndkn@gmail.com>
2017-02-17 16:53:13 +0300

    Doc for fieldset

--- src/showcase/fieldset/FieldsetDemo.js
+++ src/showcase/fieldset/FieldsetDemo.js
@@ -1,6 +1,189 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 import {Fieldset} from '../../components/fieldset/Fieldset';
+import {TabView,TabPanel} from '../../components/tabview/TabView';
+import {CodeHighlight} from '../../components/codehighlight/CodeHighlight';
 
+export class FieldsetDemo extends Component {
+        
+    render() {
+        return (
+            <div>
+                <div className="content-section">
+                    <div className="feature-intro">
+                        <h1>Fieldset</h1>
+                        <p>Fieldset is a grouping component with a content toggle feature.</p>
+                    </div>
+                </div>
+
+                <div className="content-section implementation">
+                    <Fieldset legend="Godfather I">
+                        The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
+                        His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
+                        Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
+                        kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
+                    </Fieldset>
+
+                    <br />
+
+                    <Fieldset legend="Godfather I" toggleable={true}>
+                        The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
+                        His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
+                        Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
+                        kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
+                    </Fieldset>
+                </div>
+                <FieldsetDoc></FieldsetDoc>
+            </div>
+        )
+    }
+}
+
+export class FieldsetDoc extends Component {
+    
+    render() {
+        return (
+            <div className="content-section source">
+                <TabView>
+                    <TabPanel header="Documentation">
+                        <h3>Import</h3>
+<CodeHighlight className="language-javascript">
+{`
+import {Fieldset} from 'primereact';
+
+`}
+</CodeHighlight>
+
+            <h3>Getting Started</h3>
+            <p>Fieldset is defined with Fieldset element.</p>
+<CodeHighlight className="language-markup">
+{`
+<Fieldset legend="Godfather I">
+    The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
+    His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
+    Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
+    kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
+</Fieldset>
+
+`}
+</CodeHighlight>
+
+             <h3>Toggleable</h3>
+            <p>Content of the fieldset can be expanded and collapsed using toggleable option, default state is defined with collapsed option.</p>
+<CodeHighlight className="language-markup">
+{`
+<Fieldset legend="Godfather I" toggleable={true}>
+    The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
+    His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
+    Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
+    kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
+</Fieldset>
+
+`}
+</CodeHighlight>
+
+            <h3>Attributes</h3>
+            <div className="doc-tablewrapper">
+                <table className="doc-table">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Default</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>legend</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Header text of the fieldset.</td>
+                        </tr>
+                        <tr>
+                            <td>toggleable</td>
+                            <td>boolean</td>
+                            <td>false</td>
+                            <td>When specified, content can toggled by clicking the legend.</td>
+                        </tr>
+                        <tr>
+                            <td>collapsed</td>
+                            <td>boolean</td>
+                            <td>false</td>
+                            <td>Defines the default visibility state of the content.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <h3>Events</h3>
+            <div className="doc-tablewrapper">
+                <table className="doc-table">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Parameters</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>onBeforeToggle</td>
+                            <td>event.originalEvent: browser event  <br/>
+                                event.collapsed: state as a boolean
+                            </td>
+                            <td>Callback to invoke before content toggle.</td>
+                        </tr>
+                        <tr>
+                            <td>onAfterToggle</td>
+                            <td>event.originalEvent: browser event  <br/>
+                                event.collapsed: state as a boolean 
+                            </td>
+                            <td>Callback to invoke after content toggle.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <h3>Styling</h3>
+            <p>Following is the list of structural style classes, for theming classes visit <Link to="/theming"> theming</Link> page.</p>
+            <div className="doc-tablewrapper">
+                <table className="doc-table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Element</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>ui-fieldset</td>
+                            <td>Fieldset element.</td>
+                        </tr>
+                        <tr>
+                            <td>ui-fieldset-toggleable</td>
+                            <td>Toggleable fieldset element.</td>
+                        </tr>
+                        <tr>
+                            <td>ui-fieldset-legend</td>
+                            <td>Legend element.</td>
+                        </tr>
+                        <tr>
+                            <td>ui-fieldset-content</td>
+                            <td>Content element.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <h3>Dependencies</h3>
+                <p>None.</p>
+            </div>
+            
+            </TabPanel>
+
+            <TabPanel header="Source">
+<CodeHighlight className="language-javascript">
+{`
 export class FieldsetDemo extends Component {
         
     render() {
@@ -32,5 +215,14 @@ export class FieldsetDemo extends Component {
                 </div>
             </div>
         )
+    }
+}
+
+`}
+</CodeHighlight>
+                    </TabPanel>
+                </TabView>
+            </div>
+        );
     }
 }
