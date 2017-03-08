052e26830215a93b00d072235ec8fbc94e73e7af
Mrtcndkn <mrtcndkn@gmail.com>
2017-03-08 12:22:12 +0300

    Doc for fieldset

--- src/showcase/fieldset/FieldsetDemo.js
+++ src/showcase/fieldset/FieldsetDemo.js
@@ -112,6 +112,18 @@ import {Fieldset} from 'primereact';
                             <td>false</td>
                             <td>Defines the default visibility state of the content.</td>
                         </tr>
+                        <tr>
+                            <td>style</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Inline style of the element.</td>
+                        </tr>
+                        <tr>
+                            <td>className</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Style class of the element.</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
@@ -128,18 +140,16 @@ import {Fieldset} from 'primereact';
                     </thead>
                     <tbody>
                         <tr>
-                            <td>onBeforeToggle</td>
-                            <td>event.originalEvent: browser event  <br/>
-                                event.collapsed: state as a boolean
+                            <td>onCollapse</td>
+                            <td>event.originalEvent: browser event
                             </td>
-                            <td>Callback to invoke before content toggle.</td>
+                            <td>Callback to invoke when an active tab is collapsed by clicking on the header.</td>
                         </tr>
                         <tr>
-                            <td>onAfterToggle</td>
-                            <td>event.originalEvent: browser event  <br/>
-                                event.collapsed: state as a boolean 
+                            <td>onExpand</td>
+                            <td>event.originalEvent: browser event
                             </td>
-                            <td>Callback to invoke after content toggle.</td>
+                            <td>Callback to invoke when a tab gets expanded.</td>
                         </tr>
                     </tbody>
                 </table>
