131841b7bea378ff4f0d8e51f479b4d17c1fce73
Merve7 <merveozcifci7@gmail.com>
2017-06-28 17:18:01 +0300

    fixed #39

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import DomHandler from '../utils/DomHandler';
+import ObjectUtils from '../utils/ObjectUtils';
 import classNames from 'classnames';
 
 export class Dropdown extends Component {
@@ -48,32 +49,34 @@ export class Dropdown extends Component {
         if(this.documentClickListener) {
             document.removeEventListener('click', this.documentClickListener);
         }
-    }    
+    }
 
     onDocumentClick() {
         if(!this.selfClick&&!this.itemClick) {
             this.hide();
         }
-            
+
         this.selfClick = false;
         this.optionClick = false;
     }
 
     onOptionClick(event, option, index) {
         this.optionClick = true;
-        this.selectItem(event, option, index);           
+        this.highlightOption=option;
+        this.selectItem(event, option, index);
         this.hide();
+        this.input.focus();
         event.preventDefault();
     }
 
     selectItem(event, option, index) {
-        if(!DomHandler.hasClass(event.target,'ui-state-highlight')) {
-            this.props.onChange({
-                originalEvent: event,
-                value: option.value,
-                index: index
-            });
-        }
+        //if(!DomHandler.hasClass(event.target,'ui-state-highlight')) {
+        this.props.onChange({
+            originalEvent: event,
+            value: option.value,
+            index: index
+        });
+        // }
     }
 
     onClick() {
@@ -84,6 +87,7 @@ export class Dropdown extends Component {
         this.selfClick = true;
 
         if(!this.optionClick) {
+            this.input.focus();
             if(this.panel.offsetParent)
                 this.hide();
             else
@@ -101,7 +105,7 @@ export class Dropdown extends Component {
                 }
             }
         }
-        
+
         return selectedOption;
     }
 
@@ -121,9 +125,100 @@ export class Dropdown extends Component {
     onInputFocus(event) {
         this.setState({focus: true});
     }
-    
+
     onInputBlur(event) {
         this.setState({focus: false});
+    }
+
+    onMouseEnterForItem(option) {
+        this.setState({highlightOption: option});
+    }
+
+    onMouseLeaveForItem() {
+        this.setState({highlightOption: null});
+    }
+    findOptionIndex(option) {
+        let index = 0;
+        if(this.props.options) {
+            for(let i = 0; i < this.props.options.length; i++) {
+                if(ObjectUtils.equals(option, this.props.options[i])) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+
+    onKeydown(event) {
+        let highlightItemIndex =this.highlightOption? this.findOptionIndex(this.highlightOption):0;
+
+        switch(event.which) {
+            //down
+            case 40:
+                if( event.altKey ){
+                    this.show();
+                }
+                else{
+                    if (highlightItemIndex !== -1) {
+                        var nextItemIndex = highlightItemIndex + 1;
+                        if (nextItemIndex !== (this.props.options.length)) {
+                            this.highlightOption = this.props.options[nextItemIndex];
+                            this.highlightOptionChanged = true;
+                        }
+
+                        this.selectItem(event, this.highlightOption);
+                    }
+                    else {
+                        this.highlightOption = this.props.options[0];
+                    }
+                }
+                event.preventDefault();
+                break;
+
+            //up
+            case 38:
+                if(highlightItemIndex > 0) {
+                    let prevItemIndex = highlightItemIndex - 1;
+                    this.highlightOption = this.props.options[prevItemIndex];
+                    this.highlightOptionChanged = true;
+                    this.selectItem(event, this.highlightOption);
+                }
+
+                event.preventDefault();
+                break;
+
+            //enter
+            case 13:
+                if(this.highlightOption) {
+                    this.selectItem(event, this.highlightOption);
+                    this.hide();
+                }
+                event.preventDefault();
+                break;
+
+            //escape
+            case 27:
+                this.hide();
+                event.preventDefault();
+                break;
+
+            //space
+            case 32:
+                if(this.panel.style.display==='block') {
+                    if (this.highlightOption) {
+                        this.selectItem(event, this.highlightOption);
+                        this.hide();
+                    }
+                }
+                else{
+                    this.show();
+                }
+                event.preventDefault();
+                break;
+
+        }
+        this.setState({highlightOption: this.highlightOption});
     }
 
     render() {
@@ -138,16 +233,16 @@ export class Dropdown extends Component {
 
         if(this.props.options) {
             listItems = this.props.options.map((option, index) => {
-                    var listItemContent = this.props.itemTemplate ? this.props.itemTemplate(option) : option.label;
-                    var selected = (this.props.value != null && this.props.value === option.value) || (this.props.value == null && index === 0);
-                    var listItemStyleClass = classNames('ui-dropdown-item ui-corner-all', {'ui-state-highlight': selected});
-                    var listItem = <li className={listItemStyleClass} key={option.value}
-                                onClick={(event) => this.onOptionClick(event, option)}>
-                                {listItemContent}
-                            </li>;
+                var listItemContent = this.props.itemTemplate ? this.props.itemTemplate(option) : option.label;
+                var selected = (this.props.value != null && this.props.value === option.value) || (this.props.value == null && index === 0);
+                var listItemStyleClass = classNames('ui-dropdown-item ui-corner-all', {'ui-state-highlight': selected || this.state.highlightOption === option});
+                var listItem = <li className={listItemStyleClass} key={option.value} onClick={(event) => this.onOptionClick(event, option)}
+                                   onMouseEnter={(e) => this.onMouseEnterForItem(option)} onMouseLeave={this.onMouseLeaveForItem.bind(this)}>
+                    {listItemContent}
+                </li>;
 
-                    return listItem;
-                });
+                return listItem;
+            });
 
             optionElements = this.props.options.map((option, index) => {
                 return <option value={option.value} key={option.value}>{option.label}</option>;
@@ -160,7 +255,7 @@ export class Dropdown extends Component {
                     <select tabIndex="-1" ref={(el) => {this.selectElement = el;}}>{optionElements}</select>
                 </div>
                 <div className="ui-helper-hidden-accessible">
-                    <input readOnly type="text" onFocus={this.onInputFocus.bind(this)} onBlur={this.onInputBlur.bind(this)}/>
+                    <input readOnly ref={(el) => {this.input = el;}} type="text" onFocus={this.onInputFocus.bind(this)} onKeyDown={this.onKeydown.bind(this)} onBlur={this.onInputBlur.bind(this)}/>
                 </div>
                 <label className="ui-dropdown-label ui-inputtext ui-corner-all">{label}</label>
                 <div className="ui-dropdown-trigger ui-state-default ui-corner-right">
@@ -169,7 +264,7 @@ export class Dropdown extends Component {
                 <div className="ui-dropdown-panel ui-widget-content ui-corner-all ui-helper-hidden ui-shadow" ref={(el) => {this.panel = el;}}>
                     <div className="ui-dropdown-items-wrapper" style={{maxHeight: this.props.scrollHeight}}>
                         <ul className="ui-dropdown-items ui-dropdown-list ui-widget-content ui-widget ui-corner-all ui-helper-reset">
-                            {listItems}  
+                            {listItems}
                         </ul>
                     </div>
                 </div>
