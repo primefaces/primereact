40315c8ba7c326d64c334c80ff7e8a7591d24f9f
mertsincan <sincan.mert@gmail.com>
2017-08-15 10:11:18 +0300

    Refactor on PanelMenu

--- src/components/panelmenu/PanelMenu.js
+++ src/components/panelmenu/PanelMenu.js
@@ -1,190 +1,224 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export class PanelMenu extends Component {
+export class PanelMenuSub extends Component {
+    
     static defaultProps = {
-        model:null,
+        item: null,
+        expanded: false
+    }
+
+    static propsTypes = {
+        item: PropTypes.any,
+        expanded: PropTypes.bool
+    }
+
+    constructor(props) {
+        super(props);
+        this.item = this.props.item;
+    }
+    
+    render() {
+        let items = this.item && this.item.items,
+            menuitems = null;
+
+        if(items) {
+            menuitems = items.map((child, index) => {
+                return <PanelMenuItem key={'panelmenuitem_' + index} child={child} tabIndex={this.props.expanded ? null : '-1'} index={index} />
+            });
+        }
+
+        return (<ul className="ui-menu-list ui-helper-reset" style={{maxHeight: this.props.expanded ? '1000px' : '0px', transitionTimingFunction:'cubic-bezier(0.86, 0, 0.07, 1)',transitionDuration:'400ms'}}>
+                    {menuitems}
+                </ul>);
+    }
+}
+
+export class PanelMenuItem extends Component {
+
+    static defaultProps = {
+        child: null,
+        tabIndex: null,
+        index: null
+    }
+
+    static propsTypes = {
+        child: PropTypes.any,
+        tabIndex: PropTypes.bool,
+        index: PropTypes.any
+    }
+
+    constructor(props) {
+        super(props);
+        this.child = this.props.child;
+        this.state = {expanded: this.child.expanded};
+    }
+
+    handleClick(event, item) {       
+        if(item.disabled) {
+            event.preventDefault();
+            return;
+        }
+
+        this.setState({expanded: !this.state.expanded});
+        
+        if(!item.url) {
+            event.preventDefault();
+        }
+                   
+        if(item.command) {
+            item.command({
+                originalEvent: event,
+                item: item
+            });
+        }
+    }
+
+    render() {
+        if(this.child.separator) {
+            return <li key={'separator_' + this.props.index} className="ui-menu-separator ui-widget-content" />
+        }
+        else {
+            let menuitemClass = classNames('ui-menuitem ui-corner-all', this.child.styleClass, {
+                'ui-menu-parent': this.child.items
+            }),
+            menuitemLinkClass = classNames('ui-menuitem-link ui-corner-all', {
+                'ui-menuitem-link-hasicon': this.child.icon&&this.child.items,
+                'ui-state-disabled': this.child.disabled
+            }),
+            panelMenuIconClass = classNames('ui-panelmenu-icon fa fa-fw', {
+                'fa-caret-right': !this.state.expanded,
+                'fa-caret-down': this.state.expanded
+            }),
+            menuitemIconClass = classNames('ui-menuitem-icon fa fa-fw', this.child.icon);
+
+            let panelMenuIcon = this.child.items && (<span className={panelMenuIconClass} />),
+            menuitemIcon = this.child.icon && (<span className={menuitemIconClass} />),
+            menuitemText = <span className="ui-menuitem-text">{this.child.label}</span>;
+
+
+            return (<li key={'menuitem_' + this.props.index} className={menuitemClass} style={this.child.style}>
+                        <a href={this.child.url||'#'} className={menuitemLinkClass} tabIndex={this.props.tabIndex}
+                            onClick={(e) => this.handleClick(e, this.child)} target={this.child.target}>
+                            {panelMenuIcon}
+                            {menuitemIcon}
+                            {menuitemText}                        
+                        </a>
+
+                        {this.child.items && <PanelMenuSub item={this.child} expanded={this.state.expanded} />}
+                    </li>)
+        }
+    }
+}
+
+export class PanelMenuHeaderItems extends Component {
+    static defaultProps = {
+        item: null,
+        first: false,
+        last: false
+    }
+
+    static propsTypes = {
+        item: PropTypes.array,
+        first: PropTypes.bool,
+        last: PropTypes.bool
+    }
+
+    constructor(props) {
+        super(props);
+        this.item = this.props.item;
+        this.state = {expanded: this.item.expanded};
+    }
+
+    handleClick(event, item) {        
+        if(item.disabled) {
+            event.preventDefault();
+            return;
+        }
+        
+        this.setState({expanded: !this.state.expanded});
+        
+        if(!item.url) {
+            event.preventDefault();
+        }
+                   
+        if(item.command) {
+            item.command({
+                originalEvent: event,
+                item: item
+            });
+        }
+    }
+
+    render() {
+        let panelMenuHeaderClass = classNames('ui-widget ui-panelmenu-header ui-state-default', this.item.styleClass, {
+            'ui-corner-top': this.props.first,
+            'ui-corner-bottom': this.props.last&&!this.state.expanded,
+            'ui-state-active': this.state.expanded,
+            'ui-state-disabled': this.item.disabled
+        }),
+        headerLinkClass = classNames({'ui-panelmenu-headerlink-hasicon': this.item.icon}),
+        panelMenuIconClass = classNames('ui-panelmenu-icon fa', {
+            'fa-caret-right': !this.state.expanded,
+            'fa-caret-down': this.state.expanded
+        }),
+        menuitemIconClass = classNames('ui-menuitem-icon fa', this.item.icon);
+
+        let panelMenuIcon = this.item.items && <span className={panelMenuIconClass} />,
+            menuitemIcon = this.item.index && <span className={menuitemIconClass} />,
+            menuitemText = <span className="ui-menuitem-text">{this.item.label}</span>;
+
+        let panelMenuContentWrapperClass = classNames('ui-panelmenu-content-wrapper', {
+            'ui-panelmenu-content-wrapper-overflown': !this.state.expanded
+        }),
+        panelMenuContent = this.item.items && (<div className={panelMenuContentWrapperClass} style={{maxHeight: this.state.expanded ? '1000px' : '0px', transitionTimingFunction:'cubic-bezier(0.86, 0, 0.07, 1)', transitionDuration:'400ms'}}>
+                                    <div className="ui-panelmenu-content ui-widget-content">
+                                        <PanelMenuSub item={this.item} expanded={true} />
+                                    </div>
+                                </div>);
+
+        return (<div className="ui-panelmenu-panel">
+                    <div className={panelMenuHeaderClass} style={this.item.style}>
+                        <a href={this.item.url||'#'} className={headerLinkClass} onClick={(e) => this.handleClick(e, this.item)} target={this.item.target}>
+                            {panelMenuIcon}
+                            {menuitemIcon}
+                            {menuitemText}
+                        </a>
+                    </div>
+                    {panelMenuContent}
+                </div>)
+    }
+}
+
+export class PanelMenu extends Component {
+    
+    static defaultProps = {
+        model: null,
         style: null,
         styleClass: null
     }
 
     static propsTypes = {
-        model:PropTypes.array,
-        style: PropTypes.any,
+        model: PropTypes.array,
+        style: PropTypes.string,
         styleClass: PropTypes.string
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
     render() {
-        var divClass=classNames('ui-panelmenu ui-widget',this.props.styleClass);
+        let styleClass = classNames('ui-panelmenu ui-widget', this.props.styleClass),
+            panel = null;
 
-        return (
-            <div className={divClass} style={this.props.style} ref={el=>this.container=el} >
-                {this.props.model && this.props.model.map((item,index)=>{
-                    return <PanelMenuItem item={item} index={index} length={this.props.model.length} key={index}/>})}
-            </div>
-        );
-    }
-}
+        if(this.props.model) {
+            let length = this.props.model.length;
 
-export class PanelMenuItem extends Component{
-    static defaultProps = {
-        item:null,
-        index:null,
-        length:null,
-    }
-
-    static propsTypes = {
-        item:PropTypes.any,
-        index:PropTypes.any,
-        length:PropTypes.any
-    }
-
-    constructor(props) {
-        super(props);
-        this.item=this.props.item;
-        this.state = {expanded:this.item.expanded};
-    }
-    itemClick(event,item){
-        if(item.disabled) {
-            event.preventDefault();
-            return;
-        }
-        this.setState({expanded:!this.state.expanded});
-        if(!item.url) {
-            event.preventDefault();
-        }
-
-        if(item.command) {
-            item.command({
-                originalEvent: event,
-                item: item
+            panel = this.props.model.map((item, index) => {
+                return <PanelMenuHeaderItems key={'menuItem_' + index} item={item} first={index === 0} last={length === index}/>
             });
         }
-    }
-    render(){
-
-        var subPanel=classNames('ui-panelmenu-content-wrapper',{'ui-panelmenu-content-wrapper-overflown': !this.state.expanded})
-        var iconClass=classNames('ui-menuitem-icon fa fa-fw',this.item.icon?this.item.icon:null);
-        var panelIcon=classNames('ui-panelmenu-icon fa',{'fa-caret-right':!this.state.expanded},{' fa-caret-down':this.state.expanded});
-        var itemClass=classNames('ui-widget ui-panelmenu-header ui-state-default', this.item.styleClass, {'ui-state-active':this.state.expanded},
-            {'ui-state-disabled':this.item.disabled},{'ui-corner-top':this.props.index===0},{'ui-corner-bottom':this.props.index===this.props.length-1 &&!this.state.expanded});
-
-        return (
-            <div className="ui-panelmenu-panel" key={this.props.index}>
-                <div className={itemClass} style={this.item.style}>
-                    {this.item.url?
-                        <a href={this.item.url || '#'} className={{'ui-panelmenu-headerlink-hasicon':this.item.icon}} target={this.item.target} onClick={event=>this.itemClick(event,this.item)}>
-                            {this.item.items && <span className={panelIcon}></span>}
-                            {this.item.icon && <span className={iconClass}></span>}
-                            <span className="ui-menuitem-text">{this.item.label}</span>
-                        </a>:
-                        <a href={'#'} className={{'ui-panelmenu-headerlink-hasicon':this.item.icon}} target={this.item.target} onClick={event=>this.itemClick(event,this.item)}>
-                            {this.item.items && <span className={panelIcon}></span>}
-                            {this.item.icon && <span className={iconClass}></span>}
-                            <span className="ui-menuitem-text">{this.item.label}</span>
-                        </a>
-                    }
-                </div>
-                {this.item.items && <div className={subPanel} style={{height:!this.state.expanded?'0px':'auto',
-                    transitionTimingFunction:'cubic-bezier(0.86, 0, 0.07, 1)',transitionDuration:'400ms'}} >
-                    <div className="ui-panelmenu-content ui-widget-content">
-                        <SubItem item={this.item} expanded={true}/>
-                    </div>
-                </div>}
-        </div>);
-    }
-}
-class SubItem extends Component{
-    static defaultProps = {
-        item:null,
-        expanded:null,
-    }
-
-    static propsTypes = {
-        item:PropTypes.any,
-        expanded:PropTypes.bool
-    }
-
-    constructor(props) {
-        super(props);
-        this.item=this.props.item;
-        this.state = {};
-    }
-
-    render(){
-        return(
-            <ul className="ui-menu-list ui-helper-reset" style={{height:this.props.expanded?'auto':'0px',
-                transitionTimingFunction:'cubic-bezier(0.86, 0, 0.07, 1)',transitionDuration:'400ms'}}>
-                {this.item.items && this.item.items.map((child,index)=>{
-                    return <Item child={child} tabIndex={this.item.expanded} index={index} key={index}/>;
-                })}
-            </ul>
-        );
-    }
-}
-class Item extends Component{
-    static defaultProps = {
-        child:null,
-        tabIndex:null,
-        index:null
-    }
-
-    static propsTypes = {
-        child:PropTypes.any,
-        tabIndex:PropTypes.bool,
-        index:PropTypes.any
-    }
-
-    constructor(props) {
-        super(props);
-        this.child=this.props.child;
-        this.state = {expanded:this.child.expanded};
-    }
-    itemClick(event,item){
-        if(item.disabled) {
-            event.preventDefault();
-            return;
-        }
-        this.setState({expanded:!this.state.expanded})
-        if(!item.url) {
-            event.preventDefault();
-        }
-
-        if(item.command) {
-            item.command({
-                originalEvent: event,
-                item: item
-            });
-        }
-    }
-
-    render(){
-        var panelClass=classNames('ui-panelmenu-icon fa fa-fw',{'fa-caret-right':!this.state.expanded},{' fa-caret-down':this.state.expanded})
-        var iconClass=classNames('ui-menuitem-icon fa fa-fw',this.child.icon?this.child.icon:null);
-        var aClass=classNames('ui-menuitem-link ui-corner-all',{'ui-menuitem-link-hasicon':this.child.icon&&this.child.items,'ui-state-disabled':this.child.disabled})
-        var listClass=classNames('ui-menuitem ui-corner-all',{'ui-menu-parent':this.child.items},this.child.styleClass);
-        return(
-            this.child.separator?
-                <li className="ui-menu-separator ui-widget-content" key={this.props.index}/>:
-                <li className={listClass} style={this.child.style}  key={this.props.index}>
-                    {this.child.url?
-                        <a className={aClass} href={this.child.url || '#'} target={this.child.target} tabIndex={!this.props.tabIndex?null:'-1'} onClick={event=>{this.itemClick(event,this.child);}}>
-                            {this.child.items && <span className={panelClass}></span>}
-                            {this.child.icon && <span className={iconClass}></span>}
-                            <span className="ui-menuitem-text">{this.child.label}</span>
-                        </a>:
-                        <a className={aClass} href={'#'} target={this.child.target} tabIndex={!this.props.tabIndex?null:'-1'} onClick={event=>{this.itemClick(event,this.child);}}>
-                            {this.child.items && <span className={panelClass}></span>}
-                            {this.child.icon && <span className={iconClass}></span>}
-                            <span className="ui-menuitem-text">{this.child.label}</span>
-                        </a>}
-                    {this.child.items && <SubItem item={this.child} expanded={this.state.expanded}/>}
-                </li>
-        );
+        
+        return (<div className={styleClass} style={this.props.style}>
+                    {panel}
+                </div>)
     }
 }
