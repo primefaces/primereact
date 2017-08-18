14a90a9c5b9552e8dcc696f9cd1a12f22ed8d271
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:16:54 +0300

    Update Docs

--- src/showcase/contextmenu/ContextMenuDemo.js
+++ src/showcase/contextmenu/ContextMenuDemo.js
@@ -152,6 +152,12 @@ import {ContextMenu} from 'primereact/components/contextmenu/ContextMenu';
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        <td>id</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Unique identifier of the element.</td>
+                                    </tr>
+                                    <tr>
                                         <td>model</td>
                                         <td>array</td>
                                         <td>null</td>
