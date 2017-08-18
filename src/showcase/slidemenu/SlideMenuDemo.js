15bbeec08522e1ac0dc643221ab12b0fc8ff65cd
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:18:18 +0300

    Update Docs

--- src/showcase/slidemenu/SlideMenuDemo.js
+++ src/showcase/slidemenu/SlideMenuDemo.js
@@ -219,6 +219,12 @@ export class SlideMenuDemo extends Component {
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        <td>id</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Unique identifier of the element.</td>
+                                    </tr>
+                                    <tr>
                                         <td>model</td>
                                         <td>array</td>
                                         <td>null</td>
