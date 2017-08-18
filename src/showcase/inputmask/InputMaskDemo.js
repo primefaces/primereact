109affab4b883b5c86866f8953a6b03755694eb6
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:17:22 +0300

    Update Docs

--- src/showcase/inputmask/InputMaskDemo.js
+++ src/showcase/inputmask/InputMaskDemo.js
@@ -181,6 +181,12 @@ constructor() {
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td>id</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Unique identifier of the element.</td>
+                        </tr>
                          <tr>
                             <td>type</td>
                             <td>string</td>
