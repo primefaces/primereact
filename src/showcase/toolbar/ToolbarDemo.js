c66050c008aca9eb25fac393c280208c8d55f8c5
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:18:49 +0300

    Update Docs

--- src/showcase/toolbar/ToolbarDemo.js
+++ src/showcase/toolbar/ToolbarDemo.js
@@ -92,6 +92,12 @@ import {Toolbar} from 'primereact/components/toolbar/Toolbar';
                                 </thead>
                                 <tbody>
                                 <tr>
+                                    <td>id</td>
+                                    <td>string</td>
+                                    <td>null</td>
+                                    <td>Unique identifier of the element.</td>
+                                </tr>
+                                <tr>
                                     <td>style</td>
                                     <td>string</td>
                                     <td>null</td>
