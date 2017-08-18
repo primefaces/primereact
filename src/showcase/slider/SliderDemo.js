dab3d749de60bbdab22a2f7a601505d16dc2e547
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:18:23 +0300

    Update Docs

--- src/showcase/slider/SliderDemo.js
+++ src/showcase/slider/SliderDemo.js
@@ -148,6 +148,12 @@ onChangeSlider1(e) {
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td>id</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Unique identifier of the element.</td>
+                        </tr>
                          <tr>
                             <td>animate</td>
                             <td>boolean</td>
