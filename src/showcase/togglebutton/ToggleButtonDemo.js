2031c6a41e37549a9acbd4a900bdf6601643507c
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:18:45 +0300

    Update Docs

--- src/showcase/togglebutton/ToggleButtonDemo.js
+++ src/showcase/togglebutton/ToggleButtonDemo.js
@@ -115,6 +115,12 @@ render() {
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td>id</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Unique identifier of the element.</td>
+                        </tr>
                          <tr>
                             <td>onLabel</td>
                             <td>string</td>
