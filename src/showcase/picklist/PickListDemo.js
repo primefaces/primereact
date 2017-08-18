b66796dabf7a8c8a491756b3ce325cccd47123c9
mertsincan <sincan.mert@gmail.com>
2017-08-18 16:17:58 +0300

    Update Docs

--- src/showcase/picklist/PickListDemo.js
+++ src/showcase/picklist/PickListDemo.js
@@ -123,6 +123,12 @@ componentDidMount() {
                     </thead>
                     <tbody>
                         <tr>
+                            <td>id</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Unique identifier of the element.</td>
+                        </tr>
+                        <tr>
                             <td>source</td>
                             <td>array</td>
                             <td>null</td>
