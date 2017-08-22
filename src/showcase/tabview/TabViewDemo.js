2add17941c7b6ae149038b769dc7e648d50ace34
Merve Özçifçi <merveozcifci7@gmail.com>
2017-08-22 09:37:34 +0300

    TabView left and right icons

--- src/showcase/tabview/TabViewDemo.js
+++ src/showcase/tabview/TabViewDemo.js
@@ -17,21 +17,21 @@ export class TabViewDemo extends Component {
 
                 <div className="content-section implementation">
                     <TabView>
-                        <TabPanel header="Godfather I">
+                        <TabPanel header="Godfather I"  leftIcon="fa-calendar">
                             The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughters wedding.
                             His beloved son Michael has just come home from the war, but does not intend to become part of his fathers business.
                             Through Michaels life the nature of the family business becomes clear. The business of the family is just like the head
                             of the family, kind and benevolent to those who give respect,
                             but given to ruthless violence whenever anything stands against the good of the family.
                         </TabPanel>
-                        <TabPanel header="Godfather II">
+                        <TabPanel header="Godfather II"  rightIcon="fa-print">
                             Francis Ford Coppolas legendary continuation and sequel to his landmark 1972 film, The_Godfather parallels the young
                             Vito Corleone's rise with his son Michael's spiritual fall, deepening The_Godfathers depiction of the dark side of
                             the American dream. In the early 1900s, the child Vito flees his Sicilian village for America after the local Mafia kills his family.
                             Vito struggles to make a living, legally or illegally, for his wife and growing brood in Little Italy, killing the local Black Hand
                             Fanucci after he demands his customary cut of the tyro's business. With Fanucci gone, Vito's communal stature grows.
                         </TabPanel>
-                        <TabPanel header="Godfather III">
+                        <TabPanel header="Godfather III"  leftIcon="fa-bell-o" rightIcon="fa-bookmark-o">
                             After a break of more than 15 years, director Francis Ford Coppola and writer Mario Puzo returned to the well for this
                             third and final story of the fictional Corleone crime family. Two decades have passed, and crime kingpin Michael Corleone,
                             now divorced from his wife Kay has nearly succeeded in keeping his promise that his family would one day be completely legitimate.
@@ -64,21 +64,21 @@ import {TabView,TabPanel} from 'primereact/components/tabview/TabView';
 <CodeHighlight className="language-markup">
 {`
 <TabView>
-    <TabPanel header="Godfather I">
+    <TabPanel header="Godfather I"  leftIcon="fa-calendar">
         The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughters wedding.
         His beloved son Michael has just come home from the war, but does not intend to become part of his fathers business.
         Through Michaels life the nature of the family business becomes clear. The business of the family is just like the head
         of the family, kind and benevolent to those who give respect,
         but given to ruthless violence whenever anything stands against the good of the family.
     </TabPanel>
-    <TabPanel header="Godfather II">
+    <TabPanel header="Godfather II"  rightIcon="fa-print">
         Francis Ford Coppolas legendary continuation and sequel to his landmark 1972 film, The_Godfather parallels the young
         Vito Corleone's rise with his son Michael's spiritual fall, deepening The_Godfathers depiction of the dark side of
         the American dream. In the early 1900s, the child Vito flees his Sicilian village for America after the local Mafia kills his family.
         Vito struggles to make a living, legally or illegally, for his wife and growing brood in Little Italy, killing the local Black Hand
         Fanucci after he demands his customary cut of the tyro's business. With Fanucci gone, Vito's communal stature grows.
     </TabPanel>
-    <TabPanel header="Godfather III">
+    <TabPanel header="Godfather III"  leftIcon="fa-bell-o" rightIcon="fa-bookmark-o">
         After a break of more than 15 years, director Francis Ford Coppola and writer Mario Puzo returned to the well for this
         third and final story of the fictional Corleone crime family. Two decades have passed, and crime kingpin Michael Corleone,
         now divorced from his wife Kay has nearly succeeded in keeping his promise that his family would one day be completely legitimate.
@@ -133,6 +133,18 @@ import {TabView,TabPanel} from 'primereact/components/tabview/TabView';
                             <td>string</td>
                             <td>null</td>
                             <td>Orientation of tab headers.</td>
+                        </tr>
+                        <tr>
+                            <td>leftIcon</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Icons can be placed at left of a header.</td>
+                        </tr>
+                        <tr>
+                            <td>rightIcon</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>Icons can be placed at right of a header.</td>
                         </tr>
                     </tbody>
                 </table>
@@ -221,25 +233,27 @@ export class TabViewDemo extends Component {
 
                 <div className="content-section implementation">
                     <TabView>
-                        <TabPanel header="Godfather I">
+                        <TabView>
+                        <TabPanel header="Godfather I"  leftIcon="fa-calendar">
                             The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughters wedding.
                             His beloved son Michael has just come home from the war, but does not intend to become part of his fathers business.
                             Through Michaels life the nature of the family business becomes clear. The business of the family is just like the head
                             of the family, kind and benevolent to those who give respect,
                             but given to ruthless violence whenever anything stands against the good of the family.
                         </TabPanel>
-                        <TabPanel header="Godfather II">
+                        <TabPanel header="Godfather II"  rightIcon="fa-print">
                             Francis Ford Coppolas legendary continuation and sequel to his landmark 1972 film, The_Godfather parallels the young
                             Vito Corleone's rise with his son Michael's spiritual fall, deepening The_Godfathers depiction of the dark side of
                             the American dream. In the early 1900s, the child Vito flees his Sicilian village for America after the local Mafia kills his family.
                             Vito struggles to make a living, legally or illegally, for his wife and growing brood in Little Italy, killing the local Black Hand
                             Fanucci after he demands his customary cut of the tyro's business. With Fanucci gone, Vito's communal stature grows.
                         </TabPanel>
-                        <TabPanel header="Godfather III">
+                        <TabPanel header="Godfather III"  leftIcon="fa-bell-o" rightIcon="fa-bookmark-o">
                             After a break of more than 15 years, director Francis Ford Coppola and writer Mario Puzo returned to the well for this
                             third and final story of the fictional Corleone crime family. Two decades have passed, and crime kingpin Michael Corleone,
                             now divorced from his wife Kay has nearly succeeded in keeping his promise that his family would one day be completely legitimate.
                         </TabPanel>
+                    </TabView>
                     </TabView>
                 </div>
             </div>
