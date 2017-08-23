77ba1faeacda3f75104b394b56a90de31f241e22
mertsincan <sincan.mert@gmail.com>
2017-08-23 13:58:42 +0300

    IE11 fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'babel-polyfill';
 import App from './App';
 import {AccordionDemo} from './showcase/accordion/AccordionDemo';
 import {AutoCompleteDemo} from './showcase/autocomplete/AutoCompleteDemo';
