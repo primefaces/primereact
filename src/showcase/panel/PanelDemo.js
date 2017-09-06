df0866c99fdf771c858f92a724ca8cdd79c9e531
Merve Özçifçi <merveozcifci7@gmail.com>
2017-09-06 16:21:17 +0300

    Reorder

--- src/showcase/panel/PanelDemo.js
+++ src/showcase/panel/PanelDemo.js
@@ -111,12 +111,6 @@ import {Panel} from 'primereact/components/panel/Panel';
                             <td>Defines if content of panel can be expanded and collapsed.</td>
                         </tr>
                         <tr>
-                            <td>collapsed</td>
-                            <td>boolean</td>
-                            <td>false</td>
-                            <td>Defines the initial state of panel content, supports one or two-way binding as well.</td>
-                        </tr>
-                        <tr>
                             <td>style</td>
                             <td>string</td>
                             <td>null</td>
@@ -127,6 +121,12 @@ import {Panel} from 'primereact/components/panel/Panel';
                             <td>string</td>
                             <td>null</td>
                             <td>Style class of the element.</td>
+                        </tr>
+                        <tr>
+                            <td>collapsed</td>
+                            <td>boolean</td>
+                            <td>false</td>
+                            <td>Defines the initial state of panel content, supports one or two-way binding as well.</td>
                         </tr>
                     </tbody>
                 </table>
@@ -145,14 +145,12 @@ import {Panel} from 'primereact/components/panel/Panel';
                     <tbody>
                         <tr>
                             <td>onCollapse</td>
-                            <td>event.originalEvent: browser event
-                            </td>
+                            <td>event.originalEvent: browser event </td>
                             <td>Callback to invoke when an active tab is collapsed by clicking on the header.</td>
                         </tr>
                         <tr>
                             <td>onExpand</td>
-                            <td>event.originalEvent: browser event
-                            </td>
+                            <td>event.originalEvent: browser event </td>
                             <td>Callback to invoke when a tab gets expanded.</td>
                         </tr>
                     </tbody>
