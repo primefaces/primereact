18bf79adafaadc8fefcff752d28137633fc6d1df
Çağatay Çivici <cagatay.civici@github.com>
2017-10-09 17:36:01 +0300

    Docs for selectbutton

--- src/showcase/selectbutton/SelectButtonDemo.js
+++ src/showcase/selectbutton/SelectButtonDemo.js
@@ -25,7 +25,7 @@ export class SelectButtonDemo extends Component {
                 <div className="content-section introduction">
                     <div className="feature-intro">
                         <h1>SelectButton</h1>
-                        <p>SelectButton is used to select a boolean value using a button.</p>
+                        <p>SelectButton is used to choose single or multiple items from a list using buttons.</p>
                     </div>
                 </div>
 
@@ -65,11 +65,11 @@ import {SelectButton} from 'primereact/components/selectbutton/SelectButton';
 </CodeHighlight>
 
             <h3>Getting Started</h3>
-            <p>SelectButton is used to choose single or multiple items from a list using buttons.</p>
+            <p>SelectButton requires a value, collection of options with label-value pairs and an onChange event to provide the selected value.</p>
                     
 <CodeHighlight className="html">
 {`
-<SelectButton value={this.state.option} options={option} onChange={this.onOptionChange}></SelectButton>
+<SelectButton value={this.state.val} options={this.options} onChange={(e) => this.setState({val: event.value})}></SelectButton>
 
 `}
 </CodeHighlight>
@@ -78,22 +78,24 @@ import {SelectButton} from 'primereact/components/selectbutton/SelectButton';
 {`
 constructor(props) {
     super(props);
-    this.state = {};
-    this.onOptionChange = this.onOptionChange.bind(this);
+    this.state = {
+        val: null
+    };
+    
+    this.options = [
+        {label: 'Apartment', value: 'Apartment'},
+        {label: 'House', value: 'House'},
+        {label: 'Studio', value: 'Studio'}
+    ];
 }
-
-onOptionChange(e) {
-    this.setState({option: e.value});
-}
-
 `}
 </CodeHighlight>
 
             <h3>Multiple</h3>
-            <p>SelectButton allows selecting only one item by default and setting multiple option enables choosing more than one item. In multiple case, model property should be an array instance that is not null or undefined.</p>
+            <p>SelectButton allows selecting only one item by default and setting multiple option enables choosing more than one item. In multiple case, model property should be an array.</p>
 <CodeHighlight className="html">
 {`
-<SelectButton value={this.state.options} options={options} onChange={this.onOptionsChange} multiple={true}></SelectButton>
+<SelectButton value={this.state.values} options={this.options} onChange={(e) => this.setState({val: event.value})}></SelectButton>
 
 `}
 </CodeHighlight>
@@ -117,10 +119,10 @@ onOptionChange(e) {
                             <td>Unique identifier of the element.</td>
                         </tr>
                         <tr>
-                            <td>activeIndex</td>
+                            <td>value</td>
                             <td>any</td>
                             <td>null</td>
-                            <td>Active Index of the element.</td>
+                            <td>Value of the component.</td>
                         </tr>
                          <tr>
                             <td>options</td>
@@ -129,8 +131,8 @@ onOptionChange(e) {
                             <td>An array of objects to display as the available options.</td>
                         </tr>
                         <tr>
-                            <td>tabindex</td>
-                            <td>number</td>
+                            <td>tabIndex</td>
+                            <td>string</td>
                             <td>null</td>
                             <td>Index of the element in tabbing order.</td>
                         </tr>
@@ -158,6 +160,12 @@ onOptionChange(e) {
                             <td>null</td>
                             <td>Style class of the element.</td>
                         </tr>
+                        <tr>
+                            <td>dataKey</td>
+                            <td>string</td>
+                            <td>null</td>
+                            <td>A property to uniquely identify a value in options.</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
@@ -176,8 +184,7 @@ onOptionChange(e) {
                         <tr>
                             <td>onChange</td>
                             <td>event.originalEvent: browser event <br />
-                                event.value: single value or an array of values that are selected. <br />
-                                event.index: index of selected button </td>
+                                event.value: single value or an array of values that are selected.</td>
                             <td>Callback to invoke on state change.</td>
                         </tr>
                     </tbody>
@@ -195,52 +202,44 @@ onOptionChange(e) {
             </a>
 <CodeHighlight className="javascript">
 {`
+import React, {Component} from 'react';
+import {SelectButton} from 'primeng/components/selectbutton/SelectButton';
+
 export class SelectButtonDemo extends Component {
+    
     constructor(props) {
         super(props);
-        this.state = {};
-        this.onOptionChange = this.onOptionChange.bind(this);
-        this.onOptionsChange = this.onOptionsChange.bind(this);
-    }
-
-    onOptionChange(e) {
-        this.setState({option: e.value});
-    }
-
-    onOptionsChange(e) {
-        this.setState({options: e.value});
+        this.state = {
+            value1: null,
+            value2: null
+        };
     }
     
     render() {
-        var option = [];
-        option.push({label: 'Apartment', value: 'Apartment'});
-        option.push({label: 'House', value: 'House'});
-        option.push({label: 'Studio', value: 'Studio'});
-
-        var options = [];
-        options.push({label: 'Apartment', value: 'Apartment'});
-        options.push({label: 'House', value: 'House'});
-        options.push({label: 'Studio', value: 'Studio'});
+        let options = [
+            {label: 'Apartment', value: 'Apartment'},
+            {label: 'House', value: 'House'},
+            {label: 'Studio', value: 'Studio'}
+        ];
 
         return (
             <div>
-                <div className="content-section">
+                <div className="content-section introduction">
                     <div className="feature-intro">
                         <h1>SelectButton</h1>
-                        <p>SelectButton is used to select a boolean value using a button.</p>
+                        <p>SelectButton is used to choose single or multiple items from a list using buttons.</p>
                     </div>
                 </div>
 
                 <div className="content-section implementation">
-                    <h3>Basic</h3>
-                    <SelectButton value={this.state.option} options={option} onChange={this.onOptionChange}></SelectButton>
-                    <p>Selected Value: {this.state.option}</p>
+                    <h3>Single</h3>
+                    <SelectButton value={this.state.value1} options={options} onChange={(event) => this.setState({value1: event.value})} />
+                    <p>Selected Value: {this.state.value1}</p>
 
                     <h3>Multiple</h3>
-                    <SelectButton value={this.state.options} options={options} onChange={this.onOptionsChange} multiple={true}></SelectButton>
+                    <SelectButton value={this.state.value2} multiple={true} options={options} onChange={(event) => this.setState({value2: event.value})} />
+                    <p>Selected Values: {this.state.value2 && this.state.value2.map((val) => val)}</p>
                 </div>
-
-                <SelectButtonDoc></SelectButtonDoc>
             </div>
         );
     }
