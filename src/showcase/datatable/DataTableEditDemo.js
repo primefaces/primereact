2a93ba8e85612d6bbdaad925b911c3ad9ff106af
Çağatay Çivici <cagatay.civici@github.com>
2017-10-13 13:05:07 +0300

    Demo code

--- src/showcase/datatable/DataTableEditDemo.js
+++ src/showcase/datatable/DataTableEditDemo.js
@@ -35,12 +35,16 @@ export class DataTableEditDemo extends Component {
         this.setState({cars: updatedCars});
     }
     
+    inputTextEditor(props, field) {
+        return <InputText type="text" value={this.state.cars[props.rowIndex][field]} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+    }
+    
     vinEditor(props) {
-        return <InputText type="text" value={this.state.cars[props.rowIndex].vin} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+        return this.inputTextEditor(props, 'vin');
     }
     
     yearEditor(props) {
-        return <InputText type="text" value={this.state.cars[props.rowIndex].year} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+        return this.inputTextEditor(props, 'year');
     }
     
     brandEditor(props) {
@@ -64,7 +68,7 @@ export class DataTableEditDemo extends Component {
     }
     
     colorEditor(props) {
-        return <InputText type="text" value={this.state.cars[props.rowIndex].color} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+        return this.inputTextEditor(props, 'color');
     }
     
     saleDateEditor(props) {        
@@ -129,52 +133,121 @@ export class DataTableEditDemoDoc extends Component {
 <CodeHighlight className="javascript">
 {`
 import React, { Component } from 'react';
-import {DataTable} from 'primereact/components/datatable/DataTable';
-import {Column} from 'primereact/components/column/Column';
-import {Button} from 'primereact/components/button/Button';
+import {DataTable} from 'primereactcomponents/datatable/DataTable';
+import {Column} from 'primereactcomponents/column/Column';
+import {InputText} from 'primereactcomponents/inputtext/InputText';
+import {Dropdown} from 'primereactcomponents/dropdown/Dropdown';
+import {Calendar} from 'primereactcomponents/calendar/Calendar';
 import {CarService} from '../service/CarService';
+import {DataTableSubmenu} from '../../showcase/datatable/DataTableSubmenu';
 
-export class DataTableExportDemo extends Component {
+export class DataTableEditDemo extends Component {
 
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            cars: null
+        };
         this.carservice = new CarService();
-        this.export = this.export.bind(this);
+        this.vinEditor = this.vinEditor.bind(this);
+        this.yearEditor = this.yearEditor.bind(this);
+        this.brandEditor = this.brandEditor.bind(this);
+        this.colorEditor = this.colorEditor.bind(this);
+        this.saleDateEditor = this.saleDateEditor.bind(this);
+        this.requiredValidator = this.requiredValidator.bind(this);
     }
 
     componentDidMount() {
         this.carservice.getCarsSmall().then(data => this.setState({cars: data}));
     }
-
-    export() {
-        this.dt.exportCSV();
+    
+    onEditorValueChange(props, value) {
+        let updatedCars = [...this.state.cars];
+        updatedCars[props.rowIndex][props.field] = value;
+        this.setState({cars: updatedCars});
+    }
+    
+    inputTextEditor(props, field) {
+        return <InputText type="text" value={this.state.cars[props.rowIndex][field]} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+    }
+    
+    vinEditor(props) {
+        return this.inputTextEditor(props, 'vin');
+    }
+    
+    yearEditor(props) {
+        return this.inputTextEditor(props, 'year');
+    }
+    
+    brandEditor(props) {
+        let brands = [
+            {label: 'Audi', value: 'Audi'},
+            {label: 'BMW', value: 'BMW'},
+            {label: 'Fiat', value: 'Fiat'},
+            {label: 'Ford', value: 'Ford'},
+            {label: 'Honda', value: 'Honda'},
+            {label: 'Jaguar', value: 'Jaguar'},
+            {label: 'Mercedes', value: 'Mercedes'},
+            {label: 'Renault', value: 'Renault'},
+            {label: 'VW', value: 'VW'},
+            {label: 'Volvo', value: 'Volvo'}
+        ];
+        
+        return (
+            <Dropdown value={this.state.cars[props.rowIndex].brand} options={brands} 
+                    onChange={(e) => this.onEditorValueChange(props, e.value)} style={{width:'100%'}} placeholder="Select a City"/>
+        );
+    }
+    
+    colorEditor(props) {
+        return this.inputTextEditor(props, 'color');
+    }
+    
+    saleDateEditor(props) {        
+        return (
+            <Calendar value={this.state.cars[props.rowIndex].saleDate}
+                    onChange={(e) => this.onEditorValueChange(props, e.value)} style={{width:'100%'}} />
+        );
+    }
+    
+    saleDateTemplate(rowData, column) {
+        let value = rowData['saleDate'];
+        return value ? (value.getMonth() + 1) + '/' + value.getDate() + '/' + value.getFullYear()  : '';
+    }
+    
+    requiredValidator(props) {
+        let value = props.rowData[props.field];
+        return value && value.length > 0;
     }
 
     render() {
-        var header = <div style={{textAlign:'left'}}><Button type="button" icon="fa-file-o" iconPos="left" label="CSV" onClick={this.export}></Button></div>;
-
         return (
             <div>
-                <div className="content-section">
+                <DataTableSubmenu />
+
+                <div className="content-section introduction">
                     <div className="feature-intro">
-                        <h1>DataTable - Export</h1>
-                        <p>DataTable can export its data to CSV format.</p>
+                        <h1>DataTable - Edit</h1>
+                        <p>Incell editing is enabled by setting editable property true both on datatable and columns, 
+                        when a cell is clicked edit mode is activated, clicking outside of cell or hitting the enter 
+                        key switches back to view mode after updating the value.</p>
                     </div>
                 </div>
 
                 <div className="content-section implementation">
-                    <DataTable value={this.state.cars} header={header} ref={(el) => { this.dt = el; }}>
-                        <Column field="vin" header="Vin" />
-                        <Column field="year" header="Year" />
-                        <Column field="brand" header="Brand" />
-                        <Column field="color" header="Color" />
+                    <DataTable value={this.state.cars} editable={true}>
+                        <Column field="vin" header="Vin" editor={this.vinEditor} editorValidator={this.requiredValidator} />
+                        <Column field="year" header="Year" editor={this.yearEditor}/>
+                        <Column field="brand" header="Brand" editor={this.brandEditor}/>
+                        <Column field="color" header="Color" editor={this.colorEditor}/>
+                        <Column field="saleDate" header="Sale Date" editor={this.saleDateEditor} body={this.saleDateTemplate} />
                     </DataTable>
                 </div>
             </div>
         );
     }
 }
+
 
 `}
 </CodeHighlight>
