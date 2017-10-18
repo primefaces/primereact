557c40ec4377e17bb5e9ebdc874cbe6f7b5fdfa4
Çağatay Çivici <cagatay.civici@github.com>
2017-10-18 10:07:35 +0300

    Docs

--- src/showcase/setup/SetupPage.js
+++ src/showcase/setup/SetupPage.js
@@ -14,8 +14,8 @@ export class SetupPage extends Component {
                 </div>
 
                 <div className="content-section source">
-                <h3 style={{margin:'0'}}>Download</h3>
-                <p>PrimeReact is available at npm, if you have an existing application run the following command to download it to your project.</p>
+                    <h3 style={{margin:'0'}}>Download</h3>
+                    <p>PrimeReact is available at npm, if you have an existing application run the following command to download it to your project.</p>
 <CodeHighlight className="javascript">
 {`
 npm install primereact --save
@@ -25,8 +25,6 @@ npm install primereact --save
 
 
                 <h3>Import</h3>
-
-                <p>Importing from primereact will load all other components as well, to only import a specific component pattern would result in a smaller bundle size.</p>
 <CodeHighlight className="javascript">
 {`
 //import {ComponentName} from 'primereact/components/componentname/componentname';
@@ -35,11 +33,12 @@ import {Accordion,AccordionTab} from 'primereact/components/accordion/Accordion'
 `}
 </CodeHighlight>
                 
-                <h3>Dependencies</h3>
-                <p>Majority of PrimeReact components (95%) are native and there are some exceptions having 3rd party dependencies. In addition, components require font-awesome for icons 
-                    and <a href="https://www.npmjs.com/package/classnames">classNames</a> package to manage style classes.</p>
-
-                <p>The css dependencies are as follows, note that you may change the theme with another one of your choice.</p>
+                    <h3>Dependencies</h3>
+                    <p>Majority of PrimeReact components (95%) are native and there are some exceptions having 3rd party dependencies. In addition, components require font-awesome for icons 
+                        and <a href="https://www.npmjs.com/package/classnames">classNames</a> package to manage style classes.</p>
+                        
+                    <h3>Styles</h3>
+                    <p>The css dependencies are as follows, note that you may change the theme with another one of your choice.</p>
 <CodeHighlight className="javascript">
 {`
 primereact/resources/themes/omega/theme.css
@@ -48,7 +47,17 @@ primereact/resources/primereact.min.css
 `}
 </CodeHighlight>
 
-               <p>If you are using webpack with a css loader you may also import them to your main application component.</p>
+                    <p>primereact.min.css is a bundle that contains styles of all components, if you require a style of a specific component import the css from the folder of the component along with the common.css.</p>
+<CodeHighlight className="javascript">
+{`
+primereact/resources/themes/omega/theme.css
+primereact/components/common/common/common.css
+primereact/components/autocomplete/AutoComplete.css
+
+`}
+</CodeHighlight>
+
+                    <p>If you are using a bundler such as webpack with a css loader you may also import them to your main application component, an example from create-react-app would be.</p>
 <CodeHighlight className="javascript">
 {`
 import 'primereact/resources/primereact.min.css';
@@ -58,32 +67,36 @@ import 'font-awesome/css/font-awesome.css';
 `}
 </CodeHighlight>
 
-                <p>Here is the list of components with 3rd party dependencies.</p>
+                    <p>Here is the list of components with 3rd party dependencies.</p>
 
-                <div className="doc-tablewrapper">
-                    <table className="doc-table">
-                        <thead>
-                            <tr>
-                                <th>Component</th>
-                                <th>Dependency</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Schedule</td>
-                                <td>FullCalendar, jQuery and Moment.js</td>
-                            </tr>
-                            <tr>
-                                <td>Charts</td>
-                                <td>Charts.js 2.1.x</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                
-                <h3>Quickstart</h3>
-                <p>An example application based on create-react-app is available at <a href="https://github.com/primefaces/primereact-quickstart">github</a>.</p>
-                </div>            
+                    <div className="doc-tablewrapper">
+                        <table className="doc-table">
+                            <thead>
+                                <tr>
+                                    <th>Component</th>
+                                    <th>Dependency</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Schedule</td>
+                                    <td>FullCalendar, jQuery and Moment.js</td>
+                                </tr>
+                                <tr>
+                                    <td>Charts</td>
+                                    <td>Charts.js 2.1.x</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                    <h3>Quickstart</h3>
+                    <p>An example application based on create-react-app is available at <a href="https://github.com/primefaces/primereact-quickstart">github</a>.</p>
+                    
+                    <h3>Typescript</h3>
+                    <p>Typescript is fully supported as type definition files are provided in the npm package of PrimeReact. A sample typescript-primereact application 
+                    is available as well at <a href="https://github.com/primefaces/primereact-typescript-quickstart">github</a>.</p>
+                </div>        
             </div>
         );
     }
