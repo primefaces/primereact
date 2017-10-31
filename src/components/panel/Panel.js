6fec4fda1f47012c2c35d2132c069932f47752a0
Çağatay Çivici <cagatay.civici@github.com>
2017-10-31 13:21:54 +0300

    Fixed #170

--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import DomHandler from '../utils/DomHandler';
 import classNames from 'classnames';
 
 export class Panel extends Component {
@@ -28,13 +29,30 @@ export class Panel extends Component {
     
     constructor(props) {
         super(props);
-        this.state = {collapsed: this.props.collapsed};
+        this.state = {
+            collapsed: this.props.collapsed
+        };
         this.toggle = this.toggle.bind(this);
     }
+    
+    componentDidUpdate() {
+        setTimeout(() => {
+            DomHandler.removeClass(this.contentWrapper, 'ui-panel-content-wrapper-expanding');
+        }, 500);
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.collapsed !== this.state.collapsed) {
+            this.setState({
+                collapsed: nextProps.collapsed
+            });
+        }
+    }
 
-     toggle(e) {
+    toggle(e) {
         if(this.props.toggleable) {
-            var collapsed = this.state.collapsed;
+            let collapsed = this.state.collapsed;
+            
             if(collapsed && this.onExpand)
                 this.onExpand(e);
             else if(!collapsed && this.onCollapse)
@@ -42,30 +60,55 @@ export class Panel extends Component {
 
             this.setState({collapsed: !collapsed});
         }
+        
         e.preventDefault();
     }
-
-    render() {
-        var toggleIcon = null;
-        var className = classNames('ui-panel ui-widget ui-widget-content ui-corner-all', this.props.className);
-        
+    
+    renderToggleIcon() {
         if(this.props.toggleable) {
-            toggleIcon = <a className="ui-panel-titlebar-icon ui-panel-titlebar-toggler ui-corner-all ui-state-default" href="#" onClick={this.toggle}>
-                            <span className={classNames('fa fa-fw', {'fa-plus': this.state.collapsed, 'fa-minus': !this.state.collapsed})}></span>
-                         </a>;
+            return (
+                <a className="ui-panel-titlebar-icon ui-panel-titlebar-toggler ui-corner-all ui-state-default" href="#" onClick={this.toggle}>
+                   <span className={classNames('fa fa-fw', {'fa-plus': this.state.collapsed, 'fa-minus': !this.state.collapsed})}></span>
+                </a>
+            );
         }
+        else {
+            return null;
+        }
+    }
+    
+    renderHeader() {
+        let toggleIcon = this.renderToggleIcon();
+        
+        return (
+            <div className="ui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all">
+                <span className="ui-panel-title">{this.props.header}</span>
+                {toggleIcon}
+            </div>
+        );
+    }
+    
+    renderContent() {
+        let className = classNames('ui-panel-content-wrapper', {'ui-panel-content-wrapper-collapsed': this.state.collapsed, 'ui-panel-content-wrapper-expanded ui-panel-content-wrapper-expanding': !this.state.collapsed});
+        
+        return (
+            <div ref={(el) => this.contentWrapper = el} className={className}>
+                <div className="ui-panel-content ui-widget-content">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+    
+    render() {
+        let className = classNames('ui-panel ui-widget ui-widget-content ui-corner-all', this.props.className);
+        let header = this.renderHeader();
+        let content = this.renderContent();
         
         return (
             <div id={this.props.id} className={className} style={this.props.style}>
-                {this.props.header != null && <div className="ui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all">
-                    <span className="ui-panel-title">{this.props.header}</span>
-                    {toggleIcon}
-                </div>}
-                <div className="ui-panel-content-wrapper" style={{display: this.state.collapsed ? 'none' : 'block'}}>
-                    <div className="ui-panel-content ui-widget-content">
-                        {this.props.children}
-                    </div>
-                </div>
+                {header}
+                {content}
             </div>
         );
     }
