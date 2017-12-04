32a7e2b454547d561d008d2ea834407741ceddfd
Çağatay Çivici <cagatay.civici@github.com>
2017-12-04 11:29:33 +0300

    #207 for RadioButton

--- src/components/radiobutton/RadioButton.js
+++ src/components/radiobutton/RadioButton.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import DomHandler from '../utils/DomHandler';
 
 export class RadioButton extends Component {
 
@@ -21,6 +22,8 @@ export class RadioButton extends Component {
     constructor() {
         super();
         this.onClick = this.onClick.bind(this);
+        this.onFocus = this.onFocus.bind(this);
+        this.onBlur = this.onBlur.bind(this);
     }
 
     select(e) {
@@ -38,6 +41,14 @@ export class RadioButton extends Component {
         }
     }
 
+    onFocus(e) {
+        DomHandler.addClass(this.box, 'ui-state-focus');
+    }
+
+    onBlur(e) {
+        DomHandler.removeClass(this.box, 'ui-state-focus');
+    }
+
     render() {
         let containerClass = classNames('ui-radiobutton ui-widget', this.props.className);
         let boxClass = classNames('ui-radiobutton-box ui-widget ui-corner-all ui-state-default', { 'ui-state-active': this.props.checked });
@@ -46,9 +57,10 @@ export class RadioButton extends Component {
         return (
             <div className={containerClass} onClick={this.onClick}>
                 <div className="ui-helper-hidden-accessible">
-                    <input ref={(el) => this.input = el} type="radio" />
+                    <input ref={(el) => this.input = el} type="radio" checked={this.props.checked}
+                        onFocus={this.onFocus} onBlur={this.onBlur}/>
                 </div>
-                <div className={boxClass} >
+                <div className={boxClass} ref={(el) => { this.box = el; }}>
                     <span className={iconClass}></span>
                 </div>
             </div>
