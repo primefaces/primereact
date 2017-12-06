b49880cbd0413f7878a89c2363c65f9dbe3b6aca
mertsincan <sincan.mert@gmail.com>
2017-12-06 11:16:48 +0300

    Refactor on Paginator demo

--- src/showcase/paginator/PaginatorDemo.js
+++ src/showcase/paginator/PaginatorDemo.js
@@ -8,14 +8,22 @@ export class PaginatorDemo extends Component {
 
     constructor() {
         super();
-        this.state = {first: 0, rows: 10};
+        this.state = {first: 0, rows: 10, first2: 0, rows2: 10};
         this.onPageChange = this.onPageChange.bind(this);
+        this.onPageChange2 = this.onPageChange2.bind(this);
     }
 
     onPageChange(event) {
         this.setState({
             first: event.first,
             rows: event.rows
+        });
+    }
+
+    onPageChange2(event) {
+        this.setState({
+            first2: event.first,
+            rows2: event.rows
         });
     }
 
@@ -34,7 +42,7 @@ export class PaginatorDemo extends Component {
                     <Paginator first={this.state.first} rows={this.state.rows} totalRecords={120} rowsPerPageOptions={[10,20,30]} onPageChange={this.onPageChange}></Paginator>
                     
                     <h3>Custom Template</h3>
-                    <Paginator first={this.state.first} rows={this.state.rows} totalRecords={120} rowsPerPageOptions={[10,20,30]} onPageChange={this.onPageChange}
+                    <Paginator first={this.state.first2} rows={this.state.rows2} totalRecords={120} rowsPerPageOptions={[10,20,30]} onPageChange={this.onPageChange2}
                         template="RowsPerPageDropdown PageLinks FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"></Paginator>
                 </div>
 
@@ -270,38 +278,50 @@ import {Paginator} from 'primereact/components/paginator/Paginator';
 <CodeHighlight className="javascript">
 {`
 export class PaginatorDemo extends Component {
-
-    constructor() {
-        super();
-        this.state = {first: 0, rows: 10};
-        this.onPageChange = this.onPageChange.bind(this);
-    }
-
-    onPageChange(event) {
-        this.setState({
-            first: event.first,
-            rows: event.rows
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="content-section">
-                    <div className="feature-intro">
-                        <h1>Paginator</h1>
-                        <p>Paginator is a generic widget to display content in paged format.</p>
+    
+        constructor() {
+            super();
+            this.state = {first: 0, rows: 10, first2: 0, rows2: 10};
+            this.onPageChange = this.onPageChange.bind(this);
+            this.onPageChange2 = this.onPageChange2.bind(this);
+        }
+    
+        onPageChange(event) {
+            this.setState({
+                first: event.first,
+                rows: event.rows
+            });
+        }
+    
+        onPageChange2(event) {
+            this.setState({
+                first2: event.first,
+                rows2: event.rows
+            });
+        }
+    
+        render() {
+            return (
+                <div>
+                    <div className="content-section introduction">
+                        <div className="feature-intro">
+                            <h1>Paginator</h1>
+                            <p>Paginator is a generic widget to display content in paged format.</p>
+                        </div>
+                    </div>
+    
+                    <div className="content-section implementation">
+                        <h3>Default</h3>
+                        <Paginator first={this.state.first} rows={this.state.rows} totalRecords={120} rowsPerPageOptions={[10,20,30]} onPageChange={this.onPageChange}></Paginator>
+                        
+                        <h3>Custom Template</h3>
+                        <Paginator first={this.state.first2} rows={this.state.rows2} totalRecords={120} rowsPerPageOptions={[10,20,30]} onPageChange={this.onPageChange2}
+                            template="RowsPerPageDropdown PageLinks FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"></Paginator>
                     </div>
                 </div>
-
-                <div className="content-section implementation">
-                    <Paginator first={this.state.first} rows={this.state.rows} totalRecords={120} rowsPerPageOptions={[10,20,30]} onPageChange={this.onPageChange}></Paginator>
-                </div>
-
-            </div>
-        );
+            );
+        }
     }
-}
 
 `}
 </CodeHighlight>
