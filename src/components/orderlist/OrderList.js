813793b4a7aec93b2709ccd28f25f057088fdc18
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-02-12 10:40:24 +0300

    Fixed #308

--- src/components/orderlist/OrderList.js
+++ src/components/orderlist/OrderList.js
@@ -17,7 +17,6 @@ export class OrderList extends Component {
         listStyle: null,
         responsive: false,
         dragdrop: false,
-        dragdropScope: null,
         onChange: null,
         itemTemplate: null
     }
@@ -31,7 +30,6 @@ export class OrderList extends Component {
         listStyle: PropTypes.object,
         responsive: PropTypes.bool,
         dragdrop: PropTypes.func,
-        dragdropScope: PropTypes.string,
         onChange: PropTypes.func,
         itemTemplate: PropTypes.func
     }
@@ -44,11 +42,6 @@ export class OrderList extends Component {
 
         this.onItemClick = this.onItemClick.bind(this);
         this.onReorder = this.onReorder.bind(this);
-        this.onDragOver = this.onDragOver.bind(this);
-        this.onDrop = this.onDrop.bind(this);
-        this.onDragLeave = this.onDragLeave.bind(this);
-        this.onDragStart = this.onDragStart.bind(this);
-        this.onDragEnd = this.onDragEnd.bind(this);
     }
 
     onItemClick(event) {
@@ -71,78 +64,6 @@ export class OrderList extends Component {
         }
             
         this.setState({selection: selection});
-    }
-    
-    isSelected(item) {
-        return this.findIndexInList(item, this.state.selection) !== -1;
-    }
-
-    findIndexInList(item, list) {
-        let index = -1;
-
-        if (list) {
-            for (let i = 0; i < list.length; i++) {
-                if (list[i] === item) {
-                    index = i;
-                    break;
-                }
-            }
-        }
-
-        return index;
-    }
-
-    onDragStart(event, index) {
-        this.dragging = true;
-        this.draggedItemIndex = index;
-        if(this.props.dragdropScope) {
-            event.dataTransfer.setData("text", this.props.dragdropScope);
-        }
-    }
-    
-    onDragOver(event, index) {
-        if(this.draggedItemIndex !== index && this.draggedItemIndex + 1 !== index) {
-            this.dragOverItemIndex = index;
-            DomHandler.addClass(event.target, 'ui-state-highlight');
-            event.preventDefault();
-        }
-    }
-    
-    onDragLeave(event, index) {
-        this.dragOverItemIndex = null;
-        DomHandler.removeClass(event.target, 'ui-state-highlight');
-    }
-    
-    onDrop(event, index) {
-        let dropIndex = (this.draggedItemIndex > index) ? index : (index === 0) ? 0 : index - 1;
-        let value = [...this.props.value];
-        ObjectUtils.reorderArray(value, this.draggedItemIndex, dropIndex);
-        this.dragOverItemIndex = null;
-        DomHandler.removeClass(event.target, 'ui-state-highlight');
-
-        if(this.props.onChange) {
-            this.props.onChange({
-                originalEvent: event,
-                value: value
-            })
-        }
-    }
-    
-    onDragEnd(event) {
-        this.dragging = false;
-    }
-    
-    onListMouseMove(event) {
-        if(this.dragging) {
-            let offsetY = this.listContainer.getBoundingClientRect().top + document.body.scrollTop;
-            let bottomDiff = (offsetY + this.listContainer.clientHeight) - event.pageY;
-            let topDiff = (event.pageY - offsetY);
-            if(bottomDiff < 25 && bottomDiff > 0)
-               
-            this.listContainer.scrollTop += 15;
-            else if(topDiff < 25 && topDiff > 0)
-                this.listContainer.scrollTop -= 15;
-        }
     }
 
     onReorder(event) {
@@ -199,7 +120,9 @@ export class OrderList extends Component {
             <div ref={(el) => this.element = el} id={this.props.id} className={className} style={this.props.style}>
                 <OrderListControls value={this.props.value} selection={this.state.selection} onReorder={this.onReorder} />
                 <OrderListSubList ref={(el) => this.subList = el} value={this.props.value} selection={this.state.selection} onItemClick={this.onItemClick} 
-                            itemTemplate={this.props.itemTemplate} header={this.props.header} listStyle={this.props.listStyle}/>
+                            itemTemplate={this.props.itemTemplate} header={this.props.header} listStyle={this.props.listStyle}
+                            dragdrop={this.props.dragdrop} onDragStart={this.onDragStart} onDragEnter={this.onDragEnter} onDragEnd={this.onDragEnd} onDragLeave={this.onDragEnter} onDrop={this.onDrop}
+                            onChange={this.props.onChange} />
             </div>
         );
     }
--- src/components/orderlist/OrderListSubList.js
+++ src/components/orderlist/OrderListSubList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { OrderListItem } from './OrderListItem';
 import ObjectUtils from '../utils/ObjectUtils';
+import DomHandler from '../utils/DomHandler';
 
 export class OrderListSubList extends Component {
     
@@ -12,7 +12,13 @@ export class OrderListSubList extends Component {
         header: null,
         listStyle: null,
         itemTemplate: null,
-        onItemClick: null
+        dragdrop: false,
+        onDragStart: null,
+        onDragEnd: null,
+        onDragLeave: null,
+        onDrop: null,
+        onItemClick: null,
+        onChange: null
     }
 
     static propsTypes = {
@@ -21,11 +27,86 @@ export class OrderListSubList extends Component {
         header: PropTypes.string,
         listStyle: PropTypes.object,
         itemTemplate: PropTypes.func,
-        onItemClick: PropTypes.func
+        dragdrop: PropTypes.func,
+        onDragStart: PropTypes.func,
+        onDragEnd: PropTypes.func,
+        onDragLeave: PropTypes.func,
+        onDrop: PropTypes.func,
+        onItemClick: PropTypes.func,
+        onChange: PropTypes.func
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.onDragEnd = this.onDragEnd.bind(this);
+        this.onDragLeave = this.onDragLeave.bind(this);
+        this.onDrop = this.onDrop.bind(this);
+        this.onListMouseMove = this.onListMouseMove.bind(this);
     }
          
     isSelected(item) {
         return ObjectUtils.findIndexInList(item, this.props.selection) !== -1;
+    }
+
+    onDragStart(event, index) {
+        this.dragging = true;
+        this.draggedItemIndex = index;
+        if(this.props.dragdropScope) {
+            event.dataTransfer.setData('text', 'orderlist');
+        }
+    }
+
+    onDragOver(event, index) {
+        if(this.draggedItemIndex !== index && this.draggedItemIndex + 1 !== index) {
+            this.dragOverItemIndex = index;
+            DomHandler.addClass(event.target, 'ui-state-highlight');
+            event.preventDefault();
+        }
+    }
+    
+    onDragLeave(event) {
+        this.dragOverItemIndex = null;
+        DomHandler.removeClass(event.target, 'ui-state-highlight');
+    }
+    
+    onDrop(event) {
+        let dropIndex = (this.draggedItemIndex > this.dragOverItemIndex) ? this.dragOverItemIndex : (this.dragOverItemIndex === 0) ? 0 : this.dragOverItemIndex - 1;
+        let value = [...this.props.value];
+        ObjectUtils.reorderArray(value, this.draggedItemIndex, dropIndex);
+        this.dragOverItemIndex = null;
+        DomHandler.removeClass(event.target, 'ui-state-highlight');
+
+        if(this.props.onChange) {
+            this.props.onChange({
+                originalEvent: event,
+                value: value
+            })
+        }
+    }
+    
+    onDragEnd(event) {
+        this.dragging = false;
+    }
+
+    onListMouseMove(event) {
+        if(this.dragging) {
+            let offsetY = this.listElement.getBoundingClientRect().top + DomHandler.getWindowScrollTop();
+            let bottomDiff = (offsetY + this.listElement.clientHeight) - event.pageY;
+            let topDiff = (event.pageY - offsetY);
+
+            if(bottomDiff < 25 && bottomDiff > 0)
+                this.listElement.scrollTop += 15;
+            else if(topDiff < 25 && topDiff > 0)
+                this.listElement.scrollTop -= 15;
+        }
+    }
+
+    renderDropPoint(item, index, key) {
+        return (
+            <li key={key} className="ui-orderlist-droppoint"
+                            onDragOver={(e) => this.onDragOver(e, index + 1)} onDragLeave={this.onDragLeave} onDrop={this.onDrop}></li>
+        );
     }
         
     render() {
@@ -39,14 +120,35 @@ export class OrderListSubList extends Component {
         
         if (this.props.value) {
             items = this.props.value.map((item, i) => {
-                return <OrderListItem key={JSON.stringify(item)} index={i} value={item} template={this.props.itemTemplate} selected={this.isSelected(item)} onClick={this.props.onItemClick} />
+                let content = this.props.itemTemplate ? this.props.itemTemplate(item) : item;
+                let itemClassName = classNames('ui-orderlist-item', this.props.className, {'ui-state-highlight': this.isSelected(item)});
+                let key = JSON.stringify(item);
+
+                if(this.props.dragdrop) {
+                    let items = [
+                        this.renderDropPoint(item, i, key + '_droppoint'),
+                        <li key={key} className={itemClassName} onClick={(e) => this.props.onItemClick({originalEvent: e, value: item, index: i})}
+                            draggable="true" onDragStart={(e) => this.onDragStart(e, i)} onDragEnd={this.onDragEnd}>{content}</li>
+                    ];
+
+                    if(i === this.props.value.length - 1) {
+                        items.push(this.renderDropPoint(item, i, key + '_droppoint_end'));
+                    }
+
+                    return items;
+                }
+                else {
+                    return (
+                        <li key={JSON.stringify(item)} className={itemClassName} onClick={(e) => this.props.onItemClick({originalEvent: e, value: item, index: i})}>{content}</li>
+                    );
+                }
             });
         }
         
         return (
             <div className="ui-orderlist-list-container">
                 {header}
-                <ul ref={(el) => this.listElement = el} className={listClassName} style={this.props.listStyle}>
+                <ul ref={(el) => this.listElement = el} className={listClassName} style={this.props.listStyle} onDragOver={this.onListMouseMove}>
                     {items}
                 </ul>
             </div>
