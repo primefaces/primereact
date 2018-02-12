9781ca628c8df275bedf44272bf192ddc3c59676
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-02-12 15:30:33 +0300

    Demo update

--- src/showcase/datatable/DataTableColResizeDemo.js
+++ src/showcase/datatable/DataTableColResizeDemo.js
@@ -11,13 +11,15 @@ export class DataTableColResizeDemo extends Component {
     constructor() {
         super();
         this.state = {
-            cars: []
+            cars1: [],
+            cars2: []
         };
         this.carservice = new CarService();
     }
 
     componentDidMount() {
-        this.carservice.getCarsSmall().then(data => this.setState({cars: data}));
+        this.carservice.getCarsSmall().then(data => this.setState({cars1: data}));
+        this.carservice.getCarsMedium().then(data => this.setState({cars2: data}));
     }
 
     render() {
@@ -35,7 +37,7 @@ export class DataTableColResizeDemo extends Component {
 
                 <div className="content-section implementation">
                     <h3>Fit Mode</h3>
-                    <DataTable value={this.state.cars} resizableColumns={true} columnResizeMode="fit">
+                    <DataTable value={this.state.cars1} resizableColumns={true} columnResizeMode="fit">
                         <Column field="vin" header="Vin" style={{width:'20%'}}/>
                         <Column field="year" header="Year" style={{width:'40%'}}/>
                         <Column field="brand" header="Brand" style={{width:'20%'}}/>
@@ -43,11 +45,19 @@ export class DataTableColResizeDemo extends Component {
                     </DataTable>
 
                     <h3>Expand Mode</h3>
-                    <DataTable value={this.state.cars} resizableColumns={true} columnResizeMode="expand">
+                    <DataTable value={this.state.cars1} resizableColumns={true} columnResizeMode="expand">
                         <Column field="vin" header="Vin" />
                         <Column field="year" header="Year" />
                         <Column field="brand" header="Brand" />
                         <Column field="color" header="Color" />
+                    </DataTable>
+
+                    <h3>Scrollable Mode</h3>
+                    <DataTable value={this.state.cars2} resizableColumns={true} scrollable={true} scrollHeight="200px">
+                        <Column field="vin" header="Vin" style={{width:'20%'}}/>
+                        <Column field="year" header="Year" style={{width:'40%'}}/>
+                        <Column field="brand" header="Brand" style={{width:'20%'}}/>
+                        <Column field="color" header="Color" style={{width:'20%'}}/>
                     </DataTable>
                 </div>
 
@@ -80,43 +90,43 @@ export class DataTableColResizeDemo extends Component {
     constructor() {
         super();
         this.state = {
-            cars: []
+            cars1: [],
+            cars2: []
         };
         this.carservice = new CarService();
     }
 
     componentDidMount() {
-        this.carservice.getCarsSmall().then(data => this.setState({cars: data}));
+        this.carservice.getCarsSmall().then(data => this.setState({cars1: data}));
+        this.carservice.getCarsMedium().then(data => this.setState({cars2: data}));
     }
 
     render() {
         return (
             <div>
-                <div className="content-section introduction">
-                    <div className="feature-intro">
-                        <h1>DataTable - Column Resize</h1>
-                        <p>Columns can be resized using drag drop by setting the resizableColumns to true. There are two resize modes; "fit" and "expand". Fit is the default one and the overall table width does not change when a column is resized. 
-                            In "expand" mode, table width also changes along with the column width. onColumnResize is a callback that passes the resized column header as a parameter.</p>
-                    </div>
-                </div>
+                <h3>Fit Mode</h3>
+                <DataTable value={this.state.cars1} resizableColumns={true} columnResizeMode="fit">
+                    <Column field="vin" header="Vin" style={{width:'20%'}}/>
+                    <Column field="year" header="Year" style={{width:'40%'}}/>
+                    <Column field="brand" header="Brand" style={{width:'20%'}}/>
+                    <Column field="color" header="Color" style={{width:'20%'}}/>
+                </DataTable>
 
-                <div className="content-section implementation">
-                    <h3>Fit Mode</h3>
-                    <DataTable value={this.state.cars} resizableColumns={true} columnResizeMode="fit">
-                        <Column field="vin" header="Vin" style={{width:'20%'}}/>
-                        <Column field="year" header="Year" style={{width:'40%'}}/>
-                        <Column field="brand" header="Brand" style={{width:'20%'}}/>
-                        <Column field="color" header="Color" style={{width:'20%'}}/>
-                    </DataTable>
+                <h3>Expand Mode</h3>
+                <DataTable value={this.state.cars1} resizableColumns={true} columnResizeMode="expand">
+                    <Column field="vin" header="Vin" />
+                    <Column field="year" header="Year" />
+                    <Column field="brand" header="Brand" />
+                    <Column field="color" header="Color" />
+                </DataTable>
 
-                    <h3>Expand Mode</h3>
-                    <DataTable value={this.state.cars} resizableColumns={true} columnResizeMode="expand">
-                        <Column field="vin" header="Vin" />
-                        <Column field="year" header="Year" />
-                        <Column field="brand" header="Brand" />
-                        <Column field="color" header="Color" />
-                    </DataTable>
-                </div>
+                <h3>Scrollable Mode</h3>
+                <DataTable value={this.state.cars2} resizableColumns={true} scrollable={true} scrollHeight="200px">
+                    <Column field="vin" header="Vin" style={{width:'20%'}}/>
+                    <Column field="year" header="Year" style={{width:'40%'}}/>
+                    <Column field="brand" header="Brand" style={{width:'20%'}}/>
+                    <Column field="color" header="Color" style={{width:'20%'}}/>
+                </DataTable>
             </div>
         );
     }
