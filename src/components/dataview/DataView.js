873208ff9bbd3cb3c644429067ed798f5ad5d33a
mertsincan <sincan.mert@gmail.com>
2018-05-24 15:11:43 +0300

    Fixed #420

--- src/components/dataview/DataView.js
+++ src/components/dataview/DataView.js
@@ -125,7 +125,7 @@ export class DataView extends Component {
                 else if (value1 == null && value2 == null)
                     result = 0;
                 else if (typeof value1 === 'string' && typeof value2 === 'string')
-                    result = value1.localeCompare(value2);
+                    result = value1.localeCompare(value2, undefined, { numeric: true });
                 else
                     result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
                 return (this.state.sortOrder * result);
