399fee91788f5e54b7d1b2f4ce2e845e5ed20f75
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-07-06 12:00:07 +0300

    Documentation update for schedule

--- src/showcase/schedule/ScheduleDemo.js
+++ src/showcase/schedule/ScheduleDemo.js
@@ -6,81 +6,83 @@ import { CodeHighlight } from '../codehighlight/CodeHighlight';
 export class ScheduleDemo extends Component {
 
 	constructor() {
-		super();
-		this.header = {
+        super();
+        this.state = {
+            events: [
+                {
+                    "id": 1,
+                    "title": "All Day Event",
+                    "start": "2017-02-01"
+                },
+                {
+                    "id": 2,
+                    "title": "Long Event",
+                    "start": "2017-02-07",
+                    "end": "2017-02-10"
+                },
+                {
+                    "id": 3,
+                    "title": "Repeating Event",
+                    "start": "2017-02-09T16:00:00"
+                },
+                {
+                    "id": 4,
+                    "title": "Repeating Event",
+                    "start": "2017-02-16T16:00:00"
+                },
+                {
+                    "id": 5,
+                    "title": "Conference",
+                    "start": "2017-02-11",
+                    "end": "2017-02-13"
+                },
+                {
+                    "id": 6,
+                    "title": "Meeting",
+                    "start": "2017-02-12T10:30:00",
+                    "end": "2017-02-12T12:30:00"
+                },
+                {
+                    "id": 7,
+                    "title": "Lunch",
+                    "start": "2017-02-12T12:00:00"
+                },
+                {
+                    "id": 8,
+                    "title": "Meeting",
+                    "start": "2017-02-12T14:30:00"
+                },
+                {
+                    "id": 9,
+                    "title": "Happy Hour",
+                    "start": "2017-02-12T17:30:00"
+                },
+                {
+                    "id": 10,
+                    "title": "Dinner",
+                    "start": "2017-02-12T20:00:00"
+                },
+                {
+                    "id": 11,
+                    "title": "Birthday Party",
+                    "start": "2017-02-13T07:00:00"
+                },
+                {
+                    "id": 12,
+                    "title": "Click for Google",
+                    "url": "http://google.com/",
+                    "start": "2017-02-28"
+                }
+            ]
+        }
+	}
+
+	render() {
+        const header = {
 			left: 'prev,next today',
 			center: 'title',
 			right: 'month,agendaWeek,agendaDay'
 		};
-	}
-
-	render() {
-		this.events = [
-			{
-				"id": 1,
-				"title": "All Day Event",
-				"start": "2017-02-01"
-			},
-			{
-				"id": 2,
-				"title": "Long Event",
-				"start": "2017-02-07",
-				"end": "2017-02-10"
-			},
-			{
-				"id": 3,
-				"title": "Repeating Event",
-				"start": "2017-02-09T16:00:00"
-			},
-			{
-				"id": 4,
-				"title": "Repeating Event",
-				"start": "2017-02-16T16:00:00"
-			},
-			{
-				"id": 5,
-				"title": "Conference",
-				"start": "2017-02-11",
-				"end": "2017-02-13"
-			},
-			{
-				"id": 6,
-				"title": "Meeting",
-				"start": "2017-02-12T10:30:00",
-				"end": "2017-02-12T12:30:00"
-			},
-			{
-				"id": 7,
-				"title": "Lunch",
-				"start": "2017-02-12T12:00:00"
-			},
-			{
-				"id": 8,
-				"title": "Meeting",
-				"start": "2017-02-12T14:30:00"
-			},
-			{
-				"id": 9,
-				"title": "Happy Hour",
-				"start": "2017-02-12T17:30:00"
-			},
-			{
-				"id": 10,
-				"title": "Dinner",
-				"start": "2017-02-12T20:00:00"
-			},
-			{
-				"id": 11,
-				"title": "Birthday Party",
-				"start": "2017-02-13T07:00:00"
-			},
-			{
-				"id": 12,
-				"title": "Click for Google",
-				"url": "http://google.com/",
-				"start": "2017-02-28"
-			}
-		];
 
 		return (
 			<div>
@@ -92,11 +94,11 @@ export class ScheduleDemo extends Component {
 				</div>
 
 				<div className="content-section implementation">
-					<Schedule events={this.events} header={this.header} defaultDate="2017-02-01" eventLimit={4}></Schedule>
-				</div >
+					<Schedule events={this.state.events} header={header} defaultDate="2017-02-01" eventLimit={4} />
+				</div>
 
 				<ScheduleDoc />
-			</div >
+			</div>
 		);
 	}
 }
@@ -132,81 +134,85 @@ import {Schedule} from 'primereact/schedule';
 
 <CodeHighlight className="language-javascript">
 {`
-constructor() {
-    super();
-}
+export class ScheduleDemo extends Component {
 
-render() {
-    this.events = [
-			{
-				"id": 1,
-				"title": "All Day Event",
-				"start": "2017-02-01"
-			},
-			{
-				"id": 2,
-				"title": "Long Event",
-				"start": "2017-02-07",
-				"end": "2017-02-10"
-			},
-			{
-				"id": 3,
-				"title": "Repeating Event",
-				"start": "2017-02-09T16:00:00"
-			},
-			{
-				"id": 4,
-				"title": "Repeating Event",
-				"start": "2017-02-16T16:00:00"
-			},
-			{
-				"id": 5,
-				"title": "Conference",
-				"start": "2017-02-11",
-				"end": "2017-02-13"
-			},
-			{
-				"id": 6,
-				"title": "Meeting",
-				"start": "2017-02-12T10:30:00",
-				"end": "2017-02-12T12:30:00"
-			},
-			{
-				"id": 7,
-				"title": "Lunch",
-				"start": "2017-02-12T12:00:00"
-			},
-			{
-				"id": 8,
-				"title": "Meeting",
-				"start": "2017-02-12T14:30:00"
-			},
-			{
-				"id": 9,
-				"title": "Happy Hour",
-				"start": "2017-02-12T17:30:00"
-			},
-			{
-				"id": 10,
-				"title": "Dinner",
-				"start": "2017-02-12T20:00:00"
-			},
-			{
-				"id": 11,
-				"title": "Birthday Party",
-				"start": "2017-02-13T07:00:00"
-			},
-			{
-				"id": 12,
-				"title": "Click for Google",
-				"url": "http://google.com/",
-				"start": "2017-02-28"
-			}
-		];
+	constructor() {
+        super();
+        this.state = {
+            events: [
+                {
+                    "id": 1,
+                    "title": "All Day Event",
+                    "start": "2017-02-01"
+                },
+                {
+                    "id": 2,
+                    "title": "Long Event",
+                    "start": "2017-02-07",
+                    "end": "2017-02-10"
+                },
+                {
+                    "id": 3,
+                    "title": "Repeating Event",
+                    "start": "2017-02-09T16:00:00"
+                },
+                {
+                    "id": 4,
+                    "title": "Repeating Event",
+                    "start": "2017-02-16T16:00:00"
+                },
+                {
+                    "id": 5,
+                    "title": "Conference",
+                    "start": "2017-02-11",
+                    "end": "2017-02-13"
+                },
+                {
+                    "id": 6,
+                    "title": "Meeting",
+                    "start": "2017-02-12T10:30:00",
+                    "end": "2017-02-12T12:30:00"
+                },
+                {
+                    "id": 7,
+                    "title": "Lunch",
+                    "start": "2017-02-12T12:00:00"
+                },
+                {
+                    "id": 8,
+                    "title": "Meeting",
+                    "start": "2017-02-12T14:30:00"
+                },
+                {
+                    "id": 9,
+                    "title": "Happy Hour",
+                    "start": "2017-02-12T17:30:00"
+                },
+                {
+                    "id": 10,
+                    "title": "Dinner",
+                    "start": "2017-02-12T20:00:00"
+                },
+                {
+                    "id": 11,
+                    "title": "Birthday Party",
+                    "start": "2017-02-13T07:00:00"
+                },
+                {
+                    "id": 12,
+                    "title": "Click for Google",
+                    "url": "http://google.com/",
+                    "start": "2017-02-28"
+                }
+            ]
+        }
+	}
 
-    return (
-        <Schedule events={this.events}></Schedule>
-    );
+	render() {
+		return (
+			<Schedule events={this.state.events} header={header} defaultDate="2017-02-01" eventLimit={4} />
+		);
+	}
 }
 
 `}
@@ -503,88 +509,93 @@ render() {
                 </a>
 <CodeHighlight className="language-javascript">
 {`
+import React, {Component} from 'react';
+import {Schedule} from 'primereact/schedule';
+
 export class ScheduleDemo extends Component {
 
 	constructor() {
-		super();
-		this.header = {
+        super();
+        this.state = {
+            events: [
+                {
+                    "id": 1,
+                    "title": "All Day Event",
+                    "start": "2017-02-01"
+                },
+                {
+                    "id": 2,
+                    "title": "Long Event",
+                    "start": "2017-02-07",
+                    "end": "2017-02-10"
+                },
+                {
+                    "id": 3,
+                    "title": "Repeating Event",
+                    "start": "2017-02-09T16:00:00"
+                },
+                {
+                    "id": 4,
+                    "title": "Repeating Event",
+                    "start": "2017-02-16T16:00:00"
+                },
+                {
+                    "id": 5,
+                    "title": "Conference",
+                    "start": "2017-02-11",
+                    "end": "2017-02-13"
+                },
+                {
+                    "id": 6,
+                    "title": "Meeting",
+                    "start": "2017-02-12T10:30:00",
+                    "end": "2017-02-12T12:30:00"
+                },
+                {
+                    "id": 7,
+                    "title": "Lunch",
+                    "start": "2017-02-12T12:00:00"
+                },
+                {
+                    "id": 8,
+                    "title": "Meeting",
+                    "start": "2017-02-12T14:30:00"
+                },
+                {
+                    "id": 9,
+                    "title": "Happy Hour",
+                    "start": "2017-02-12T17:30:00"
+                },
+                {
+                    "id": 10,
+                    "title": "Dinner",
+                    "start": "2017-02-12T20:00:00"
+                },
+                {
+                    "id": 11,
+                    "title": "Birthday Party",
+                    "start": "2017-02-13T07:00:00"
+                },
+                {
+                    "id": 12,
+                    "title": "Click for Google",
+                    "url": "http://google.com/",
+                    "start": "2017-02-28"
+                }
+            ]
+        }
+	}
+
+	render() {
+        const header = {
 			left: 'prev,next today',
 			center: 'title',
 			right: 'month,agendaWeek,agendaDay'
 		};
-	}
-
-	render() {
-		this.events = [
-			{
-				"id": 1,
-				"title": "All Day Event",
-				"start": "2017-02-01"
-			},
-			{
-				"id": 2,
-				"title": "Long Event",
-				"start": "2017-02-07",
-				"end": "2017-02-10"
-			},
-			{
-				"id": 3,
-				"title": "Repeating Event",
-				"start": "2017-02-09T16:00:00"
-			},
-			{
-				"id": 4,
-				"title": "Repeating Event",
-				"start": "2017-02-16T16:00:00"
-			},
-			{
-				"id": 5,
-				"title": "Conference",
-				"start": "2017-02-11",
-				"end": "2017-02-13"
-			},
-			{
-				"id": 6,
-				"title": "Meeting",
-				"start": "2017-02-12T10:30:00",
-				"end": "2017-02-12T12:30:00"
-			},
-			{
-				"id": 7,
-				"title": "Lunch",
-				"start": "2017-02-12T12:00:00"
-			},
-			{
-				"id": 8,
-				"title": "Meeting",
-				"start": "2017-02-12T14:30:00"
-			},
-			{
-				"id": 9,
-				"title": "Happy Hour",
-				"start": "2017-02-12T17:30:00"
-			},
-			{
-				"id": 10,
-				"title": "Dinner",
-				"start": "2017-02-12T20:00:00"
-			},
-			{
-				"id": 11,
-				"title": "Birthday Party",
-				"start": "2017-02-13T07:00:00"
-			},
-			{
-				"id": 12,
-				"title": "Click for Google",
-				"url": "http://google.com/",
-				"start": "2017-02-28"
-			}
-		];
 
 		return (
 			<div>
-				<div className="content-section">
+				<div className="content-section introduction">
 					<div className="feature-intro">
 						<h1>Schedule</h1>
 						<p>Schedule is an event calendar based on FullCalendar.</p>
@@ -592,12 +603,13 @@ export class ScheduleDemo extends Component {
 				</div>
 
 				<div className="content-section implementation">
-					<Schedule events={this.events} header={this.header} defaultDate="2017-02-01" eventLimit={4}></Schedule>
-				</div >
-			</div >
+					<Schedule events={this.state.events} header={header} defaultDate="2017-02-01" eventLimit={4} />
+				</div>
+			</div>
 		);
 	}
 }
+
 `}
 </CodeHighlight>
                     </TabPanel>
