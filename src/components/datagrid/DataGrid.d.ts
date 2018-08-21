41cbb3dc6738a94203032b52dff200e79f27abcf
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-08-21 23:36:48 +0300

    Add d.ts

---
+++ src/components/datagrid/DataGrid.d.ts
@@ -0,0 +1,23 @@
+import React = require("react");
+
+interface DataGridProps {
+    id?: string,
+    value?: Array<any>,
+    rows?: number,
+    first?:number,
+    paginator?: boolean,
+    totalRecords?: number,
+    pageLinks?: number,
+    rowsPerPageOptions?: Array<any>,
+    lazy?: boolean,
+    style?: string,
+    className?: string,
+    paginatorPosition?: string,
+    paginatorTemplate?: string,
+    onLazyLoad?(e: {first: number, rows:number}): void,
+    itemTemplate?(item: any): JSX.Element | undefined,
+    header?:string,
+    footer?:string;
+}
+
+export class DataGrid extends React.Component<DataGridProps,any> {}
