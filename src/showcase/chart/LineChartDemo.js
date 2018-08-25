fe7d1627d9b875d3fb6c58502773911c94f1f46b
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-08-25 12:22:54 +0300

    New LineChart Demos

--- src/showcase/chart/LineChartDemo.js
+++ src/showcase/chart/LineChartDemo.js
@@ -13,13 +13,82 @@ export class LineChartDemo extends Component {
                     label: 'First Dataset',
                     data: [65, 59, 80, 81, 56, 55, 40],
                     fill: false,
-                    borderColor: '#4bc0c0'
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#42A5F5'
                 },
                 {
                     label: 'Second Dataset',
                     data: [28, 48, 40, 19, 86, 27, 90],
                     fill: false,
-                    borderColor: '#565656'
+                    backgroundColor: '#66BB6A',
+                    borderColor: '#66BB6A'
+                }
+            ]   
+        };
+
+        const multiAxisData = {
+			labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+			datasets: [{
+                label: 'Dataset 1',
+                fill: false,
+				backgroundColor: '#42A5F5',
+                borderColor: '#42A5F5',
+				yAxisID: 'y-axis-1',
+				data: [65, 59, 80, 81, 56, 55, 10]
+			}, {
+                label: 'Dataset 2',
+                fill: false,
+				backgroundColor: '#66BB6A',
+                borderColor: '#66BB6A',
+				yAxisID: 'y-axis-2',
+				data: [28, 48, 40, 19, 86, 27, 90]
+			}]
+        };
+
+        const multiAxisOptions = {
+            responsive: true,
+            hoverMode: 'index',
+            stacked: false,
+            scales: {
+                yAxes: [{
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    id: 'y-axis-1',
+                }, {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    id: 'y-axis-2',
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }]
+            }
+        }
+
+        const lineStylesData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: '#42A5F5'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderDash: [5, 5],
+                    borderColor: '#66BB6A'
+                },
+                {
+                    label: 'Third Dataset',
+                    data: [12, 51, 62, 33, 21, 62, 45],
+                    fill: true,
+                    borderColor: '#FFA726',
+                    backgroundColor: '#FFCC80'
                 }
             ]   
         };
@@ -34,7 +103,14 @@ export class LineChartDemo extends Component {
                 </div>
 
                 <div className="content-section implementation">
+                    <h3>Basic</h3>
                     <Chart type="line" data={data} />
+
+                    <h3>Multi Axis</h3>
+                    <Chart type="line" data={multiAxisData} options={multiAxisOptions} />
+
+                    <h3>Line Styles</h3>
+                    <Chart type="line" data={lineStylesData}  />
                 </div>
 
                 <LineChartDemoDoc></LineChartDemoDoc>
@@ -69,13 +145,82 @@ export class LineChartDemo extends Component {
                     label: 'First Dataset',
                     data: [65, 59, 80, 81, 56, 55, 40],
                     fill: false,
-                    borderColor: '#4bc0c0'
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#42A5F5'
                 },
                 {
                     label: 'Second Dataset',
                     data: [28, 48, 40, 19, 86, 27, 90],
                     fill: false,
-                    borderColor: '#565656'
+                    backgroundColor: '#66BB6A',
+                    borderColor: '#66BB6A'
+                }
+            ]   
+        };
+
+        const multiAxisData = {
+			labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+			datasets: [{
+                label: 'Dataset 1',
+                fill: false,
+				backgroundColor: '#42A5F5',
+                borderColor: '#42A5F5',
+				yAxisID: 'y-axis-1',
+				data: [65, 59, 80, 81, 56, 55, 10]
+			}, {
+                label: 'Dataset 2',
+                fill: false,
+				backgroundColor: '#66BB6A',
+                borderColor: '#66BB6A',
+				yAxisID: 'y-axis-2',
+				data: [28, 48, 40, 19, 86, 27, 90]
+			}]
+        };
+
+        const multiAxisOptions = {
+            responsive: true,
+            hoverMode: 'index',
+            stacked: false,
+            scales: {
+                yAxes: [{
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    id: 'y-axis-1',
+                }, {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    id: 'y-axis-2',
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }]
+            }
+        }
+
+        const lineStylesData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: '#42A5F5'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderDash: [5, 5],
+                    borderColor: '#66BB6A'
+                },
+                {
+                    label: 'Third Dataset',
+                    data: [12, 51, 62, 33, 21, 62, 45],
+                    fill: true,
+                    borderColor: '#FFA726',
+                    backgroundColor: '#FFCC80'
                 }
             ]   
         };
@@ -90,7 +235,14 @@ export class LineChartDemo extends Component {
                 </div>
 
                 <div className="content-section implementation">
+                    <h3>Basic</h3>
                     <Chart type="line" data={data} />
+
+                    <h3>Multi Axis</h3>
+                    <Chart type="line" data={multiAxisData} options={multiAxisOptions} />
+
+                    <h3>Line Styles</h3>
+                    <Chart type="line" data={lineStylesData}  />
                 </div>
             </div>
         )
