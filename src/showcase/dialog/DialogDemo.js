389e15c8cb425805330a590a74eb03260f8ffc07
cagataycivici <cagatay.civici@gmail.com>
2018-12-21 13:39:52 +0300

    Cosmetics on demo

--- src/showcase/dialog/DialogDemo.js
+++ src/showcase/dialog/DialogDemo.js
@@ -39,10 +39,10 @@ export class DialogDemo extends Component {
                     </div>
                 </div>
 
-                <div className="content-section implementation">
+                <div className="content-section implementation dialog-demo">
                     <Dialog header="Godfather I" visible={this.state.visible} style={{width: '50vw'}} footer={footer} minY={70} onHide={this.onHide} maximizable={true}>
                         The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
-                        His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. xw
+                        His beloved son Michael has just come home from the war, but does not intend to become part of his father's business.
                         Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
                         kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
                     </Dialog>
