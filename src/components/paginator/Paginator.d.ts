fcfea79af6bbfe52ae2a289cc52782de4a843dce
mertsincan <sincan.mert@gmail.com>
2019-05-21 11:56:21 +0300

    Update paginator.d.ts

--- src/components/paginator/Paginator.d.ts
+++ src/components/paginator/Paginator.d.ts
@@ -13,6 +13,7 @@ interface PaginatorProps {
     first?: number;
     pageLinkSize?: number;
     rowsPerPageOptions?: any[];
+    alwaysShow?: boolean;
     style?: object;
     className?: string;
     template?: string;
