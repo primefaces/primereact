15ac5f3e06cabe7c0ca4e6febb308b25592685dd
Ted Piecora <tpiecora@gmail.com>
2019-10-10 15:05:38 -0400

    add types for onBlur, onKeyUp, onFocus

--- src/components/inputtextarea/InputTextarea.d.ts
+++ src/components/inputtextarea/InputTextarea.d.ts
@@ -5,6 +5,9 @@ interface InputTextareaProps extends React.HTMLProps<HTMLTextAreaElement> {
     autoResize?: boolean;
     tooltip?: any;
     tooltipOptions?: TooltipOptions;
+    onBlur?(event: React.FocusEvent<HTMLTextAreaElement>): void;
+    onKeyUp?(event: React.KeyboardEvent<HTMLTextAreaElement>): void; 
+    onFocus?(event: React.FocusEvent<HTMLTextAreaElement>): void; 
     [key: string]: any;
 }
 
