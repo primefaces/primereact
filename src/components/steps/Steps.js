f99b0d3a4fe43a9ae619c9fe0c112272685a7a3b
mertsincan <sincan.mert@gmail.com>
2020-02-13 11:06:46 +0300

    Fixed #1192 - The disabled items can be focused in the Steps

--- src/components/steps/Steps.js
+++ src/components/steps/Steps.js
@@ -85,14 +85,16 @@ export class Steps extends Component {
     }
 
     renderItem(item, index) {
+        const isDisabled = (item.disabled || (index !== this.props.activeIndex && this.props.readOnly))
         const className = classNames('p-steps-item', item.className, {
                 'p-highlight p-steps-current': (index === this.props.activeIndex),
                 'p-state-default': (index !== this.props.activeIndex),
-                'p-disabled': (item.disabled || (index !== this.props.activeIndex && this.props.readOnly))});
+                'p-disabled': isDisabled
+            });
 
         return (
             <li key={item.label + '_' + index} className={className} style={item.style} role="tab" aria-selected={index === this.props.activeIndex} aria-expanded={index === this.props.activeIndex}>
-                <a href={item.url || '#'} className="p-menuitem-link" role="presentation" target={item.target} onClick={event => this.itemClick(event, item, index)}>
+                <a href={item.url || '#'} className="p-menuitem-link" role="presentation" target={item.target} onClick={event => this.itemClick(event, item, index)} tabIndex={isDisabled ? -1 : ''}>
                     <span className="p-steps-number">{index + 1}</span>
                     <span className="p-steps-title">{item.label}</span>
                 </a>
