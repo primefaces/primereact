db5844260e2cea73d6fdc95c6fe2d920193e880e
mertsincan <sincan.mert@gmail.com>
2020-02-19 16:10:27 +0300

    Added galleria d.ts files

---
+++ src/components/galleria/Galleria.d.ts
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+interface GalleriaProps {
+    id?: string;
+    value?: any[];
+    activeIndex?: number;
+    fullScreen?: boolean;
+    previewItemTemplate?: any;
+    thumbnailItemTemplate?: any;
+    indicatorItemTemplate?: any;
+    className?: string;
+    style?: object;
+    header?: any;
+    footer?: any;
+    numVisible?: number;
+    responsiveOptions?: any;
+    showPreviewNavButtons?: boolean;
+    showIndicatorNavButtons?: boolean;
+    showNavButtonsOnPreviewHover?: boolean;
+    changePreviewOnIndicatorHover?: boolean;
+    circular?: boolean;
+    autoPlay?: boolean;
+    transitionInterval?: number;
+    captionTemplate?: any;
+    showThumbnails?: boolean;
+    thumbnailsPosition?: string;
+    showIndicators?: boolean;
+    showIndicatorsOnPreview?: boolean;
+    indicatorsPosition?: string;
+    baseZIndex?: number;
+    onItemChange?(e: {index: number}): void;
+}
+
+export class Galleria extends React.Component<GalleriaProps,any> {
+    public show():void;
+    public hide():void;
+    public isAutoPlayActive():boolean;
+    public startSlideShow():void;
+    public stopSlideShow():void;
+}
---
+++ src/components/galleria/GalleriaPreview.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+// tslint:disable-next-line:no-empty-interface
+interface GalleriaPreviewProps {
+}
+
+export class GalleriaPreview extends React.Component<GalleriaPreviewProps,any> {}
