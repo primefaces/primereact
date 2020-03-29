980e3ae26e748418b9b5653ba32448ebe68f85cd
cagataycivici <cagatay.civici@gmail.com>
2020-03-29 17:54:25 +0300

    Shadow for table demo

--- src/showcase/datatable/DataTableDemo.js
+++ src/showcase/datatable/DataTableDemo.js
@@ -2777,6 +2777,8 @@ export class DataTableStateDemo extends Component {
     }
 
     .p-datatable.p-datatable-customers {
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 2px 1px -1px rgba(0, 0, 0, 0.12);
+        
         .p-datatable-header {
             border: 0 none;
             padding: 12px;
