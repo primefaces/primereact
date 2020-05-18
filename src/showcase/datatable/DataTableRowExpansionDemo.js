db1c8cddb4b9458a17ed928e2c9281d05aa2b27f
mertsincan <sincan.mert@gmail.com>
2020-05-18 14:10:29 +0300

    Refactor on liveeditor demos

--- src/showcase/datatable/DataTableRowExpansionDemo.js
+++ src/showcase/datatable/DataTableRowExpansionDemo.js
@@ -88,9 +88,13 @@ export class DataTableRowExpansionDemoDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -119,7 +123,7 @@ export class DataTableRowExpansionDemo extends Component {
         return  (
             <div className="p-grid p-fluid" style={{padding: '2em 1em 1em 1em'}}>
                 <div className="p-col-12 p-md-3" style={{textAlign:'center'}}>
-                    <img src={src} alt={data.brand}/>
+                    <img src={src} srcSet="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" alt={data.brand}/>
                 </div>
                     <div className="p-col-12 p-md-9">
                     <div className="p-grid">
@@ -158,6 +162,7 @@ export class DataTableRowExpansionDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -180,7 +185,7 @@ const DataTableRowExpansionDemo = () => {
         return  (
             <div className="p-grid p-fluid" style={{padding: '2em 1em 1em 1em'}}>
                 <div className="p-col-12 p-md-3" style={{textAlign:'center'}}>
-                    <img src={src} alt={data.brand}/>
+                    <img src={src} srcSet="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" alt={data.brand}/>
                 </div>
                     <div className="p-col-12 p-md-9">
                     <div className="p-grid">
@@ -217,6 +222,7 @@ const DataTableRowExpansionDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -239,7 +245,7 @@ const DataTableRowExpansionDemo = () => {
         return  (
             <div className="p-grid p-fluid" style={{padding: '2em 1em 1em 1em'}}>
                 <div className="p-col-12 p-md-3" style={{textAlign:'center'}}>
-                    <img src={src} alt={data.brand}/>
+                    <img src={src} srcSet="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" alt={data.brand}/>
                 </div>
                     <div className="p-col-12 p-md-9">
                     <div className="p-grid">
@@ -278,7 +284,11 @@ const DataTableRowExpansionDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -289,7 +299,7 @@ const DataTableRowExpansionDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="DataTableRowExpansionDemo" sources={this.sources} service="CarService" data="cars-small" />
+                <LiveEditor name="DataTableRowExpansionDemo" sources={this.sources} service="CarService" data="cars-small" activeButtonIndex={this.state.activeIndex} />
             </div>
         )
     }
@@ -299,12 +309,11 @@ const DataTableRowExpansionDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
--- src/showcase/datatable/DataTableRowGroupDemo.js
+++ src/showcase/datatable/DataTableRowGroupDemo.js
@@ -106,9 +106,13 @@ export class DataTableColGroupDemoDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -194,6 +198,7 @@ export class DataTableRowGroupDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -268,6 +273,7 @@ const DataTableRowGroupDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -344,7 +350,11 @@ const DataTableRowGroupDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -355,7 +365,7 @@ const DataTableRowGroupDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="DataTableRowGroupDemo" sources={this.sources} service="CarService" data="cars-medium" />
+                <LiveEditor name="DataTableRowGroupDemo" sources={this.sources} service="CarService" data="cars-medium" activeButtonIndex={this.state.activeIndex} />
             </div>
         )
     }
@@ -365,12 +375,11 @@ const DataTableRowGroupDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
