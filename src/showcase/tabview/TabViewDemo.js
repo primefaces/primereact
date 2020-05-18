aa6193a794b5e01e34bd0d69b2c4695a45d18e5b
mertsincan <sincan.mert@gmail.com>
2020-05-18 14:12:22 +0300

    Refactor on liveeditor demos

--- src/showcase/tabview/TabViewDemo.js
+++ src/showcase/tabview/TabViewDemo.js
@@ -83,9 +83,13 @@ export class TabViewDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import {TabView,TabPanel} from 'primereact/tabview';
@@ -152,6 +156,7 @@ export class TabViewDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState } from 'react';
 import {TabView,TabPanel} from 'primereact/tabview';
@@ -210,6 +215,7 @@ const TabViewDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState } from 'react';
 import {TabView,TabPanel} from 'primereact/tabview';
@@ -270,7 +276,11 @@ const TabViewDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -281,7 +291,7 @@ const TabViewDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="TabViewDemo" sources={this.sources} />
+                <LiveEditor name="TabViewDemo" sources={this.sources} activeButtonIndex={this.state.activeIndex - 1} />
             </div>
         )
     }
@@ -291,7 +301,7 @@ const TabViewDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     <TabPanel header="Documentation">
                         <h3>Import</h3>
 <CodeHighlight className="language-javascript">
@@ -519,9 +529,8 @@ import {TabView,TabPanel} from 'primereact/tabview';
 
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
