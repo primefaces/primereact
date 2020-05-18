4a58f10fb047fa860e5a8a374c6cd9f16d544c5c
mertsincan <sincan.mert@gmail.com>
2020-05-18 16:34:56 +0300

    Added styling to liveeditor demos

--- src/showcase/inputmask/InputMaskDemo.js
+++ src/showcase/inputmask/InputMaskDemo.js
@@ -104,7 +104,7 @@ export class InputMaskDemo extends Component {
 
     render() {
         return (
-            <div>
+            <div className="inputgrid-demo">
                 <div className="p-grid p-fluid">
                     <div className="p-col-12 p-md-4">
                         <h3>Basic {this.state.val1}</h3>
@@ -157,7 +157,7 @@ const InputMaskDemo = () => {
     const [val6, setVal6] = useState();
 
     return (
-        <div>
+        <div className="inputgrid-demo">
             <div className="p-grid p-fluid">
                 <div className="p-col-12 p-md-4">
                     <h3>Basic {val1}</h3>
@@ -209,7 +209,7 @@ const InputMaskDemo = () => {
     const [val6, setVal6] = useState<string|undefined>(undefined);
 
     return (
-        <div>
+        <div className="inputgrid-demo">
             <div className="p-grid p-fluid">
                 <div className="p-col-12 p-md-4">
                     <h3>Basic {val1}</h3>
@@ -247,6 +247,14 @@ const InputMaskDemo = () => {
                 `
             }
         }
+
+        this.extFiles = {
+            'index.css': `
+.inputgrid-demo {
+    padding-top: 0 !important;
+}
+            `
+        }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -264,7 +272,7 @@ const InputMaskDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="InputMaskDemo" sources={this.sources} activeButtonIndex={this.state.activeIndex - 1} />
+                <LiveEditor name="InputMaskDemo" sources={this.sources} extFiles={this.extFiles} activeButtonIndex={this.state.activeIndex - 1} />
             </div>
         )
     }
