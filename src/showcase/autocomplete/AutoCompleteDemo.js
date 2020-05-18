973ea0a87b6acf3ed24583c537db0681911142a3
mertsincan <sincan.mert@gmail.com>
2020-05-18 14:09:49 +0300

    Refactor on liveeditor demos

--- src/showcase/autocomplete/AutoCompleteDemo.js
+++ src/showcase/autocomplete/AutoCompleteDemo.js
@@ -119,9 +119,13 @@ class AutoCompleteDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import { AutoComplete } from 'primereact/autocomplete';
@@ -192,7 +196,7 @@ export class AutoCompleteDemo extends Component {
     itemTemplate(brand) {
         return (
             <div className="p-clearfix">
-                <img alt={brand} src={"\`showcase/resources/demo/images/car/\${brand}.png\`"} style={{ width: '32px', display: 'inline-block', margin: '5px 0 2px 5px' }} />
+                <img alt={brand} src={\`showcase/resources/demo/images/car/\${brand}.png\`} srcSet="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" style={{ width: '32px', display: 'inline-block', margin: '5px 0 2px 5px' }} />
                 <div style={{ fontSize: '16px', float: 'right', margin: '10px 10px 0 0' }}>{brand}</div>
             </div>
         );
@@ -223,6 +227,7 @@ export class AutoCompleteDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import { AutoComplete } from 'primereact/autocomplete';
@@ -282,7 +287,7 @@ const AutoCompleteDemo = () => {
     const itemTemplate = (brand) => {
         return (
             <div className="p-clearfix">
-                <img alt={brand} src={\`showcase/resources/demo/images/car/\${brand}.png\`} style={{ width: '32px', display: 'inline-block', margin: '5px 0 2px 5px' }} />
+                <img alt={brand} src={\`showcase/resources/demo/images/car/\${brand}.png\`} srcSet="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" style={{ width: '32px', display: 'inline-block', margin: '5px 0 2px 5px' }} />
                 <div style={{ fontSize: '16px', float: 'right', margin: '10px 10px 0 0' }}>{brand}</div>
             </div>
         );
@@ -311,6 +316,7 @@ const AutoCompleteDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import { AutoComplete } from 'primereact/autocomplete';
@@ -321,7 +327,7 @@ const AutoCompleteDemo = () => {
     const [filteredCountriesSingle, setFilteredCountriesSingle] = useState<any[]>([]);
     const [filteredBrands, setFilteredBrands] = useState<string[]>([]);
     const [filteredCountriesMultiple, setFilteredCountriesMultiple] = useState<any[]>([]);
-    const [selectedCountry, setSelectedCountry] = useState(null);
+    const [selectedCountry, setSelectedCountry] = useState<any>(null);
     const [selectedBrand, setSelectedBrand] = useState(null);
     const [selectedCountries, setSelectedCountries] = useState<any[]>([]);
     const countryservice = new CountryService();
@@ -370,7 +376,7 @@ const AutoCompleteDemo = () => {
     const itemTemplate = (brand: string) => {
         return (
             <div className="p-clearfix">
-                <img alt={brand} src={\`showcase/resources/demo/images/car/\${brand}.png\`} style={{ width: '32px', display: 'inline-block', margin: '5px 0 2px 5px' }} />
+                <img alt={brand} src={\`showcase/resources/demo/images/car/\${brand}.png\`} srcSet="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" style={{ width: '32px', display: 'inline-block', margin: '5px 0 2px 5px' }} />
                 <div style={{ fontSize: '16px', float: 'right', margin: '10px 10px 0 0' }}>{brand}</div>
             </div>
         );
@@ -401,7 +407,11 @@ const AutoCompleteDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -412,7 +422,7 @@ const AutoCompleteDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="AutoCompleteDemo" sources={this.sources} service="CountryService" data="countries" />
+                <LiveEditor name="AutoCompleteDemo" sources={this.sources} service="CountryService" data="countries" activeButtonIndex={this.state.activeIndex - 1} />
             </div>
         )
     }
@@ -422,7 +432,7 @@ const AutoCompleteDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     <TabPanel header="Documentation">
                         <h3>Import</h3>
                         <CodeHighlight className="language-javascript">
@@ -870,9 +880,8 @@ brandTemplate(brand) {
 
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
