3221af8604ffe31a4b795f73fad53914968930df
mertsincan <sincan.mert@gmail.com>
2020-05-18 14:10:23 +0300

    Refactor on liveeditor demos

--- src/showcase/datatable/DataTableLazyDemo.js
+++ src/showcase/datatable/DataTableLazyDemo.js
@@ -94,9 +94,13 @@ export class DataTableLazyDemoDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -168,6 +172,7 @@ export class DataTableLazyDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -223,6 +228,7 @@ const DataTableLazyDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import {DataTable} from 'primereact/datatable';
@@ -280,7 +286,11 @@ const DataTableLazyDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -291,7 +301,7 @@ const DataTableLazyDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="DataTableLazyDemo" sources={this.sources} service="CarService" data="cars-large" />
+                <LiveEditor name="DataTableLazyDemo" sources={this.sources} service="CarService" data="cars-large" activeButtonIndex={this.state.activeIndex} />
             </div>
         )
     }
@@ -301,12 +311,11 @@ const DataTableLazyDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
