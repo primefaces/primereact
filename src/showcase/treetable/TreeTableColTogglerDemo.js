2e173940b73f42e6d6df651b2900939889454f7d
mertsincan <sincan.mert@gmail.com>
2020-05-18 14:12:41 +0300

    Refactor on liveeditor demos

--- src/showcase/treetable/TreeTableColTogglerDemo.js
+++ src/showcase/treetable/TreeTableColTogglerDemo.js
@@ -84,9 +84,13 @@ class TreeTableColTogglerDemoDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import { TreeTable } from 'primereact/treetable';
@@ -150,6 +154,7 @@ export class TreeTableColTogglerDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import { TreeTable } from 'primereact/treetable';
@@ -203,6 +208,7 @@ const TreeTableColTogglerDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import { TreeTable } from 'primereact/treetable';
@@ -258,7 +264,11 @@ const TreeTableColTogglerDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -269,7 +279,7 @@ const TreeTableColTogglerDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="TreeTableColTogglerDemo" sources={this.sources} service="NodeService" data="treetablenodes" />
+                <LiveEditor name="TreeTableColTogglerDemo" sources={this.sources} service="NodeService" data="treetablenodes" activeButtonIndex={this.state.activeIndex} />
             </div>
         )
     }
@@ -279,12 +289,11 @@ const TreeTableColTogglerDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
--- src/showcase/treetable/TreeTableDemo.js
+++ src/showcase/treetable/TreeTableDemo.js
@@ -80,9 +80,13 @@ export class TreeTableDoc extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
         this.sources = {
             'app': {
+                tabName: 'Source',
                 content: `
 import React, { Component } from 'react';
 import { TreeTable } from 'primereact/treetable';
@@ -141,6 +145,7 @@ export class TreeTableDemo extends Component {
                 `
             },
             'hooks': {
+                tabName: 'Hooks Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import { TreeTable } from 'primereact/treetable';
@@ -190,6 +195,7 @@ const TreeTableDemo = () => {
                 `
             },
             'ts': {
+                tabName: 'TS Source',
                 content: `
 import React, { useState, useEffect } from 'react';
 import { TreeTable } from 'primereact/treetable';
@@ -241,7 +247,11 @@ const TreeTableDemo = () => {
         }
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
         return false;
     }
 
@@ -252,7 +262,7 @@ const TreeTableDemo = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                     <span>View on GitHub</span>
                 </a>
-                <LiveEditor name="TreeTableDemo" sources={this.sources} service="NodeService" data="treetablenodes" />
+                <LiveEditor name="TreeTableDemo" sources={this.sources} service="NodeService" data="treetablenodes" activeButtonIndex={this.state.activeIndex - 1} />
             </div>
         )
     }
@@ -262,7 +272,7 @@ const TreeTableDemo = () => {
 
         return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     <TabPanel header="Documentation">
                         <h3>Import</h3>
                         <CodeHighlight className="language-javascript">
@@ -2819,9 +2829,8 @@ export class TreeTableResponsiveDemo extends Component {
 
                     {
                         this.sources && Object.entries(this.sources).map(([key, value], index) => {
-                            const header = key === 'app' ? 'Source' : `${key} Source`;
                             return (
-                                <TabPanel key={`source_${index}`} header={header}>
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
                                     {sourceButtons}
 
                                     <CodeHighlight className="language-javascript">
