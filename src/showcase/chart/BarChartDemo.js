a8dbea9cb6eb9da371a63fb5005895fec76a1d4e
mertsincan <sincan.mert@gmail.com>
2020-05-19 04:54:25 +0300

    Added liveeditor to charts

--- src/showcase/chart/BarChartDemo.js
+++ src/showcase/chart/BarChartDemo.js
@@ -3,6 +3,7 @@ import {TabView, TabPanel} from '../../components/tabview/TabView';
 import { CodeHighlight } from '../codehighlight/CodeHighlight';
 import AppContentContext from '../../AppContentContext';
 import {Chart} from '../../components/chart/Chart';
+import { LiveEditor } from '../liveeditor/LiveEditor';
 
 export class BarChartDemo extends Component {
 
@@ -174,18 +175,17 @@ export class BarChartDemo extends Component {
 
 export class BarChartDemoDoc extends Component {
 
-    shouldComponentUpdate() {
-        return false;
-    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
-    render() {
-        return (
-            <div className="content-section documentation">
-                <TabView>
-                    <TabPanel header="Source">
-                        <CodeHighlight className="language-javascript">
-                            {`
-import React, {Component} from 'react';
+        this.sources = {
+            'app': {
+                tabName: 'Source',
+                content: `
+import React, { Component } from 'react';
 import {Chart} from 'primereact/chart';
 
 export class BarChartDemo extends Component {
@@ -325,34 +325,379 @@ export class BarChartDemo extends Component {
 
         return (
             <div>
-                <div className="content-section introduction">
-                    <div className="feature-intro">
-                        <h1>BarChart</h1>
-                        <p>A bar chart or bar graph is a chart that presents grouped data with rectangular bars with lengths proportional to the values that they represent.</p>
-                    </div>
-                </div>
+                <h3>Vertical</h3>
+                <Chart type="bar" data={data} />
 
-                <div className="content-section implementation">
-                    <h3>Vertical</h3>
-                    <Chart type="bar" data={data} />
+                <h3>Horizontal</h3>
+                <Chart type="horizontalBar" data={data} />
 
-                    <h3>Horizontal</h3>
-                    <Chart type="horizontalBar" data={data} />
+                <h3>Multi Axis</h3>
+                <Chart type="bar" data={multiAxisData} options={multiAxisOptions} />
 
-                    <h3>Multi Axis</h3>
-                    <Chart type="bar" data={multiAxisData} options={multiAxisOptions} />
-
-                    <h3>Stacked</h3>
-                    <Chart type="bar" data={stackedData} options={stackedOptions} />
-                </div>
+                <h3>Stacked</h3>
+                <Chart type="bar" data={stackedData} options={stackedOptions} />
             </div>
         )
     }
 }
+                `
+            },
+            'hooks': {
+                tabName: 'Hooks Source',
+                content: `
+import React from 'react';
+import {Chart} from 'primereact/chart';
 
-`}
-                        </CodeHighlight>
-                    </TabPanel>
+const BarChartDemo = () => {
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: '#42A5F5',
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: 'My Second dataset',
+                backgroundColor: '#9CCC65',
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    const multiAxisData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Dataset 1',
+            backgroundColor: [
+                '#EC407A',
+                '#AB47BC',
+                '#42A5F5',
+                '#7E57C2',
+                '#66BB6A',
+                '#FFCA28',
+                '#26A69A'
+            ],
+            yAxisID: 'y-axis-1',
+            data: [65, 59, 80, 81, 56, 55, 10]
+        }, {
+            label: 'Dataset 2',
+            backgroundColor: '#78909C',
+            yAxisID: 'y-axis-2',
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }]
+    };
+
+    const multiAxisOptions = {
+        responsive: true,
+        tooltips: {
+            mode: 'index',
+            intersect: true
+        },
+        scales: {
+            yAxes: [{
+                type: 'linear',
+                display: true,
+                position: 'left',
+                id: 'y-axis-1',
+                ticks: {
+                    min: 0,
+                    max: 100
+                }
+            },
+            {
+                type: 'linear',
+                display: true,
+                position: 'right',
+                id: 'y-axis-2',
+                gridLines: {
+                    drawOnChartArea: false
+                },
+                ticks: {
+                    min: 0,
+                    max: 100
+                }
+            }]
+        }
+    }
+
+    const stackedData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            type: 'bar',
+            label: 'Dataset 1',
+            backgroundColor: '#66BB6A',
+            data: [
+                50,
+                25,
+                12,
+                48,
+                90,
+                76,
+                42
+            ]
+        }, {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: '#FFCA28',
+            data: [
+                21,
+                84,
+                24,
+                75,
+                37,
+                65,
+                34
+            ]
+        }, {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: '#42A5F5',
+            data: [
+                41,
+                52,
+                24,
+                74,
+                23,
+                21,
+                32
+            ]
+        }]
+    };
+
+    const stackedOptions = {
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        responsive: true,
+        scales: {
+            xAxes: [{
+                stacked: true,
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    };
+
+    return (
+        <div>
+            <h3>Vertical</h3>
+            <Chart type="bar" data={data} />
+
+            <h3>Horizontal</h3>
+            <Chart type="horizontalBar" data={data} />
+
+            <h3>Multi Axis</h3>
+            <Chart type="bar" data={multiAxisData} options={multiAxisOptions} />
+
+            <h3>Stacked</h3>
+            <Chart type="bar" data={stackedData} options={stackedOptions} />
+        </div>
+    )
+}
+                `
+            },
+            'ts': {
+                tabName: 'TS Source',
+                content: `
+import React from 'react';
+import {Chart} from 'primereact/chart';
+
+const BarChartDemo = () => {
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: '#42A5F5',
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: 'My Second dataset',
+                backgroundColor: '#9CCC65',
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    const multiAxisData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Dataset 1',
+            backgroundColor: [
+                '#EC407A',
+                '#AB47BC',
+                '#42A5F5',
+                '#7E57C2',
+                '#66BB6A',
+                '#FFCA28',
+                '#26A69A'
+            ],
+            yAxisID: 'y-axis-1',
+            data: [65, 59, 80, 81, 56, 55, 10]
+        }, {
+            label: 'Dataset 2',
+            backgroundColor: '#78909C',
+            yAxisID: 'y-axis-2',
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }]
+    };
+
+    const multiAxisOptions = {
+        responsive: true,
+        tooltips: {
+            mode: 'index',
+            intersect: true
+        },
+        scales: {
+            yAxes: [{
+                type: 'linear',
+                display: true,
+                position: 'left',
+                id: 'y-axis-1',
+                ticks: {
+                    min: 0,
+                    max: 100
+                }
+            },
+            {
+                type: 'linear',
+                display: true,
+                position: 'right',
+                id: 'y-axis-2',
+                gridLines: {
+                    drawOnChartArea: false
+                },
+                ticks: {
+                    min: 0,
+                    max: 100
+                }
+            }]
+        }
+    }
+
+    const stackedData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            type: 'bar',
+            label: 'Dataset 1',
+            backgroundColor: '#66BB6A',
+            data: [
+                50,
+                25,
+                12,
+                48,
+                90,
+                76,
+                42
+            ]
+        }, {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: '#FFCA28',
+            data: [
+                21,
+                84,
+                24,
+                75,
+                37,
+                65,
+                34
+            ]
+        }, {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: '#42A5F5',
+            data: [
+                41,
+                52,
+                24,
+                74,
+                23,
+                21,
+                32
+            ]
+        }]
+    };
+
+    const stackedOptions = {
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        responsive: true,
+        scales: {
+            xAxes: [{
+                stacked: true,
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    };
+
+    return (
+        <div>
+            <h3>Vertical</h3>
+            <Chart type="bar" data={data} />
+
+            <h3>Horizontal</h3>
+            <Chart type="horizontalBar" data={data} />
+
+            <h3>Multi Axis</h3>
+            <Chart type="bar" data={multiAxisData} options={multiAxisOptions} />
+
+            <h3>Stacked</h3>
+            <Chart type="bar" data={stackedData} options={stackedOptions} />
+        </div>
+    )
+}
+                `
+            }
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
+        return false;
+    }
+
+    renderSourceButtons() {
+        return (
+            <div className="source-button-group">
+                <a href="https://github.com/primefaces/primereact/tree/master/src/showcase/chart" className="btn-viewsource" target="_blank" rel="noopener noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                    <span>View on GitHub</span>
+                </a>
+                <LiveEditor name="BarChartDemo" sources={this.sources} dependencies={{'chart.js': '2.7.3'}} activeButtonIndex={this.state.activeIndex} />
+            </div>
+        )
+    }
+
+    render() {
+        const sourceButtons = this.renderSourceButtons();
+
+        return (
+            <div className="content-section documentation">
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
+                    {
+                        this.sources && Object.entries(this.sources).map(([key, value], index) => {
+                            return (
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
+                                    {sourceButtons}
+
+                                    <CodeHighlight className="language-javascript">
+                                        {value.content}
+                                    </CodeHighlight>
+                                </TabPanel>
+                            );
+                        })
+                    }
                 </TabView>
             </div>
         )
--- src/showcase/chart/ComboChartDemo.js
+++ src/showcase/chart/ComboChartDemo.js
@@ -3,6 +3,7 @@ import {TabView, TabPanel} from '../../components/tabview/TabView';
 import {CodeHighlight} from '../codehighlight/CodeHighlight';
 import AppContentContext from '../../AppContentContext';
 import {Chart} from '../../components/chart/Chart';
+import { LiveEditor } from '../liveeditor/LiveEditor';
 
 export class ComboChartDemo extends Component {
 
@@ -93,21 +94,20 @@ export class ComboChartDemo extends Component {
 
 export class ComboChartDemoDoc extends Component {
 
-    shouldComponentUpdate() {
-        return false;
-    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
-    render() {
-        return (
-            <div className="content-section documentation">
-                <TabView>
-                    <TabPanel header="Source">
-                        <CodeHighlight className="language-javascript">
-                            {`
-import React, {Component} from 'react';
+        this.sources = {
+            'app': {
+                tabName: 'Source',
+                content: `
+import React, { Component } from 'react';
 import {Chart} from 'primereact/chart';
 
-export class PieChartDemo extends Component {
+export class ComboChartDemo extends Component {
 
     render() {
         const data = {
@@ -172,25 +172,208 @@ export class PieChartDemo extends Component {
 
         return (
             <div>
-                <div className="content-section introduction">
-                    <div className="feature-intro">
-                        <h1>Combo Chart</h1>
-                        <p>Different chart types can be combined in the same graph.</p>
-                    </div>
-                </div>
-
-                <div className="content-section implementation">
-                    <h3>Combo Chart</h3>
-                    <Chart type="bar" data={data} options={options} />
-                </div>
+                <h3>Combo Chart</h3>
+                <Chart type="bar" data={data} options={options} />
             </div>
         )
     }
 }
+                `
+            },
+            'hooks': {
+                tabName: 'Hooks Source',
+                content: `
+import React from 'react';
+import {Chart} from 'primereact/chart';
 
-`}
-                        </CodeHighlight>
-                    </TabPanel>
+const ComboChartDemo = () => {
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            type: 'line',
+            label: 'Dataset 1',
+            borderColor: '#2196F3',
+            borderWidth: 2,
+            fill: false,
+            data: [
+                50,
+                25,
+                12,
+                48,
+                56,
+                76,
+                42
+            ]
+        }, {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: '#4CAF50',
+            data: [
+                21,
+                84,
+                24,
+                75,
+                37,
+                65,
+                34
+            ],
+            borderColor: 'white',
+            borderWidth: 2
+        }, {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: '#FFC107',
+            data: [
+                41,
+                52,
+                24,
+                74,
+                23,
+                21,
+                32
+            ]
+        }]
+    };
+
+    const options = {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Combo Bar Line Chart'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: true
+        }
+    };
+
+    return (
+        <div>
+            <h3>Combo Chart</h3>
+            <Chart type="bar" data={data} options={options} />
+        </div>
+    )
+}
+                `
+            },
+            'ts': {
+                tabName: 'TS Source',
+                content: `
+import React from 'react';
+import {Chart} from 'primereact/chart';
+
+const ComboChartDemo = () => {
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            type: 'line',
+            label: 'Dataset 1',
+            borderColor: '#2196F3',
+            borderWidth: 2,
+            fill: false,
+            data: [
+                50,
+                25,
+                12,
+                48,
+                56,
+                76,
+                42
+            ]
+        }, {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: '#4CAF50',
+            data: [
+                21,
+                84,
+                24,
+                75,
+                37,
+                65,
+                34
+            ],
+            borderColor: 'white',
+            borderWidth: 2
+        }, {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: '#FFC107',
+            data: [
+                41,
+                52,
+                24,
+                74,
+                23,
+                21,
+                32
+            ]
+        }]
+    };
+
+    const options = {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Combo Bar Line Chart'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: true
+        }
+    };
+
+    return (
+        <div>
+            <h3>Combo Chart</h3>
+            <Chart type="bar" data={data} options={options} />
+        </div>
+    )
+}
+                `
+            }
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
+        return false;
+    }
+
+    renderSourceButtons() {
+        return (
+            <div className="source-button-group">
+                <a href="https://github.com/primefaces/primereact/tree/master/src/showcase/chart" className="btn-viewsource" target="_blank" rel="noopener noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                    <span>View on GitHub</span>
+                </a>
+                <LiveEditor name="ComboChartDemo" sources={this.sources} dependencies={{'chart.js': '2.7.3'}} activeButtonIndex={this.state.activeIndex} />
+            </div>
+        )
+    }
+
+    render() {
+        const sourceButtons = this.renderSourceButtons();
+
+        return (
+            <div className="content-section documentation">
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
+                    {
+                        this.sources && Object.entries(this.sources).map(([key, value], index) => {
+                            return (
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
+                                    {sourceButtons}
+
+                                    <CodeHighlight className="language-javascript">
+                                        {value.content}
+                                    </CodeHighlight>
+                                </TabPanel>
+                            );
+                        })
+                    }
                 </TabView>
             </div>
         )
