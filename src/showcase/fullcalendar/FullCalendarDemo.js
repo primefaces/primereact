0e71ba2672524a0dae8f41d78171688006660675
mertsincan <sincan.mert@gmail.com>
2020-05-19 04:55:09 +0300

    Refactor

--- src/showcase/fullcalendar/FullCalendarDemo.js
+++ src/showcase/fullcalendar/FullCalendarDemo.js
@@ -7,25 +7,28 @@ import {EventService} from '../service/EventService';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { LiveEditor } from '../liveeditor/LiveEditor';
 
 export class FullCalendarDemo extends Component {
 
     constructor() {
         super();
         this.state = {
-            events: [],
-            options: {
-                plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-                defaultView: 'dayGridMonth',
-                defaultDate: '2017-02-01',
-                header: {
-                    left: 'prev,next',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                },
-                editable: true
-            }
+            events: []
         };
+
+        this.options = {
+            plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+            defaultView: 'dayGridMonth',
+            defaultDate: '2017-02-01',
+            header: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            editable: true
+        };
+
         this.eventService = new EventService();
     }
 
@@ -49,7 +52,7 @@ export class FullCalendarDemo extends Component {
                 </div>
 
                 <div className="content-section implementation">
-                    <FullCalendar  events={this.state.events} options={this.state.options} />
+                    <FullCalendar events={this.state.events} options={this.options} />
                 </div>
 
                 <FullCalendarDoc/>
@@ -60,14 +63,182 @@ export class FullCalendarDemo extends Component {
 
 export class FullCalendarDoc extends Component {
 
-    shouldComponentUpdate(){
-        return false;
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0
+        };
+
+        this.sources = {
+            'app': {
+                tabName: 'Source',
+                content: `
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+
+import React, {Component} from 'react';
+import {FullCalendar} from 'primereact/fullcalendar';
+import {EventService} from '../service/EventService';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+export class FullCalendarDemo extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            events: []
+        };
+
+        this.options = {
+            plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+            defaultView: 'dayGridMonth',
+            defaultDate: '2017-02-01',
+            header: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            editable: true
+        };
+
+        this.eventService = new EventService();
+    }
+
+    componentDidMount() {
+        this.eventService.getEvents().then(data => this.setState({events: data}));
     }
 
     render() {
         return (
+            <div>
+                <FullCalendar events={this.state.events} options={this.options} />
+            </div>
+        );
+    }
+}
+                `
+            },
+            'hooks': {
+                tabName: 'Hooks Source',
+                content: `
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+
+import React, { useState, useEffect } from 'react';
+import {FullCalendar} from 'primereact/fullcalendar';
+import {EventService} from '../service/EventService';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const FullCalendarDemo = () => {
+    const [events, setEvents] = useState([]);
+    const options = {
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+        defaultView: 'dayGridMonth',
+        defaultDate: '2017-02-01',
+        header: {
+            left: 'prev,next',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        editable: true
+    };
+    const eventService = new EventService();
+
+    useEffect(() => {
+        eventService.getEvents().then(data => setEvents(data));
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <FullCalendar events={events} options={options} />
+        </div>
+    );
+}
+                `
+            },
+            'ts': {
+                tabName: 'TS Source',
+                content: `
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+
+import React, { useState, useEffect } from 'react';
+import {FullCalendar} from 'primereact/fullcalendar';
+import {EventService} from '../service/EventService';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const FullCalendarDemo = () => {
+    const [events, setEvents] = useState([]);
+    const options = {
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+        defaultView: 'dayGridMonth',
+        defaultDate: '2017-02-01',
+        header: {
+            left: 'prev,next',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        editable: true
+    };
+    const eventService = new EventService();
+
+    useEffect(() => {
+        eventService.getEvents().then(data => setEvents(data));
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <FullCalendar events={events} options={options} />
+        </div>
+    );
+}
+                `
+            }
+        }
+
+        this.dependencies = {
+            "@fullcalendar/core": "4.2.0",
+            "@fullcalendar/daygrid": "4.2.0",
+            "@fullcalendar/timegrid": "4.2.0",
+            "@fullcalendar/interaction": "4.2.0"
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
+        return false;
+    }
+
+    renderSourceButtons() {
+        return (
+            <div className="source-button-group">
+                <a href="https://github.com/primefaces/primereact/tree/master/src/showcase/fullcalendar" className="btn-viewsource" target="_blank" rel="noopener noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                    <span>View on GitHub</span>
+                </a>
+                <LiveEditor name="FullCalendarDemo" sources={this.sources} service="EventService" data="events" dependencies={this.dependencies} activeButtonIndex={this.state.activeIndex - 1} />
+            </div>
+        )
+    }
+
+    render() {
+        const sourceButtons = this.renderSourceButtons();
+
+        return (
             <div className="content-section documentation">
-                <TabView>
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
                     <TabPanel header="Documentation">
                         <h3>Import</h3>
                         <CodeHighlight className="language-javascript">
@@ -223,67 +394,19 @@ this.fc.calendar.nextYear();
 
                     </TabPanel>
 
-                    <TabPanel header="Source">
-                        <a href="https://github.com/primefaces/primereact/tree/master/src/showcase/fullcalendar" className="btn-viewsource" target="_blank" rel="noopener noreferrer">
-                            <span>View on GitHub</span>
-                        </a>
-                        <CodeHighlight className="language-javascript">
-                            {`
-import React, {Component} from 'react';
-import {FullCalendar} from 'primereact/fullcalendar';
-import {EventService} from '../service/EventService';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
+                    {
+                        this.sources && Object.entries(this.sources).map(([key, value], index) => {
+                            return (
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
+                                    {sourceButtons}
 
-export class FullCalendarDemo extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            events: [],
-            options: {
-                plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-                defaultView: 'dayGridMonth',
-                defaultDate: '2017-02-01',
-                header: {
-                    left: 'prev,next',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                },
-                editable: true
-            }
-        };
-        this.eventService = new EventService();
-    }
-
-    componentDidMount() {
-        this.eventService.getEvents().then(data => this.setState({events: data}));
-    }
-
-    render() {
-
-        return (
-            <div>
-                <div className="content-section introduction">
-                    <div className="feature-intro">
-                        <h1>FullCalendar</h1>
-                        <p>An event calendar based on the <a href="https://fullcalendar.io/">FullCalendar</a> library.</p>
-                    </div>
-                </div>
-
-                <div className="content-section implementation">
-                    <FullCalendar  events={this.state.events} options={this.state.options} />
-                </div>
-
-            </div>
-        );
-    }
-}
-
-`}
-                        </CodeHighlight>
-                    </TabPanel>
+                                    <CodeHighlight className="language-javascript">
+                                        {value.content}
+                                    </CodeHighlight>
+                                </TabPanel>
+                            );
+                        })
+                    }
                 </TabView>
             </div>
         );
