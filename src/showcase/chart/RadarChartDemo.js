b0e162f9ddb97217bfd32ba6eb1669f91072806b
mertsincan <sincan.mert@gmail.com>
2020-05-19 04:54:41 +0300

    Added liveeditor to charts

--- src/showcase/chart/RadarChartDemo.js
+++ src/showcase/chart/RadarChartDemo.js
@@ -3,6 +3,7 @@ import {TabView, TabPanel} from '../../components/tabview/TabView';
 import { CodeHighlight } from '../codehighlight/CodeHighlight';
 import AppContentContext from '../../AppContentContext';
 import {Chart} from '../../components/chart/Chart';
+import { LiveEditor } from '../liveeditor/LiveEditor';
 
 export class RadarChartDemo extends Component {
 
@@ -58,19 +59,18 @@ export class RadarChartDemo extends Component {
 
 export class RadarChartDemoDoc extends Component {
 
-    shouldComponentUpdate() {
-        return false;
-    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0
+        };
 
-    render() {
-        return (
-            <div className="content-section documentation">
-                <TabView>
-                    <TabPanel header="Source">
-                        <CodeHighlight className="language-javascript">
-                            {`
-import React, {Component} from 'react';
-import {Chart} from '../../components/chart/Chart';
+        this.sources = {
+            'app': {
+                tabName: 'Source',
+                content: `
+import React, { Component } from 'react';
+import {Chart} from 'primereact/chart';
 
 export class RadarChartDemo extends Component {
 
@@ -103,24 +103,137 @@ export class RadarChartDemo extends Component {
 
         return (
             <div>
-                <div className="content-section introduction">
-                    <div className="feature-intro">
-                        <h1>RadarChart</h1>
-                        <p>A radar chart is a graphical method of displaying multivariate data in the form of a two-dimensional chart of three or more quantitative variables represented on axes starting from the same point.</p>
-                    </div>
-                </div>
-
-                <div className="content-section implementation">
-                    <Chart type="radar" data={data} />
-                </div>
+                <Chart type="radar" data={data} />
             </div>
         )
     }
 }
+                `
+            },
+            'hooks': {
+                tabName: 'Hooks Source',
+                content: `
+import React from 'react';
+import {Chart} from 'primereact/chart';
 
-`}
-                        </CodeHighlight>
-                    </TabPanel>
+const RadarChartDemo = () => {
+    const data = {
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(179,181,198,0.2)',
+                borderColor: 'rgba(179,181,198,1)',
+                pointBackgroundColor: 'rgba(179,181,198,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(179,181,198,1)',
+                data: [65, 59, 90, 81, 56, 55, 40]
+            },
+            {
+                label: 'My Second dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                pointBackgroundColor: 'rgba(255,99,132,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(255,99,132,1)',
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }
+        ]
+    };
+
+    return (
+        <div>
+            <Chart type="radar" data={data} />
+        </div>
+    )
+}
+                `
+            },
+            'ts': {
+                tabName: 'TS Source',
+                content: `
+import React from 'react';
+import {Chart} from 'primereact/chart';
+
+const RadarChartDemo = () => {
+    const data = {
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(179,181,198,0.2)',
+                borderColor: 'rgba(179,181,198,1)',
+                pointBackgroundColor: 'rgba(179,181,198,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(179,181,198,1)',
+                data: [65, 59, 90, 81, 56, 55, 40]
+            },
+            {
+                label: 'My Second dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                pointBackgroundColor: 'rgba(255,99,132,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(255,99,132,1)',
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }
+        ]
+    };
+
+    return (
+        <div>
+            <Chart type="radar" data={data} />
+        </div>
+    )
+}
+                `
+            }
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.activeIndex !== nextState.activeIndex) {
+            return true;
+        }
+
+        return false;
+    }
+
+    renderSourceButtons() {
+        return (
+            <div className="source-button-group">
+                <a href="https://github.com/primefaces/primereact/tree/master/src/showcase/chart" className="btn-viewsource" target="_blank" rel="noopener noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                    <span>View on GitHub</span>
+                </a>
+                <LiveEditor name="RadarChartDemo" sources={this.sources} dependencies={{'chart.js': '2.7.3'}} activeButtonIndex={this.state.activeIndex} />
+            </div>
+        )
+    }
+
+    render() {
+        const sourceButtons = this.renderSourceButtons();
+
+        return (
+            <div className="content-section documentation">
+                <TabView activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
+                    {
+                        this.sources && Object.entries(this.sources).map(([key, value], index) => {
+                            return (
+                                <TabPanel key={`source_${index}`} header={value.tabName}>
+                                    {sourceButtons}
+
+                                    <CodeHighlight className="language-javascript">
+                                        {value.content}
+                                    </CodeHighlight>
+                                </TabPanel>
+                            );
+                        })
+                    }
                 </TabView>
             </div>
         )
