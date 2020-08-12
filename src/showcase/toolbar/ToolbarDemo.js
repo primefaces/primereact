95d95d31cccc5c0d12b6f52868283d4004176cb9
mertsincan <sincan.mert@gmail.com>
2020-08-12 10:33:32 +0300

    Fixed #1502 - Add left and right properties to Toolbar

--- src/showcase/toolbar/ToolbarDemo.js
+++ src/showcase/toolbar/ToolbarDemo.js
@@ -214,32 +214,44 @@ import {Toolbar} from 'primereact/toolbar';
                         <div className="doc-tablewrapper">
                             <table className="doc-table">
                                 <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Type</th>
-                                    <th>Default</th>
-                                    <th>Description</th>
-                                </tr>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Type</th>
+                                        <th>Default</th>
+                                        <th>Description</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
-                                    <td>id</td>
-                                    <td>string</td>
-                                    <td>null</td>
-                                    <td>Unique identifier of the element.</td>
-                                </tr>
-                                <tr>
-                                    <td>style</td>
-                                    <td>string</td>
-                                    <td>null</td>
-                                    <td>Inline style of the component.</td>
-                                </tr>
-                                <tr>
-                                    <td>className</td>
-                                    <td>string</td>
-                                    <td>null</td>
-                                    <td>Style class of the component.</td>
-                                </tr>
+                                    <tr>
+                                        <td>id</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Unique identifier of the element.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>style</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Inline style of the component.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>className</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Style class of the component.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>left</td>
+                                        <td>any</td>
+                                        <td>null</td>
+                                        <td>The template of left element.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>right</td>
+                                        <td>any</td>
+                                        <td>null</td>
+                                        <td>The template of right element</td>
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
