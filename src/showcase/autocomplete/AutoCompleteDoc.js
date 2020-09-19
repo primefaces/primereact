c742c3fa2f36bbcaf5372f9fa74c2975fed29345
mertsincan <sincan.mert@gmail.com>
2020-09-20 01:27:44 +0300

    Update Autocomplete doc

--- src/showcase/autocomplete/AutoCompleteDoc.js
+++ src/showcase/autocomplete/AutoCompleteDoc.js
@@ -498,6 +498,18 @@ itemTemplate(item) {
                                         <td>Inline style of the input field.</td>
                                     </tr>
                                     <tr>
+                                        <td>panelClassName</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Style class of the overlay panel element.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>panelStyle</td>
+                                        <td>string</td>
+                                        <td>null</td>
+                                        <td>Inline style of the overlay panel element.</td>
+                                    </tr>
+                                    <tr>
                                         <td>placeholder</td>
                                         <td>string</td>
                                         <td>null</td>
