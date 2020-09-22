ee0531b1cc61b5764a028e6391c46cccfcedaac5
mertsincan <sincan.mert@gmail.com>
2020-09-22 21:52:09 +0300

    Fixed #1597 - Add template property support to MenuModel API

--- src/components/splitbutton/SplitButtonItem.js
+++ src/components/splitbutton/SplitButtonItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import ObjectUtils from '../utils/ObjectUtils';
 
 export class SplitButtonItem extends Component {
 
@@ -32,15 +33,18 @@ export class SplitButtonItem extends Component {
     }
 
     render() {
-        var className = classNames('p-menuitem-link', { 'p-disabled': this.props.menuitem.disabled });
-        var icon = this.props.menuitem.icon ? <span className={classNames('p-menuitem-icon', this.props.menuitem.icon)}></span> : null;
-        var label = <span className="p-menuitem-text">{this.props.menuitem.label}</span>;
+        let { disabled, icon, label, template } = this.props.menuitem;
+        const className = classNames('p-menuitem-link', { 'p-disabled': disabled });
+        const itemContent = template ? ObjectUtils.getJSXElement(template, this.props.menuitem) : null;
+        icon = icon && <span className={classNames('p-menuitem-icon', icon)}></span>;
+        label = label && <span className="p-menuitem-text">{label}</span>;
 
         return (
             <li className="p-menuitem" role="none">
                 <a href={this.props.menuitem.url || '#'} role="menuitem" className={className} target={this.props.menuitem.target} onClick={this.onClick}>
                     {icon}
                     {label}
+                    {itemContent}
                 </a>
             </li>
         );
