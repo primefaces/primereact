6284b72e843939026999f0e631623c2875069d6c
Danial Haddadi <danial.haddadi@bbc.co.uk>
2020-10-02 12:48:16 +0100

    Remove SplitButton FindDOMNode, forward Button ref to DOM Element

--- src/components/splitbutton/SplitButton.js
+++ src/components/splitbutton/SplitButton.js
@@ -225,7 +225,7 @@ export class SplitButton extends Component {
 
         return (
             <div id={this.id} className={className} style={this.props.style} ref={el => this.container = el}>
-                <Button ref={(el) => this.defaultButton = ReactDOM.findDOMNode(el)} type="button" className="p-splitbutton-defaultbutton" icon={this.props.icon} label={this.props.label} onClick={this.props.onClick} disabled={this.props.disabled} tabIndex={this.props.tabIndex}>
+                <Button ref={(el) => this.defaultButton = el} type="button" className="p-splitbutton-defaultbutton" icon={this.props.icon} label={this.props.label} onClick={this.props.onClick} disabled={this.props.disabled} tabIndex={this.props.tabIndex}>
                     {buttonContent}
                 </Button>
                 <Button type="button" className="p-splitbutton-menubutton" icon="pi pi-chevron-down" onClick={this.onDropdownButtonClick} disabled={this.props.disabled}
