ff30625a130e5bfb8a178e29e0c9c3250b0126ed
mertsincan <sincan.mert@gmail.com>
2020-11-26 08:40:20 +0300

    Fixed #1687 - New Component: Timeline

---
+++ src/components/timeline/Timeline.d.ts
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface TimelineProps {
+    id?: string;
+    value?: array;
+    align?: string;
+    layout?: string;
+    dataKey?: string;
+    className?: string;
+    style?: object;
+    opposite?: ((item: any, index: number) => any | any);
+    marker?: ((item: any, index: number) => any | any);
+    content?: ((item: any, index: number) => any | any);
+}
+
+export class Timeline extends React.Component<TimelineProps,any> {}
