f3811d51fb4120b2f3f59ecfa2cdf8310c3b8e39
mertsincan <sincan.mert@gmail.com>
2021-01-13 11:26:09 +0300

    Change Sass API link

--- src/showcase/theming/ThemingPage.js
+++ src/showcase/theming/ThemingPage.js
@@ -29,7 +29,7 @@ export class ThemingPage extends Component {
                     <h5>Customization</h5>
                     <p>CSS of the themes share the same license as PrimeReact which is MIT, this means the generated CSS can be customized per your needs however this should be avoided if your customizations
                         are not simple. For instance even to change a primary color, since there is no variable a find and replace should be performed various times. On the other hand, this can be achieved
-                        by changing a single variable e.g. $primaryColor. Visit the <a href="https://www.primefaces.org/designer/api/primereact/5.0.0">SASS API</a> for the documentation of available customization options.</p>
+                        by changing a single variable e.g. $primaryColor. Visit the <a href="https://www.primefaces.org/designer/api/primereact/6.0.0">SASS API</a> for the documentation of available customization options.</p>
 
                     <p><a href="https://www.primefaces.org/designer/primereact">Designer</a> is the ultimate tool to create your own PrimeReact experience powered by a SASS based theme engine
                         with 500+ variables and a Visual Designer. PrimeReact only ships the generated CSS of <b>Material</b>, <b>Bootstrap</b>, <b>Saga</b>, <b>Vela</b>, <b>Arya</b> and legacy themes whereas Designer provides
