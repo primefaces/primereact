3b980692b4fb541891c657a65e1e63d4cc35f7ee
Merve Özçifçi <merveozcifci7@gmail.com>
2021-02-26 14:40:19 +0300

    url fix

--- src/AppNews.js
+++ src/AppNews.js
@@ -21,7 +21,7 @@ export class AppNews extends Component {
                                 <img className="layouts-news-mockup-image" src="showcase/images/news/topbar-ultima-device.png" alt="layout" />
                                 <span className="layout-news-details">
                                 </span>
-                                <a href="https://www.primefaces.org/diamond-react" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{textDecoration: 'none'}} className="layout-news-button">
+                                <a href="https://www.primefaces.org/ultima-react" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{textDecoration: 'none'}} className="layout-news-button">
                                     LEARN MORE<i className="pi pi-angle-right"></i>
                                 </a>
                                 <button type="button" className="p-link layout-news-close" onClick={this.props.onHideNews}>
