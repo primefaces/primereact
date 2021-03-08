fe0ec0e45d96910a3e3c806320f9fb030727e364
mertsincan <sincan.mert@gmail.com>
2021-03-08 13:40:55 +0300

    Added CSV, PDF and Excel export demo to DataTable

--- src/showcase/datatable/DataTableExportDemo.js
+++ src/showcase/datatable/DataTableExportDemo.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { DataTable } from '../../components/datatable/DataTable';
 import { Column } from '../../components/column/Column';
 import { Button } from '../../components/button/Button';
+import { Tooltip } from '../../components/tooltip/Tooltip';
 import ProductService from '../service/ProductService';
 import { TabView } from '../../components/tabview/TabView';
-import { useLiveEditorTabs }from '../liveeditor/LiveEditor';
+import { useLiveEditorTabs } from '../liveeditor/LiveEditor';
 import { AppInlineHeader } from '../../AppInlineHeader';
 
 export class DataTableExportDemo extends Component {
@@ -13,23 +14,73 @@ export class DataTableExportDemo extends Component {
         super(props);
 
         this.state = {
-            products: []
+            products: [],
+            selectedProducts: []
         };
 
-        this.productService = new ProductService();
         this.exportCSV = this.exportCSV.bind(this);
+        this.exportPdf = this.exportPdf.bind(this);
+        this.exportExcel = this.exportExcel.bind(this);
+
+        this.productService = new ProductService();
+
+        this.cols = [
+            { field: 'code', header: 'Code' },
+            { field: 'name', header: 'Name' },
+            { field: 'category', header: 'Category' },
+            { field: 'quantity', header: 'Quantity' }
+        ];
+
+        this.exportColumns = this.cols.map(col => ({ title: col.header, dataKey: col.field }));
     }
 
     componentDidMount() {
         this.productService.getProductsSmall().then(data => this.setState({ products: data }));
     }
 
-    exportCSV() {
-        this.dt.exportCSV();
+    exportCSV(selectionOnly) {
+        this.dt.exportCSV({ selectionOnly });
+    }
+
+    exportPdf() {
+        import('jspdf').then(jsPDF => {
+            import('jspdf-autotable').then(() => {
+                const doc = new jsPDF.default(0, 0);
+                doc.autoTable(this.exportColumns, this.state.products);
+                doc.save('products.pdf');
+            })
+        })
+    }
+
+    exportExcel() {
+        import('xlsx').then(xlsx => {
+            const worksheet = xlsx.utils.json_to_sheet(this.state.products);
+            const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+            const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            this.saveAsExcelFile(excelBuffer, 'products');
+        });
+    }
+
+    saveAsExcelFile(buffer, fileName) {
+        import('file-saver').then(FileSaver => {
+            let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+            let EXCEL_EXTENSION = '.xlsx';
+            const data = new Blob([buffer], {
+                type: EXCEL_TYPE
+            });
+            FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        });
     }
 
     render() {
-        const header = <div style={{textAlign:'left'}}><Button type="button" icon="pi pi-external-link" label="Export" onClick={this.exportCSV}></Button></div>;
+        const header = (
+            <div className="p-d-flex export-buttons">
+                <Button type="button" icon="pi pi-file-o" onClick={() => this.exportCSV(false)} className="p-mr-2" data-pr-tooltip="CSV" />
+                <Button type="button" icon="pi pi-file-excel" onClick={this.exportExcel} className="p-button-success p-mr-2" data-pr-tooltip="XLS" />
+                <Button type="button" icon="pi pi-file-pdf" onClick={this.exportPdf} className="p-button-warning p-mr-2" data-pr-tooltip="PDF" />
+                <Button type="button" icon="pi pi-filter" onClick={() => this.exportCSV(true)} className="p-button-info p-ml-auto" data-pr-tooltip="Selection Only" />
+            </div>
+        );
 
         return (
             <div>
@@ -42,11 +93,13 @@ export class DataTableExportDemo extends Component {
 
                 <div className="content-section implementation">
                     <div className="card">
-                        <DataTable value={this.state.products} header={header} ref={(el) => { this.dt = el; }}>
-                            <Column field="code" header="Code"></Column>
-                            <Column field="name" header="Name"></Column>
-                            <Column field="category" header="Category"></Column>
-                            <Column field="quantity" header="Quantity"></Column>
+                        <Tooltip target=".export-buttons>button" position="bottom" />
+
+                        <DataTable ref={(el) => { this.dt = el; }} value={this.state.products} header={header} dataKey="id"
+                            selectionMode="multiple" selection={this.state.selectedProducts} onSelectionChange={e => this.setState({ selectedProducts: e.value })}>
+                            {
+                                this.cols.map((col, index) => <Column key={index} field={col.field} header={col.header} />)
+                            }
                         </DataTable>
                     </div>
                 </div>
@@ -70,6 +123,7 @@ import React, { Component } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
+import { Tooltip } from 'primereact/tooltip';
 import ProductService from '../service/ProductService';
 
 export class DataTableExportDemo extends Component {
@@ -78,32 +132,84 @@ export class DataTableExportDemo extends Component {
         super(props);
 
         this.state = {
-            products: []
+            products: [],
+            selectedProducts: []
         };
 
-        this.productService = new ProductService();
         this.exportCSV = this.exportCSV.bind(this);
+        this.exportPdf = this.exportPdf.bind(this);
+        this.exportExcel = this.exportExcel.bind(this);
+
+        this.productService = new ProductService();
+
+        this.cols = [
+            { field: 'code', header: 'Code' },
+            { field: 'name', header: 'Name' },
+            { field: 'category', header: 'Category' },
+            { field: 'quantity', header: 'Quantity' }
+        ];
+
+        this.exportColumns = this.cols.map(col => ({ title: col.header, dataKey: col.field }));
     }
 
     componentDidMount() {
         this.productService.getProductsSmall().then(data => this.setState({ products: data }));
     }
 
-    exportCSV() {
-        this.dt.exportCSV();
+    exportCSV(selectionOnly) {
+        this.dt.exportCSV({ selectionOnly });
+    }
+
+    exportPdf() {
+        import('jspdf').then(jsPDF => {
+            import('jspdf-autotable').then(() => {
+                const doc = new jsPDF.default(0, 0);
+                doc.autoTable(this.exportColumns, this.state.products);
+                doc.save('products.pdf');
+            })
+        })
+    }
+
+    exportExcel() {
+        import('xlsx').then(xlsx => {
+            const worksheet = xlsx.utils.json_to_sheet(this.state.products);
+            const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+            const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            this.saveAsExcelFile(excelBuffer, 'products');
+        });
+    }
+
+    saveAsExcelFile(buffer, fileName) {
+        import('file-saver').then(FileSaver => {
+            let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+            let EXCEL_EXTENSION = '.xlsx';
+            const data = new Blob([buffer], {
+                type: EXCEL_TYPE
+            });
+            FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        });
     }
 
     render() {
-        const header = <div style={{textAlign:'left'}}><Button type="button" icon="pi pi-external-link" label="Export" onClick={this.exportCSV}></Button></div>;
+        const header = (
+            <div className="p-d-flex export-buttons">
+                <Button type="button" icon="pi pi-file-o" onClick={() => this.exportCSV(false)} className="p-mr-2" data-pr-tooltip="CSV" />
+                <Button type="button" icon="pi pi-file-excel" onClick={this.exportExcel} className="p-button-success p-mr-2" data-pr-tooltip="XLS" />
+                <Button type="button" icon="pi pi-file-pdf" onClick={this.exportPdf} className="p-button-warning p-mr-2" data-pr-tooltip="PDF" />
+                <Button type="button" icon="pi pi-filter" onClick={() => this.exportCSV(true)} className="p-button-info p-ml-auto" data-pr-tooltip="Selection Only" />
+            </div>
+        );
 
         return (
             <div>
                 <div className="card">
-                    <DataTable value={this.state.products} header={header} ref={(el) => { this.dt = el; }}>
-                        <Column field="code" header="Code"></Column>
-                        <Column field="name" header="Name"></Column>
-                        <Column field="category" header="Category"></Column>
-                        <Column field="quantity" header="Quantity"></Column>
+                    <Tooltip target=".export-buttons>button" position="bottom" />
+
+                    <DataTable ref={(el) => { this.dt = el; }} value={this.state.products} header={header} dataKey="id"
+                        selectionMode="multiple" selection={this.state.selectedProducts} onSelectionChange={e => this.setState({ selectedProducts: e.value })}>
+                        {
+                            this.cols.map((col, index) => <Column key={index} field={col.field} header={col.header} />)
+                        }
                     </DataTable>
                 </div>
             </div>
@@ -119,31 +225,81 @@ import React, { useState, useEffect, useRef } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
+import { Tooltip } from 'primereact/tooltip';
 import ProductService from '../service/ProductService';
 
 const DataTableExportDemo = () => {
     const [products, setProducts] = useState([]);
+    const [selectedProducts, setSelectedProducts] = useState([]);
     const dt = useRef(null);
     const productService = new ProductService();
+
+    const cols = [
+        { field: 'code', header: 'Code' },
+        { field: 'name', header: 'Name' },
+        { field: 'category', header: 'Category' },
+        { field: 'quantity', header: 'Quantity' }
+    ];
+
+    const exportColumns = cols.map(col => ({ title: col.header, dataKey: col.field }));
 
     useEffect(() => {
         productService.getProductsSmall().then(data => setProducts(data));
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const exportCSV = () => {
-        dt.current.exportCSV();
+    const exportCSV = (selectionOnly) => {
+        dt.current.exportCSV({ selectionOnly });
     }
 
-    const header = <div style={{textAlign:'left'}}><Button type="button" icon="pi pi-external-link" label="Export" onClick={exportCSV}></Button></div>;
+    const exportPdf = () => {
+        import('jspdf').then(jsPDF => {
+            import('jspdf-autotable').then(() => {
+                const doc = new jsPDF.default(0, 0);
+                doc.autoTable(exportColumns, products);
+                doc.save('products.pdf');
+            })
+        })
+    }
+
+    const exportExcel = () => {
+        import('xlsx').then(xlsx => {
+            const worksheet = xlsx.utils.json_to_sheet(products);
+            const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+            const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            saveAsExcelFile(excelBuffer, 'products');
+        });
+    }
+
+    const saveAsExcelFile = (buffer, fileName) => {
+        import('file-saver').then(FileSaver => {
+            let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+            let EXCEL_EXTENSION = '.xlsx';
+            const data = new Blob([buffer], {
+                type: EXCEL_TYPE
+            });
+            FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        });
+    }
+
+    const header = (
+        <div className="p-d-flex export-buttons">
+            <Button type="button" icon="pi pi-file-o" onClick={() => exportCSV(false)} className="p-mr-2" data-pr-tooltip="CSV" />
+            <Button type="button" icon="pi pi-file-excel" onClick={exportExcel} className="p-button-success p-mr-2" data-pr-tooltip="XLS" />
+            <Button type="button" icon="pi pi-file-pdf" onClick={exportPdf} className="p-button-warning p-mr-2" data-pr-tooltip="PDF" />
+            <Button type="button" icon="pi pi-filter" onClick={() => exportCSV(true)} className="p-button-info p-ml-auto" data-pr-tooltip="Selection Only" />
+        </div>
+    );
 
     return (
         <div>
             <div className="card">
-                <DataTable value={products} header={header} ref={dt}>
-                    <Column field="code" header="Code"></Column>
-                    <Column field="name" header="Name"></Column>
-                    <Column field="category" header="Category"></Column>
-                    <Column field="quantity" header="Quantity"></Column>
+                <Tooltip target=".export-buttons>button" position="bottom" />
+
+                <DataTable ref={dt} value={products} header={header} dataKey="id"
+                    selectionMode="multiple" selection={selectedProducts} onSelectionChange={e => setSelectedProducts(e.value)}>
+                    {
+                        cols.map((col, index) => <Column key={index} field={col.field} header={col.header} />)
+                    }
                 </DataTable>
             </div>
         </div>
@@ -158,31 +314,81 @@ import React, { useState, useEffect, useRef } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
+import { Tooltip } from 'primereact/tooltip';
 import ProductService from '../service/ProductService';
 
 const DataTableExportDemo = () => {
     const [products, setProducts] = useState([]);
+    const [selectedProducts, setSelectedProducts] = useState([]);
     const dt = useRef(null);
     const productService = new ProductService();
+
+    const cols = [
+        { field: 'code', header: 'Code' },
+        { field: 'name', header: 'Name' },
+        { field: 'category', header: 'Category' },
+        { field: 'quantity', header: 'Quantity' }
+    ];
+
+    const exportColumns = cols.map(col => ({ title: col.header, dataKey: col.field }));
 
     useEffect(() => {
         productService.getProductsSmall().then(data => setProducts(data));
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const exportCSV = () => {
-        dt.current.exportCSV();
+    const exportCSV = (selectionOnly) => {
+        dt.current.exportCSV({ selectionOnly });
     }
 
-    const header = <div style={{textAlign:'left'}}><Button type="button" icon="pi pi-external-link" label="Export" onClick={exportCSV}></Button></div>;
+    const exportPdf = () => {
+        import('jspdf').then(jsPDF => {
+            import('jspdf-autotable').then(() => {
+                const doc = new jsPDF.default(0, 0);
+                doc.autoTable(exportColumns, products);
+                doc.save('products.pdf');
+            })
+        })
+    }
+
+    const exportExcel = () => {
+        import('xlsx').then(xlsx => {
+            const worksheet = xlsx.utils.json_to_sheet(products);
+            const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+            const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            saveAsExcelFile(excelBuffer, 'products');
+        });
+    }
+
+    const saveAsExcelFile = (buffer, fileName) => {
+        import('file-saver').then(FileSaver => {
+            let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+            let EXCEL_EXTENSION = '.xlsx';
+            const data = new Blob([buffer], {
+                type: EXCEL_TYPE
+            });
+            FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        });
+    }
+
+    const header = (
+        <div className="p-d-flex export-buttons">
+            <Button type="button" icon="pi pi-file-o" onClick={() => exportCSV(false)} className="p-mr-2" data-pr-tooltip="CSV" />
+            <Button type="button" icon="pi pi-file-excel" onClick={exportExcel} className="p-button-success p-mr-2" data-pr-tooltip="XLS" />
+            <Button type="button" icon="pi pi-file-pdf" onClick={exportPdf} className="p-button-warning p-mr-2" data-pr-tooltip="PDF" />
+            <Button type="button" icon="pi pi-filter" onClick={() => exportCSV(true)} className="p-button-info p-ml-auto" data-pr-tooltip="Selection Only" />
+        </div>
+    );
 
     return (
         <div>
             <div className="card">
-                <DataTable value={products} header={header} ref={dt}>
-                    <Column field="code" header="Code"></Column>
-                    <Column field="name" header="Name"></Column>
-                    <Column field="category" header="Category"></Column>
-                    <Column field="quantity" header="Quantity"></Column>
+                <Tooltip target=".export-buttons>button" position="bottom" />
+
+                <DataTable ref={dt} value={products} header={header} dataKey="id"
+                    selectionMode="multiple" selection={selectedProducts} onSelectionChange={e => setSelectedProducts(e.value)}>
+                    {
+                        cols.map((col, index) => <Column key={index} field={col.field} header={col.header} />)
+                    }
                 </DataTable>
             </div>
         </div>
@@ -202,7 +408,10 @@ const DataTableExportDemo = () => {
             <div className="content-section documentation">
                 <TabView>
                     {
-                        useLiveEditorTabs({ name: 'DataTableExportDemo', sources: this.sources, service: 'ProductService', data: 'products-small' })
+                        useLiveEditorTabs({
+                            name: 'DataTableExportDemo', sources: this.sources, service: 'ProductService', data: 'products-small',
+                            dependencies: { 'jspdf': '^1.5.3', 'jspdf-autotable': '^3.2.5', 'xlsx': '^0.15.1', 'file-saver': '^2.0.2' }
+                        })
                     }
                 </TabView>
             </div>
