c3b2ce12339cbe74382f2c545d0230296f77f081
mertsincan <sincan.mert@gmail.com>
2021-03-15 08:56:34 +0300

    Fixed #1869 - Server side error because OverlayEventBus

--- src/components/utils/EventBus.js
+++ src/components/utils/EventBus.js
@@ -1,25 +1,28 @@
 export default function() {
-    let eventTarget = document.body.appendChild(document.createComment(''));
-    let eventListener, eventKey;
+    let eventTarget, eventListener, eventKey;
 
     return {
         on(type, listener, key) {
+            eventTarget = eventTarget || document.body.appendChild(document.createComment(''));
             eventKey = key;
             eventListener = function(e) { listener(e.detail); };
             eventTarget.addEventListener(type, eventListener);
         },
         once(type, listener, key) {
+            eventTarget = eventTarget || document.body.appendChild(document.createComment(''));
             eventKey = key;
             eventListener = function(e) { listener(e.detail); };
             eventTarget.addEventListener(type, eventListener, { once: true });
         },
         off(type, key) {
-            if (eventListener && eventKey === key) {
+            if (eventTarget && eventListener && eventKey === key) {
                 eventTarget.removeEventListener(type, eventListener);
             }
         },
         emit(type, detail) {
-            eventTarget.dispatchEvent(new CustomEvent(type, { detail }));
+            if (eventTarget) {
+                eventTarget.dispatchEvent(new CustomEvent(type, { detail }));
+            }
         }
     }
 }
