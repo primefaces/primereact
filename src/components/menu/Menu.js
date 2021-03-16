3bf7734d815187a831f9750f7df133418f5e5313
mertsincan <sincan.mert@gmail.com>
2021-03-16 10:50:01 +0300

    Refactor #1875 - For Menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -339,9 +339,6 @@ export class Menu extends Component {
     render() {
         const element = this.renderElement();
 
-        if (this.props.appendTo)
-            return ReactDOM.createPortal(element, this.props.appendTo);
-        else
-            return element;
+        return this.props.popup ? ReactDOM.createPortal(element, this.props.appendTo || document.body) : element;
     }
 }
