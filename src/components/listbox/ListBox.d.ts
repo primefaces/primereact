4aa9ce50e5bbe9b9a4b6ba8ce2a463e4c42ed117
mertsincan <sincan.mert@gmail.com>
2021-04-08 15:15:36 +0300

    Refactor #1877 - For ListBox

--- src/components/listbox/ListBox.d.ts
+++ src/components/listbox/ListBox.d.ts
@@ -1,15 +1,40 @@
 import * as React from 'react';
 import TooltipOptions from '../tooltip/TooltipOptions';
 
+type OptionGroupTemplateType = string | JSX.Element | ((option: any, index: number) => JSX.Element);
+
+type ItemTemplateType = string | JSX.Element | ((option: any) => JSX.Element);
+
+interface OnChangeTargetOptions {
+    name: string;
+    id: string;
+    value: any;
+}
+
+interface OnChangeParams {
+    originalEvent: Event;
+    value: any;
+    stopPropagation(): void;
+    preventDefault(): void;
+    target: OnChangeTargetOptions;
+}
+
+interface OnFilterValueChangeParams {
+    originalEvent: Event;
+    value: any;
+}
+
 interface ListBoxProps {
     id?: string;
     value?: any;
     options?: any[];
     optionLabel?: string;
     optionValue?: string;
-    optionDisabled?: boolean;
+    optionDisabled?: string;
     optionGroupLabel?: string;
     optionGroupChildren?: string;
+    optionGroupTemplate?: OptionGroupTemplateType;
+    itemTemplate?: ItemTemplateType;
     style?: object;
     listStyle?: object;
     listClassName?: string;
@@ -25,13 +50,11 @@ interface ListBoxProps {
     filterPlaceholder?: string;
     filterLocale?: string;
     tabIndex?: number;
-    tooltip?: any;
+    tooltip?: string;
     tooltipOptions?: TooltipOptions;
     ariaLabelledBy?: string;
-    itemTemplate?: any;
-    optionGroupTemplate?: any;
-    onChange?(e: {originalEvent: Event, value: any, target: {name: string, id: string, value: any}}): void;
-    onFilterValueChange?(e: {originalEvent: Event, value: string}): void;
+    onChange?(e: OnChangeParams): void;
+    onFilterValueChange(e: OnFilterValueChangeParams): void;
 }
 
-export class ListBox extends React.Component<ListBoxProps,any> {}
+export class ListBox extends React.Component<ListBoxProps, any> { }
