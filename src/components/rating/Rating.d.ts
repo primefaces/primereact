a4331e557b145e230196a368b9ec4cc445acfc57
mertsincan <sincan.mert@gmail.com>
2021-04-08 15:59:27 +0300

    Refactor #1877 - For Rating

--- src/components/rating/Rating.d.ts
+++ src/components/rating/Rating.d.ts
@@ -1,6 +1,20 @@
 import * as React from 'react';
 import TooltipOptions from '../tooltip/TooltipOptions';
 
+interface OnChangeTargetOptions {
+    name: string;
+    id: string;
+    value: number | undefined | null;
+}
+
+interface OnChangeParams {
+    originalEvent: Event;
+    value: number | undefined | null;
+    stopPropagation(): void;
+    preventDefault(): void;
+    target: OnChangeTargetOptions;
+}
+
 interface RatingProps {
     id?: string;
     value?: number;
@@ -10,9 +24,9 @@ interface RatingProps {
     cancel?: boolean;
     style?: object;
     className?: string;
-    tooltip?: any;
+    tooltip?: string;
     tooltipOptions?: TooltipOptions;
-    onChange?(e: {originalEvent: Event, value: number, target: {name: string, id: string, value: number}}): void;
+    onChange?(e: OnChangeParams): void;
 }
 
-export class Rating extends React.Component<RatingProps,any> {}
+export class Rating extends React.Component<RatingProps, any> { }
