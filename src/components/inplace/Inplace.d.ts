faeae4e26078d0c70911464671bc96b50cbd2edd
mertsincan <sincan.mert@gmail.com>
2021-04-09 16:51:07 +0300

    Refactor #1877 - For Inplace

--- src/components/inplace/Inplace.d.ts
+++ src/components/inplace/Inplace.d.ts
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
+interface OnToggleParams {
+    originalEvent: Event;
+    value: boolean;
+}
 interface InplaceProps {
     style?: object;
     className?: string;
@@ -8,15 +12,15 @@ interface InplaceProps {
     disabled?: boolean;
     tabIndex?: number;
     ariaLabel?: string;
-    onOpen?(event: Event): void;
-    onClose?(event: Event): void;
-    onToggle?(e:{originalEvent: Event, value: boolean}): void;
+    onOpen?(event: React.MouseEvent<HTMLElement>): void;
+    onClose?(event: React.MouseEvent<HTMLElement>): void;
+    onToggle?(e: OnToggleParams): void;
 }
 
-export class Inplace extends React.Component<InplaceProps,any> {}
+export class Inplace extends React.Component<InplaceProps, any> { }
 
 // tslint:disable-next-line:max-classes-per-file
-export class InplaceDisplay extends React.Component{}
+export class InplaceDisplay extends React.Component { }
 
 // tslint:disable-next-line:max-classes-per-file
-export class InplaceContent extends React.Component {}
+export class InplaceContent extends React.Component { }
