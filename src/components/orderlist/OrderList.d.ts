8edfd2bd752ca078f95de6564bb0a6ff51498b76
mertsincan <sincan.mert@gmail.com>
2021-04-09 11:07:28 +0300

    Refactor #1877 - For OrderList

--- src/components/orderlist/OrderList.d.ts
+++ src/components/orderlist/OrderList.d.ts
@@ -1,16 +1,21 @@
 import * as React from 'react';
 
+interface OnChangeParams {
+    originalEvent: Event;
+    value: any;
+}
+
 interface OrderListProps {
     id?: string;
     value?: any[];
-    header?: any;
+    header?: React.ReactNode;
     style?: object;
     className?: string;
     listStyle?: object;
     dragdrop?: boolean;
     tabIndex?: number;
-    onChange?(e: {originalEvent: Event, value: any}): void;
-    itemTemplate?(item: any): JSX.Element | undefined;
+    onChange?(e: OnChangeParams): void;
+    itemTemplate?(item: any): React.ReactNode;
 }
 
-export class OrderList extends React.Component<OrderListProps,any> {}
+export class OrderList extends React.Component<OrderListProps, any> { }
