1d7553a3b562a8a9e5e0239c3d17f38e073c9bde
mertsincan <sincan.mert@gmail.com>
2021-04-09 16:53:57 +0300

    Refactor #1877 - For ScrollTop

--- src/components/scrolltop/ScrollTop.d.ts
+++ src/components/scrolltop/ScrollTop.d.ts
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
+type TargetType = 'window' | undefined | null;
+
+type ScrollBehavior = 'auto' | 'smooth';
+
 interface ScrollTopProps {
-    target?: string;
+    target?: TargetType;
     threshold?: number;
     icon?: string;
-    behavior?: string;
+    behavior?: ScrollBehavior;
     className?: string;
     style?: object;
 }
 
-export class ScrollTop extends React.Component<ScrollTopProps,any> {}
+export class ScrollTop extends React.Component<ScrollTopProps, any> { }
