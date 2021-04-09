dcda0191ff64783253dd6b914d1db4e5a8edaf7b
mertsincan <sincan.mert@gmail.com>
2021-04-09 13:37:20 +0300

    Refactor #1877 - For Card

--- src/components/card/Card.d.ts
+++ src/components/card/Card.d.ts
@@ -1,13 +1,15 @@
 import * as React from 'react';
 
+type TemplateTypes = React.ReactNode | ((props: CardProps) => React.ReactNode);
+
 interface CardProps {
     id?: string;
-    header?: any;
-    footer?: any;
-    title?: any;
-    subTitle?: any;
+    header?: TemplateTypes;
+    footer?: TemplateTypes;
+    title?: TemplateTypes;
+    subTitle?: TemplateTypes;
     style?: object;
     className?: string;
 }
 
-export class Card extends React.Component<CardProps,any> {}
+export class Card extends React.Component<CardProps, any> { }
