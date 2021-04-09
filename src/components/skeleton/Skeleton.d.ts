6493156c080060f684497e4dbce43b4366d72d2f
mertsincan <sincan.mert@gmail.com>
2021-04-09 16:56:06 +0300

    Refactor #1877 - For Skeleton

--- src/components/skeleton/Skeleton.d.ts
+++ src/components/skeleton/Skeleton.d.ts
@@ -1,14 +1,18 @@
 import * as React from 'react';
 
+type ShapeType = 'rectangle' | 'circle';
+
+type AnimationType = 'wave' | 'none';
+
 interface SkeletonProps {
-    shape?: string;
+    shape?: ShapeType;
     size?: string;
     width?: string;
     height?: string;
     borderRadius?: string;
-    animation?: string;
+    animation?: AnimationType;
     style?: object;
     className?: string;
 }
 
-export class Skeleton extends React.Component<SkeletonProps,any> {}
+export class Skeleton extends React.Component<SkeletonProps, any> { }
