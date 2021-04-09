60e6a7a334eaa491cff36e27a0014b10bec91e6a
mertsincan <sincan.mert@gmail.com>
2021-04-09 13:39:38 +0300

    Refactor #1877 - For DeferredContent

--- src/components/deferredcontent/DeferredContent.d.ts
+++ src/components/deferredcontent/DeferredContent.d.ts
@@ -4,4 +4,4 @@ interface DeferredContentProps {
     onLoad?(event: Event): void;
 }
 
-export class DeferredContent extends React.Component<DeferredContentProps,any> {}
+export class DeferredContent extends React.Component<DeferredContentProps, any> { }
