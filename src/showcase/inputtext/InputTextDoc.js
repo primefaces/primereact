364d7344137f4c8948d990f69df5b00153b87cab
mcandu <dundarmehmetcan@gmail.com>
2021-04-13 10:45:49 +0300

    Refactor

--- src/showcase/inputtext/InputTextDoc.js
+++ src/showcase/inputtext/InputTextDoc.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { TabView, TabPanel } from '../../components/tabview/TabView';
-<<<<<<< Updated upstream
 import { useLiveEditorTabs } from '../liveeditor/LiveEditor';
-=======
-import {  useLiveEditorTabs } from '../liveeditor/LiveEditor';
->>>>>>> Stashed changes
 import { CodeHighlight } from '../codehighlight/CodeHighlight';
 
 export class InputTextDoc extends Component {
