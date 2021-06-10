4d4fe966b1e2dbae4a12e0677596b86c2eb5fb04
mertsincan <sincan.mert@gmail.com>
2021-06-10 04:13:57 +0300

    Update DropdownDoc.js

--- src/showcase/dropdown/DropdownDoc.js
+++ src/showcase/dropdown/DropdownDoc.js
@@ -951,6 +951,12 @@ const groupedCities = [
                                         <td>Locale to use in filtering. The default locale is the host environment's current locale.</td>
                                     </tr>
                                     <tr>
+                                        <td>emptyMessage</td>
+                                        <td>string</td>
+                                        <td>No results found</td>
+                                        <td>Text to display when there are no options available.</td>
+                                    </tr>
+                                    <tr>
                                         <td>emptyFilterMessage</td>
                                         <td>any</td>
                                         <td>No results found</td>
