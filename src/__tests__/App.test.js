e754c35c708f6cd9e5874a83f7a50fa6ced4f40c
mertsincan <sincan.mert@gmail.com>
2021-06-23 11:34:12 +0300

    Test: refactor

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App';
+import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
