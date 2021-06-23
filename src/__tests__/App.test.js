dde81b52997f5aff00b0bd91f4db4d6ea70ffa96
mertsincan <sincan.mert@gmail.com>
2021-06-23 12:39:26 +0300

    Test: App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import { HashRouter } from 'react-router-dom';
+
+// ignore window['gtag']
+Object.defineProperty(window, 'gtag', {
+    value: () => ({
+        getPropertyValue: () => { }
+    })
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const root = document.createElement('div');
+
+    ReactDOM.render(
+        <HashRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </HashRouter>,
+        root
+    );
 });
