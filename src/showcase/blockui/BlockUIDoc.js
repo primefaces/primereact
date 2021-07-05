8ece808c48a595a16a33c813924f68cfbcdc3dc2
mcandu <dundarmehmetcan@gmail.com>
2021-07-05 14:46:03 +0300

    Update Terminal & BlockUI doc

--- src/showcase/blockui/BlockUIDoc.js
+++ src/showcase/blockui/BlockUIDoc.js
@@ -98,13 +98,139 @@ export class BlockUIDemo extends Component {
             'hooks': {
                 tabName: 'Hooks Source',
                 content: `
+import React, { useState, useEffect } from 'react';
+import { BlockUI } from 'primereact/blockui';
+import { Button } from 'primereact/button';
+import { Panel } from 'primereact/panel';
+import './BlockUIDemo.css';
 
+export const BlockUIDemo = () => {
+
+    const [blockedPanel, setBlockedPanel] = useState(false);
+    const [blockedDocument, setBlockedDocument] = useState(false);
+
+    useEffect(() => {
+        if(blockedDocument) {
+            setTimeout(() => {
+                setBlockedDocument(false);
+            }, 3000);
+        }
+    }, [blockedDocument])
+
+    const blockDocument = () => {
+        setBlockedDocument(true);
+    }
+
+    const blockPanel = () => {
+        setBlockedPanel(true);
+    }
+
+    const unblockPanel = () => {
+        setBlockedPanel(false);
+    }
+
+    return (
+        <div className="blockui-demo">
+            <div className="card">
+                <h5>Document</h5>
+                <BlockUI blocked={blockedDocument} fullScreen />
+
+                <Button type="button" label="Block" onClick={blockDocument} />
+
+                <h5>Panel</h5>
+                <Button type="button" label="Block" onClick={blockPanel} />
+                <Button type="button" label="Unblock" onClick={unblockPanel} />
+
+                <BlockUI blocked={blockedPanel}>
+                    <Panel header="Basic" style={{ marginTop: '20px' }}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Panel>
+                </BlockUI>
+
+                <BlockUI blocked={blockedPanel} template={<i className="pi pi-lock" style={{'fontSize': '3rem'}} />}>
+                    <Panel header="Template" style={{ marginTop: '20px' }}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Panel>
+                </BlockUI>
+            </div>
+        </div>
+    )
+}
                 `
             },
             'ts': {
                 tabName: 'TS Source',
                 content: `
+import React, { useState, useEffect } from 'react';
+import { BlockUI } from 'primereact/blockui';
+import { Button } from 'primereact/button';
+import { Panel } from 'primereact/panel';
+import './BlockUIDemo.css';
 
+export const BlockUIDemo = () => {
+
+    const [blockedPanel, setBlockedPanel] = useState<boolean>(false);
+    const [blockedDocument, setBlockedDocument] = useState<boolean>(false);
+
+    useEffect(() => {
+        if(blockedDocument){
+            setTimeout(() => {
+                setBlockedDocument(false);
+            }, 3000);
+        }
+    }, [blockedDocument])
+
+    const blockDocument = () => {
+        setBlockedDocument(true);
+    }
+
+    const blockPanel = () => {
+        setBlockedPanel(true);
+    }
+
+    const unblockPanel = () => {
+        setBlockedPanel(false);
+    }
+
+    return (
+        <div className="blockui-demo">
+            <div className="card">
+                <h5>Document</h5>
+                <BlockUI blocked={blockedDocument} fullScreen />
+
+                <Button type="button" label="Block" onClick={blockDocument} />
+
+                <h5>Panel</h5>
+                <Button type="button" label="Block" onClick={blockPanel} />
+                <Button type="button" label="Unblock" onClick={unblockPanel} />
+
+                <BlockUI blocked={blockedPanel}>
+                    <Panel header="Basic" style={{ marginTop: '20px' }}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Panel>
+                </BlockUI>
+
+                <BlockUI blocked={blockedPanel} template={<i className="pi pi-lock" style={{'fontSize': '3rem'}} />}>
+                    <Panel header="Template" style={{ marginTop: '20px' }}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Panel>
+                </BlockUI>
+            </div>
+        </div>
+    )
+}
                 `
             }
         }
