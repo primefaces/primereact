2b13c03ed85e300b6b3bfadbb5625f626c092ef7
mertsincan <sincan.mert@gmail.com>
2021-11-04 15:18:53 +0300

    Update DataTable.d.ts

--- src/components/datatable/DataTable.d.ts
+++ src/components/datatable/DataTable.d.ts
@@ -339,7 +339,7 @@ export interface DataTableProps {
 
 export declare class DataTable extends React.Component<DataTableProps, any> {
     public reset(): void;
-    public exportCSV(options: { selectionOnly: boolean }): void;
+    public exportCSV(options?: { selectionOnly: boolean }): void;
     public filter<T>(value: T, field: string, mode: DataTableFilterMatchModeType, index?: number): void;
     public resetColumnOrder(): void;
     public closeEditingCell(): void;
