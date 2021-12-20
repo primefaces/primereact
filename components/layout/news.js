6ea4282e59815d202958b5f8b0223dde675e6d19
mcandu <dundarmehmetcan@gmail.com>
2021-12-20 20:15:00 +0300

    Updated news

--- components/layout/news.js
+++ components/layout/news.js
@@ -3,19 +3,21 @@ import getConfig from 'next/config';
 
 export default function News(props) {
     const contextPath = getConfig().publicRuntimeConfig.contextPath;
-    
+
+    const redirect = () => {
+        window.open('https://www.primefaces.org/store/', '_blank');
+    }
+
     return (
         <>
             {
                 props.active && (
                     <div className="layout-news">
                         <div className="layout-news-container">
-                            <a href="https://www.primefaces.org/primeblocks-react" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{textDecoration: 'none'}}>
-                                <img className="layouts-news-mockup-image" src={`${contextPath}/images/news/topbar-primeblocks-device.png`} alt="News" />
-                            </a>
-                            <a href="https://www.primefaces.org/primeblocks-react" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{textDecoration: 'none'}} className="layout-news-button">
-                                LEARN MORE
-                                <i className="pi pi-angle-right"></i>
+                            <img onClick={redirect} className="layout-news-logo p-ml-2" src={`${contextPath}/images/news/topbar-newyear-logo.svg`}/>
+                            <h3 onClick={redirect} className="layout-news-header p-px-2"><span>DISCOUNT UP TO 50% </span><span>ON EVERYTHING AT PRIMESTORE</span></h3>
+                            <a href="https://www.primefaces.org/primeblocks-react" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{ textDecoration: 'none' }} className="layout-news-button">
+                                Read More
                             </a>
                             <button type="button" className="p-link layout-news-close" onClick={props.onHide}>
                                 <i className="pi pi-times"></i>
