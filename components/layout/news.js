30ad253d7900a7ce7ade6c8243de18447f98bbf7
mcandu <dundarmehmetcan@gmail.com>
2021-12-23 10:25:41 +0300

    Refactor news

--- components/layout/news.js
+++ components/layout/news.js
@@ -16,7 +16,7 @@ export default function News(props) {
                         <div className="layout-news-container">
                             <img onClick={redirect} className="layout-news-logo p-ml-2" src={`${contextPath}/images/news/topbar-newyear-logo.svg`}/>
                             <h3 onClick={redirect} className="layout-news-header p-px-2"><span>DISCOUNT UP TO 50% </span><span>ON EVERYTHING AT PRIMESTORE</span></h3>
-                            <a href="https://www.primefaces.org/primeblocks-react" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{ textDecoration: 'none' }} className="layout-news-button">
+                            <a href="https://www.primefaces.org/store/" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{ textDecoration: 'none' }} className="layout-news-button">
                                 Read More
                             </a>
                             <button type="button" className="p-link layout-news-close" onClick={props.onHide}>
