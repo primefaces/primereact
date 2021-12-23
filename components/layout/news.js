bda879e3c190b41f6b01f4a95c156885924b8d46
mcandu <dundarmehmetcan@gmail.com>
2021-12-23 11:05:05 +0300

    Updated news texts

--- components/layout/news.js
+++ components/layout/news.js
@@ -15,9 +15,9 @@ export default function News(props) {
                     <div className="layout-news">
                         <div className="layout-news-container">
                             <img onClick={redirect} className="layout-news-logo p-ml-2" src={`${contextPath}/images/news/topbar-newyear-logo.svg`}/>
-                            <h3 onClick={redirect} className="layout-news-header p-px-2"><span>DISCOUNT UP TO 50% </span><span>ON EVERYTHING AT PRIMESTORE</span></h3>
+                            <h3 onClick={redirect} className="layout-news-header p-px-2"><span>UP TO 50% </span><span>DISCOUNT AT PRIMESTORE</span></h3>
                             <a href="https://www.primefaces.org/store/" target="_blank" rel="noopener noreferrer" tabIndex="-1" style={{ textDecoration: 'none' }} className="layout-news-button">
-                                Read More
+                                VISIT STORE
                             </a>
                             <button type="button" className="p-link layout-news-close" onClick={props.onHide}>
                                 <i className="pi pi-times"></i>
