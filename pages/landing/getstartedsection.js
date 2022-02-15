d08ae9cca58e10f3c648aaf546334fdddc53c534
Cagatay Civici <cagatay.civici@gmail.com>
2022-02-15 11:12:45 +0300

    Cosmetics

--- pages/landing/getstartedsection.js
+++ pages/landing/getstartedsection.js
@@ -4,7 +4,7 @@ export default function GetStartedSection() {
     return (
         <section className="landing-getstarted flex align-items-center justify-content-center mt-8 z-1">
             <Link href="/setup">
-                <a className="linkbox active font-medium py-3 px-4 fadeinleft animation-duration-500">Get Started <i className="pi pi-arrow-right ml-3"></i></a>
+                <a className="linkbox active font-semibold py-3 px-4 fadeinleft animation-duration-500">Get Started <i className="pi pi-arrow-right ml-3"></i></a>
             </Link>
             <div className="box font-medium p-3 px-4 ml-3 bg-transparent inline-flex align-items-center fadeinright animation-duration-500">
                 <i className="download-icon pi pi-download mr-3"></i>
