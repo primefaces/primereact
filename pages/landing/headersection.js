d5d136048b67bcfa40a6e791be4bd6c2fb8040c9
Cagatay Civici <cagatay.civici@gmail.com>
2022-03-01 15:25:47 +0300

    Keep sticky header when menu is deactivated

--- pages/landing/headersection.js
+++ pages/landing/headersection.js
@@ -40,6 +40,12 @@ export default function HeroSection(props) {
         }
     }, []);
 
+    useEffect(() => {
+       if (scrollListener.current) {
+           scrollListener.current();
+       } 
+    });
+
     return (
         <section ref={containerElement} className={headerClassName}>
             <span>
