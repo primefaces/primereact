4cfc7dec7a63d0672e4ae805e277755be0653393
melloware <mellowaredev@gmail.com>
2022-03-02 08:51:15 -0500

    Fix #2667: Dropdown filled state if not empty

--- components/lib/dropdown/Dropdown.js
+++ components/lib/dropdown/Dropdown.js
@@ -980,7 +980,7 @@ export class Dropdown extends Component {
             'p-disabled': this.props.disabled,
             'p-focus': this.state.focused,
             'p-dropdown-clearable': this.props.showClear && !this.props.disabled,
-            'p-inputwrapper-filled': this.props.value,
+            'p-inputwrapper-filled': ObjectUtils.isNotEmpty(this.props.value),
             'p-inputwrapper-focus': this.state.focused || this.state.overlayVisible
         });
         let visibleOptions = this.getVisibleOptions();
