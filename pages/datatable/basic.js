0c3e7b0f8071a1beabbb457f69519e6ccdd93bcb
mertsincan <sincan.mert@gmail.com>
2022-04-10 16:44:04 +0100

    Update basic.js

--- pages/datatable/basic.js
+++ pages/datatable/basic.js
@@ -32,14 +32,14 @@ const DataTableBasicDemo = () => {
             </div>
 
             <div className="content-section implementation">
-            <div className="card">
-            <DataTable value={products} responsiveLayout="scroll">
-                <Column field="code" header="Code"></Column>
-                <Column field="name" header="Name"></Column>
-                <Column field="category" header="Category"></Column>
-                <Column field="quantity" header="Quantity"></Column>
-            </DataTable>
-        </div>
+                <div className="card">
+                    <DataTable value={products} responsiveLayout="scroll">
+                        <Column field="code" header="Code"></Column>
+                        <Column field="name" header="Name"></Column>
+                        <Column field="category" header="Category"></Column>
+                        <Column field="quantity" header="Quantity"></Column>
+                    </DataTable>
+                </div>
             </div>
 
             <DataTableBasicDemoDoc></DataTableBasicDemoDoc>
