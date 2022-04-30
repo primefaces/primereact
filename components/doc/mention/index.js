93f424a93ed8f0df153ede7638eb0e008f329571
Cagatay Civici <cagatay.civici@gmail.com>
2022-04-30 12:01:58 +0300

    a11y for mention

--- components/doc/mention/index.js
+++ components/doc/mention/index.js
@@ -818,6 +818,66 @@ const onSearch = (event) => {
                         </table>
                     </div>
 
+                    <h5>Accessibility</h5>
+                    <h6>Screen Reader</h6>
+                    <p>Value to describe the component can either be provided via <i>label</i> tag combined with <i>inputId</i> prop or using <i>aria-labelledby</i>, <i>aria-label</i> props. The input element has <i>combobox</i> role
+                    in addition to <i>aria-autocomplete</i>, <i>aria-haspopup</i> and <i>aria-expanded</i> attributes. The relation between the input and the popup is created with <i>aria-owns</i> and <i>aria-activedescendant</i> attribute is used
+                    to instruct screen reader which option to read during keyboard navigation within the popup list.</p>
+                    <p>The popup list has an id that refers to the <i>aria-owns</i> attribute of the input element and uses <i>listbox</i> as the role. Each list item has <i>option</i> role and an id to match the <i>aria-activedescendant</i> of the input element.</p>
+<CodeHighlight>
+{`
+<label htmlFor="men1">Username</label>
+<Mention inputId="men1" />
+
+<span id="men2">Email</span>
+<Mention aria-labelledby="men2" />
+
+<Mention aria-label="City" />
+`}
+</CodeHighlight>
+                    <h6>Keyboard Support</h6>
+                    <div className="doc-tablewrapper">
+                        <table className="doc-table">
+                            <thead>
+                                <tr>
+                                    <th>Key</th>
+                                    <th>Function</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><i>tab</i></td>
+                                    <td>Moves focus to the input element when popup is not visible. 
+                                        If the popup is open and an item is highlighted then popup gets closed, item gets selected and focus moves to the next focusable element.</td>
+                                </tr>
+                                <tr>
+                                    <td><i>up arrow</i></td>
+                                    <td>Highlights the previous item if popup is visible.</td>
+                                </tr>
+                                <tr>
+                                    <td><i>down arrow</i></td>
+                                    <td>Highlights the next item if popup is visible.</td>
+                                </tr>
+                                <tr>
+                                    <td><i>enter</i></td>
+                                    <td>Selects the highlighted item and closes the popup if popup is visible.</td>
+                                </tr>
+                                <tr>
+                                    <td><i>home</i></td>
+                                    <td>Highlights the first item if popup is visible.</td>
+                                </tr>
+                                <tr>
+                                    <td><i>end</i></td>
+                                    <td>Highlights the last item if popup is visible.</td>
+                                </tr>
+                                <tr>
+                                    <td><i>escape</i></td>
+                                    <td>Hides the popup.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
                     <h5>Dependencies</h5>
                     <p>None.</p>
 
