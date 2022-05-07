d1a1ae67dc20fc88ae152918ef2fb0878db34acc
Cagatay Civici <cagatay.civici@gmail.com>
2022-05-07 14:40:28 +0300

    a11y update for accordion

--- components/doc/accordion/index.js
+++ components/doc/accordion/index.js
@@ -807,34 +807,55 @@ import { Accordion, AccordionTab } from 'primereact/accordion';
 
                         <h5>Accessibility</h5>
                         <h6>Screen Reader</h6>
-                        <p>Accordion header elements use <i>aria-controls</i> to define the id of the content section along with <i>aria-expanded</i> for the visibility state. The content
-                        uses <i>region</i>, defines an id that matches the <i>aria-controls</i> of the header and <i>aria-labelledby</i> referring to the id of the header.</p>
+                        <p>Accordion header elements have a <i>button</i> role and use <i>aria-controls</i> to define the id of the content section along with <i>aria-expanded</i> for the visibility state. The value to read a header element 
+                        defaults to the value of the <i>header</i> property and can be customized by defining an <i>aria-label</i> or <i>aria-labelledby</i> via the <i>headerProps</i> property.</p>
+                        <p>The content uses <i>region</i>, defines an id that matches the <i>aria-controls</i> of the header and <i>aria-labelledby</i> referring to the id of the header.</p>
 
-                        <h6>Keyboard Support</h6>
+                        <h6>Header Keyboard Support</h6>
                         <div className="doc-tablewrapper">
-                        <table className="doc-table">
-                            <thead>
-                                <tr>
-                                    <th>Key</th>
-                                    <th>Function</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                    <td><i>tab</i></td>
-                                    <td>Moves focus through the header elements.</td>
-                                </tr>
-                                <tr>
-                                    <td><i>enter</i></td>
-                                    <td>Toggles the visibility of the content.</td>
-                                </tr>
-                                <tr>
-                                    <td><i>space</i></td>
-                                    <td>Toggles the visibility of the content.</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                            <table className="doc-table">
+                                <thead>
+                                    <tr>
+                                        <th>Key</th>
+                                        <th>Function</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i>tab</i></td>
+                                        <td>Moves focus to the next the focusable element in the page tab sequence.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>shift</i> + <i>tab</i></td>
+                                        <td>Moves focus to the previous the focusable element in the page tab sequence.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>enter</i></td>
+                                        <td>Toggles the visibility of the content.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>space</i></td>
+                                        <td>Toggles the visibility of the content.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>down arrow</i></td>
+                                        <td>Moves focus to the next header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>up arrow</i></td>
+                                        <td>Moves focus to the previous header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>home</i></td>
+                                        <td>Moves focus to the first header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>end</i></td>
+                                        <td>Moves focus to the last header.</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
 
                         <h5>Dependencies</h5>
                         <p>None.</p>
