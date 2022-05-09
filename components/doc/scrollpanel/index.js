f56d695ba92667f7da1ecd0e61612dc03124a235
Cagatay Civici <cagatay.civici@gmail.com>
2022-05-09 14:53:03 +0300

    a11y for ScrollPanel

--- components/doc/scrollpanel/index.js
+++ components/doc/scrollpanel/index.js
@@ -481,6 +481,40 @@ import { ScrollPanel } from 'primereact/scrollpanel';
                             </tbody>
                         </table>
 
+                        <h5>Accessibility</h5>
+                        <h6>Screen Reader</h6>
+                        <p>Scrollbars of the ScrollPanel has a <i>scrollbar</i> role along with the <i>aria-controls</i> attribute that refers to the id of the scrollable content container and the <i>aria-orientation</i> to indicate the orientation of scrolling.</p>
+
+                        <h6>Header Keyboard Support</h6>
+                        <div className="doc-tablewrapper">
+                            <table className="doc-table">
+                                <thead>
+                                    <tr>
+                                        <th>Key</th>
+                                        <th>Function</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i>down arrow</i></td>
+                                        <td>Scrolls content down when vertical scrolling is available.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>up arrow</i></td>
+                                        <td>Scrolls content up when vertical scrolling is available.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>left</i></td>
+                                        <td>Scrolls content left when horizontal scrolling is available.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>right</i></td>
+                                        <td>Scrolls content right when horizontal scrolling is available.</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
                         <h5>Dependencies</h5>
                         <p>None.</p>
                     </div>
