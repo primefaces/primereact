a3959a5c7b052d8838d035620767b4e89eeab7e2
Cagatay Civici <cagatay.civici@gmail.com>
2022-05-10 11:04:30 +0300

    a11y for TabView

--- components/doc/tabview/index.js
+++ components/doc/tabview/index.js
@@ -1177,10 +1177,59 @@ template: (options) => {
                                 </tr>
                             </tbody>
                         </table>
-
-                        <h5>Dependencies</h5>
-                        <p>None.</p>
                     </div>
+
+                    <h5>Accessibility</h5>
+                        <h6>Screen Reader</h6>
+                        <p>TabView container is defined with the <i>tablist</i> role, as any attribute is passed to the container element <i>aria-labelledby</i> can be optionally used to specify an element to describe the TabView. Each tab header
+                        has a <i>tab</i> role along with <i>aria-selected</i> state attribute and <i>aria-controls</i> to refer to the corresponding tab content element. The content element of each tab has <i>tabpanel</i> role, an id to match the
+                        <i>aria-controls</i> of the header and <i>aria-labelledby</i> reference to the header as the accessible name.</p>
+
+                        <h6>Tab Header Keyboard Support</h6>
+                        <div className="doc-tablewrapper">
+                            <table className="doc-table">
+                                <thead>
+                                    <tr>
+                                        <th>Key</th>
+                                        <th>Function</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><i>tab</i></td>
+                                        <td>Moves focus through the header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>enter</i></td>
+                                        <td>Activates the focused tab header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>space</i></td>
+                                        <td>Activates the focused tab header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>right arrow</i></td>
+                                        <td>Moves focus to the next header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>left arrow</i></td>
+                                        <td>Moves focus to the previous header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>home</i></td>
+                                        <td>Moves focus to the last header.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i>end</i></td>
+                                        <td>Moves focus to the first header.</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                    <h5>Dependencies</h5>
+                    <p>None.</p>
 
                 </TabPanel>
 
