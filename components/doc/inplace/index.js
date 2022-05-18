7252796193ed151d2d5b67fb27b0308349544992
Cagatay Civici <cagatay.civici@gmail.com>
2022-05-18 13:37:16 +0300

    a11y for ScrollTop

--- components/doc/inplace/index.js
+++ components/doc/inplace/index.js
@@ -493,7 +493,9 @@ const onOpen = () => {
                     <h5>Accessibility</h5>
                 <DevelopmentSection>
                     <h6>Screen Reader</h6>
-                    <p>Inplace uses <i>button</i> role in view mode by default, <i>displayProps</i> can be used for customizations like adding <i>aria-label</i> or <i>aria-labelledby</i> attributes to describe the content of the view mode or even overriding the default role.</p>
+                    <p>Inplace component defines <i>aria-live</i> as "polite" by default, since any valid attribute is passed to the main container aria roles and attributes of the root element can be customized easily.</p>
+                    <p>Display element uses <i>button</i> role in view mode by default, <i>displayProps</i> can be used for customizations like adding <i>aria-label</i> or <i>aria-labelledby</i> attributes 
+                    to describe the content of the view mode or even overriding the default role.</p>
                     <p>Closable inplace components displays a button with an <i>aria-label</i> that refers to the <i>aria.close</i> property of the <Link href="/locale">locale</Link> API by default, you may use
                     <i>closeButtonProps</i> to customize the element and override the default <i>aria-label</i>.</p>
 
