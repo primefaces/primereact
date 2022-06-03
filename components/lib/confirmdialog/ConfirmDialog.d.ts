07c62cd415dcdfece70deb520d908dac4a66a987
Ruan23 <ruanvm101@gmail.com>
2022-06-03 18:23:15 +1200

    Fix ConfirmDialogProps extension of  DialogProps

--- components/lib/confirmdialog/ConfirmDialog.d.ts
+++ components/lib/confirmdialog/ConfirmDialog.d.ts
@@ -2,24 +2,12 @@ import * as React from 'react';
 import { DialogProps } from '../dialog';
 import { IconType } from '../utils';
 
-type ConfirmDialogTemplateType = React.ReactNode | ((options: ConfirmDialogOptions) => React.ReactNode);
+type ConfirmDialogTemplateType = React.ReactNode | ((options: ConfirmDialogProps) => React.ReactNode);
 
 type ConfirmDialogAppendToType = 'self' | HTMLElement | undefined | null;
 
 interface ConfirmDialogBreakpoints {
     [key: string]: string;
-}
-
-interface ConfirmDialogOptions {
-    accept(): void;
-    reject(): void;
-    acceptClassName: string;
-    rejectClassName: string;
-    acceptLabel: string;
-    rejectLabel: string;
-    element: React.ReactNode;
-    props: ConfirmDialogProps;
-    [key: string]: any;
 }
 
 export interface ConfirmDialogProps extends Omit<DialogProps, 'onHide'> {
