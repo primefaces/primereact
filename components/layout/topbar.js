506f8fb3d58a682acfa3822b31017e0d974f6edf
Cagatay Civici <cagatay.civici@gmail.com>
2022-07-26 14:33:09 +0300

    Add UI Kit

--- components/layout/topbar.js
+++ components/layout/topbar.js
@@ -161,7 +161,7 @@ export default function Topbar(props) {
                             <li role="none"><a href="https://www.primefaces.org/designer/primereact" role="menuitem"><i className="pi pi-fw pi-palette" /><span>Designer</span></a></li>
                             <li role="none"><a href="https://www.primefaces.org/designer-react" role="menuitem"><i className="pi pi-fw pi-desktop" /><span>Visual Editor</span></a></li>
                             <li role="none"><Link href="/icons"><a role="menuitem"><i className="pi pi-fw pi-info-circle" /><span>Icons</span></a></Link></li>
-                            <li role="none"><a href="https://www.figma.com/community/file/890589747170608208" role="menuitem"><i className="pi pi-fw pi-pencil" /><span>Figma UI Kit</span></a></li>
+                            <li role="none"><Link href="/uikit"><a role="menuitem"><i className="pi pi-fw pi-pencil" /><span>Figma UI Kit</span></a></Link></li>
 
                             <li role="none" className="topbar-submenu-header">BOOTSTRAP</li>
                             <li role="none"><button type="button" className="p-link" onClick={() => onThemeChange('bootstrap4-light-blue')} role="menuitem"><img src={`${contextPath}/images/themes/bootstrap4-light-blue.svg`} alt="Blue Light" /><span>Blue Light</span></button></li>
