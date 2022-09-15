f10f4f13e0d8a7263d03b920d3792758dcab2e99
Melloware <mellowaredev@gmail.com>
2022-09-15 13:18:16 -0400

    Fix #3327: SortOrder typecript constants (#3328)

--- components/lib/api/Api.js
+++ components/lib/api/Api.js
@@ -1,11 +1,12 @@
-import PrimeReact from './PrimeReact';
-import { locale, addLocale, localeOption, localeOptions, updateLocaleOption, updateLocaleOptions, ariaLabel } from './Locale';
-import { PrimeIcons } from './PrimeIcons';
-import { MessageSeverity } from './MessageSeverity';
 import { FilterMatchMode } from './FilterMatchMode';
 import { FilterOperator } from './FilterOperator';
 import { FilterService } from './FilterService';
+import { addLocale, ariaLabel, locale, localeOption, localeOptions, updateLocaleOption, updateLocaleOptions } from './Locale';
+import { MessageSeverity } from './MessageSeverity';
+import { PrimeIcons } from './PrimeIcons';
+import PrimeReact from './PrimeReact';
+import { SortOrder } from './SortOrder';
 
 export default PrimeReact;
 
-export { locale, addLocale, ariaLabel, localeOption, localeOptions, updateLocaleOption, updateLocaleOptions, PrimeIcons, MessageSeverity, FilterMatchMode, FilterOperator, FilterService };
+export { locale, addLocale, ariaLabel, localeOption, localeOptions, updateLocaleOption, updateLocaleOptions, PrimeIcons, MessageSeverity, FilterMatchMode, FilterOperator, FilterService, SortOrder };
