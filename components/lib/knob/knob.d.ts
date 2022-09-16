917e97be930b67a92bcf83c8c810833bd48bbf34
Emir Boyacı <emir.boyaci@pribas.com>
2022-09-16 14:16:23 +0300

    fix: knob, rating, tristatecheckbox component ref type incompatibility
    by omitting ref from props will solve this
    
    fixes #3334

--- components/lib/knob/knob.d.ts
+++ components/lib/knob/knob.d.ts
@@ -4,7 +4,7 @@ interface KnobChangeParams {
     value: number;
 }
 
-export interface KnobProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'onChange'> {
+export interface KnobProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'onChange' | 'ref'> {
     id?: string;
     style?: object;
     className?: string;
