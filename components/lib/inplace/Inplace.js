009c0fba03d332503436c1bd74b417dcf56cd6e2
Melloware <mellowaredev@gmail.com>
2022-09-21 09:46:30 -0400

    Fix #839: ARIA fixes (#3349)

--- components/lib/inplace/Inplace.js
+++ components/lib/inplace/Inplace.js
@@ -66,7 +66,7 @@ export const Inplace = React.forwardRef((props, ref) => {
 
     const createCloseButton = () => {
         if (props.closable) {
-            return <Button type="button" className="p-inplace-content-close" icon="pi pi-times" onClick={close} ariaLabel={localeOption('close')} />;
+            return <Button type="button" className="p-inplace-content-close" icon="pi pi-times" onClick={close} aria-label={localeOption('close')} />;
         }
 
         return null;
