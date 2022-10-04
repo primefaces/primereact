4c24ac06ac89b54d93200e7e38af6abdb2a3998e
melloware <mellowaredev@gmail.com>
2022-10-04 16:56:05 -0400

    Fix #3419: Mention update TS def for InputTextarea pops

--- components/lib/mention/mention.d.ts
+++ components/lib/mention/mention.d.ts
@@ -28,32 +28,33 @@ interface MentionSelectParams {
     suggestion: any;
 }
 
-export interface MentionProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'onSelect' | 'onChange' | 'onInput' | 'onFocus' | 'onBlur' | 'ref'> {
+export interface MentionProps extends Omit<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'onSelect' | 'onChange' | 'onInput' | 'onFocus' | 'onBlur' | 'ref'> {
+    autoHighlight?: boolean;
+    autoResize?: boolean;
+    children?: React.ReactNode;
+    delay?: number;
+    field?: MentionFieldType;
+    footerTemplate?: MentionFooterTemplateType;
+    headerTemplate?: MentionHeaderTemplateType;
+    inputClassName?: string;
     inputId?: string;
     inputRef?: React.Ref<HTMLInputElement>;
-    trigger?: MentionTriggerType;
-    suggestions?: any[];
-    field?: MentionFieldType;
     inputStyle?: object;
-    inputClassName?: string;
+    itemTemplate?: MentionItemTemplateType;
     panelClassName?: string;
     panelStyle?: object;
     scrollHeight?: string;
-    autoHighlight?: boolean;
-    delay?: number;
-    headerTemplate?: MentionHeaderTemplateType;
-    footerTemplate?: MentionFooterTemplateType;
-    itemTemplate?: MentionItemTemplateType;
+    suggestions?: any[];
     transitionOptions?: CSSTransitionProps;
+    trigger?: MentionTriggerType;
+    onBlur?(event: React.FocusEvent<HTMLInputElement>): void;
     onChange?(event: React.FormEvent<HTMLInputElement>): void;
+    onFocus?(event: React.FocusEvent<HTMLInputElement>): void;
+    onHide?(): void;
     onInput?(event: React.FormEvent<HTMLInputElement>): void;
     onSearch?(e: MentionSearchParams): void;
     onSelect?(e: MentionSelectParams): void;
-    onFocus?(event: React.FocusEvent<HTMLInputElement>): void;
-    onBlur?(event: React.FocusEvent<HTMLInputElement>): void;
     onShow?(): void;
-    onHide?(): void;
-    children?: React.ReactNode;
 }
 
 export declare class Mention extends React.Component<MentionProps, any> {
