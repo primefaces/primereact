544d1f2f3969722cb3de005cb4203960f937f2e2
mertsincan <sincan.mert@gmail.com>
2022-10-31 14:35:29 +0000

    Update Button.spec.js

--- components/lib/button/Button.spec.js
+++ components/lib/button/Button.spec.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { Button } from './Button';  
+import { Button } from './Button';
 import '@testing-library/jest-dom';
 
 describe('Button', () => {
