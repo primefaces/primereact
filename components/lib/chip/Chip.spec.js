cfd87397c9011c00348fa131376ec79bdaf8059a
Bugra <71140097+habubey@users.noreply.github.com>
2022-11-02 20:38:01 +0300

    Add jest unit testing to Chip component (#3558)
    * Add jest unit testing to Chip component
    
    * Fix request changes below;
    user-event library should move to devDependencies
    check if label text is test
    check if the icon value is correct

---
+++ components/lib/chip/Chip.spec.js
@@ -0,0 +1,44 @@
+import { fireEvent, render, within } from '@testing-library/react';
+import { Chip } from './Chip';
+
+import '@testing-library/jest-dom';
+
+describe('Chip', () => {
+    test('when removable is true it returns with remove icon', () => {
+        const removeOn = jest.fn();
+        const { container } = render(<Chip removable onRemove={removeOn} />);
+        const chipRemoveIcon = container.getElementsByClassName('p-chip-remove-icon')[0];
+
+        fireEvent.click(chipRemoveIcon);
+
+        expect(container.getElementsByClassName('p-chip-remove-icon pi pi-times-circle').length).toBe(0);
+        expect(removeOn).toHaveBeenCalledTimes(1);
+    });
+
+    test('when image is property it returns with image class', () => {
+        const { container } = render(<Chip image={'test'} />);
+
+        const wrapper = container.getElementsByClassName('p-chip p-component p-chip-image');
+        const chipImage = within(wrapper[0]).getByRole('img');
+
+        expect(chipImage.getAttribute('alt')).toBe('chip');
+    });
+
+    test('when icon is property it returns with icon class', () => {
+        const { container } = render(<Chip icon={'pi pi-check'} />);
+
+        const icon = container.getElementsByClassName('p-chip-icon');
+        const iconClass = icon[0].getAttribute('class');
+        
+        expect(iconClass).toBe('p-chip-icon pi pi-check');
+    });
+
+    test('when label is property it returns with label class', () => {
+        const { container } = render(<Chip label={'test'} />);
+
+        const label = container.getElementsByClassName('p-chip-text');
+        const spanTextContent = label[0].textContent;
+
+        expect(spanTextContent).toBe('test');
+    });
+});
