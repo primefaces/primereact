65619dc9958b4cd9a6374ec52ad755ac1d4a0ed0
habubey <habubey@outlook.com>
2022-11-03 17:54:34 +0300

    Improve Badge component unit tests

--- components/lib/badge/Badge.spec.js
+++ components/lib/badge/Badge.spec.js
@@ -27,6 +27,22 @@ describe('Badge', () => {
         // Act + Assert
         expect(container.getElementsByClassName('p-badge p-component p-badge-lg').length).toBe(1);
     });
+    test('when size as property it returns with class', () => {
+        // Arrange
+        const { container } = render(<Badge size={'xlarge'} />);
+
+        // Act + Assert
+        expect(container.getElementsByClassName('p-badge p-component p-badge-xl').length).toBe(1);
+    });
+
+    test('when value of size property is invalid it returns with default class', () => {
+        // Arrange
+        const { container } = render(<Badge size={'invalid'} />);
+
+        // Act + Assert
+
+        expect(container.getElementsByClassName('p-badge p-component').length).toBe(1);
+    });
 
     test('when severity as property it returns with class', () => {
         // Arrange
@@ -34,5 +50,34 @@ describe('Badge', () => {
 
         // Act + Assert
         expect(container.getElementsByClassName('p-badge p-component p-badge-success').length).toBe(1);
+    });
+    test('when severity as property it returns with class', () => {
+        // Arrange
+        const { container } = render(<Badge severity={'info'} />);
+
+        // Act + Assert
+        expect(container.getElementsByClassName('p-badge p-component p-badge-info').length).toBe(1);
+    });
+    test('when severity as property it returns with class', () => {
+        // Arrange
+        const { container } = render(<Badge severity={'warning'} />);
+
+        // Act + Assert
+        expect(container.getElementsByClassName('p-badge p-component p-badge-warning').length).toBe(1);
+    });
+    test('when severity as property it returns with class', () => {
+        // Arrange
+        const { container } = render(<Badge severity={'danger'} />);
+
+        // Act + Assert
+        expect(container.getElementsByClassName('p-badge p-component p-badge-danger').length).toBe(1);
+    });
+
+    test('when value of severity property is invalid it returns with class that has property additional class', () => {
+        // Arrange
+        const { container } = render(<Badge severity={'invalid'} />);
+
+        // Act + Assert
+        expect(container.getElementsByClassName('p-badge p-component p-badge-invalid').length).toBe(1);
     });
 });
