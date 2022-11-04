1432fcdcd5ef045644eb1c9ee374d7b5966a0126
Cagatay Civici <cagatay.civici@gmail.com>
2022-11-04 14:54:46 +0300

    New Slider docs

---
+++ components/doc/slider/stepdoc.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Slider } from "../../lib/slider/Slider";
+import { DocSectionText } from "../common/docsectiontext";
+import { DocSectionCode } from "../common/docsectioncode";
+
+export function StepDoc(props) {
+    const [value, setValue] = useState(20);
+
+    const code = {
+        basic: `
+<Slider value={value} onChange={(e) => setValue(e.value)} className="w-14rem" step={20} />
+        `,
+        javascript: `
+import { useState } from "react";
+import { Slider } from "primereact/slider";
+
+export default function StepDemo() {
+    const [value, setValue] = useState(20);
+
+    return (
+        <Slider value={value} onChange={(e) => setValue(e.value)} className="w-14rem" step={20} />
+    )
+}
+        `,
+        typescript: `
+import { useState } from "react";
+import { Slider } from "primereact/slider";
+
+export default function StepDemo() {
+    const [value, setValue] = useState<number>(20);
+
+    return (
+        <Slider value={value} onChange={(e: SliderChangeParams) => setValue(e.value)} className="w-14rem" step={20} />
+    )
+}
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}>
+                Size of each movement is defined with the <i>step</i> property.
+            </DocSectionText>
+            <div className="card flex justify-content-center">
+                <Slider value={value} onChange={(e) => setValue(e.value)} className="w-14rem" step={20} />
+            </div>
+            <DocSectionCode code={code} />
+        </>
+    )
+}
