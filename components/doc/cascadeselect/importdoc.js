ceb688b26dce7e5c52023c5d5c2c9948665986d3
habubey <habubey@outlook.com>
2022-11-08 20:57:41 +0300

    Add new Docs: CascadeSelect

---
+++ components/doc/cascadeselect/importdoc.js
@@ -0,0 +1,17 @@
+import { DocSectionText } from '../common/docsectiontext';
+import { DocSectionCode } from '../common/docsectioncode';
+
+export function ImportDoc(props) {
+    const code = {
+        basic: `
+import { CascadeSelect } from 'primereact/cascadeselect';
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}></DocSectionText>
+            <DocSectionCode code={code} hideToggleCode hideCodeSandbox />
+        </>
+    );
+}
