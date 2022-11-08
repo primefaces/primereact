20ebc31ac6de754e6cfc307a0a47479e07602fc0
Melloware <mellowaredev@gmail.com>
2022-11-08 08:24:44 -0500

    Badge and Chip snapshot tests (#3592)

--- components/lib/badge/Badge.spec.js
+++ components/lib/badge/Badge.spec.js
@@ -2,82 +2,19 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { Badge } from './Badge';
 
+const snapshot = (props, name) => expect(render(<Badge {...props} />).container).toMatchSnapshot(name);
+
 describe('Badge', () => {
-    test('when value is not property it returns with dot class', () => {
-        // Arrange
-        const { container } = render(<Badge />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge-dot').length).toBe(1);
-    });
-
-    test('when value is property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge value={22} />);
-        const badge = container.getElementsByClassName('p-badge p-component')[0].textContent;
-
-        // Act + Assert
-        expect(badge).toBe('22');
-    });
-
-    test('when size as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge size={'large'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-lg').length).toBe(1);
-    });
-    test('when size as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge size={'xlarge'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-xl').length).toBe(1);
-    });
-
-    test('when value of size property is invalid it returns with default class', () => {
-        // Arrange
-        const { container } = render(<Badge size={'invalid'} />);
-
-        // Act + Assert
-
-        expect(container.getElementsByClassName('p-badge p-component').length).toBe(1);
-    });
-
-    test('when severity as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge severity={'success'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-success').length).toBe(1);
-    });
-    test('when severity as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge severity={'info'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-info').length).toBe(1);
-    });
-    test('when severity as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge severity={'warning'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-warning').length).toBe(1);
-    });
-    test('when severity as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Badge severity={'danger'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-danger').length).toBe(1);
-    });
-
-    test('when value of severity property is invalid it returns with class that has property additional class', () => {
-        // Arrange
-        const { container } = render(<Badge severity={'invalid'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-badge p-component p-badge-invalid').length).toBe(1);
+    test('check snapshots', () => {
+        snapshot({}, 'default');
+        snapshot({ value: 22 }, 'value');
+        snapshot({ size: 'large' }, 'size large');
+        snapshot({ size: 'xlarge' }, 'size xlarge');
+        snapshot({ size: 'invalid' }, 'size invalid');
+        snapshot({ severity: 'success' }, 'severity success');
+        snapshot({ severity: 'info' }, 'severity info');
+        snapshot({ severity: 'warning' }, 'severity warning');
+        snapshot({ severity: 'danger' }, 'severity danger');
+        snapshot({ severity: 'invalid' }, 'severity invalid');
     });
 });
