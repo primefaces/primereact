889e37f3b9a93637ef5781aa0ee76e54e916a808
melloware <mellowaredev@gmail.com>
2022-11-08 08:22:13 -0500

    Badge and Chip snapshot tests

--- components/lib/chip/Chip.spec.js
+++ components/lib/chip/Chip.spec.js
@@ -1,8 +1,17 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, within } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { Chip } from './Chip';
 
+const snapshot = (props, name) => expect(render(<Chip {...props} />).container).toMatchSnapshot(name);
+
 describe('Chip', () => {
+    test('check snapshots', () => {
+        snapshot({}, 'default');
+        snapshot({ image: 'http://google.com/my.png' }, 'image');
+        snapshot({ image: 'http://google.com/my.png', imageAlt: 'jest' }, 'image and alt');
+        snapshot({ icon: 'pi pi-check' }, 'icon');
+        snapshot({ label: 'jest' }, 'label');
+    });
     test('when removable is true it returns with remove icon', () => {
         // Arrange
         const removeOn = jest.fn();
@@ -16,7 +25,6 @@ describe('Chip', () => {
         expect(container.getElementsByClassName('p-chip-remove-icon pi pi-times-circle').length).toBe(0);
         expect(removeOn).toHaveBeenCalledTimes(1);
     });
-
     test('when removable is true the chip is removed when ENTER is pressed', () => {
         // Arrange
         const removeOn = jest.fn();
@@ -29,49 +37,5 @@ describe('Chip', () => {
         // Assert
         expect(container.getElementsByClassName('p-chip-remove-icon pi pi-times-circle').length).toBe(0);
         expect(removeOn).toHaveBeenCalledTimes(1);
-    });
-
-    test('when image is property it returns with image class', () => {
-        // Arrange
-        const { container } = render(<Chip image={'test'} />);
-
-        // Act + Assert
-        const wrapper = container.getElementsByClassName('p-chip p-component p-chip-image');
-        const chipImage = within(wrapper[0]).getByRole('img');
-
-        expect(chipImage.getAttribute('alt')).toBe('chip');
-    });
-
-    test('when imageAlt property is set the alt is set', () => {
-        // Arrange
-        const { container } = render(<Chip image={'test'} imageAlt="jest" />);
-
-        // Act + Assert
-        const wrapper = container.getElementsByClassName('p-chip p-component p-chip-image');
-        const chipImage = within(wrapper[0]).getByRole('img');
-
-        expect(chipImage.getAttribute('alt')).toBe('jest');
-    });
-
-    test('when icon is property it returns with icon class', () => {
-        // Arrange
-        const { container } = render(<Chip icon={'pi pi-check'} />);
-
-        // Act + Assert
-        const icon = container.getElementsByClassName('p-chip-icon');
-        const iconClass = icon[0].getAttribute('class');
-
-        expect(iconClass).toBe('p-chip-icon pi pi-check');
-    });
-
-    test('when label is property it returns with label class', () => {
-        // Arrange
-        const { container } = render(<Chip label={'test'} />);
-
-        // Act + Assert
-        const label = container.getElementsByClassName('p-chip-text');
-        const spanTextContent = label[0].textContent;
-
-        expect(spanTextContent).toBe('test');
     });
 });
