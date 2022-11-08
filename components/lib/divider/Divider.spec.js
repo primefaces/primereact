0f20078a7d43be10a65d71b960270d4e10cd70d7
Kalin Krustev <kalin.krustev@gmail.com>
2022-11-08 15:11:15 +0200

    test: switch to snapshot tests (#3579)

--- components/lib/divider/Divider.spec.js
+++ components/lib/divider/Divider.spec.js
@@ -2,84 +2,18 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { Divider } from './Divider';
 
-describe('Divider', () => {
-    test('when component has no properties it returns with default class', () => {
-        // Arrange
-        const { container } = render(<Divider />);
+const snapshot = (props, name) => expect(render(<Divider {...props} />).container).toMatchSnapshot(name);
 
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-horizontal p-divider-solid p-divider-left').length).toBe(1);
-    });
-    test('when layout and align as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Divider layout={'horizontal'} align={'left'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-horizontal p-divider-left').length).toBe(1);
-    });
-    test('when layout and align as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Divider layout={'horizontal'} align={'right'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-horizontal p-divider-right').length).toBe(1);
-    });
-    test('when layout and align as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Divider layout={'horizontal'} align={'center'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-horizontal p-divider-center').length).toBe(1);
-    });
-
-    test('when tpe has not any property it returns with default class', () => {
-        // Arrange
-        const { container } = render(<Divider />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-horizontal p-divider-solid').length).toBe(1);
-    });
-    test('when layout and align as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Divider layout={'vertical'} align={'center'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-vertical p-divider-center').length).toBe(1);
-    });
-    test('when layout and align as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Divider layout={'vertical'} align={'top'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-vertical p-divider-top').length).toBe(1);
-    });
-    test('when layout and align as property it returns with class', () => {
-        // Arrange
-        const { container } = render(<Divider layout={'vertical'} align={'bottom'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-vertical p-divider-bottom').length).toBe(1);
-    });
-
-    test('when type has no property it returns with the default class', () => {
-        // Arrange
-        const { container } = render(<Divider />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-solid').length).toBe(1);
-    });
-    test('when type is dashed it is styled as a dashed line', () => {
-        // Arrange
-        const { container } = render(<Divider type={'dashed'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-dashed').length).toBe(1);
-    });
-    test('when type is dotted is is styled as a dotted line', () => {
-        // Arrange
-        const { container } = render(<Divider type={'dotted'} />);
-
-        // Act + Assert
-        expect(container.getElementsByClassName('p-divider p-component p-divider-dotted').length).toBe(1);
+describe('Divider Snapshot', () => {
+    test('check snapshots', () => {
+        snapshot({}, 'default');
+        snapshot({ layout: 'horizontal', align: 'left' }, 'horizontal left');
+        snapshot({ layout: 'horizontal', align: 'right' }, 'horizontal right');
+        snapshot({ layout: 'horizontal', align: 'center' }, 'horizontal center');
+        snapshot({ layout: 'vertical', align: 'center' }, 'vertical center');
+        snapshot({ layout: 'vertical', align: 'top' }, 'vertical top');
+        snapshot({ layout: 'vertical', align: 'bottom' }, 'vertical bottom');
+        snapshot({ type: 'dashed' }, 'dashed');
+        snapshot({ type: 'dotted' }, 'dotted');
     });
 });
