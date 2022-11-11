13e7a5f81b29efedcb3921a25e2746af87d866ac
habubey <habubey@outlook.com>
2022-11-11 17:20:01 +0300

    Add new Docs: ScrollPanel

--- pages/scrollpanel/index.js
+++ pages/scrollpanel/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { ScrollPanel } from '../../components/lib/scrollpanel/ScrollPanel';
-import ScrollPanelDoc from '../../components/doc/scrollpanel';
-import { DocActions } from '../../components/doc/common/docactions';
 import Head from 'next/head';
+import { DocSectionNav } from '../../components/doc/common/docsectionnav';
+import { DocSections } from '../../components/doc/common/docsections';
+import { DocActions } from '../../components/doc/common/docactions';
+import { ImportDoc } from '../../components/doc/scrollpanel/importdoc';
+import { ApiDoc } from '../../components/doc/scrollpanel/apidoc';
+import { ScrollDemo } from '../../components/doc/scrollpanel/scrolldemo';
 
 const ScrollPanelDemo = () => {
+    const docs = [
+        {
+            id: 'import',
+            label: 'Import',
+            component: ImportDoc
+        },
+        {
+            id: 'scrollpanel',
+            label: 'ScrollPanel',
+            component: ScrollDemo
+        },
+        {
+            id: 'api',
+            label: 'API',
+            component: ApiDoc
+        }
+    ];
+
     return (
         <div>
             <Head>
@@ -19,47 +39,10 @@ const ScrollPanelDemo = () => {
                 <DocActions github="scrollpanel/index.js" />
             </div>
 
-            <div className="content-section implementation scrollpanel-demo">
-                <div className="card">
-                    <div className="grid">
-                        <div className="col-12 md:col-4">
-                            <ScrollPanel style={{ width: '100%', height: '200px' }}>
-                                <div style={{ padding: '1em', lineHeight: '1.5' }}>
-                                    The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. His beloved son Michael has just come home from the war, but does not intend to become part of his
-                                    father's business. Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, kind and benevolent to those who give respect, but given to
-                                    ruthless violence whenever anything stands against the good of the family. The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. His beloved son Michael has
-                                    just come home from the war, but does not intend to become part of his father's business. Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of
-                                    the family, kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
-                                </div>
-                            </ScrollPanel>
-                        </div>
-                        <div className="col-12 md:col-4">
-                            <ScrollPanel style={{ width: '100%', height: '200px' }} className="custombar1">
-                                <div style={{ padding: '1em', lineHeight: '1.5' }}>
-                                    The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. His beloved son Michael has just come home from the war, but does not intend to become part of his
-                                    father's business. Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, kind and benevolent to those who give respect, but given to
-                                    ruthless violence whenever anything stands against the good of the family. The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. His beloved son Michael has
-                                    just come home from the war, but does not intend to become part of his father's business. Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of
-                                    the family, kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
-                                </div>
-                            </ScrollPanel>
-                        </div>
-                        <div className="col-12 md:col-4">
-                            <ScrollPanel style={{ width: '100%', height: '200px' }} className="custombar2">
-                                <div style={{ padding: '1em', lineHeight: '1.5', width: '600px' }}>
-                                    The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. His beloved son Michael has just come home from the war, but does not intend to become part of his
-                                    father's business. Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, kind and benevolent to those who give respect, but given to
-                                    ruthless violence whenever anything stands against the good of the family. The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. His beloved son Michael has
-                                    just come home from the war, but does not intend to become part of his father's business. Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of
-                                    the family, kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
-                                </div>
-                            </ScrollPanel>
-                        </div>
-                    </div>
-                </div>
+            <div className="content-section doc">
+                <DocSections docs={docs} />
+                <DocSectionNav docs={docs} />
             </div>
-
-            <ScrollPanelDoc></ScrollPanelDoc>
         </div>
     );
 };
