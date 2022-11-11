d00fca7f2593dd43d50c124012ad038fc34184a8
habubey <habubey@outlook.com>
2022-11-11 15:42:17 +0300

    Add new Docs: Panel

---
+++ components/doc/panel/templatedoc.js
@@ -0,0 +1,112 @@
+import { Panel } from '../../lib/panel/Panel';
+import { Ripple } from '../../lib/ripple/Ripple';
+import { DocSectionText } from '../common/docsectiontext';
+import { DocSectionCode } from '../common/docsectioncode';
+
+export function TemplateDoc(props) {
+    const template = (options) => {
+        const toggleIcon = options.collapsed ? 'pi pi-chevron-down' : 'pi pi-chevron-up';
+        const className = `${options.className} justify-content-start`;
+        const titleClassName = `${options.titleClassName} pl-1`;
+
+        return (
+            <div className={className}>
+                <button className={options.togglerClassName} onClick={options.onTogglerClick}>
+                    <span className={toggleIcon}></span>
+                    <Ripple />
+                </button>
+                <span className={titleClassName}>Header</span>
+            </div>
+        );
+    };
+
+    const code = {
+        basic: `
+<Panel headerTemplate={template} toggleable>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</Panel>
+        `,
+        javascript: `
+import { Panel } from 'primereact/panel';
+import { Ripple } from 'primereact/ripple';
+
+export default function TemplateDoc() {
+    const template = (options) => {
+        const toggleIcon = options.collapsed ? 'pi pi-chevron-down' : 'pi pi-chevron-up';
+        const className = \`\${options.className} justify-content-start\`;
+        const titleClassName = \`\${options.titleClassName} pl-1\`;
+
+        return (
+            <div className={className}>
+                <button className={options.togglerClassName} onClick={options.onTogglerClick}>
+                    <span className={toggleIcon}></span>
+                    <Ripple />
+                </button>
+                <span className={titleClassName}>Header</span>
+            </div>
+        );
+    };
+
+    return (
+        <Panel headerTemplate={template} toggleable>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </Panel>
+    )
+}
+        `,
+        typescript: `
+import { Panel } from 'primereact/panel';
+import { Ripple } from 'primereact/ripple';
+
+export default function TemplateDoc() {
+    const template = (options) => {
+        const toggleIcon = options.collapsed ? 'pi pi-chevron-down' : 'pi pi-chevron-up';
+        const className = \`\${options.className} justify-content-start\`;
+        const titleClassName = \`\${options.titleClassName} pl-1\`;
+
+        return (
+            <div className={className}>
+                <button className={options.togglerClassName} onClick={options.onTogglerClick}>
+                    <span className={toggleIcon}></span>
+                    <Ripple />
+                </button>
+                <span className={titleClassName}>Header</span>
+            </div>
+        );
+    };
+
+    return (
+        <Panel headerTemplate={template} toggleable>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </Panel>
+    )
+}
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}>
+                The header element is fully customizable on Panel. To make special header, an object can be given to the <i>headerTemplate</i> property as below.
+            </DocSectionText>
+            <div className="card">
+                <Panel headerTemplate={template} toggleable>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </Panel>
+            </div>
+            <DocSectionCode code={code} />
+        </>
+    );
+}
