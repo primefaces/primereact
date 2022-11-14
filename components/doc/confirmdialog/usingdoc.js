ddcf21431a45285d716ae05328bcc45532a8089b
habubey <habubey@outlook.com>
2022-11-14 16:26:16 +0300

    Add new Docs: ConfirmDialog Fix: Toolbar description

---
+++ components/doc/confirmdialog/usingdoc.js
@@ -0,0 +1,99 @@
+import { useState, useRef } from 'react';
+import { ConfirmDialog, confirmDialog } from '../../lib/confirmdialog/ConfirmDialog';
+import { Toast } from '../../lib/toast/Toast';
+import { Button } from '../../lib/button/Button';
+import { DocSectionText } from '../common/docsectiontext';
+import { DocSectionCode } from '../common/docsectioncode';
+
+export function UsingConfirmDialogDoc(props) {
+    const [visible, setVisible] = useState(false);
+    const toast = useRef(null);
+
+    const accept = () => {
+        toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+    };
+
+    const reject = () => {
+        toast.current.show({ severity: 'warn', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+    };
+
+    const code = {
+        basic: `
+<Toast ref={toast} />
+<ConfirmDialog />
+<ConfirmDialog visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?"
+    header="Confirmation" icon="pi pi-exclamation-triangle" accept={accept} reject={reject} />
+<Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
+        `,
+        javascript: `
+import { useState, useRef } from 'react';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { Toast } from 'primereact/toast';
+import { Button } from '../../lib/button/Button';
+
+export default function UsingConfirmDialogDoc() {
+    const [visible, setVisible] = useState(false);
+    const toast = useRef(null);
+
+    const accept = () => {
+        toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+    }
+
+    const reject = () => {
+        toast.current.show({ severity: 'warn', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+    }
+
+    return (
+        <div>
+            <Toast ref={toast} />
+            <ConfirmDialog />
+            <ConfirmDialog visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?"
+                header="Confirmation" icon="pi pi-exclamation-triangle" accept={accept} reject={reject} />
+            <Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
+        </div>
+    )
+}
+        `,
+        typescript: `
+import { useState, useRef } from 'react';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { Toast } from 'primereact/toast';
+import { Button } from '../../lib/button/Button';
+
+export default function UsingConfirmDialogDoc() {
+    const [visible, setVisible] = useState(false);
+    const toast = useRef(null);
+
+    const accept = () => {
+        toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+    }
+
+    const reject = () => {
+        toast.current.show({ severity: 'warn', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+    }
+
+    return (
+        <div>
+            <Toast ref={toast} />
+            <ConfirmDialog />
+            <ConfirmDialog visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?"
+                header="Confirmation" icon="pi pi-exclamation-triangle" accept={accept} reject={reject} />
+            <Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
+        </div>
+    )
+}
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}>ConfirmDialog is used as a container and visibility is managed with visible property where onHide event is required to update the visibility state.</DocSectionText>
+            <div className="card flex justify-content-center">
+                <Toast ref={toast} />
+                <ConfirmDialog visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?" header="Confirmation" icon="pi pi-exclamation-triangle" accept={accept} reject={reject} />
+                <Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
+            </div>
+            <DocSectionCode code={code} />
+        </>
+    );
+}
--- pages/confirmdialog/index.js
+++ pages/confirmdialog/index.js
@@ -1,54 +1,41 @@
-import React, { useState, useRef } from 'react';
-import { ConfirmDialog, confirmDialog } from '../../components/lib/confirmdialog/ConfirmDialog';
-import { Button } from '../../components/lib/button/Button';
-import { Toast } from '../../components/lib/toast/Toast';
-import ConfirmDialogDoc from '../../components/doc/confirmdialog';
-import { DocActions } from '../../components/doc/common/docactions';
 import Head from 'next/head';
+import { DocSectionNav } from '../../components/doc/common/docsectionnav';
+import { DocSections } from '../../components/doc/common/docsections';
+import { DocActions } from '../../components/doc/common/docactions';
+import { BasicDoc } from '../../components/doc/confirmdialog/basicdoc';
+import { PositionDoc } from '../../components/doc/confirmdialog/positiondoc';
+import { UsingConfirmDialogDoc } from '../../components/doc/confirmdialog/usingdoc';
+import { ApiDoc } from '../../components/doc/confirmdialog/apidoc';
+import { ImportDoc } from '../../components/doc/confirmdialog/importdoc';
 
 const ConfirmDialogDemo = () => {
-    const [visible, setVisible] = useState(false);
-    const toast = useRef(null);
-
-    const accept = () => {
-        toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
-    };
-
-    const reject = () => {
-        toast.current.show({ severity: 'warn', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
-    };
-
-    const confirm1 = () => {
-        confirmDialog({
-            message: 'Are you sure you want to proceed?',
-            header: 'Confirmation',
-            icon: 'pi pi-exclamation-triangle',
-            accept,
-            reject
-        });
-    };
-
-    const confirm2 = () => {
-        confirmDialog({
-            message: 'Do you want to delete this record?',
-            header: 'Delete Confirmation',
-            icon: 'pi pi-info-circle',
-            acceptClassName: 'p-button-danger',
-            accept,
-            reject
-        });
-    };
-
-    const confirmPosition = (position) => {
-        confirmDialog({
-            message: 'Do you want to delete this record?',
-            header: 'Delete Confirmation',
-            icon: 'pi pi-info-circle',
-            position,
-            accept,
-            reject
-        });
-    };
+    const docs = [
+        {
+            id: 'import',
+            label: 'Import',
+            component: ImportDoc
+        },
+        {
+            id: 'basic',
+            label: 'Basic',
+            component: BasicDoc
+        },
+        {
+            id: 'position',
+            label: 'Position',
+            component: PositionDoc
+        },
+        {
+            id: 'using',
+            label: 'Using ConfirmDialog tag',
+            component: UsingConfirmDialogDoc
+        },
+        {
+            id: 'api',
+            label: 'API',
+            component: ApiDoc
+        }
+    ];
 
     return (
         <div>
@@ -66,41 +53,10 @@ const ConfirmDialogDemo = () => {
                 <DocActions github="confirmdialog/index.js" />
             </div>
 
-            <div className="content-section implementation">
-                <Toast ref={toast} />
-
-                <div className="card">
-                    <ConfirmDialog />
-
-                    <h5>Basic</h5>
-                    <Button onClick={confirm1} icon="pi pi-check" label="Confirm" className="mr-2"></Button>
-                    <Button onClick={confirm2} icon="pi pi-times" label="Delete"></Button>
-
-                    <h5>Position</h5>
-                    <div className="grid">
-                        <div className="col-12">
-                            <Button onClick={() => confirmPosition('left')} icon="pi pi-arrow-right" label="Left" className="p-button-help mr-2"></Button>
-                            <Button onClick={() => confirmPosition('right')} icon="pi pi-arrow-left" label="Right" className="p-button-help"></Button>
-                        </div>
-                        <div className="col-12">
-                            <Button onClick={() => confirmPosition('top-left')} icon="pi pi-arrow-down-right" label="TopLeft" className="p-button-warning mr-2"></Button>
-                            <Button onClick={() => confirmPosition('top')} icon="pi pi-arrow-down" label="Top" className="p-button-warning mr-2"></Button>
-                            <Button onClick={() => confirmPosition('top-right')} icon="pi pi-arrow-down-left" label="TopRight" className="p-button-warning"></Button>
-                        </div>
-                        <div className="col-12">
-                            <Button onClick={() => confirmPosition('bottom-left')} icon="pi pi-arrow-up-right" label="BottomLeft" className="p-button-success mr-2"></Button>
-                            <Button onClick={() => confirmPosition('bottom')} icon="pi pi-arrow-up" label="Bottom" className="p-button-success mr-2"></Button>
-                            <Button onClick={() => confirmPosition('bottom-right')} icon="pi pi-arrow-up-left" label="BottomRight" className="p-button-success"></Button>
-                        </div>
-                    </div>
-
-                    <h5>Using ConfirmDialog tag</h5>
-                    <ConfirmDialog visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?" header="Confirmation" icon="pi pi-exclamation-triangle" accept={accept} reject={reject} />
-                    <Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
-                </div>
+            <div className="content-section doc">
+                <DocSections docs={docs} />
+                <DocSectionNav docs={docs} />
             </div>
-
-            <ConfirmDialogDoc />
         </div>
     );
 };
