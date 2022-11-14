edd041a7eec7c078b170fa7983abb6ecd29fa213
habubey <habubey@outlook.com>
2022-11-14 17:43:22 +0300

    Add new Docs: ConfirmPopup

---
+++ components/doc/confirmpopup/importdoc.js
@@ -0,0 +1,18 @@
+import { DocSectionText } from '../common/docsectiontext';
+import { DocSectionCode } from '../common/docsectioncode';
+
+export function ImportDoc(props) {
+    const code = {
+        basic: `
+import { ConfirmPopup } from 'primereact/confirmpopup'; // To use <ConfirmPopup> tag
+import { confirmPopup } from 'primereact/confirmpopup'; // To use confirmPopup method
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}></DocSectionText>
+            <DocSectionCode code={code} hideToggleCode hideCodeSandbox />
+        </>
+    );
+}
