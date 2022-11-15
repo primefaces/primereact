468dc63885029af8eff5466dfb10fdb55e4f6992
habubey <habubey@outlook.com>
2022-11-15 21:26:10 +0300

    Add new Docs: Sidebar

---
+++ components/doc/sidebar/basicdoc.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { Sidebar } from '../../lib/sidebar/Sidebar';
+import { Button } from '../../lib/button/Button';
+import { DocSectionCode } from '../common/docsectioncode';
+import { DocSectionText } from '../common/docsectiontext';
+
+export function BasicDoc(props) {
+    const [visibleLeft, setVisibleLeft] = useState(false);
+
+    const code = {
+        basic: `
+<Sidebar visible={visibleLeft} onHide={() => setVisibleLeft(false)}>
+    <h3>Left Sidebar</h3>
+</Sidebar>
+<Button icon="pi pi-arrow-right" onClick={() => setVisibleLeft(true)} className="mr-2" />
+        `,
+        javascript: `
+import { useState } from 'react';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+
+export default function BasicDoc() {
+    const [visibleLeft, setVisibleLeft] = useState(false);
+
+    return (
+        <div>
+            <Sidebar visible={visibleLeft} onHide={() => setVisibleLeft(false)}>
+                <h3>Left Sidebar</h3>
+            </Sidebar>
+            <Button icon="pi pi-arrow-right" onClick={() => setVisibleLeft(true)} className="mr-2" />
+        </div>
+    )
+}
+        `,
+        typescript: `
+import { useState } from 'react';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+
+export default function BasicDoc() {
+    const [visibleLeft, setVisibleLeft] = useState(false);
+
+    return (
+        <div>
+            <Sidebar visible={visibleLeft} onHide={() => setVisibleLeft(false)}>
+                <h3>Left Sidebar</h3>
+            </Sidebar>
+            <Button icon="pi pi-arrow-right" onClick={() => setVisibleLeft(true)} className="mr-2" />
+        </div>
+    )
+}
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}>
+                Sidebar is used as a container and visibility is controlled with <i>visible</i> property.
+            </DocSectionText>
+            <div className="card flex justify-content-center">
+                <Sidebar visible={visibleLeft} onHide={() => setVisibleLeft(false)}>
+                    <h3>Left Sidebar</h3>
+                </Sidebar>
+                <Button icon="pi pi-arrow-right" onClick={() => setVisibleLeft(true)} className="mr-2" />
+            </div>
+            <DocSectionCode code={code} />
+        </>
+    );
+}
