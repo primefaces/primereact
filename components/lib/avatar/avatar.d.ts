fda641b3faffc04adb7db748c1bec1df243644ae
Melloware <mellowaredev@gmail.com>
2022-11-15 16:57:03 -0500

    Fix #3647: Image fallback if there is an error (#3648)

--- components/lib/avatar/avatar.d.ts
+++ components/lib/avatar/avatar.d.ts
@@ -8,16 +8,17 @@ type AvatarShapeType = 'square' | 'circle';
 type AvatarTemplateType = React.ReactNode | ((props: AvatarProps) => React.ReactNode);
 
 export interface AvatarProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'ref'> {
-    label?: string;
+    children?: React.ReactNode;
     icon?: IconType<AvatarProps>;
     image?: string;
-    size?: AvatarSizeType;
-    shape?: AvatarShapeType;
-    template?: AvatarTemplateType;
     imageAlt?: string;
+    imageFallback?: string;
+    label?: string;
+    shape?: AvatarShapeType;
+    size?: AvatarSizeType;
+    template?: AvatarTemplateType;
     onImageError?(event: React.SyntheticEvent): void;
     onClick?(event: React.MouseEvent<HTMLElement>): void;
-    children?: React.ReactNode;
 }
 
 export declare class Avatar extends React.Component<AvatarProps, any> {
