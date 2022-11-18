068409e378fe44774a53726ace9402e8d00ce711
habubey <habubey@outlook.com>
2022-11-18 16:25:51 +0300

    Add new Docs: ContextMenu

--- pages/contextmenu/index.js
+++ pages/contextmenu/index.js
@@ -1,138 +1,55 @@
-import React, { useRef } from 'react';
-import { ContextMenu } from '../../components/lib/contextmenu/ContextMenu';
-import ContextMenuDoc from '../../components/doc/contextmenu';
-import { DocActions } from '../../components/doc/common/docactions';
 import Head from 'next/head';
-import getConfig from 'next/config';
+import { DocSectionNav } from '../../components/doc/common/docsectionnav';
+import { DocSections } from '../../components/doc/common/docsections';
+import { DocActions } from '../../components/doc/common/docactions';
+import { ImportDoc } from '../../components/doc/contextmenu/importdoc';
+import { BasicDoc } from '../../components/doc/contextmenu/BasicDoc';
+import { ApiDoc } from '../../components/doc/contextmenu/apidoc';
+import { DocumentDoc } from '../../components/doc/contextmenu/documentdoc';
 
 const ContextMenuDemo = () => {
-    const cm = useRef(null);
-    const contextPath = getConfig().publicRuntimeConfig.contextPath;
-
-    const items = [
+    const docs = [
         {
-            label: 'File',
-            icon: 'pi pi-fw pi-file',
-            items: [
+            id: 'import',
+            label: 'Import',
+            component: ImportDoc
+        },
+        {
+            id: 'basic',
+            label: 'Basic',
+            component: BasicDoc
+        },
+        {
+            id: 'document',
+            label: 'Document Menu',
+            component: DocumentDoc
+        },
+        {
+            id: 'api',
+            label: 'API',
+            component: ApiDoc,
+            children: [
                 {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {
-                            label: 'Bookmark',
-                            icon: 'pi pi-fw pi-bookmark'
-                        },
-                        {
-                            label: 'Video',
-                            icon: 'pi pi-fw pi-video'
-                        }
-                    ]
+                    id: 'properties',
+                    label: 'Properties'
                 },
                 {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-trash'
+                    id: 'methods',
+                    label: 'Methods'
                 },
                 {
-                    separator: true
+                    id: 'events',
+                    label: 'Events'
                 },
                 {
-                    label: 'Export',
-                    icon: 'pi pi-fw pi-external-link'
+                    id: 'styling',
+                    label: 'Styling'
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility'
                 }
             ]
-        },
-        {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {
-                    label: 'Left',
-                    icon: 'pi pi-fw pi-align-left'
-                },
-                {
-                    label: 'Right',
-                    icon: 'pi pi-fw pi-align-right'
-                },
-                {
-                    label: 'Center',
-                    icon: 'pi pi-fw pi-align-center'
-                },
-                {
-                    label: 'Justify',
-                    icon: 'pi pi-fw pi-align-justify'
-                }
-            ]
-        },
-        {
-            label: 'Users',
-            icon: 'pi pi-fw pi-user',
-            items: [
-                {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-user-plus'
-                },
-                {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-user-minus'
-                },
-                {
-                    label: 'Search',
-                    icon: 'pi pi-fw pi-users',
-                    items: [
-                        {
-                            label: 'Filter',
-                            icon: 'pi pi-fw pi-filter',
-                            items: [
-                                {
-                                    label: 'Print',
-                                    icon: 'pi pi-fw pi-print'
-                                }
-                            ]
-                        },
-                        {
-                            icon: 'pi pi-fw pi-bars',
-                            label: 'List'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Events',
-            icon: 'pi pi-fw pi-calendar',
-            items: [
-                {
-                    label: 'Edit',
-                    icon: 'pi pi-fw pi-pencil',
-                    items: [
-                        {
-                            label: 'Save',
-                            icon: 'pi pi-fw pi-calendar-plus'
-                        },
-                        {
-                            label: 'Delete',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                },
-                {
-                    label: 'Archieve',
-                    icon: 'pi pi-fw pi-calendar-times',
-                    items: [
-                        {
-                            label: 'Remove',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            separator: true
-        },
-        {
-            label: 'Quit',
-            icon: 'pi pi-fw pi-power-off'
         }
     ];
 
@@ -153,15 +70,10 @@ const ContextMenuDemo = () => {
                 <DocActions github="contextmenu/index.js" />
             </div>
 
-            <div className="content-section implementation">
-                <div className="card">
-                    <ContextMenu model={items} ref={cm}></ContextMenu>
-
-                    <img src={`${contextPath}/images/nature/nature3.jpg`} onError={(e) => (e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')} alt="Logo" onContextMenu={(e) => cm.current.show(e)} />
-                </div>
+            <div className="content-section doc">
+                <DocSections docs={docs} />
+                <DocSectionNav docs={docs} />
             </div>
-
-            <ContextMenuDoc />
         </div>
     );
 };
