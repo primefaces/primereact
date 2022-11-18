7a12c8b6a40815f19a4f89e8269a42d37916859f
habubey <habubey@outlook.com>
2022-11-18 18:07:05 +0300

    Add new Docs: MegaMenu

--- pages/megamenu/index.js
+++ pages/megamenu/index.js
@@ -1,132 +1,55 @@
-import React from 'react';
-import { MegaMenu } from '../../components/lib/megamenu/MegaMenu';
-import { InputText } from '../../components/lib/inputtext/InputText';
-import MegaMenuDoc from '../../components/doc/megamenu';
-import { DocActions } from '../../components/doc/common/docactions';
 import Head from 'next/head';
-import getConfig from 'next/config';
+import { DocSectionNav } from '../../components/doc/common/docsectionnav';
+import { DocSections } from '../../components/doc/common/docsections';
+import { DocActions } from '../../components/doc/common/docactions';
+import { ImportDoc } from '../../components/doc/megamenu/importdoc';
+import { HorizontalDoc } from '../../components/doc/megamenu/horizontaldoc';
+import { VerticalDoc } from '../../components/doc/megamenu/verticaldoc';
+import { TemplatingDoc } from '../../components/doc/megamenu/templatingdoc';
+import { ApiDoc } from '../../components/doc/megamenu/apidoc';
 
 const MegaMenuDemo = () => {
-    const items = [
+    const docs = [
         {
-            label: 'Videos',
-            icon: 'pi pi-fw pi-video',
-            items: [
-                [
-                    {
-                        label: 'Video 1',
-                        items: [{ label: 'Video 1.1' }, { label: 'Video 1.2' }]
-                    },
-                    {
-                        label: 'Video 2',
-                        items: [{ label: 'Video 2.1' }, { label: 'Video 2.2' }]
-                    }
-                ],
-                [
-                    {
-                        label: 'Video 3',
-                        items: [{ label: 'Video 3.1' }, { label: 'Video 3.2' }]
-                    },
-                    {
-                        label: 'Video 4',
-                        items: [{ label: 'Video 4.1' }, { label: 'Video 4.2' }]
-                    }
-                ]
-            ]
+            id: 'import',
+            label: 'Import',
+            component: ImportDoc
         },
         {
-            label: 'Users',
-            icon: 'pi pi-fw pi-users',
-            items: [
-                [
-                    {
-                        label: 'User 1',
-                        items: [{ label: 'User 1.1' }, { label: 'User 1.2' }]
-                    },
-                    {
-                        label: 'User 2',
-                        items: [{ label: 'User 2.1' }, { label: 'User 2.2' }]
-                    }
-                ],
-                [
-                    {
-                        label: 'User 3',
-                        items: [{ label: 'User 3.1' }, { label: 'User 3.2' }]
-                    },
-                    {
-                        label: 'User 4',
-                        items: [{ label: 'User 4.1' }, { label: 'User 4.2' }]
-                    }
-                ],
-                [
-                    {
-                        label: 'User 5',
-                        items: [{ label: 'User 5.1' }, { label: 'User 5.2' }]
-                    },
-                    {
-                        label: 'User 6',
-                        items: [{ label: 'User 6.1' }, { label: 'User 6.2' }]
-                    }
-                ]
-            ]
+            id: 'horizontal',
+            label: 'Horizontal',
+            component: HorizontalDoc
         },
         {
-            label: 'Events',
-            icon: 'pi pi-fw pi-calendar',
-            items: [
-                [
-                    {
-                        label: 'Event 1',
-                        items: [{ label: 'Event 1.1' }, { label: 'Event 1.2' }]
-                    },
-                    {
-                        label: 'Event 2',
-                        items: [{ label: 'Event 2.1' }, { label: 'Event 2.2' }]
-                    }
-                ],
-                [
-                    {
-                        label: 'Event 3',
-                        items: [{ label: 'Event 3.1' }, { label: 'Event 3.2' }]
-                    },
-                    {
-                        label: 'Event 4',
-                        items: [{ label: 'Event 4.1' }, { label: 'Event 4.2' }]
-                    }
-                ]
-            ]
+            id: 'Vertical',
+            label: 'Vertical',
+            component: VerticalDoc
         },
         {
-            label: 'Settings',
-            icon: 'pi pi-fw pi-cog',
-            items: [
-                [
-                    {
-                        label: 'Setting 1',
-                        items: [{ label: 'Setting 1.1' }, { label: 'Setting 1.2' }]
-                    },
-                    {
-                        label: 'Setting 2',
-                        items: [{ label: 'Setting 2.1' }, { label: 'Setting 2.2' }]
-                    },
-                    {
-                        label: 'Setting 3',
-                        items: [{ label: 'Setting 3.1' }, { label: 'Setting 3.2' }]
-                    }
-                ],
-                [
-                    {
-                        label: 'Technology 4',
-                        items: [{ label: 'Setting 4.1' }, { label: 'Setting 4.2' }]
-                    }
-                ]
+            id: 'templating',
+            label: 'Templating',
+            component: TemplatingDoc
+        },
+        {
+            id: 'api',
+            label: 'API',
+            component: ApiDoc,
+            children: [
+                {
+                    id: 'properties',
+                    label: 'Properties'
+                },
+                {
+                    id: 'styling',
+                    label: 'Styling'
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility'
+                }
             ]
         }
     ];
-
-    const contextPath = getConfig().publicRuntimeConfig.contextPath;
-    const start = <img alt="logo" src={`${contextPath}/images/logo.png`} onError={(e) => (e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')} height="40"></img>;
-    const end = <InputText placeholder="Search" type="text" style={{ width: '12rem' }} />;
 
     return (
         <div>
@@ -142,20 +65,10 @@ const MegaMenuDemo = () => {
                 <DocActions github="megamenu/index.js" />
             </div>
 
-            <div className="content-section implementation">
-                <div className="card">
-                    <h5>Horizontal</h5>
-                    <MegaMenu model={items} orientation="horizontal" />
-
-                    <h5>Vertical</h5>
-                    <MegaMenu model={items} orientation="vertical" />
-
-                    <h5>Templating</h5>
-                    <MegaMenu model={items} orientation="horizontal" start={start} end={end} />
-                </div>
+            <div className="content-section doc">
+                <DocSections docs={docs} />
+                <DocSectionNav docs={docs} />
             </div>
-
-            <MegaMenuDoc />
         </div>
     );
 };
