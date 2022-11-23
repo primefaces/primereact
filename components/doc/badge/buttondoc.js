5d1cca51b9de5731a398395861bd3f4f319872d3
Ulaş Turan <ulasturan@Ulas-MBP.home>
2022-11-23 23:55:35 +0300

    Add a new doc to Badge Component.

---
+++ components/doc/badge/buttondoc.js
@@ -0,0 +1,60 @@
+import { Badge } from '../../lib/badge/Badge';
+import { Button } from '../../lib/button/Button';
+import { DocSectionCode } from '../common/docsectioncode';
+import { DocSectionText } from '../common/docsectiontext';
+
+export function ButtonDemo(props) {
+    const code = {
+        basic: `
+<Button type="button" label="Emails" className="mr-2"><Badge value="8" ></Badge></Button>
+<Button type="button" label="Messages" icon="pi pi-users" className="p-button-warning"><Badge value="8" severity="danger" ></Badge></Button>
+            `,
+        javascript: `
+import React from 'react';
+import { Button } from 'primereact/button';
+import { Badge } from 'primereact/badge';
+
+export const ButtonDemo = () => {
+
+    return (
+        <div className="card flex justify-content-center">
+            <Button type="button" label="Emails" className="mr-2"><Badge value="8" ></Badge></Button>
+            <Button type="button" label="Messages" icon="pi pi-users" className="p-button-warning"><Badge value="8" severity="danger" ></Badge></Button>
+        </div>
+    );
+}
+        `,
+        typescript: `
+import React from 'react';
+import { Button } from 'primereact/button';
+import { Badge } from 'primereact/badge';
+
+export const ButtonDemo = () => {
+
+    return (
+        <div className="card flex justify-content-center">
+            <Button type="button" label="Emails" className="mr-2"><Badge value="8" ></Badge></Button>
+            <Button type="button" label="Messages" icon="pi pi-users" className="p-button-warning"><Badge value="8" severity="danger" ></Badge></Button>
+        </div>
+    );
+}
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}>
+                <p>Badge - Button Demo Content.</p>
+            </DocSectionText>
+            <div className="card flex justify-content-center">
+                <Button type="button" label="Emails" className="mr-2">
+                    <Badge value="8"></Badge>
+                </Button>
+                <Button type="button" label="Messages" icon="pi pi-users" className="p-button-warning">
+                    <Badge value="8" severity="danger"></Badge>
+                </Button>
+            </div>
+            <DocSectionCode code={code} />
+        </>
+    );
+}
--- pages/badge/index.js
+++ pages/badge/index.js
@@ -1,61 +1,84 @@
-import React from 'react';
-import { Button } from '../../components/lib/button/Button';
-import { Badge } from '../../components/lib/badge/Badge';
-import BadgeDoc from '../../components/doc/badge';
-import { DocActions } from '../../components/doc/common/docactions';
 import Head from 'next/head';
+import React from 'react';
+import { DocSectionNav } from '../../components/doc/common/docsectionnav';
+import { DocSections } from '../../components/doc/common/docsections';
+import { ApiDoc } from '../../components/doc/badge/apidoc';
+import { ImportDoc } from '../../components/doc/badge/importdoc';
+import { NumbersDemo } from '../../components/doc/badge/numbersdoc';
+import { ButtonDemo } from '../../components/doc/badge/buttondoc';
+import { PositionedDemo } from '../../components/doc/badge/positioneddoc';
+import { SizesDemo } from '../../components/doc/badge/sizesdoc';
 
 const BadgeDemo = () => {
+    const docs = [
+        {
+            id: 'import',
+            label: 'Import',
+            component: ImportDoc
+        },
+        {
+            id: 'numbers',
+            label: 'Numbers',
+            component: NumbersDemo
+        },
+        {
+            id: 'sizes',
+            label: 'Sizes',
+            component: SizesDemo
+        },
+        {
+            id: 'button',
+            label: 'Button',
+            component: ButtonDemo
+        },
+        {
+            id: 'positioned',
+            label: 'Positioned',
+            component: PositionedDemo
+        },
+        {
+            id: 'api',
+            label: 'API',
+            component: ApiDoc,
+            children: [
+                {
+                    id: 'severities',
+                    label: 'Severities'
+                },
+                {
+                    id: 'properties',
+                    label: 'Properties'
+                },
+                {
+                    id: 'styling',
+                    label: 'Styling'
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility'
+                }
+            ]
+        }
+    ];
+
     return (
         <div>
             <Head>
                 <title>React Badge Component</title>
                 <meta name="description" content="Badge is a small status indicator for another element." />
             </Head>
+
             <div className="content-section introduction">
                 <div className="feature-intro">
                     <h1>Badge</h1>
                     <p>Badge is a small status indicator for another element.</p>
                 </div>
-                <DocActions github="badge/index.js" />
             </div>
 
-            <div className="content-section implementation">
-                <div className="card">
-                    <h5>Numbers</h5>
-                    <Badge value="2" className="mr-2"></Badge>
-                    <Badge value="8" severity="success" className="mr-2"></Badge>
-                    <Badge value="4" severity="info" className="mr-2"></Badge>
-                    <Badge value="12" severity="warning" className="mr-2"></Badge>
-                    <Badge value="3" severity="danger"></Badge>
-
-                    <h5 className="mb-4">Positioned Badge</h5>
-                    <i className="pi pi-bell mr-4 p-text-secondary p-overlay-badge" style={{ fontSize: '2rem' }}>
-                        <Badge value="2"></Badge>
-                    </i>
-                    <i className="pi pi-calendar mr-4 p-text-secondary p-overlay-badge" style={{ fontSize: '2rem' }}>
-                        <Badge value="10+" severity="danger"></Badge>
-                    </i>
-                    <i className="pi pi-envelope p-text-secondary p-overlay-badge" style={{ fontSize: '2rem' }}>
-                        <Badge severity="danger"></Badge>
-                    </i>
-
-                    <h5>Button Badge</h5>
-                    <Button type="button" label="Emails" className="mr-2">
-                        <Badge value="8"></Badge>
-                    </Button>
-                    <Button type="button" label="Messages" icon="pi pi-users" className="p-button-warning">
-                        <Badge value="8" severity="danger"></Badge>
-                    </Button>
-
-                    <h5>Sizes</h5>
-                    <Badge value="2" className="mr-2"></Badge>
-                    <Badge value="4" className="mr-2" size="large" severity="warning"></Badge>
-                    <Badge value="6" size="xlarge" severity="success"></Badge>
-                </div>
+            <div className="content-section doc">
+                <DocSections docs={docs} />
+                <DocSectionNav docs={docs} />
             </div>
-
-            <BadgeDoc />
         </div>
     );
 };
