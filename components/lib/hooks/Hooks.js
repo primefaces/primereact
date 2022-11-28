80db5601af63b5f847e7c427691362b54f129fab
Melloware <mellowaredev@gmail.com>
2022-11-28 17:21:35 -0500

    Revert "Fix #2042: Dialog better handling of draggable (#3709)" (#3714)
    This reverts commit 6bac6b397e7eca41bc9cad2e6cfdfcd175c055f4.

--- components/lib/hooks/Hooks.js
+++ components/lib/hooks/Hooks.js
@@ -1,14 +1,13 @@
-import { useDraggable } from './useDraggable';
-import { useEventListener } from './useEventListener';
-import { useInterval } from './useInterval';
+import { usePrevious } from './usePrevious';
 import { useMountEffect } from './useMountEffect';
+import { useUpdateEffect } from './useUpdateEffect';
+import { useUnmountEffect } from './useUnmountEffect';
+import { useEventListener } from './useEventListener';
 import { useOverlayListener } from './useOverlayListener';
 import { useOverlayScrollListener } from './useOverlayScrollListener';
-import { usePrevious } from './usePrevious';
 import { useResizeListener } from './useResizeListener';
-import { useLocalStorage, useSessionStorage, useStorage } from './useStorage';
+import { useInterval } from './useInterval';
+import { useStorage, useLocalStorage, useSessionStorage } from './useStorage';
 import { useTimeout } from './useTimeout';
-import { useUnmountEffect } from './useUnmountEffect';
-import { useUpdateEffect } from './useUpdateEffect';
 
-export { usePrevious, useMountEffect, useUpdateEffect, useUnmountEffect, useEventListener, useOverlayListener, useOverlayScrollListener, useResizeListener, useInterval, useStorage, useLocalStorage, useSessionStorage, useTimeout, useDraggable };
+export { usePrevious, useMountEffect, useUpdateEffect, useUnmountEffect, useEventListener, useOverlayListener, useOverlayScrollListener, useResizeListener, useInterval, useStorage, useLocalStorage, useSessionStorage, useTimeout };
