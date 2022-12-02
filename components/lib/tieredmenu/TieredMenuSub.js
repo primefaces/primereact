d5331d174d39f182f2b269257c132f5f633a82ba
Melloware <mellowaredev@gmail.com>
2022-12-02 11:28:45 -0500

    Fix #2837: TieredMenu/ContextMenu hieght handling (#3737)

--- components/lib/tieredmenu/TieredMenuSub.js
+++ components/lib/tieredmenu/TieredMenuSub.js
@@ -23,6 +23,13 @@ export const TieredMenuSub = React.memo((props) => {
             const viewport = DomHandler.getViewport();
             const sublistWidth = elementRef.current.offsetParent ? elementRef.current.offsetWidth : DomHandler.getHiddenElementOuterWidth(elementRef.current);
             const itemOuterWidth = DomHandler.getOuterWidth(parentItem.children[0]);
+            const top = parseInt(containerOffset.top, 10) + elementRef.current.offsetHeight - DomHandler.getWindowScrollTop();
+
+            if (top > viewport.height) {
+                elementRef.current.style.top = viewport.height - top + 'px';
+            } else {
+                elementRef.current.style.top = '0px';
+            }
 
             if (parseInt(containerOffset.left, 10) + itemOuterWidth + sublistWidth > viewport.width - DomHandler.calculateScrollbarWidth()) {
                 DomHandler.addClass(elementRef.current, 'p-submenu-list-flipped');
