74d29e04f11e47913d30fb7904d9b647784ab76b
melloware <mellowaredev@gmail.com>
2022-12-13 12:29:12 -0500

    Fix #3781: InputText typescript def value force string

--- components/lib/inputtext/inputtext.d.ts
+++ components/lib/inputtext/inputtext.d.ts
@@ -2,12 +2,13 @@ import * as React from 'react';
 import { KeyFilterType } from '../keyfilter';
 import TooltipOptions from '../tooltip/tooltipoptions';
 
-export interface InputTextProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'onInput' | 'ref'> {
+export interface InputTextProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'onInput' | 'ref' | 'value'> {
     children?: React.ReactNode;
     keyfilter?: KeyFilterType;
     tooltip?: string;
     tooltipOptions?: TooltipOptions;
     validateOnly?: boolean;
+    value?: string;
     onInput?(event: React.FormEvent<HTMLInputElement>, validatePattern: boolean): void;
 }
 
