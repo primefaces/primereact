702375fcd175bd42beefd875e5ae7fb988aea335
mertsincan <sincan.mert@gmail.com>
2022-12-26 16:49:40 +0300

    Update templates.js

--- components/doc/common/codeeditor/templates.js
+++ components/doc/common/codeeditor/templates.js
@@ -46,6 +46,10 @@ const getCRA = (props = {}, template = 'javascript') => {
                 dependencies,
                 eslintConfig: {
                     extends: ['react-app']
+                },
+                browserslist: {
+                    production: ['>0.2%', 'not dead', 'not op_mini all'],
+                    development: ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version']
                 }
             }
         },
