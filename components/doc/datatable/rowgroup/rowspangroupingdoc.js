bb926b87c963ce5cde717227937276c012164202
habubey <habubey@outlook.com>
2022-12-27 11:28:08 +0300

    Add customerservice json to demos

--- components/doc/datatable/rowgroup/rowspangroupingdoc.js
+++ components/doc/datatable/rowgroup/rowspangroupingdoc.js
@@ -182,7 +182,28 @@ const RowSpanGroupingDoc = () => {
     vertical-align: middle;
     margin-right: .25rem;
 }
-        `
+        `,
+        data: `
+{
+    id: 1000,
+    name: 'James Butt',
+    country: {
+        name: 'Algeria',
+        code: 'dz'
+    },
+    company: 'Benton, John B Jr',
+    date: '2015-09-13',
+    status: 'unqualified',
+    verified: true,
+    activity: 17,
+    representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png'
+    },
+    balance: 70663
+},
+...
+       `
     };
 
     return (
--- components/doc/mention/autodoc.js
+++ components/doc/mention/autodoc.js
@@ -161,7 +161,28 @@ export default function AutoDoc() {
         <Mention suggestions={suggestions} onSearch={onSearch} field="nickname" placeholder="Please enter @ to mention people"  rows={5} cols={40} autoResize itemTemplate={itemTemplate} />
     )
 }
-        `
+        `,
+        data: `
+{
+    id: 1000,
+    name: 'James Butt',
+    country: {
+        name: 'Algeria',
+        code: 'dz'
+    },
+    company: 'Benton, John B Jr',
+    date: '2015-09-13',
+    status: 'unqualified',
+    verified: true,
+    activity: 17,
+    representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png'
+    },
+    balance: 70663
+},
+...
+       `
     };
 
     return (
