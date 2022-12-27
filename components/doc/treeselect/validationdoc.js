c3e531ba1b3566eaf25c832bd8c9e99c7ff380e5
habubey <habubey@outlook.com>
2022-12-27 13:33:07 +0300

    Add nodeservice to componets db section

--- components/doc/treeselect/validationdoc.js
+++ components/doc/treeselect/validationdoc.js
@@ -44,6 +44,7 @@ import { Controller, useForm } from 'react-hook-form';
 import { Button } from 'primereact/button';
 import { classNames } from 'primereact/utils';
 import { TreeSelect } from "primereact/treeselect";
+import { NodeService } from '../../../service/NodeService';
 
 export default function ValidationDemo() {
     const [formData, setFormData] = useState({});
@@ -98,6 +99,7 @@ import { Controller, useForm } from 'react-hook-form';
 import { Button } from 'primereact/button';
 import { TreeSelect } from "primereact/treeselect";
 import { classNames } from 'primereact/utils';
+import { NodeService } from '../../../service/NodeService';
 
 export default function InvalidDemo() {
     const [formData, setFormData] = useState<any>({});
@@ -145,7 +147,35 @@ export default function InvalidDemo() {
         </div>
     )
 }
-        `
+        `,
+        data: `
+{
+    key: '0',
+    label: 'Documents',
+    data: 'Documents Folder',
+    icon: 'pi pi-fw pi-inbox',
+    children: [
+        {
+            key: '0-0',
+            label: 'Work',
+            data: 'Work Folder',
+            icon: 'pi pi-fw pi-cog',
+            children: [
+                { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+                { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+            ]
+        },
+        {
+            key: '0-1',
+            label: 'Home',
+            data: 'Home Folder',
+            icon: 'pi pi-fw pi-home',
+            children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
+        }
+    ]
+},
+...
+`
     };
 
     return (
