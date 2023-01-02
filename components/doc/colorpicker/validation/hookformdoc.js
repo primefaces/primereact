2892dd9f6f76696c22b9800e445f646884d2b795
habubey <habubey@outlook.com>
2023-01-02 17:50:34 +0300

    Fix #3833: ColorPicker Doc Enhancements

---
+++ components/doc/colorpicker/validation/hookformdoc.js
@@ -0,0 +1,164 @@
+import { useRef } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { DocSectionText } from '../../common/docsectiontext';
+import { DocSectionCode } from '../../common/docsectioncode';
+import { Button } from '../../../lib/button/Button';
+import { Toast } from '../../../lib/toast/Toast';
+import { ColorPicker } from '../../../lib/colorpicker/ColorPicker';
+
+export function HookFormDoc(props) {
+    const toast = useRef(null);
+
+    const show = (data) => {
+        // TO DO: Add detail content to the toast.
+        toast.current.show({ severity: 'success', summary: 'Submitted', detail: `#${data.color.toUpperCase() || 'No data found.'}` });
+    };
+
+    const defaultValues = {
+        color: ''
+    };
+
+    const {
+        control,
+        formState: { errors },
+        handleSubmit,
+        reset
+    } = useForm({ defaultValues });
+
+    const onSubmit = (data) => {
+        data.color && show(data);
+
+        reset();
+    };
+
+    const getFormErrorMessage = (name) => {
+        return errors[name] && <small className="p-error">{errors[name].message}</small>;
+    };
+
+    const code = {
+        basic: `
+<form onSubmit={handleSubmit(onSubmit)} className="flex flex-column align-items-center justify-content-center">
+    <Toast ref={toast} />
+    <Controller name="color" control={control} rules={{ required: 'Color is required.' }} render={({ field }) => <ColorPicker name="color" control={control} value={field.color} onChange={(e) => field.onChange(e.value)} />} />
+    {getFormErrorMessage('color')}
+    <Button type="submit" label="Submit" className="mt-2" />
+</form>
+        `,
+        javascript: `
+import React, { useRef } from "react";
+import { useForm, Controller } from 'react-hook-form';
+import { ColorPicker } from 'primereact/colorpicker';
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+
+export default function HookFormDoc() {
+    
+    const toast = useRef(null);
+
+    const show = (data) => {
+        // TO DO: Add detail content to the toast.
+        toast.current.show({ severity: 'success', summary: 'Submitted', detail:\`#\${data.color.toUpperCase()}\` || 'No data found.'}\` });
+    };
+
+    const defaultValues = {
+        color: '22c55e'
+    };
+
+    const {
+        control,
+        formState: { errors },
+        handleSubmit,
+        reset
+    } = useForm({ defaultValues });
+
+    const onSubmit = (data) => {
+        data.color && show(data);
+
+        reset();
+    };
+
+    const getFormErrorMessage = (name) => {
+        return errors[name] && <small className="p-error">{errors[name].message}</small>;
+    };
+
+    return (
+        <div className="card flex justify-content-center">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column align-items-center justify-content-center">
+                <Toast ref={toast} />
+                <Controller name="color" control={control} rules={{ required: 'Color is required.' }} render={({ field }) => <ColorPicker name="color" control={control} value={field.color} onChange={(e) => field.onChange(e.value)} />} />
+                {getFormErrorMessage('color')}
+                <Button type="submit" label="Submit" className="mt-2" />
+            </form>
+        </div>
+    )
+}
+        `,
+        typescript: `
+import React, { useRef } from "react";
+import { useForm, Controller } from 'react-hook-form';
+import { ColorPicker } from 'primereact/colorpicker';
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+
+export default function HookFormDoc() {
+    
+    const toast = useRef(null);
+
+    const show = (data) => {
+        // TO DO: Add detail content to the toast.
+        toast.current.show({ severity: 'success', summary: 'Submitted', detail:\`#\${data.color.toUpperCase()}\` || 'No data found.'}\` });
+    };
+
+    const defaultValues = {
+        color: '22c55e'
+    };
+
+    const {
+        control,
+        formState: { errors },
+        handleSubmit,
+        reset
+    } = useForm({ defaultValues });
+
+    const onSubmit = (data) => {
+        data.color && show(data);
+
+        reset();
+    };
+
+    const getFormErrorMessage = (name) => {
+        return errors[name] && <small className="p-error">{errors[name].message}</small>;
+    };
+
+    return (
+        <div className="card flex justify-content-center">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column align-items-center justify-content-center">
+                <Toast ref={toast} />
+                <Controller name="color" control={control} rules={{ required: 'Color is required.' }} render={({ field }) => <ColorPicker name="color" control={control} value={field.color} onChange={(e) => field.onChange(e.value)} />} />
+                {getFormErrorMessage('color')}
+                <Button type="submit" label="Submit" className="mt-2" />
+            </form>
+        </div>
+    )
+}
+        `
+    };
+
+    return (
+        <>
+            <DocSectionText {...props}>
+                {/* TO DO: Add demo content. */}
+                <p></p>
+            </DocSectionText>
+            <div className="card flex justify-content-center">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column align-items-center justify-content-center">
+                    <Toast ref={toast} />
+                    <Controller name="color" control={control} rules={{ required: 'Color is required.' }} render={({ field }) => <ColorPicker name="color" control={control} value={field.color} onChange={(e) => field.onChange(e.value)} />} />
+                    {getFormErrorMessage('color')}
+                    <Button type="submit" label="Submit" className="mt-2" />
+                </form>
+            </div>
+            <DocSectionCode code={code} dependencies={{ 'react-hook-form': '^7.39.4' }} />
+        </>
+    );
+}
--- pages/colorpicker/index.js
+++ pages/colorpicker/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import { ApiDoc } from '../../components/doc/colorpicker/apidoc';
+import { DisabledDoc } from '../../components/doc/colorpicker/disableddoc';
 import { FormatDoc } from '../../components/doc/colorpicker/formatdoc';
 import { ImportDoc } from '../../components/doc/colorpicker/importdoc';
 import { InlineDoc } from '../../components/doc/colorpicker/inlinedoc';
 import { OverlayDoc } from '../../components/doc/colorpicker/overlaydoc';
-import { ValidationDoc } from '../../components/doc/colorpicker/validationdoc';
+import { FormikDoc } from '../../components/doc/colorpicker/validation/formikdoc';
+import { HookFormDoc } from '../../components/doc/colorpicker/validation/hookformdoc';
 import { DocActions } from '../../components/doc/common/docactions';
 import { DocSectionNav } from '../../components/doc/common/docsectionnav';
 import { DocSections } from '../../components/doc/common/docsections';
@@ -32,9 +34,26 @@ const ColorPickerDemo = () => {
             component: FormatDoc
         },
         {
+            id: 'disabled',
+            label: 'Disabled',
+            component: DisabledDoc
+        },
+        {
             id: 'validation',
             label: 'Validation',
-            component: ValidationDoc
+            description: 'Validate using popular React validation libraries.',
+            children: [
+                {
+                    id: 'formik',
+                    label: 'Formik',
+                    component: FormikDoc
+                },
+                {
+                    id: 'hookform',
+                    label: 'Hook Form',
+                    component: HookFormDoc
+                }
+            ]
         },
         {
             id: 'api',
