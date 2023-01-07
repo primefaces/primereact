993ae78c9647d2a5b672abcd97b6c8887693942d
Cagatay Civici <cagatay.civici@gmail.com>
2023-01-07 11:39:25 +0300

    Demo refactor for Progress Spinner

--- components/doc/progressspinner/customdoc.js
+++ components/doc/progressspinner/customdoc.js
@@ -6,17 +6,16 @@ import { ProgressSpinner } from '../../lib/progressspinner/ProgressSpinner';
 export function CustomDoc(props) {
     const code = {
         basic: `
-<ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s"/>
+<ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
         `,
         javascript: `
 import React from 'react';
 import { ProgressSpinner } from 'primereact/progressspinner';
 
-export default function CustomDoc() {
-
+export default function CustomDemo() {
     return (
         <div className="card">
-            <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s"/>
+            <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
         </div>
     );
 }
@@ -25,11 +24,10 @@ export default function CustomDoc() {
 import React from 'react';
 import { ProgressSpinner } from 'primereact/progressspinner';
 
-export default function CustomDoc() {
-
+export default function CustomDemo() {
     return (
         <div className="card">
-            <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s"/>
+            <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
         </div>
     );
 }
@@ -39,7 +37,7 @@ export default function CustomDoc() {
     return (
         <>
             <DocSectionText {...props}>
-                <p>Custom Demo Content.</p>
+                <p>ProgressSpinner can be customized with styling property like <i>style</i>, <i>strokeWidth</i> and <i>fill</i>.</p>
             </DocSectionText>
             <div className="card">
                 <ProgressSpinner style={{ width: '50px', height: '50px' }} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
