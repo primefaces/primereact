9a0e5fe512b8eb89c65c1a2b4c0d3e610afcbc21
Cagatay Civici <cagatay.civici@gmail.com>
2023-01-11 11:38:52 +0300

    Corrected class name

--- components/doc/avatar/labeldoc.js
+++ components/doc/avatar/labeldoc.js
@@ -25,21 +25,21 @@ import { Badge } from 'primereact/badge';
 export default function LabelDemo() {
     return (
         <div className="flex flex-wrap gap-5">
-            <div class="flex-auto">
+            <div className="flex-auto">
                 <h5>Label</h5>
                 <Avatar label="P" className="mr-2" size="xlarge" />
                 <Avatar label="V" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} />
                 <Avatar label="U" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} />
             </div>
 
-            <div class="flex-auto">
+            <div className="flex-auto">
                 <h5>Circle</h5>
                 <Avatar label="P" className="mr-2" size="xlarge" shape="circle" />
                 <Avatar label="V" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} shape="circle" />
                 <Avatar label="U" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} shape="circle" />
             </div>
 
-            <div class="flex-auto">
+            <div className="flex-auto">
                 <h5>Badge</h5>
                 <Avatar label="U" size="xlarge" className="p-overlay-badge" style={{ backgroundColor: '#4caf4f', color: '#ffffff' }}>
                     <Badge value="4" />
@@ -57,21 +57,21 @@ import { Badge } from 'primereact/badge';
 export default function LabelDemo() {
     return (
         <div className="flex flex-wrap gap-5">
-            <div class="flex-auto">
+            <div className="flex-auto">
                 <h5>Label</h5>
                 <Avatar label="P" className="mr-2" size="xlarge" />
                 <Avatar label="V" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} />
                 <Avatar label="U" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} />
             </div>
 
-            <div class="flex-auto">
+            <div className="flex-auto">
                 <h5>Circle</h5>
                 <Avatar label="P" className="mr-2" size="xlarge" shape="circle" />
                 <Avatar label="V" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} shape="circle" />
                 <Avatar label="U" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} shape="circle" />
             </div>
 
-            <div class="flex-auto">
+            <div className="flex-auto">
                 <h5>Badge</h5>
                 <Avatar label="U" size="xlarge" className="p-overlay-badge" style={{ backgroundColor: '#4caf4f', color: '#ffffff' }}>
                     <Badge value="4" />
@@ -92,21 +92,21 @@ export default function LabelDemo() {
             </DocSectionText>
             <div className="card">
                 <div className="flex flex-wrap gap-5">
-                    <div class="flex-auto">
+                    <div className="flex-auto">
                         <h5>Label</h5>
                         <Avatar label="P" className="mr-2" size="xlarge" />
                         <Avatar label="V" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} />
                         <Avatar label="U" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} />
                     </div>
 
-                    <div class="flex-auto">
+                    <div className="flex-auto">
                         <h5>Circle</h5>
                         <Avatar label="P" className="mr-2" size="xlarge" shape="circle" />
                         <Avatar label="V" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} shape="circle" />
                         <Avatar label="U" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} shape="circle" />
                     </div>
 
-                    <div class="flex-auto">
+                    <div className="flex-auto">
                         <h5>Badge</h5>
                         <Avatar label="U" size="xlarge" className="p-overlay-badge" style={{ backgroundColor: '#4caf4f', color: '#ffffff' }}>
                             <Badge value="4" />
