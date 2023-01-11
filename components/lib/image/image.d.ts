dff47c8f18c996ed38fbb4e6cf91aae7ac672fc0
habubey <habubey@outlook.com>
2023-01-11 18:14:54 +0300

    Refactor on image.d.ts

--- components/lib/image/image.d.ts
+++ components/lib/image/image.d.ts
@@ -1,24 +1,95 @@
+/**
+ *
+ * Displays a single image with preview and tranformation options.
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/image/)
+ *
+ * @module image
+ *
+ */
 import * as React from 'react';
 
+/**
+ * Defines valid properties in Image component. In addition to these, all properties of HTMLSpanElement can be used in this component.
+ * @group Properties
+ */
 export interface ImageProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>, 'ref'> {
-    alt?: string;
-    children?: React.ReactNode;
-    downloadable?: boolean;
-    height?: string;
-    imageClassName?: string;
-    imageStyle?: React.CSSProperties;
-    preview?: boolean;
-    src?: string;
-    template?: any;
-    width?: string;
-    zoomSrc?: string;
+    /**
+     * @todo Write the documentation.
+     */
+    alt?: string | undefined;
+    /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
+    /**
+     * Adds a download button to the preview control menu.
+     * @default false
+     */
+    downloadable?: boolean | undefined;
+    /**
+     * @todo Write the documentation.
+     */
+    height?: string | undefined;
+    /**
+     * Style class of the image element.
+     */
+    imageClassName?: string | undefined;
+    /**
+     * Inline style of the image element.
+     */
+    imageStyle?: React.CSSProperties | undefined;
+    /**
+     * Controls the preview functionality.
+     * @default false
+     */
+    preview?: boolean | undefined;
+    /**
+     * Specifies the path to the image.
+     */
+    src?: string | undefined;
+    /**
+     * Changing the default icon when the image is hovered in preview mode.
+     */
+    template?: any | undefined;
+    /**
+     * @todo Write the documentation.
+     */
+    width?: string | undefined;
+    /**
+     * Zoomed image that may be different than "src" image.
+     */
+    zoomSrc?: string | undefined;
+    /**
+     * Triggered when the preview overlay is hidden.
+     */
     onHide?(): void;
+    /**
+     * Triggered when the preview overlay is shown.
+     */
     onShow?(): void;
 }
 
+/**
+ * @group Component
+ */
 export declare class Image extends React.Component<ImageProps, any> {
+    /**
+     * Used to show the overlay.
+     */
     public show(): void;
+    /**
+     * Used to hide the overlay.
+     */
     public hide(): void;
+    /**
+     * Used to get container element.
+     * @return {HTMLSpanElement} Container element
+     */
     public getElement(): HTMLSpanElement;
+    /**
+     * @todo Write the documentation.
+     */
     public getImage(): HTMLImageElement;
 }
