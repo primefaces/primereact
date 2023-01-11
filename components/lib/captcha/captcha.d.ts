91bc899f3c605f321eff29f5a207e6f264a34d49
habubey <habubey@outlook.com>
2023-01-11 13:25:03 +0300

    Refactor on captcha.d.ts

--- components/lib/captcha/captcha.d.ts
+++ components/lib/captcha/captcha.d.ts
@@ -1,19 +1,74 @@
+/**
+ *
+ * Captcha is a form validation component based on Recaptcha.
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/captcha)
+ *
+ * @module captcha
+ *
+ */
 import * as React from 'react';
 
+/**
+ * Defines valid properties in Captcha component. In addition to these, all properties of HTMLDivElement can be used in this component.
+ * @group Properties
+ */
 export interface CaptchaProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'ref'> {
-    children?: React.ReactNode;
-    language?: string;
+    /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
+    /**
+     * Language of the widget.
+     * @defaultValue en
+     */
+    language?: string | undefined;
+    /**
+     * The callback function to be executed when the recaptcha response expires and the user needs to solve a new CAPTCHA.
+     */
     onExpire?(): void;
+    /**
+     * The callback function to be executed when the user submits a successful CAPTCHA response.
+     * @param {any} response
+     */
     onResponse?(response: any): void;
-    siteKey?: string;
-    size?: string;
-    sourceUrl?: string;
-    theme?: string;
-    type?: string;
+    /**
+     * Public sitekey.
+     */
+    siteKey?: string | undefined;
+    /**
+     * The size of the widget.
+     * @defaultValue normal
+     */
+    size?: string | undefined;
+    /**
+     * Source URL of the Captcha as some countries do not allow Google access.
+     * @defaultValue https://www.google.com/recaptcha/api.js?render=explicit
+     *
+     */
+    sourceUrl?: string | undefined;
+    /**
+     * The color scheme of the widget.
+     * @defaultValue light
+     */
+    theme?: string | undefined;
+    /**
+     * The type of CAPTCHA to serve.
+     * @defaultValue image
+     */
+    type?: string | undefined;
 }
 
+/**
+ * @group Component
+ */
 export declare class Captcha extends React.Component<CaptchaProps, any> {
     public reset(): void;
     public getResponse(): any;
+    /**
+     * Used to get container element.
+     * @return {HTMLDivElement} Container element
+     */
     public getElement(): HTMLDivElement;
 }
