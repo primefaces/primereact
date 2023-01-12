60e45c50b9fbf47eaccadf3eccae56f9045ffba9
Ulaş Turan <ulasturan06@hotmail.com>
2023-01-12 20:21:05 +0300

    Sidebar: Add typedoc to .d.ts

--- components/lib/sidebar/sidebar.d.ts
+++ components/lib/sidebar/sidebar.d.ts
@@ -1,35 +1,124 @@
+/**
+ *
+ * Sidebar is an input component that provides real-time suggestions when being typed.
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/sidebar/)
+ *
+ * @module sidebar
+ *
+ */
 import * as React from 'react';
 import { CSSTransitionProps } from '../csstransition';
 
-type SidebarPositionType = 'top' | 'bottom' | 'left' | 'right';
-
-type SidebarTemplateType = React.ReactNode | ((props: SidebarProps) => React.ReactNode);
-
-type SidebarAppendToType = 'self' | HTMLElement | undefined | null;
-
+/**
+ * Defines valid properties in Sidebar component. In addition to these, all properties of HTMLDivElement can be used in this component.
+ * @group Properties
+ */
 export interface SidebarProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'ref'> {
-    maskStyle?: React.CSSProperties;
-    maskClassName?: string;
-    visible?: boolean;
-    position?: SidebarPositionType;
-    fullScreen?: boolean;
-    blockScroll?: boolean;
-    baseZIndex?: number;
-    dismissable?: boolean;
-    showCloseIcon?: boolean;
-    ariaCloseLabel?: string;
-    closeOnEscape?: boolean;
-    icons?: SidebarTemplateType;
-    modal?: boolean;
-    appendTo?: SidebarAppendToType;
-    transitionOptions?: CSSTransitionProps;
+    /**
+     * Inline style of the mask.
+     */
+    maskStyle?: React.CSSProperties | undefined;
+    /**
+     * Style class of the mask.
+     */
+    maskClassName?: string | undefined;
+    /**
+     * Specifies the visibility of the dialog.
+     * @defaultValue false
+     */
+    visible?: boolean | undefined;
+    /**
+     * Specifies the position of the sidebar, valid values are "left" and "right".
+     * @defaultValue left
+     */
+    position?: 'top' | 'bottom' | 'left' | 'right' | undefined;
+    /**
+     * Adds a close icon to the header to hide the dialog.
+     * @defaultValue false
+     */
+    fullScreen?: boolean | undefined;
+    /**
+     * Whether to block scrolling of the document when sidebar is active.
+     * @defaultValue false
+     */
+    blockScroll?: boolean | undefined;
+    /**
+     * Base zIndex value to use in layering.
+     * @defaultValue 0
+     */
+    baseZIndex?: number | undefined;
+    /**
+     * Whether to dismiss sidebar on click of the mask.
+     * @defaultValue true
+     */
+    dismissable?: boolean | undefined;
+    /**
+     * Whether to display a close icon inside the panel.
+     * @defaultValue true
+     */
+    showCloseIcon?: boolean | undefined;
+    /**
+     * Aria label of the close icon.
+     * @defaultValue close
+     */
+    ariaCloseLabel?: string | undefined;
+    /**
+     * Specifies if pressing escape key should hide the sidebar.
+     * @defaultValue true
+     */
+    closeOnEscape?: boolean | undefined;
+    /**
+     * Custom icons template for the header.
+     * @defaultValue true
+     */
+    icons?: React.ReactNode | ((props: SidebarProps) => React.ReactNode);
+    /**
+     * Whether to a modal layer behind the sidebar.
+     * @defaultValue true
+     */
+    modal?: boolean | undefined;
+    /**
+     * DOM element instance where the overlay panel should be mounted. Valid values are any DOM Element and self. The self value is used to render a component where it is located.
+     * @defaultValue document.body
+     */
+    appendTo?: 'self' | HTMLElement | null | undefined;
+    /**
+     * The properties of {@link CSSTransitionProps} can be customized, except for "nodeRef" and "in" properties.
+     */
+    transitionOptions?: CSSTransitionProps | undefined;
+    /**
+     * Callback to invoke when sidebar gets shown.
+     */
     onShow?(): void;
+    /**
+     * Callback to invoke when the actions used to close the sidebar are triggered. Exp; close icon, mask and esc key.
+     */
     onHide(): void;
-    children?: React.ReactNode;
+    /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
 }
 
+/**
+ * @group Component
+ */
 export declare class Sidebar extends React.Component<SidebarProps, any> {
+    /**
+     * Used to get container element.
+     * @return {HTMLDivElement} Container element
+     */
     public getElement(): HTMLDivElement;
+    /**
+     * Used to get mask element.
+     * @return {HTMLElement} Mask element
+     */
     public getMask(): HTMLElement;
+    /**
+     * Used to get close icon element.
+     * @return {HTMLButtonElement} Close icon element
+     */
     public getCloseIcon(): HTMLButtonElement;
 }
