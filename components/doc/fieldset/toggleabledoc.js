ff6dc4a72e88c271845c794f1dc2a7965999ec35
Ulaş Turan <ulasturan06@hotmail.com>
2023-01-13 00:31:57 +0300

    Fieldset: Refactor toggleabledoc

--- components/doc/fieldset/toggleabledoc.js
+++ components/doc/fieldset/toggleabledoc.js
@@ -16,7 +16,7 @@ import { Fieldset } from 'primereact/fieldset';
 export default function ToggleableDoc() {
 
     return (
-        <div className="card flex justify-content-center">
+        <div className="card">
             <Fieldset legend="Header" toggleable>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </Fieldset>
@@ -31,7 +31,7 @@ import { Fieldset } from 'primereact/fieldset';
 export default function ToggleableDoc() {
 
     return (
-        <div className="card flex justify-content-center">
+        <div className="card">
             <Fieldset legend="Header" toggleable>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </Fieldset>
@@ -49,7 +49,7 @@ export default function ToggleableDoc() {
                     properties need to be defined to control the collapsed state.
                 </p>
             </DocSectionText>
-            <div className="card flex justify-content-center">
+            <div className="card">
                 <Fieldset legend="Header" toggleable>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
