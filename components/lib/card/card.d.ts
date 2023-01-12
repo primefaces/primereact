48679cc1a8d9b15eff7a32a53656dfdfe923966d
Ulaş Turan <ulasturan06@hotmail.com>
2023-01-13 00:04:08 +0300

    Card: Add typedoc to .d.ts

--- components/lib/card/card.d.ts
+++ components/lib/card/card.d.ts
@@ -1,13 +1,43 @@
+/**
+ *
+ * Card is an input component that provides real-time suggestions when being typed.
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/card/)
+ *
+ * @module card
+ *
+ */
 import * as React from 'react';
 
-type CardTemplateTypes = React.ReactNode | ((props: CardProps) => React.ReactNode);
-
+/**
+ * Defines valid properties in Card component. In addition to these, all properties of HTMLDivElement can be used in this component.
+ * @group Properties
+ */
 export interface CardProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'ref' | 'title'> {
-    header?: CardTemplateTypes;
-    footer?: CardTemplateTypes;
-    title?: CardTemplateTypes;
-    subTitle?: CardTemplateTypes;
-    children?: React.ReactNode;
+    /**
+     * Header of the card.
+     */
+    header?: React.ReactNode | ((props: CardProps) => React.ReactNode);
+    /**
+     * Footer of the card.
+     */
+    footer?: React.ReactNode | ((props: CardProps) => React.ReactNode);
+    /**
+     * Title of the card.
+     */
+    title?: React.ReactNode | ((props: CardProps) => React.ReactNode);
+    /**
+     * Secondary title of the card.
+     */
+    subTitle?: React.ReactNode | ((props: CardProps) => React.ReactNode);
+    /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
 }
 
+/**
+ * @group Component
+ */
 export declare class Card extends React.Component<CardProps, any> {}
