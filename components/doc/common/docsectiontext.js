f0983996603e61cdd28e87b7c10295790a3060cb
Cagatay Civici <cagatay.civici@gmail.com>
2023-01-13 13:34:47 +0300

    Aligned style and a11y docs visuals

--- components/doc/common/docsectiontext.js
+++ components/doc/common/docsectiontext.js
@@ -1,26 +1,25 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import React from 'react';
 
 export function DocSectionText(props) {
     const router = useRouter();
+    const content = (
+        <>
+            {props.label}
+            <Link href={router.basePath + router.pathname + '#' + props.id} target="_self">
+                <a id={props.id}>#</a>
+            </Link>
+        </>
+    );
+    const tag = props.level === '2' ? 'h3' : 'h2';
+    const Title = (titleProps) => { 
+        return React.createElement(tag, { className: 'doc-section-label' }, titleProps.children); 
+    }; 
 
     return (
         <>
-            {props.level === '2' ? (
-                <h3 className="doc-section-label">
-                    {props.label}
-                    <Link href={router.basePath + router.pathname + '#' + props.id} target="_self">
-                        <a id={props.id}>#</a>
-                    </Link>
-                </h3>
-            ) : (
-                <h2 className="doc-section-label">
-                    {props.label}
-                    <Link href={router.basePath + router.pathname + '#' + props.id} target="_self">
-                        <a id={props.id}>#</a>
-                    </Link>
-                </h2>
-            )}
+            <Title>{content}</Title>
             <div className="doc-section-description">{props.children}</div>
         </>
     );
