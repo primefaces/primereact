c2f6a0467e1b45f4a0e35d2bea60815b6595be8a
Ulaş Turan <ulasturan06@hotmail.com>
2023-01-13 13:43:58 +0300

    Menubar: Add typedoc to .d.ts

--- components/lib/menubar/menubar.d.ts
+++ components/lib/menubar/menubar.d.ts
@@ -1,19 +1,56 @@
+/**
+ *
+ * Manubar is an input component that provides real-time suggestions when being typed.
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/menubar/)
+ *
+ * @module menubar
+ *
+ */
 import * as React from 'react';
 import { MenuItem } from '../menuitem';
 
-type MenubarStartTemplate = React.ReactNode | ((props: MenubarProps) => React.ReactNode);
-
-type MenubarEndTemplate = React.ReactNode | ((props: MenubarProps) => React.ReactNode);
-
+/**
+ * Defines valid properties in Menubar component. In addition to these, all properties of HTMLDivElement can be used in this component.
+ * @group Properties
+ */
 export interface MenubarProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'ref'> {
-    model?: MenuItem[];
-    start?: MenubarStartTemplate;
-    end?: MenubarEndTemplate;
-    children?: React.ReactNode;
+    /**
+     * An array of menuitems.
+     */
+    model?: MenuItem[] | undefined;
+    /**
+     * The template of starting element.
+     */
+    start?: React.ReactNode | ((props: MenubarProps) => React.ReactNode);
+    /**
+     * The template of trailing element.
+     */
+    end?: React.ReactNode | ((props: MenubarProps) => React.ReactNode);
+    /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
 }
 
+/**
+ * @group Component
+ */
 export declare class Menubar extends React.Component<MenubarProps, any> {
+    /**
+     * Used to get container element.
+     * @return {HTMLDivElement} Container element
+     */
     public getElement(): HTMLDivElement;
+    /**
+     * @todo Write the documentation
+     * @return {HTMLElement} Root menu element
+     */
     public getRootMenu(): HTMLElement;
+    /**
+     * @todo Write the documentation
+     * @return {HTMLElement} Menu button element
+     */
     public getMenuButton(): HTMLElement;
 }
