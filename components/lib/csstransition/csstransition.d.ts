cb636b177705df7f764227856bec2ef72247ca01
habubey <habubey@outlook.com>
2023-01-19 14:35:23 +0300

    Refactor csstransition.d.ts & selectitem.d.ts & format necessary docs

--- components/lib/csstransition/csstransition.d.ts
+++ components/lib/csstransition/csstransition.d.ts
@@ -1,8 +1,28 @@
+/**
+ *
+ * @todo Write the documentation.
+ *
+ * @todo Write the documentation.
+ *
+ * @module csstransition
+ *
+ */
 import * as React from 'react';
 import { CSSTransitionProps as ReactCSSTransitionProps } from 'react-transition-group/CSSTransition';
 
+/**
+ * Defines valid properties in CSSTransition component. In addition to these, all properties of HTMLElement can be used in this component.
+ * @group Properties
+ */
 export type CSSTransitionProps<Ref extends undefined | HTMLElement = undefined> = ReactCSSTransitionProps<Ref> & {
-    disabled?: boolean;
+    /**
+     * When present, it specifies that the component should be disabled.
+     * @defaultValue false
+     */
+    disabled?: boolean | undefined;
 };
 
+/**
+ * @group Component
+ */
 export declare class CSSTransition<Ref extends undefined | HTMLElement> extends React.Component<CSSTransitionProps<Ref>, any> {}
--- components/lib/selectitem/selectitem.d.ts
+++ components/lib/selectitem/selectitem.d.ts
@@ -1,12 +1,47 @@
+/**
+ *
+ * @todo Write the documentation.
+ *
+ * @todo Write the documentation.
+ *
+ * @module selectitem
+ *
+ */
 import { IconType } from '../utils';
 
+/**
+ * @group Misc
+ */
 export type SelectItemOptionsType = SelectItem[] | any[];
 
+/**
+ * Defines valid properties in SelectItem component.
+ * @group Properties
+ */
 export interface SelectItem {
+    /**
+     * Label of the option.
+     */
     label?: string;
+    /**
+     * Value of the option.
+     */
     value?: any;
+    /**
+     * ClassName of the option.
+     */
     className?: string;
+    /**
+     * Icon to display to the option.
+     */
     icon?: IconType<SelectItem>;
+    /**
+     * Tooltip text of the option. (Not supported)
+     */
     title?: string;
+    /**
+     * Whether the option is disabled or not. (Not supported)
+     * @defaultValue false
+     */
     disabled?: boolean;
 }
