14a079735db6822a897a86271080428586805828
habubey <habubey@outlook.com>
2023-01-19 15:40:12 +0300

    Refactor .d.ts docs

--- components/lib/overlayservice/overlayservice.d.ts
+++ components/lib/overlayservice/overlayservice.d.ts
@@ -1,16 +1,55 @@
+/**
+ *
+ * @todo Write the documentation.
+ *
+ * @todo Write the documentation.
+ *
+ * @module speeddial
+ *
+ */
 import React from 'react';
 
-type OverlayServiceActionType = 'overlay-click';
-
-interface OverlayServiceParams {
+/**
+ * Custom tab open event.
+ * @see {@link @todo Write the documentation.}
+ * @event
+ */
+interface OverlayServiceEvent {
+    /**
+     * Original event that triggered the overlay.
+     */
     originalEvent: React.SyntheticEvent;
+    /**
+     * Target element that the overlay is bound to.
+     */
     target: HTMLElement;
 }
 
+/**
+ * @group Misc
+ */
 export interface OverlayServiceOptions {
-    on(action: OverlayServiceActionType, fn: any): void;
-    emit(action: OverlayServiceActionType, params?: OverlayServiceParams): void;
-    off(action: OverlayServiceActionType, fn: any): void;
+    /**
+     * Add event listener for overlay click.
+     * @param {"overlay-click"} action - Custom listener.
+     * @param {*} fn - Custom listener.
+     */
+    on(action: 'overlay-click', fn: any): void;
+    /**
+     * Trigger overlay click event.
+     * @param {"overlay-click"} action - Custom listener.
+     * @param {*} params - Custom listener.
+     */
+    emit(action: 'overlay-click', params?: OverlayServiceEvent): void;
+    /**
+     * Remove event listener for overlay click.
+     * @param {"overlay-click"} action - Custom listener.
+     * @param {*} fn - Custom listener.
+     */
+    off(action: 'overlay-click', fn: any): void;
 }
 
+/**
+ * @todo Write the documentation.
+ */
 export declare const OverlayService: OverlayServiceOptions;
--- components/lib/terminalservice/terminalservice.d.ts
+++ components/lib/terminalservice/terminalservice.d.ts
@@ -1,9 +1,38 @@
-type TerminalServiceActionType = 'command' | 'response' | 'clear';
+/**
+ *
+ * Terminal is a text based user interface.
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/terminal/)
+ *
+ * @module terminal
+ *
+ */
 
+/**
+ * @group Misc
+ */
 export interface TerminalServiceOptions {
-    on(action: TerminalServiceActionType, fn: any): void;
-    emit(action: TerminalServiceActionType, params?: any): void;
-    off(action: TerminalServiceActionType, fn: any): void;
+    /**
+     * Method to attach an event listener to a specific action.
+     * @param {'command' | 'response' | 'clear'} action - Custom listener.
+     * @param {*} fn - Custom listener.
+     */
+    on(action: 'command' | 'response' | 'clear', fn: any): void;
+    /**
+     * Method to emit an event for a specific action.
+     * @param {'command' | 'response' | 'clear'} action - Custom listener.
+     * @param {*} params - Custom listener.
+     */
+    emit(action: 'command' | 'response' | 'clear', params?: any): void;
+    /**
+     * Method to detach an event listener from a specific action.
+     * @param {'command' | 'response' | 'clear'} action - Custom listener.
+     * @param {*} fn - Custom listener.
+     */
+    off(action: 'command' | 'response' | 'clear', fn: any): void;
 }
 
+/**
+ * @group Component
+ */
 export declare const TerminalService: TerminalServiceOptions;
