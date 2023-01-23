c215bc9c0ac832dfe49810ec5e06a6c2bd4b38ad
habubey <habubey@outlook.com>
2023-01-23 10:41:28 +0300

    Refactor on row.d.ts

--- components/lib/row/row.d.ts
+++ components/lib/row/row.d.ts
@@ -1,9 +1,43 @@
+/**
+ *
+ * @todo
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/datatable/)
+ *
+ * @module row
+ *
+ */
 import * as React from 'react';
 
+/**
+ * Defines valid properties in Row component.
+ * @group Properties
+ */
 export interface RowProps {
-    style?: React.CSSProperties;
-    className?: string;
-    children?: React.ReactNode;
+    /**
+     * Inline style of the element.
+     */
+    style?: React.CSSProperties | undefined;
+    /**
+     * Style class of the row.
+     */
+    className?: string | undefined;
+    /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
 }
 
+/**
+ * **PrimeReact - Row**
+ *
+ * @todo
+ *
+ * [Live Demo](https://www.primefaces.org/primereact/datatable/)
+ * --- ---
+ * ![PrimeReact](https://www.primefaces.org/primereact/images/logo.png|height=100|width=100)
+ *
+ * @module row
+ */
 export declare class Row extends React.Component<RowProps, any> {}
