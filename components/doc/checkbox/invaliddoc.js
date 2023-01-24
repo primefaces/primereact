488a48ce3d81a9a4c9bc18aef4d51322c8adbf23
Cagatay Civici <cagatay.civici@gmail.com>
2023-01-24 16:22:17 +0300

    Refactored checkbox docs

--- components/doc/checkbox/invaliddoc.js
+++ components/doc/checkbox/invaliddoc.js
@@ -1,25 +1,20 @@
-import { useState } from 'react';
 import { Checkbox } from '../../lib/checkbox/Checkbox';
 import { DocSectionCode } from '../common/docsectioncode';
 import { DocSectionText } from '../common/docsectiontext';
 
 export function InvalidDoc(props) {
-    const [checked, setChecked] = useState(false);
-
     const code = {
         basic: `
-<Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
+<Checkbox className="p-invalid"></Checkbox>
         `,
         javascript: `
 import React, { useState } from "react";
 import { Checkbox } from "primereact/checkbox";
 
 export default function InvalidDemo() {
-    const [checked, setChecked] = useState(false);
-
     return (
         <div className="card flex justify-content-center">
-            <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
+            <Checkbox className="p-invalid"></Checkbox>
         </div>
     )
 }
@@ -29,11 +24,9 @@ import React, { useState } from "react";
 import { Checkbox } from "primereact/checkbox";
 
 export default function InvalidDemo() {
-    const [checked, setChecked] = useState<boolean>(false);
-
     return (
         <div className="card flex justify-content-center">
-            <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
+            <Checkbox className="p-invalid"></Checkbox>
         </div>
     )
 }
@@ -46,7 +39,7 @@ export default function InvalidDemo() {
                 <p>Invalid state style is added using the <i>p-invalid</i> class to indicate a failed validation.</p>
             </DocSectionText>
             <div className="card flex justify-content-center">
-                <Checkbox onChange={(e) => setChecked(e.checked)} checked={checked} className="p-invalid"></Checkbox>
+                <Checkbox className="p-invalid"></Checkbox>
             </div>
             <DocSectionCode code={code} />
         </>
