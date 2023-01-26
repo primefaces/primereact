a550b564023a174a4d74e55dec1f3249dc91082e
Cagatay Civici <cagatay.civici@gmail.com>
2023-01-26 16:35:27 +0300

    Embed versions

--- service/VersionService.js
+++
@@ -1,7 +0,0 @@
-export const VersionService = {
-    getVersions() {
-        return fetch('https://www.primefaces.org/primereact/versions.json')
-            .then((res) => res.json())
-            .then((d) => d.versions);
-    }
-};
