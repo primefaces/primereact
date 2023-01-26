8896eb24bb9ac1c241cfdb1f838be9216d0f61f9
Cagatay Civici <cagatay.civici@gmail.com>
2023-01-26 15:46:12 +0300

    Set version

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
         appVersion: process.env.npm_package_version || '',
         contextPath: process.env.NODE_ENV === 'production' ? '/primereact' : '',
         uploadPath: process.env.NODE_ENV === 'production' ? '/primereact/upload.php' : '/api/upload',
-        apiDocUrl: 'https://primefaces.github.io/primereact/api/9.0.0'
+        apiDocUrl: 'https://primefaces.github.io/primereact/api/9.0.0-beta.1'
     }
 };
