1dc0afcd263783cb6c51483e42a44667c5401ea0
Ulaş Turan <ulasturan06@hotmail.com>
2023-01-26 13:50:55 +0300

    Refactor

--- components/doc/common/developmentsection.js
+++ components/doc/common/developmentsection.js
@@ -3,7 +3,8 @@ import * as React from 'react';
 export const DevelopmentSection = React.memo((props) => {
     return (
         <>
-            <div className="line-height-3 bg-indigo-600 text-white p-3 text-lg" style={{ borderRadius: '10px' }}>
+            <div className="flex align-items-center line-height-3 bg-primary-600 text-white p-3 text-lg border-round">
+                <i className="pi pi-info-circle text-lg text-white mr-2"></i>
                 Accessibility guide documents the specification of this component based on WCAG guidelines, the implementation is in progress.
             </div>
             {props.children}
