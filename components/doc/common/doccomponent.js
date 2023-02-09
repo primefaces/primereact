90941e6db2a2e59bc9e4f7993544325b70fa2471
Ulaş Turan <ulasturan06@hotmail.com>
2023-02-09 17:49:22 +0300

    Refactor on dock demo

--- components/doc/common/doccomponent.js
+++ components/doc/common/doccomponent.js
@@ -16,7 +16,7 @@ export function DocComponent(props) {
     };
 
     return (
-        <div className="doc-component">
+        <div className="doc-component dock-demo">
             <Head>
                 <title>{props.title}</title>
                 <meta name="description" content={props.description} />
