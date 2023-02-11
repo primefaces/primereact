3a8cf8155f6eaccd3edaf0e320fd7c0026e93c19
mertsincan <sincan.mert@gmail.com>
2023-02-11 16:03:47 +0000

    Refactor on section nav

--- components/doc/common/docsectionnav.js
+++ components/doc/common/docsectionnav.js
@@ -8,101 +8,95 @@ export function DocSectionNav(props) {
     const router = useRouter();
     const [activeId, setActiveId] = useState(null);
     const navRef = useRef(null);
-    const blockedScroll = useRef(false);
+    const topbarHeight = useRef(0);
+    const isScrollBlocked = useRef(false);
     const scrollEndTimer = useRef(undefined);
-
-    const onClick = (id) => {
-        const label = document.getElementById(id);
-
-        setActiveId(id);
-        label && label.parentElement.scrollIntoView({ block: 'start', behavior: 'smooth' });
-        blockedScroll.current = true;
-    };
 
     const [bindDocumentScrollListener] = useEventListener({
         target: 'window',
         type: 'scroll',
-        listener: () => {
+        listener: (event) => {
+            if (!isScrollBlocked.current) {
+                const labels = DomHandler.find(event.target, ':is(h1,h2,h3).doc-section-label');
+                const windowScrollTop = DomHandler.getWindowScrollTop();
+
+                labels.forEach((label) => {
+                    const { top } = DomHandler.getOffset(label);
+                    const threshold = getThreshold(label);
+
+                    if (top - threshold <= windowScrollTop) {
+                        const link = DomHandler.findSingle(label, 'a');
+
+                        setActiveId(link.id);
+                    }
+                });
+            }
+
             clearTimeout(scrollEndTimer.current);
             scrollEndTimer.current = setTimeout(() => {
-                blockedScroll.current = false;
+                isScrollBlocked.current = false;
             }, 100);
         }
     });
 
+    const onClick = (id) => {
+        setActiveId(id);
+        scrollToLabelById(id, 'smooth');
+        isScrollBlocked.current = true;
+    };
+
+    const scrollToLabelById = (id, behavior) => {
+        const label = document.getElementById(id);
+
+        label && label.parentElement.scrollIntoView({ block: 'start', behavior });
+    };
+
+    const getThreshold = (label) => {
+        if (!topbarHeight.current) {
+            const topbar = DomHandler.findSingle(document.body, '.layout-topbar');
+
+            topbarHeight.current = topbar ? DomHandler.getHeight(topbar) : 0;
+        }
+
+        return topbarHeight.current + DomHandler.getHeight(label) * 1.5;
+    };
+
     useEffect(() => {
         const hash = window.location.hash.substring(1);
-        const label = document.getElementById(hash);
 
         setActiveId(hash);
-        label && label.parentElement.scrollIntoView({ block: 'start' });
+        scrollToLabelById(hash);
         bindDocumentScrollListener();
     }, []);
 
     useEffect(() => {
-        const observerOptions = {
-            root: null,
-            rootMargin: '50%',
-            threshold: 0
-        };
+        const activeItem = DomHandler.findSingle(navRef.current, '.active-navbar-item');
 
-        const observerCallback = (entries, observer) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting && !blockedScroll.current) {
-                    const link = DomHandler.findSingle(entry.target, 'a');
+        activeItem && activeItem.scrollIntoView({ block: 'nearest', inline: 'start' });
+    }, [activeId]);
 
-                    if (link) {
-                        setActiveId(link.id);
-                        entry.target.scrollIntoView({ block: 'nearest', inline: 'start' });
-                    }
-                }
-            });
-        };
+    const createItem = ({ id, label, children }, level = 0) => {
+        const { basePath, pathname } = router;
+        const href = `${basePath}${pathname}#${id}`;
 
-        const observer = new IntersectionObserver(observerCallback, observerOptions);
-        const labels = DomHandler.find(document.body, ':is(h1,h2,h3).doc-section-label');
+        return (
+            <li key={id} className={classNames('navbar-item', { 'active-navbar-item': activeId === id })}>
+                <div className="navbar-item-content">
+                    <Link href={href}>
+                        <button className="p-link" onClick={() => onClick(id)} title={label}>
+                            {label}
+                        </button>
+                    </Link>
+                </div>
 
-        labels.forEach((label) => observer.observe(label));
-
-        return () => {
-            labels.forEach((label) => observer.unobserve(label));
-        };
-    }, [router]);
+                <ul>{level !== 1 && children && children.map((child) => createItem(child, 1))}</ul>
+            </li>
+        );
+    };
 
     return (
         <ul ref={navRef} className="doc-section-nav">
-            {props.docs.map((doc) => {
-                const hash = doc.id;
-
-                return (
-                    <React.Fragment key={doc.label}>
-                        <li key={doc.label} className={classNames('navbar-item', { 'active active-navbar-item': activeId === doc.id })}>
-                            <Link href={router.basePath + router.pathname + '#' + hash}>
-                                <button className="p-link" onClick={() => onClick(doc.id)}>
-                                    {doc.label}
-                                </button>
-                            </Link>
-                        </li>
-                        {doc.children && (
-                            <ul>
-                                {doc.children.map((child, i) => {
-                                    const hash = child.id;
-
-                                    return (
-                                        <li key={child.id + '_' + child.label + '_' + i} className={classNames('navbar-child-item', { 'active active-navbar-child-item': activeId === child.id })}>
-                                            <Link href={router.basePath + router.pathname + '#' + hash}>
-                                                <button className="p-link" onClick={() => onClick(child.id)}>
-                                                    {child.label}
-                                                </button>
-                                            </Link>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        )}
-                    </React.Fragment>
-                );
-            })}
+            {props.docs.map(createItem)}
         </ul>
     );
 }
