5e770f127e15363ed68a45574fa8eea3810f61bf
Cagatay Civici <cagatay.civici@gmail.com>
2023-02-17 16:47:04 +0300

    Updated responsive doc

--- components/doc/datatable/responsivedoc.js
+++ components/doc/datatable/responsivedoc.js
@@ -1,87 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { DataTable } from '../../lib/datatable/DataTable';
-import { Column } from '../../lib/column/Column';
+import React, { useEffect, useState } from 'react';
 import { ProductService } from '../../../service/ProductService';
+import { Column } from '../../lib/column/Column';
+import { DataTable } from '../../lib/datatable/DataTable';
+import { SelectButton } from '../../lib/selectbutton/SelectButton';
 import { DocSectionCode } from '../common/docsectioncode';
 import { DocSectionText } from '../common/docsectiontext';
-import { Rating } from '../../lib/rating/Rating';
-import { RadioButton } from '../../lib/radiobutton/RadioButton';
 
 export function ResponsiveDoc(props) {
     const [products, setProducts] = useState([]);
-
-    const [selectedOptionValue, setSelectedOptionValue] = useState('scroll');
-
-    const statusTemplate = (rowData) => {
-        return <span className={`product-badge status-${rowData.inventoryStatus ? rowData.inventoryStatus.toLowerCase() : ''}`}>{rowData.inventoryStatus}</span>;
-    };
-
-    const ratingTemplate = (rowData) => {
-        return <Rating value={rowData.rating} readOnly cancel={false} />;
-    };
-
-    const onRadioButtonChange = (option) => {
-        setSelectedOptionValue(option.value);
-    };
+    const [responsiveOptions] = useState([
+        { label: 'Stacked', value: 'stack' },
+        { label: 'Scroll', value: 'scroll' }
+    ]);
+    const [responsiveMode, setResponsiveMode] = useState(responsiveOptions[0].value);
 
     useEffect(() => {
         ProductService.getProductsSmall().then((data) => setProducts(data));
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const demoOptions = [
-        {
-            label: 'Scroll',
-            value: 'scroll'
-        },
-        {
-            label: 'Stack',
-            value: 'stack'
-        }
-    ];
-
     const code = {
         basic: `
-<DataTable value={products} responsiveLayout="${selectedOptionValue}">
-    <Column field="code" header="Code" />
-    <Column field="name" header="Name" />
-    <Column field="category" header="Category" />
-    <Column field="quantity" header="Quantity" />
-    <Column field="inventoryStatus" header="Status" body={statusTemplate} />
-    <Column field="rating" header="Rating" body={ratingTemplate} />
+<DataTable value={products} responsiveLayout={responsiveMode}>
+    <Column field="code" header="Code"></Column>
+    <Column field="name" header="Name" bodyClassName="white-space-nowrap"></Column>
+    <Column field="category" header="Category"></Column>
+    <Column field="quantity" header="Quantity"></Column>
 </DataTable>
         `,
         javascript: `
 import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Rating } from 'primereact/rating';
+import { SelectButton } from 'primereact/selectbutton';
 import { ProductService } from './service/ProductService';
 
-const ScrollDoc = () => {
+export default function ResponsiveDemo() {
     const [products, setProducts] = useState([]);
-    
+    const [responsiveOptions] = useState([
+        { label: 'Stacked', value: 'stack' },
+        { label: 'Scroll', value: 'scroll' }
+    ]);
+    const [responsiveMode, setResponsiveMode] = useState(responsiveOptions[1].value);
 
     useEffect(() => {
-        ProductService.getProductsSmall().then(data => setProducts(data));
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-    const statusTemplate = (rowData) => {
-        return <span className={\`product-badge status-\${(rowData.inventoryStatus ? rowData.inventoryStatus.toLowerCase() : '')}\`}>{rowData.inventoryStatus}</span>;
-    }
-
-    const ratingTemplate = (rowData) => {
-        return <Rating value={rowData.rating} readOnly cancel={false} />
-    }
+        ProductService.getProductsSmall().then((data) => setProducts(data));
+    }, []);
 
     return (
         <div className="card">
-            <DataTable value={products} responsiveLayout="${selectedOptionValue}">
-                <Column field="code" header="Code" />
-                <Column field="name" header="Name" />
-                <Column field="category" header="Category" />
-                <Column field="quantity" header="Quantity" />
-                <Column field="inventoryStatus" header="Status" body={statusTemplate} />
-                <Column field="rating" header="Rating" body={ratingTemplate} />
+            <div className="flex justify-content-center mb-4">
+                <SelectButton value={responsiveMode} onChange={(e) => setResponsiveMode(e.value)} options={responsiveOptions} />
+            </div>
+            <DataTable value={products} responsiveLayout={responsiveMode}>
+                <Column field="code" header="Code"></Column>
+                <Column field="name" header="Name" bodyClassName="white-space-nowrap"></Column>
+                <Column field="category" header="Category"></Column>
+                <Column field="quantity" header="Quantity"></Column>
             </DataTable>
         </div>
     );
@@ -91,41 +65,55 @@ const ScrollDoc = () => {
 import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Rating } from 'primereact/rating';
+import { SelectButton } from 'primereact/selectbutton';
 import { ProductService } from './service/ProductService';
 
-const ScrollDoc = () => {
-    const [products, setProducts] = useState([]);
-    
+interface Product {
+    id: string;
+    code: string;
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+    category: string;
+    quantity: number;
+    inventoryStatus: string;
+    rating: number;
+}
+
+interface ResponsiveOption {
+    label: string;
+    value: string;
+}
+
+export default function ResponsiveDemo() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [responsiveOptions] = useState<ResponsiveOption[]>([
+        { label: 'Stacked', value: 'stack' },
+        { label: 'Scroll', value: 'scroll' }
+    ]);
+    const [responsiveMode, setResponsiveMode] = useState<string>(responsiveOptions[1].value);
 
     useEffect(() => {
-        ProductService.getProductsSmall().then(data => setProducts(data));
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-    const statusTemplate = (rowData) => {
-        return <span className={\`product-badge status-\${(rowData.inventoryStatus ? rowData.inventoryStatus.toLowerCase() : '')}\`}>{rowData.inventoryStatus}</span>;
-    }
-
-    const ratingTemplate = (rowData) => {
-        return <Rating value={rowData.rating} readOnly cancel={false} />
-    }
+        ProductService.getProductsSmall().then((data) => setProducts(data));
+    }, []);
 
     return (
         <div className="card">
-            <DataTable value={products} responsiveLayout="${selectedOptionValue}">
-                <Column field="code" header="Code" />
-                <Column field="name" header="Name" />
-                <Column field="category" header="Category" />
-                <Column field="quantity" header="Quantity" />
-                <Column field="inventoryStatus" header="Status" body={statusTemplate} />
-                <Column field="rating" header="Rating" body={ratingTemplate} />
+            <div className="flex justify-content-center mb-4">
+                <SelectButton value={responsiveMode} onChange={(e) => setResponsiveMode(e.value)} options={responsiveOptions} />
+            </div>
+            <DataTable value={products} responsiveLayout={responsiveMode}>
+                <Column field="code" header="Code"></Column>
+                <Column field="name" header="Name" bodyClassName="white-space-nowrap"></Column>
+                <Column field="category" header="Category"></Column>
+                <Column field="quantity" header="Quantity"></Column>
             </DataTable>
         </div>
     );
 }
         `,
         data: `
-/* ProductService */        
 {
     id: '1000',
     code: 'f230fh0g3',
@@ -145,32 +133,20 @@ const ScrollDoc = () => {
     return (
         <>
             <DocSectionText {...props}>
-                <p>DataTable responsive layout can be achieved in two ways; first approach is displaying a horizontal scrollbar for smaller screens and second one is defining a breakpoint to display the cells of a row as stacked.</p>
+                <p>
+                    DataTable responsive layout is controlled with the <i>responsiveLayout</i> property that can either be <i>scroll</i> or <i>stack</i>. In smaller screens, <i>scroll</i> mode displays a horizontal scrollbar and <i>stack</i> mode
+                    displays columns as vertically. The <i>breakpoint</i> property defines the max-width for the <i>stack</i> mode.
+                </p>
             </DocSectionText>
-            <div className="card mt-3 flex flex-column justify-content-center">
-                <div className="flex flex-row justify-content-center align-items-center flex-wrap">
-                    <div className="card flex flex-wrap justify-content-center align-items-center w-full gap-3">
-                        {demoOptions.map((option) => {
-                            const { value, label } = option;
-
-                            return (
-                                <div className="mr-4" key={label}>
-                                    <RadioButton value={label} onChange={() => onRadioButtonChange(option)} checked={selectedOptionValue === value} />
-                                    <label htmlFor={label} className="ml-2">
-                                        {label} Responsive Layout
-                                    </label>
-                                </div>
-                            );
-                        })}
-                    </div>
+            <div className="card">
+                <div className="flex justify-content-center mb-4">
+                    <SelectButton value={responsiveMode} onChange={(e) => setResponsiveMode(e.value)} options={responsiveOptions} />
                 </div>
-                <DataTable value={products} responsiveLayout={selectedOptionValue}>
-                    <Column field="code" header="Code" />
-                    <Column field="name" header="Name" />
-                    <Column field="category" header="Category" />
-                    <Column field="quantity" header="Quantity" />
-                    <Column field="inventoryStatus" header="Status" body={statusTemplate} />
-                    <Column field="rating" header="Rating" body={ratingTemplate} />
+                <DataTable value={products} responsiveLayout={responsiveMode}>
+                    <Column field="code" header="Code"></Column>
+                    <Column field="name" header="Name" bodyClassName="white-space-nowrap"></Column>
+                    <Column field="category" header="Category"></Column>
+                    <Column field="quantity" header="Quantity"></Column>
                 </DataTable>
             </div>
             <DocSectionCode code={code} service={['ProductService']} />
