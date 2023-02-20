6d2761d86af1176b6cfc0d6fa60e874e7dcdd7ab
Ulaş Turan <ulasturan06@hotmail.com>
2023-02-20 23:46:44 +0300

    Implement api for getting mock data. Related - #4086

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     reactStrictMode: process.env.NODE_ENV === 'production' ? false : true,
     trailingSlash: true,
     publicRuntimeConfig: {
+        apiUrl: process.env.NODE_ENV === 'production' ? 'https://www.primereact.org/api/data/customers' : 'http://localhost:3000/api/data/customers',
         appVersion: process.env.npm_package_version || ''
     },
     webpack(config) {
@@ -20,6 +21,6 @@ module.exports = {
                 destination: '/installation',
                 permanent: true
             }
-        ]
+        ];
     }
 };
