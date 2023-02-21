1069bc9106c8467699c8fd87e6b6e53d55a0f69e
mertsincan <sincan.mert@gmail.com>
2023-02-21 10:53:37 +0000

    Refactor #4082

--- components/lib/tieredmenu/TieredMenu.js
+++ components/lib/tieredmenu/TieredMenu.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import PrimeReact from '../api/Api';
 import { CSSTransition } from '../csstransition/CSSTransition';
-import { useMountEffect, useOverlayListener, useUnmountEffect, useUpdateEffect } from '../hooks/Hooks';
+import { useMatchMedia, useMountEffect, useOverlayListener, useUnmountEffect, useUpdateEffect } from '../hooks/Hooks';
 import { OverlayService } from '../overlayservice/OverlayService';
 import { Portal } from '../portal/Portal';
-import { classNames, DomHandler, ZIndexUtils, UniqueComponentId } from '../utils/Utils';
+import { classNames, DomHandler, UniqueComponentId, ZIndexUtils } from '../utils/Utils';
 import { TieredMenuBase } from './TieredMenuBase';
 import { TieredMenuSub } from './TieredMenuSub';
 
@@ -17,6 +17,7 @@ export const TieredMenu = React.memo(
         const menuRef = React.useRef(null);
         const targetRef = React.useRef(null);
         const styleElementRef = React.useRef(null);
+        const isMobileMode = useMatchMedia(`screen and (max-width: ${props.breakpoint})`, !!props.breakpoint);
 
         const [bindOverlayListener, unbindOverlayListener] = useOverlayListener({
             target: targetRef,
@@ -56,6 +57,12 @@ export const TieredMenu = React.memo(
             }
         };
 
+        const onItemToggle = () => {
+            if (props.popup && isMobileMode) {
+                DomHandler.absolutePosition(menuRef.current, targetRef.current);
+            }
+        };
+
         const createStyle = () => {
             if (!styleElementRef.current) {
                 styleElementRef.current = DomHandler.createInlineStyle(PrimeReact.nonce);
@@ -79,9 +86,11 @@ export const TieredMenu = React.memo(
         padding: 0 0 0 calc(var(--inline-spacing) * 2); /* @todo */
     }
 
-    .p-tieredmenu[${selector}] .p-submenu-icon:before {           
+    .p-tieredmenu[${selector}] .p-submenu-icon:before {
         content: "\\e930";
     }
+
+    ${!props.popup ? `.p-tieredmenu[${selector}] { width: 100%; }` : ''}
 }
 `;
 
@@ -93,35 +102,18 @@ export const TieredMenu = React.memo(
             styleElementRef.current = DomHandler.removeInlineStyle(styleElementRef.current);
         };
 
-        useMountEffect(() => {
-            if (props.breakpoint) {
-                !attributeSelectorState && setAttributeSelectorState(UniqueComponentId());
-            }
-        });
-
         const onEnter = () => {
             if (props.autoZIndex) {
                 ZIndexUtils.set('menu', menuRef.current, PrimeReact.autoZIndex, props.baseZIndex || PrimeReact.zIndex['menu']);
             }
 
+            DomHandler.absolutePosition(menuRef.current, targetRef.current);
+
             if (attributeSelectorState && props.breakpoint) {
                 menuRef.current.setAttribute(attributeSelectorState, '');
                 createStyle();
             }
-
-            DomHandler.absolutePosition(menuRef.current, targetRef.current);
         };
-
-        useUpdateEffect(() => {
-            if (attributeSelectorState && menuRef.current) {
-                menuRef.current.setAttribute(attributeSelectorState, '');
-                createStyle();
-            }
-
-            return () => {
-                destroyStyle();
-            };
-        }, [attributeSelectorState, props.breakpoint]);
 
         const onEntered = () => {
             bindOverlayListener();
@@ -136,6 +128,23 @@ export const TieredMenu = React.memo(
             ZIndexUtils.clear(menuRef.current);
             destroyStyle();
         };
+
+        useMountEffect(() => {
+            if (props.breakpoint) {
+                !attributeSelectorState && setAttributeSelectorState(UniqueComponentId());
+            }
+        });
+
+        useUpdateEffect(() => {
+            if (attributeSelectorState && menuRef.current) {
+                menuRef.current.setAttribute(attributeSelectorState, '');
+                createStyle();
+            }
+
+            return () => {
+                destroyStyle();
+            };
+        }, [attributeSelectorState, props.breakpoint]);
 
         useUnmountEffect(() => {
             ZIndexUtils.clear(menuRef.current);
@@ -175,7 +184,7 @@ export const TieredMenu = React.memo(
                     onExited={onExited}
                 >
                     <div ref={menuRef} id={props.id} className={className} style={props.style} {...otherProps} onClick={onPanelClick}>
-                        <TieredMenuSub menuProps={props} model={props.model} root popup={props.popup} onHide={hide} />
+                        <TieredMenuSub menuProps={props} model={props.model} root popup={props.popup} onHide={hide} isMobileMode={isMobileMode} onItemToggle={onItemToggle} />
                     </div>
                 </CSSTransition>
             );
