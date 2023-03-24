3f52fce3372bf9f3d6ff2aed9c0d1eb68f83d6be
Ulaş Turan <ulasturan06@hotmail.com>
2023-03-24 14:03:29 +0300

    Refactor: User section

--- pages/landing/userssection.js
+++ pages/landing/userssection.js
@@ -1,5 +1,33 @@
 const UsersSection = (props) => {
     const colorScheme = props.dark ? 'light' : 'dark';
+    const usersData = ['fox', 'airbus', 'mercedes', 'ebay', 'ford', 'vw', 'intel', 'unicredit', 'lufthansa', 'nvidia', 'verizon', 'amex'];
+
+    const getUsersImages = (colorScheme) =>
+        usersData.map((name) => ({
+            name,
+            image: `https://primefaces.org/cdn/primereact/images/landing-new/whouses/${name}-${colorScheme}.svg`
+        }));
+
+    const Marquee = ({ users, colorScheme, reverse }) => (
+        <div className="marquee-wrapper overflow-hidden flex">
+            {Array(3)
+                .fill(users)
+                .map((users, index) => (
+                    <div key={index} className={`marquee${reverse ? ' marquee-reverse' : ''}`}>
+                        {users.map((user) => (
+                            <div key={user.name}>
+                                <img src={user.image} alt={`${user.name}-${colorScheme}`} />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+        </div>
+    );
+
+    const usersImages = getUsersImages(colorScheme);
+
+    const users1 = usersImages.slice(0, 6);
+    const users2 = usersImages.slice(6);
 
     return (
         <section className="landing-users py-8 px-5 lg:px-8">
@@ -16,135 +44,12 @@ const UsersSection = (props) => {
             </div>
             <div className="logo-section relative w-full md:w-8 mt-6 users-container">
                 <div className="fade-left h-6rem w-6rem block absolute top-0 left-0 z-2" style={{ background: 'linear-gradient(to right, var(--home-bg), transparent)' }}></div>
-                <div className="marquee-wrapper overflow-hidden flex ">
-                    <div className="marquee ">
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/fox-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/airbus-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/mercedes-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/ebay-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/ford-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/vw-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                    </div>
-                    <div aria-hidden="true" className="marquee ">
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/fox-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/airbus-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/mercedes-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/ebay-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/ford-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/vw-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                    </div>
-                    <div aria-hidden="true" className="marquee ">
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/fox-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/airbus-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/mercedes-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/ebay-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/ford-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/vw-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                    </div>
-                </div>
-
+                <Marquee users={users1} colorScheme={colorScheme} />
                 <div className="fade-right h-6rem w-6rem block absolute top-0 right-0 z-2" style={{ background: 'linear-gradient(to left, var(--home-bg), transparent)' }}></div>
             </div>
             <div className="logo-section relative w-full md:w-8 mt-2 users-container">
                 <div className="fade-left h-6rem w-6rem block absolute top-0 left-0 z-2" style={{ background: 'linear-gradient(to right, var(--home-bg), transparent)' }}></div>
-                <div className="marquee-wrapper overflow-hidden flex ">
-                    <div className="marquee marquee-reverse">
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/intel-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/unicredit-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/lufthansa-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" w-full">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/nvidia-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/verizon-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/amex-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                    </div>
-                    <div aria-hidden="true" className="marquee marquee-reverse">
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/intel-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/unicredit-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/lufthansa-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/nvidia-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/verizon-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/amex-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                    </div>
-                    <div aria-hidden="true" className="marquee marquee-reverse">
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/intel-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className="">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/unicredit-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/lufthansa-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/nvidia-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/verizon-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                        <div className=" ">
-                            <img src={`https://primefaces.org/cdn/primereact/images/landing-new/whouses/amex-${colorScheme}.svg`} alt={`fox-${colorScheme}`} />
-                        </div>
-                    </div>
-                </div>
+                <Marquee users={users2} colorScheme={colorScheme} reverse />
                 <div className="fade-right h-6rem w-6rem block absolute top-0 right-0 z-2" style={{ background: 'linear-gradient(to left, var(--home-bg), transparent)' }}></div>
             </div>
         </section>
