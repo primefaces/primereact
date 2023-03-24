c048b235e436ddd6032d85073753d15eb59fc39b
Ulaş Turan <ulasturan06@hotmail.com>
2023-03-24 14:47:48 +0300

    Remove unnecessary props on usersection

--- pages/landing/userssection.js
+++ pages/landing/userssection.js
@@ -2,20 +2,20 @@ const UsersSection = (props) => {
     const colorScheme = props.dark ? 'light' : 'dark';
     const usersData = ['fox', 'airbus', 'mercedes', 'ebay', 'ford', 'vw', 'intel', 'unicredit', 'lufthansa', 'nvidia', 'verizon', 'amex'];
 
-    const getUsersImages = (colorScheme) =>
+    const getUsersImages = () =>
         usersData.map((name) => ({
             name,
             image: `https://primefaces.org/cdn/primereact/images/landing-new/whouses/${name}-${colorScheme}.svg`
         }));
 
-    const Marquee = ({ users, colorScheme, reverse }) => (
+    const Marquee = ({ users, reverse }) => (
         <div className="marquee-wrapper overflow-hidden flex">
             {Array(3)
                 .fill(users)
                 .map((users, index) => (
                     <div key={index} className={`marquee${reverse ? ' marquee-reverse' : ''}`}>
                         {users.map((user) => (
-                            <div key={user.name}>
+                            <div className="w-full" key={user.name}>
                                 <img src={user.image} alt={`${user.name}-${colorScheme}`} />
                             </div>
                         ))}
@@ -24,7 +24,7 @@ const UsersSection = (props) => {
         </div>
     );
 
-    const usersImages = getUsersImages(colorScheme);
+    const usersImages = getUsersImages();
 
     const users1 = usersImages.slice(0, 6);
     const users2 = usersImages.slice(6);
@@ -44,12 +44,12 @@ const UsersSection = (props) => {
             </div>
             <div className="logo-section relative w-full md:w-8 mt-6 users-container">
                 <div className="fade-left h-6rem w-6rem block absolute top-0 left-0 z-2" style={{ background: 'linear-gradient(to right, var(--home-bg), transparent)' }}></div>
-                <Marquee users={users1} colorScheme={colorScheme} />
+                <Marquee users={users1} />
                 <div className="fade-right h-6rem w-6rem block absolute top-0 right-0 z-2" style={{ background: 'linear-gradient(to left, var(--home-bg), transparent)' }}></div>
             </div>
             <div className="logo-section relative w-full md:w-8 mt-2 users-container">
                 <div className="fade-left h-6rem w-6rem block absolute top-0 left-0 z-2" style={{ background: 'linear-gradient(to right, var(--home-bg), transparent)' }}></div>
-                <Marquee users={users2} colorScheme={colorScheme} reverse />
+                <Marquee users={users2} reverse />
                 <div className="fade-right h-6rem w-6rem block absolute top-0 right-0 z-2" style={{ background: 'linear-gradient(to left, var(--home-bg), transparent)' }}></div>
             </div>
         </section>
