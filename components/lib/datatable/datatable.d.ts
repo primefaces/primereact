7c047841ee563a13f8ccd3f40999ca4129a2fef3
deul <hicucu@gmail.com>
2023-04-24 15:25:49 +0900

    Fix #4275: Component: DataTable onContextMenuSelectionChange
    Add DataTableContextMenuSelectionChangeEvent
    
    issues: #4275

--- components/lib/datatable/datatable.d.ts
+++ components/lib/datatable/datatable.d.ts
@@ -10,8 +10,8 @@
 import * as React from 'react';
 import { Column, ColumnProps } from '../column';
 import { PaginatorTemplate } from '../paginator';
-import { VirtualScroller, VirtualScrollerProps } from '../virtualscroller/virtualscroller';
 import { IconType } from '../utils/utils';
+import { VirtualScroller, VirtualScrollerProps } from '../virtualscroller/virtualscroller';
 
 type DataTableHeaderTemplateType<TValue extends DataTableValueArray> = React.ReactNode | ((options: DataTableHeaderTemplateOptions<TValue>) => React.ReactNode);
 
@@ -279,7 +279,24 @@ interface DataTableDataSelectableEvent<TValue extends DataTableValueArray> {
 
 /**
  * Custom selection change event.
- * @see {@link DataTableProps.onContextMenuSelectionChange}, {@link DataTableProps.onSelectionChange}
+ * @see {@link DataTableProps.onContextMenuSelectionChange}
+ * @event
+ */
+interface DataTableContextMenuSelectionChangeEvent<TValue> {
+    /**
+     * Browser event.
+     */
+    originalEvent: React.SyntheticEvent;
+    /**
+     * Selection object.
+     */
+    value: TValue | undefined;
+
+}
+
+/**
+ * Custom selection change event.
+ * @see {@link DataTableProps.onSelectionChange}
  * @event
  */
 interface DataTableSelectionChangeEvent<TValue extends DataTableValueArray> {
@@ -1183,7 +1200,7 @@ export interface DataTableProps<TValue extends DataTableValueArray> extends Omit
      * Callback to invoke when a row selected with right click.
      * @param {DataTableRowEvent} event - Custom row event.
      */
-    onContextMenuSelectionChange?(event: DataTableSelectionChangeEvent<TValue>): void;
+    onContextMenuSelectionChange?(event: DataTableContextMenuSelectionChangeEvent<TValue>): void;
     /**
      * Callback to invoke on filtering.
      * @param {DataTableStateEvent} event - Custom state event.
