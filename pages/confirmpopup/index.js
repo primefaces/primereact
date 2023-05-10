05d85e3db08dd429df1241f5604b78818243a15d
habubey <habubey@outlook.com>
2023-05-10 12:53:13 +0300

    Refactor #4359 - for ConfirmPopup

--- pages/confirmpopup/index.js
+++ pages/confirmpopup/index.js
@@ -1,8 +1,11 @@
+import DocApiTable from '../../components/doc/common/docapitable';
 import { DocComponent } from '../../components/doc/common/doccomponent';
 import { AccessibilityDoc } from '../../components/doc/confirmpopup/accessibilitydoc';
 import { BasicDoc } from '../../components/doc/confirmpopup/basicdoc';
 import { DeclarativeDoc } from '../../components/doc/confirmpopup/declarativedoc';
 import { ImportDoc } from '../../components/doc/confirmpopup/importdoc';
+import { PTDoc } from '../../components/doc/confirmpopup/pt/ptdoc';
+import { Wireframe } from '../../components/doc/confirmpopup/pt/wireframe';
 import { StyleDoc } from '../../components/doc/confirmpopup/styledoc';
 import { ConfirmPopup } from '../../components/lib/confirmpopup/ConfirmPopup';
 
@@ -35,9 +38,34 @@ const ConfirmPopupDemo = () => {
         }
     ];
 
+    const ptDocs = [
+        {
+            id: 'pt.wireframe',
+            label: 'Wireframe',
+            component: Wireframe
+        },
+        {
+            id: 'pt.confirmpopup.options',
+            label: 'ConfirmPopup PT Options',
+            component: DocApiTable
+        },
+        {
+            id: 'pt.demo',
+            label: 'Example',
+            component: PTDoc
+        }
+    ];
+
     return (
         <>
-            <DocComponent title="React Confirmation Popup Component" header="ConfirmPopup" description="ConfirmPopup is an easy to use and customizable Confirmation API using a popover." componentDocs={docs} apiDocs={['ConfirmPopup']} />
+            <DocComponent
+                title="React Confirmation Popup Component"
+                header="ConfirmPopup"
+                description="ConfirmPopup is an easy to use and customizable Confirmation API using a popover."
+                componentDocs={docs}
+                apiDocs={['ConfirmPopup']}
+                ptDocs={ptDocs}
+            />
             <ConfirmPopup />
         </>
     );
