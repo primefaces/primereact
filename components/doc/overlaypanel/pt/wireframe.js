02cd280de9f201e00e25095da10dce3e487debcf
habubey <habubey@outlook.com>
2023-05-10 15:19:22 +0300

    Refactor for wireframe cdn paths Dialog and Overlaypanel

--- components/doc/overlaypanel/pt/wireframe.js
+++ components/doc/overlaypanel/pt/wireframe.js
@@ -6,7 +6,7 @@ export const Wireframe = (props) => {
         <>
             <DocSectionText {...props} />
             <div>
-                <img className="w-full" src="/images/pt/wireframe-placeholder.jpg" alt="overlaypanel" />
+                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/wireframe-placeholder.jpg" alt="overlaypanel" />
             </div>
         </>
     );
