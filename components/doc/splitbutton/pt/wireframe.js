34f74fbdf28fe08b0576a45ca343d29f03a4e477
Ulaş Turan <ulasturan06@hotmail.com>
2023-05-11 17:47:43 +0300

    Refactor wireframes

--- components/doc/splitbutton/pt/wireframe.js
+++ components/doc/splitbutton/pt/wireframe.js
@@ -6,7 +6,7 @@ export const Wireframe = (props) => {
         <>
             <DocSectionText {...props} />
             <div>
-                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/wireframe-placeholder.jpg" alt="splitbutton" />
+                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/splitbutton.jpg" alt="splitbutton" />
             </div>
         </>
     );
