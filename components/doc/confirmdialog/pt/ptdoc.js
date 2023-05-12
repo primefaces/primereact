ae379a1f030b1381c325f223356c08c81dadb90c
mertsincan <sincan.mert@gmail.com>
2023-05-12 11:32:04 +0100

    Update ptdoc.js

--- components/doc/confirmdialog/pt/ptdoc.js
+++ components/doc/confirmdialog/pt/ptdoc.js
@@ -1,29 +1,47 @@
 import React, { useState } from 'react';
-import { DocSectionText } from '../../common/docsectiontext';
-import { DocSectionCode } from '../../common/docsectioncode';
-import { ConfirmDialog } from '../../../lib/confirmdialog/ConfirmDialog';
 import { Button } from '../../../lib/button/Button';
+import { ConfirmDialog } from '../../../lib/confirmdialog/ConfirmDialog';
+import { DocSectionCode } from '../../common/docsectioncode';
+import { DocSectionText } from '../../common/docsectiontext';
 
 export function PTDoc(props) {
     const [visible, setVisible] = useState(false);
 
     const code = {
         basic: `
-<ConfirmDialog 
-pt={{ headerTitle: { className: 'text-primary' } }} 
-visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?" header="Confirmation" icon="pi pi-exclamation-triangle" />
+<ConfirmDialog
+    visible={visible}
+    onHide={() => setVisible(false)}
+    message="Are you sure you want to proceed?"
+    header="Confirmation"
+    icon="pi pi-exclamation-triangle"
+    pt={{
+        headerTitle: {
+            className: 'text-primary'
+        }
+    }}
+/>
         `,
         javascript: `
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { ConfirmDialog } from 'primereact/confirmdialog';
 
 export default function PTDemo() {
     return (
         <>
-            <ConfirmDialog 
-            pt={{ headerTitle: { className: 'text-primary' } }} 
-            visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?" header="Confirmation" icon="pi pi-exclamation-triangle" />
+            <ConfirmDialog
+                visible={visible}
+                onHide={() => setVisible(false)}
+                message="Are you sure you want to proceed?"
+                header="Confirmation"
+                icon="pi pi-exclamation-triangle"
+                pt={{
+                    headerTitle: {
+                        className: 'text-primary'
+                    }
+                }}
+            />
             <div className="card flex justify-content-center">
                 <Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
             </div>
@@ -32,16 +50,25 @@ export default function PTDemo() {
 }
         `,
         typescript: `
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { ConfirmDialog } from 'primereact/confirmdialog';
 
 export default function PTDemo() {
     return (
         <>
-            <ConfirmDialog 
-            pt={{ headerTitle: { className: 'text-primary' } }} 
-            visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to proceed?" header="Confirmation" icon="pi pi-exclamation-triangle" />
+            <ConfirmDialog
+                visible={visible}
+                onHide={() => setVisible(false)}
+                message="Are you sure you want to proceed?"
+                header="Confirmation"
+                icon="pi pi-exclamation-triangle"
+                pt={{
+                    headerTitle: {
+                        className: 'text-primary'
+                    }
+                }}
+            />
             <div className="card flex justify-content-center">
                 <Button onClick={() => setVisible(true)} icon="pi pi-check" label="Confirm" />
             </div>
