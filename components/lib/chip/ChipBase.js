503f4040bbe603226d68233aaaa15b9dd44c2d92
habubey <habubey@outlook.com>
2023-07-06 08:52:33 +0300

    Refactor #4602 - for Chip

--- components/lib/chip/ChipBase.js
+++ components/lib/chip/ChipBase.js
@@ -1,4 +1,5 @@
 import { ComponentBase } from '../componentbase/ComponentBase';
+import { classNames } from '../utils/utils';
 
 export const ChipBase = ComponentBase.extend({
     defaultProps: {
@@ -15,5 +16,43 @@ export const ChipBase = ComponentBase.extend({
         onImageError: null,
         onRemove: null,
         children: undefined
+    },
+    css: {
+        classes: {
+            root: ({ props }) =>
+                classNames(
+                    'p-chip p-component',
+                    {
+                        'p-chip-image': props.image != null
+                    },
+                    props.className
+                ),
+            removeIcon: 'p-chip-remove-icon',
+            icon: 'p-chip-icon',
+            label: 'p-chip-text'
+        },
+        styles: `
+        .p-chip {
+            display: inline-flex;
+            align-items: center;
+        }
+        
+        .p-chip-text {
+            line-height: 1.5;
+        }
+        
+        .p-chip-icon.pi {
+            line-height: 1.5;
+        }
+        
+        .p-chip .p-chip-remove-icon {
+            line-height: 1.5;
+            cursor: pointer;
+        }
+        
+        .p-chip img {
+            border-radius: 50%;
+        }
+        `
     }
 });
