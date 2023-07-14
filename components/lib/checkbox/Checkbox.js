84a1ad99b0f8a53072c7b2fb4587971e128ce022
habubey <habubey@outlook.com>
2023-07-14 14:37:15 +0200

    Refactor #4602 - for Checkbox

--- components/lib/checkbox/Checkbox.js
+++ components/lib/checkbox/Checkbox.js
@@ -1,20 +1,33 @@
 import * as React from 'react';
-import { useMountEffect, useUpdateEffect } from '../hooks/Hooks';
+import { PrimeReactContext } from '../api/Api';
+import { useMountEffect, useStyle, useUpdateEffect } from '../hooks/Hooks';
 import { CheckIcon } from '../icons/check';
 import { Tooltip } from '../tooltip/Tooltip';
-import { DomHandler, IconUtils, ObjectUtils, classNames, mergeProps } from '../utils/Utils';
+import { DomHandler, IconUtils, ObjectUtils, mergeProps } from '../utils/Utils';
 import { CheckboxBase } from './CheckboxBase';
-import { PrimeReactContext } from '../api/Api';
 
 export const Checkbox = React.memo(
     React.forwardRef((inProps, ref) => {
         const context = React.useContext(PrimeReactContext);
         const props = CheckboxBase.getProps(inProps, context);
         const [focusedState, setFocusedState] = React.useState(false);
-        const { ptm } = CheckboxBase.setMetaData({
+
+        const isChecked = () => {
+            return props.checked === props.trueValue;
+        };
+
+        const checked = isChecked();
+
+        useStyle(CheckboxBase.css.styles, { name: 'primereact_checkbox_style' });
+
+        const { ptm, cx } = CheckboxBase.setMetaData({
             props,
             state: {
                 focused: focusedState
+            },
+            instance: {
+                checked,
+                focusedState
             }
         });
         const elementRef = React.useRef(null);
@@ -82,10 +95,6 @@ export const Checkbox = React.memo(
             }
         };
 
-        const isChecked = () => {
-            return props.checked === props.trueValue;
-        };
-
         React.useImperativeHandle(ref, () => ({
             props,
             focus: () => DomHandler.focus(inputRef.current),
@@ -107,28 +116,12 @@ export const Checkbox = React.memo(
             }
         });
 
-        const checked = isChecked();
         const hasTooltip = ObjectUtils.isNotEmpty(props.tooltip);
         const otherProps = CheckboxBase.getOtherProps(props);
         const ariaProps = ObjectUtils.reduceKeys(otherProps, DomHandler.ARIA_PROPS);
-        const className = classNames(
-            'p-checkbox p-component',
-            {
-                'p-checkbox-checked': checked,
-                'p-checkbox-disabled': props.disabled,
-                'p-checkbox-focused': focusedState
-            },
-            props.className
-        );
-        const boxClass = classNames('p-checkbox-box', {
-            'p-highlight': checked,
-            'p-disabled': props.disabled,
-            'p-focus': focusedState
-        });
-        const iconClassName = 'p-checkbox-icon p-c';
         const iconProps = mergeProps(
             {
-                className: iconClassName
+                className: cx('icon')
             },
             ptm('icon')
         );
@@ -138,7 +131,7 @@ export const Checkbox = React.memo(
             {
                 ref: elementRef,
                 id: props.id,
-                className,
+                className: cx('root', { checked, focusedState }),
                 style: props.style,
                 onClick: (e) => onClick(e),
                 onContextMenu: props.onContextMenu,
@@ -150,7 +143,7 @@ export const Checkbox = React.memo(
 
         const hiddenInputWrapperProps = mergeProps(
             {
-                className: 'p-hidden-accessible'
+                className: cx('hiddenInputWrapper')
             },
             ptm('hiddenInputWrapper')
         );
@@ -176,7 +169,7 @@ export const Checkbox = React.memo(
 
         const inputProps = mergeProps(
             {
-                className: boxClass
+                className: cx('input', { checked, focusedState })
             },
             ptm('input')
         );
