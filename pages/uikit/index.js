8da846e5ed93bdf62091ad5114e936af1b115a43
habubey <habubey@outlook.com>
2023-07-31 13:41:58 +0300

    PrimeOne version updates for UI Kit page

--- pages/uikit/index.js
+++ pages/uikit/index.js
@@ -41,9 +41,13 @@ const UIKitPage = (props) => {
                                         <i className="pi pi-check-circle text-green-600 mr-2"></i>
                                         <span className="font-bold">Interactive Components</span>
                                     </li>
-                                    <li className="flex align-items-center w-12 p-3">
+                                    <li className="flex align-items-center w-6 p-3">
                                         <i className="pi pi-check-circle text-green-600 mr-2"></i>
                                         <span className="font-bold">Boolean, Instance Swap and Text Properties</span>
+                                    </li>
+                                    <li className="flex align-items-center w-6 p-3">
+                                        <i className="pi pi-check-circle text-green-600 mr-2"></i>
+                                        <span className="font-bold">Nested Instances</span>
                                     </li>
                                 </ul>
                             </div>
@@ -77,7 +81,7 @@ const UIKitPage = (props) => {
                                 <img alt="PrimeReact Designer" src="https://primefaces.org/cdn/primereact/images/uikit/uikit-system.png" className="w-full" />
                             </div>
                         </div>
-                        <div className="flex flex-column md:flex-row align-items-center gap-4 md:gap-8">
+                        <div className="flex flex-column md:flex-row align-items-center gap-4 md:gap-8 mb-8">
                             <div className="w-full md:w-6">
                                 <img alt="PrimeReact Designer" src="https://primefaces.org/cdn/primereact/images/uikit/uikit-themes.png" className="w-full" />
                             </div>
@@ -98,24 +102,51 @@ const UIKitPage = (props) => {
                                 </ul>
                             </div>
                         </div>
+                        <div className="flex flex-column md:flex-row align-items-center gap-4 md:gap-8">
+                            <div className="w-full md:w-6">
+                                <div className="text-primary font-bold mb-2">TOKENS STUDIO</div>
+                                <div className="text-900 text-5xl font-bold mb-3">Tokens Support</div>
+                                <p className="mb-3 text-lg">
+                                    Empower yourself with unprecedented control over your designs. Tokens Studio integration unlocks a whole new level of flexibility, allowing you to create and manage design tokens seamlessly.
+                                </p>
+
+                                <ul className="flex flex-wrap m-0 p-0 text-lg">
+                                    <li className="flex align-items-center w-6 p-3">
+                                        <i className="pi pi-check-circle text-green-600 mr-2"></i>
+                                        <span className="font-bold">Countless Design Tokens</span>
+                                    </li>
+                                    <li className="flex align-items-center w-6 p-3">
+                                        <i className="pi pi-check-circle text-green-600 mr-2"></i>
+                                        <span className="font-bold">Light and Dark Sets</span>
+                                    </li>
+                                    <li className="flex align-items-center w-12 p-3">
+                                        <i className="pi pi-check-circle text-green-600 mr-2"></i>
+                                        <span className="font-bold">Well Documented</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="w-full md:w-6">
+                                <img alt="Tokens Support" src="https://primefaces.org/cdn/primeng/images/uikit/uikit-tokens.png" className="w-full" />
+                            </div>
+                        </div>
                     </div>
 
                     <div className="flex flex-column md:flex-row gap-4 md:gap-8 mb-5">
                         <a
-                            href="https://www.figma.com/file/1rMJ9BAiZovyQYNUG06T0j/Preview-%7C-PrimeOne-%7C-2.0?node-id=806%3A36648&t=MnxlHT9snz8Y6DwL-1"
+                            href="https://www.figma.com/file/OBMr8vZAodP9RsQR2XYPHh/Preview-%7C-PrimeOne-%7C-2.1?type=design&node-id=806%3A36648&mode=design&t=WvG6UHUZIPhmEx0d-1"
                             className="p-5 w-full md:w-6 bg-white flex flex-column align-items-center border-2 border-transparent hover:border-primary transition-colors transition-duration-300"
                             style={{ borderRadius: '50px' }}
                         >
                             <span className="text-gray-900 text-4xl font-bold mb-5">Preview Light</span>
-                            <img alt="PrimeReact Designer" src="https://primefaces.org/cdn/primereact/images/uikit/logo-figma-light.svg" className="w-4rem" />
+                            <img alt="Figma Light" src="https://primefaces.org/cdn/primereact/images/uikit/logo-figma-light.svg" className="w-4rem" />
                         </a>
                         <a
-                            href="https://www.figma.com/file/lwsadx20eWq3UWS4eAxCYo/Preview-%7C-Dark-%7C-PrimeOne-%7C-2.0?node-id=806%3A36648&t=Qw2Pi6W4Q4rtbnZ5-1"
+                            href="https://www.figma.com/file/26zWEQAb8MuS6lGIhJQXE6/Preview-%7C-Dark-%7C-PrimeOne-%7C-2.1?type=design&node-id=806%3A36648&mode=design&t=SPpYKnTpkKLsk0qA-1"
                             className="p-5 w-full md:w-6 bg-gray-900 flex flex-column align-items-center border-2 border-transparent hover:border-primary transition-colors transition-duration-300"
                             style={{ borderRadius: '50px' }}
                         >
                             <span className="text-white text-4xl font-bold mb-5">Preview Dark</span>
-                            <img alt="PrimeReact Designer" src="https://primefaces.org/cdn/primereact/images/uikit/logo-figma-dark.svg" className="w-4rem" />
+                            <img alt="Figma Dark" src="https://primefaces.org/cdn/primereact/images/uikit/logo-figma-dark.svg" className="w-4rem" />
                         </a>
                     </div>
 
@@ -149,6 +180,10 @@ const UIKitPage = (props) => {
                                             <li className="flex align-items-center mb-3">
                                                 <i className="pi pi-check-circle text-green-500 mr-2"></i>
                                                 <span>Interactive Components</span>
+                                            </li>
+                                            <li class="flex align-items-center mb-3">
+                                                <i className="pi pi-check-circle text-green-500 mr-2"></i>
+                                                <span>Tokens Studio Support</span>
                                             </li>
                                             <li className="flex align-items-center mb-3">
                                                 <i className="pi pi-check-circle text-green-500 mr-2"></i>
@@ -190,6 +225,10 @@ const UIKitPage = (props) => {
                                                 <i className="pi pi-check-circle text-green-500 mr-2"></i>
                                                 <span>Interactive Components</span>
                                             </li>
+                                            <li class="flex align-items-center mb-3">
+                                                <i className="pi pi-check-circle text-green-500 mr-2"></i>
+                                                <span>Tokens Studio Support</span>
+                                            </li>
                                             <li className="flex align-items-center mb-3">
                                                 <i className="pi pi-check-circle text-green-500 mr-2"></i>
                                                 <span>Lifetime Support</span>
@@ -229,6 +268,10 @@ const UIKitPage = (props) => {
                                             <li className="flex align-items-center mb-3">
                                                 <i className="pi pi-check-circle text-green-500 mr-2"></i>
                                                 <span>Interactive Components</span>
+                                            </li>
+                                            <li class="flex align-items-center mb-3">
+                                                <i className="pi pi-check-circle text-green-500 mr-2"></i>
+                                                <span>Tokens Studio Support</span>
                                             </li>
                                             <li className="flex align-items-center mb-3">
                                                 <i className="pi pi-check-circle text-green-500 mr-2"></i>
