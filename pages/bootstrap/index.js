6c4abaaad68c686fb932e215411cd193e705728f
habubey <habubey@outlook.com>
2023-08-10 17:08:55 +0300

    Add Bootstrap Docs Page

---
+++ pages/bootstrap/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ExampleDoc } from '../../components/doc/bootstrap/exampledoc';
+import { SetupDoc } from '../../components/doc/bootstrap/setupdoc';
+import { DocComponent } from '../../components/doc/common/doccomponent';
+
+const TerminalDemo = () => {
+    const docs = [
+        {
+            id: 'setup',
+            label: 'Setup',
+            component: SetupDoc
+        },
+
+        {
+            id: 'example',
+            label: 'Example',
+            component: ExampleDoc
+        }
+    ];
+
+    const description = `
+    <a href="https://getbootstrap.com" class="font-semibold hover:underline text-primary">Bootstrap</a>
+    is a well-known CSS library to build responsive and mobile first web projects.
+    `;
+
+    return <DocComponent title="Bootstrap - PrimeReact" header="Bootstrap" description={description} componentDocs={docs} hideTabMenu></DocComponent>;
+};
+
+export default TerminalDemo;
