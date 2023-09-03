43d4da4b7897d9d20da50200cae733705b41d141
Diego <diegofranchina@gmail.com>
2023-09-03 13:41:34 +0200

    refactor formatSize and added localeOption fileupload

--- components/lib/fileupload/FileUpload.js
+++ components/lib/fileupload/FileUpload.js
@@ -99,16 +99,18 @@ export const FileUpload = React.memo(
         };
 
         const formatSize = (bytes) => {
+            const k = 1024
+            const dm = 3
+            const sizes = localeOption('fileSizeTypes')
+
             if (bytes === 0) {
                 return '0 B';
             }
 
-            let k = 1000,
-                dm = 3,
-                sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-                i = Math.floor(Math.log(bytes) / Math.log(k));
-
-            return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+            const i = Math.floor(Math.log(bytes) / Math.log(k));
+            const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+                
+            return `${formattedSize} ${sizes[i]}`;
         };
 
         const onFileSelect = (event) => {
