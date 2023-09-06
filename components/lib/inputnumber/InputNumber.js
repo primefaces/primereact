0f08c4b9fe0efe7c12ae129a43ba95274c765aba
Akshay Antony <63507009+akshayantony55@users.noreply.github.com>
2023-09-06 18:29:46 +0530

    Fix primefaces #4875 InputNumber: Doesn't allow minus sign in currency mode (#4881)
    * Fix primefaces #4875 InputNumber: Doesn't allow minus sign in currency mode
    
    * Fix primefaces #4875 InputNumber: Doesn't allow minus sign in currency mode
    
    * Fix primefaces #4875 InputNumber: Doesn't allow minus sign in currency mode
    
    * Fix primefaces #4875 InputNumber: Doesn't allow minus sign in currency mode
    
    * Update components/lib/inputnumber/InputNumber.js
    
    Co-authored-by: Melloware <mellowaredev@gmail.com>
    
    ---------
    
    Co-authored-by: akshayaqburst <123446355+akshayaqburst@users.noreply.github.com>
    Co-authored-by: Melloware <mellowaredev@gmail.com>

--- components/lib/inputnumber/InputNumber.js
+++ components/lib/inputnumber/InputNumber.js
@@ -510,7 +510,7 @@ export const InputNumber = React.memo(
         };
 
         const allowMinusSign = () => {
-            return props.min === null || props.min < 0;
+            return ObjetUtils.isEmpty(props.min) || props.min < 0;
         };
 
         const isMinusSign = (char) => {
