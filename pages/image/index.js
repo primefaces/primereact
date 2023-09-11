857bf7f07a14b1af5da1b21719a287d2b18559a4
habubey <habubey@outlook.com>
2023-09-11 10:40:37 +0300

    Add Tailwind Image Panel

--- pages/image/index.js
+++ pages/image/index.js
@@ -1,13 +1,15 @@
 import DocApiTable from '../../components/doc/common/docapitable';
-import { PTDoc } from '../../components/doc/image/pt/ptdoc';
-import { Wireframe } from '../../components/doc/image/pt/wireframe';
 import { DocComponent } from '../../components/doc/common/doccomponent';
 import { AccessibilityDoc } from '../../components/doc/image/accessibilitydoc';
 import { BasicDoc } from '../../components/doc/image/basicdoc';
 import { ImportDoc } from '../../components/doc/image/importdoc';
 import { PreviewDoc } from '../../components/doc/image/previewdoc';
+import { PTDoc } from '../../components/doc/image/pt/ptdoc';
+import { Wireframe } from '../../components/doc/image/pt/wireframe';
 import { StyleDoc } from '../../components/doc/image/styledoc';
 import { TemplateDoc } from '../../components/doc/image/templatedoc';
+import { StyledDoc } from '../../components/doc/image/theming/styleddoc';
+import { TailwindDoc } from '../../components/doc/image/theming/tailwinddoc';
 import { ThumbnailDoc } from '../../components/doc/image/thumbnaildoc';
 
 const ImageDemo = () => {
@@ -66,7 +68,27 @@ const ImageDemo = () => {
         }
     ];
 
-    return <DocComponent title="React Image Component" header="Image" description="Displays a single image with preview and tranformation options." componentDocs={docs} apiDocs={['Image']} ptDocs={ptDocs} />;
+    const themingDocs = [
+        {
+            id: 'styled',
+            label: 'Styled',
+            component: StyledDoc
+        },
+        {
+            id: 'unstyled',
+            label: 'Unstyled',
+            description: 'Theming is implemented with the pass through properties in unstyled mode.',
+            children: [
+                {
+                    id: 'tailwind',
+                    label: 'Tailwind',
+                    component: TailwindDoc
+                }
+            ]
+        }
+    ];
+
+    return <DocComponent title="React Image Component" header="Image" description="Displays a single image with preview and tranformation options." componentDocs={docs} apiDocs={['Image']} ptDocs={ptDocs} themingDocs={themingDocs} />;
 };
 
 export default ImageDemo;
