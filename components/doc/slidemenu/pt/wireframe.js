11584b29d4cc47c2c4bee77e94426941d897f8cd
habubey <habubey@outlook.com>
2023-09-20 17:38:56 +0300

    Add Tailwind demos for necessary Menu components Wireframe path fix

--- components/doc/slidemenu/pt/wireframe.js
+++ components/doc/slidemenu/pt/wireframe.js
@@ -6,7 +6,7 @@ export const Wireframe = (props) => {
         <>
             <DocSectionText {...props} />
             <div>
-                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/slidemenu.jpg" alt="slidemenu" />
+                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/wireframe-placeholder.jpg" alt="slidemenu" />
             </div>
         </>
     );
