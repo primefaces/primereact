e306a7a3d71edcc4277bcb178671c8953878c0fb
habubey <habubey@outlook.com>
2023-10-03 17:52:49 +0300

    Refactor necessary Media tailwind demos

--- components/doc/carousel/theming/tailwinddoc.js
+++ components/doc/carousel/theming/tailwinddoc.js
@@ -96,17 +96,17 @@ export default function UnstyledDemo() {
 
     const productTemplate = (product) => {
         return (
-            <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
+            <div className="border bg-white dark:bg-gray-900 border-gray-300 dark:border-blue-900/40 rounded-lg m-2 text-center py-5 px-3">
                 <div className="mb-3">
-                    <img src={\`https://primefaces.org/cdn/primereact/images/product/\${product.image}\`} alt={product.name} className="w-6 shadow-2" />
+                    <img src={\`https://primefaces.org/cdn/primereact/images/product/\${product.image}\`} alt={product.name} className="w-1/2 shadow-sm" />
                 </div>
                 <div>
-                    <h4 className="mb-1">{product.name}</h4>
-                    <h6 className="mt-0 mb-3">\${product.price}</h6>
+                    <h4 className="mb-1 text-gray-700 dark:text-white/80">{product.name}</h4>
+                    <h6 className="mt-0 mb-3 text-gray-700 dark:text-white/80">\${product.price}</h6>
                     <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
                     <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
-                        <Button icon="pi pi-search" rounded />
-                        <Button icon="pi pi-star-fill" rounded severity="success" />
+                        <Button icon="pi pi-search" rounded className="mr-2 text-gray-700 dark:text-white/80 inline-flex"/>
+                        <Button icon="pi pi-star-fill" rounded severity="success" className="mr-2 text-gray-700 dark:text-white/80 inline-flex"/>
                     </div>
                 </div>
             </div>
