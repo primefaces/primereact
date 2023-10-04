a97f994fd3c9cff07fd637d285a880ecebad6a58
Melloware <mellowaredev@gmail.com>
2023-10-04 07:51:27 -0400

    Update componentbase.d.ts

--- components/lib/componentbase/componentbase.d.ts
+++ components/lib/componentbase/componentbase.d.ts
@@ -4,6 +4,12 @@ export interface ComponentHooks {
     useUnmountEffect?(): void;
 }
 
+/**
+ * Custom hook to use to load style options for a component.
+ * @param {string} styles - The style text content.
+ * @param {boolean} isUnstyled - Flag removes component related styles in the core.
+ * @param config - The component configuration.
+ */
 export declare function useHandleStyle(styles: string, isUnstyled: () => boolean, config: { name: string; styled?: boolean; hostName?: string }): void;
 
 export interface ComponentBasePassThroughOptions {
