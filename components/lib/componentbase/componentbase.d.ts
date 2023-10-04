512dd1127af5b79d189087acda695e606a3f512d
Jared Scott <jared.scott@variable.team>
2023-10-04 15:13:50 +0800

    chore: Remove unnecessary newline

--- components/lib/componentbase/componentbase.d.ts
+++ components/lib/componentbase/componentbase.d.ts
@@ -4,8 +4,7 @@ export interface ComponentHooks {
     useUnmountEffect?(): void;
 }
 
-
-export declare function useHandleStyle(styles: string, isUnstyled: () => boolean, config: { name: string; styled?: boolean; hostName?: string; }): void;
+export declare function useHandleStyle(styles: string, isUnstyled: () => boolean, config: { name: string; styled?: boolean; hostName?: string }): void;
 
 export interface ComponentBasePassThroughOptions {
     hooks?: ComponentHooks;
