2c201df8ae9b7ed8151bb3cc71ef0b9e7f59a7a4
Cagatay Civici <cagatay.civici@gmail.com>
2023-10-11 12:34:08 +0300

    Refactor setup doc

--- components/doc/tailwind/unstyledmode/setupdoc.js
+++ components/doc/tailwind/unstyledmode/setupdoc.js
@@ -6,6 +6,7 @@ export function SetupDoc(props) {
     const code1 = {
         basic: `
 module.exports = {
+    ...
     content: [
         "./index.html",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,11 +17,7 @@ module.exports = {
         // Or if using \`\src\` directory:
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
       ],
-      darkMode: "class",
-      theme: {
-        extend: {},
-      },
-      plugins: [],
+      ...
     }
 `
     };
@@ -42,63 +39,59 @@ return(
         basic: `
 import { PrimeReactProvider } from "primereact/api";
 
-export default function MyApp({ Component }) {
+export default function MyApp({ Component, pageProps }) {
     
     //My Design System with Tailwind
-    const MyDesignSystem = usePassThrough(
-        Tailwind,
-        {
-            inputtext: {
-                root: ({ props, context }) => ({
-                    className: classNames(
-                        'm-0',
-                        'font-sans text-gray-600 dark:text-white/80 bg-white dark:bg-gray-900 border border-gray-300 dark:border-blue-900/40 transition-colors duration-200 appearance-none rounded-lg',
-                        {
-                            'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]': !context.disabled,
-                            'opacity-60 select-none pointer-events-none cursor-default': context.disabled
-                        },
-                        {
-                            'text-lg px-4 py-4': props.size == 'large',
-                            'text-xs px-2 py-2': props.size == 'small',
-                            'p-3 text-base': props.size == null
-                        }
-                    )
-                })
-            },
-            panel: {
-                header: ({ props }) => ({
-                    className: classNames(
-                        'flex items-center justify-between', // flex and alignments
-                        'border border-gray-300 bg-gray-100 text-gray-700 rounded-tl-lg rounded-tr-lg', // borders and colors
-                        'dark:bg-gray-900 dark:border-blue-900/40 dark:text-white/80', // Dark mode
-                        { 'p-5': !props.toggleable, 'py-3 px-5': props.toggleable } // condition
-                    )
-                }),
-                title: 'leading-none font-bold',
-                toggler: {
-                    className: classNames(
-                        'inline-flex items-center justify-center overflow-hidden relative no-underline', // alignments
-                        'w-8 h-8 text-gray-600 border-0 bg-transparent rounded-full transition duration-200 ease-in-out', // widths, borders, and transitions
-                        'hover:text-gray-900 hover:border-transparent hover:bg-gray-200 dark:hover:text-white/80 dark:hover:bg-gray-800/80 dark:focus:shadow-[inset_0_0_0_0.2rem_rgba(147,197,253,0.5)]', // hover
-                        'focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]' // focus
-                    )
-                },
-                togglerIcon: 'inline-block',
-                content: {
-                    className: classNames(
-                        'p-5 border border-gray-300 bg-white text-gray-700 border-t-0 last:rounded-br-lg last:rounded-bl-lg',
-                        'dark:bg-gray-900 dark:border-blue-900/40 dark:text-white/80' // Dark mode
-                    )
-                },
-                transition: TRANSITIONS.toggleable
-            } 
+    const MyDesignSystem = {
+        inputtext: {
+            root: ({ props, context }) => ({
+                className: classNames(
+                    'm-0',
+                    'font-sans text-gray-600 dark:text-white/80 bg-white dark:bg-gray-900 border border-gray-300 dark:border-blue-900/40 transition-colors duration-200 appearance-none rounded-lg',
+                    {
+                        'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]': !context.disabled,
+                        'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                    },
+                    {
+                        'text-lg px-4 py-4': props.size == 'large',
+                        'text-xs px-2 py-2': props.size == 'small',
+                        'p-3 text-base': props.size == null
+                    }
+                )
+            })
         },
-        { mergeSections: true, mergeProps: false }
-    );
+        panel: {
+            header: ({ props }) => ({
+                className: classNames(
+                    'flex items-center justify-between', // flex and alignments
+                    'border border-gray-300 bg-gray-100 text-gray-700 rounded-tl-lg rounded-tr-lg', // borders and colors
+                    'dark:bg-gray-900 dark:border-blue-900/40 dark:text-white/80', // Dark mode
+                    { 'p-5': !props.toggleable, 'py-3 px-5': props.toggleable } // condition
+                )
+            }),
+            title: 'leading-none font-bold',
+            toggler: {
+                className: classNames(
+                    'inline-flex items-center justify-center overflow-hidden relative no-underline', // alignments
+                    'w-8 h-8 text-gray-600 border-0 bg-transparent rounded-full transition duration-200 ease-in-out', // widths, borders, and transitions
+                    'hover:text-gray-900 hover:border-transparent hover:bg-gray-200 dark:hover:text-white/80 dark:hover:bg-gray-800/80 dark:focus:shadow-[inset_0_0_0_0.2rem_rgba(147,197,253,0.5)]', // hover
+                    'focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]' // focus
+                )
+            },
+            togglerIcon: 'inline-block',
+            content: {
+                className: classNames(
+                    'p-5 border border-gray-300 bg-white text-gray-700 border-t-0 last:rounded-br-lg last:rounded-bl-lg',
+                    'dark:bg-gray-900 dark:border-blue-900/40 dark:text-white/80' // Dark mode
+                )
+            },
+            transition: TRANSITIONS.toggleable
+        } 
+    };
 
     return (
         <PrimeReactProvider value={{ unstyled: true, pt: MyDesignSystem }}>
-            <App />
+            <Component {...pageProps} />
         </PrimeReactProvider>
     );
 }
