dda3e1803ab07ddafc9cb01e924471d1948263c7
habubey <habubey@outlook.com>
2023-10-11 12:18:37 +0300

    Updated tailwind setup doc

--- components/doc/tailwind/unstyledmode/setupdoc.js
+++ components/doc/tailwind/unstyledmode/setupdoc.js
@@ -7,10 +7,12 @@ export function SetupDoc(props) {
         basic: `
 module.exports = {
     content: [
+        "./index.html",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
-   
+        "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
+
         // Or if using \`\src\` directory:
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
       ],
