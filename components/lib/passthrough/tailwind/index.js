0ba3dbd418808cdfde10104810f1fdd9352b3fb6
mertsincan <sincan.mert@gmail.com>
2023-10-19 09:00:51 +0100

    Fixed #5117 - Overlay animations are not working with Tailwind preset

--- components/lib/passthrough/tailwind/index.js
+++ components/lib/passthrough/tailwind/index.js
@@ -11,11 +11,12 @@ export const TRANSITIONS = {
         }
     },
     overlay: {
+        timeout: 150,
         classNames: {
             enter: 'opacity-0 scale-75',
-            enterActive: 'transition-transform transition-opacity duration-150 ease-in',
-            exitActive: 'transition-opacity duration-150 ease-linear',
-            exitDone: 'opacity-0'
+            enterActive: 'opacity-100 !scale-100 transition-transform transition-opacity duration-150 ease-in',
+            exit: 'opacity-100',
+            exitActive: '!opacity-0 transition-opacity duration-150 ease-linear'
         }
     }
 };
