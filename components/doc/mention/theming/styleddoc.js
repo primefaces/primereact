d79ce90fd163aed009740708be89e2be2100cf6c
gucal <gucalaliriza@outlook.com>
2023-10-23 16:43:18 +0300

    Create theming tab for Mention

---
+++ components/doc/mention/theming/styleddoc.js
@@ -0,0 +1,44 @@
+
+import Link from 'next/link';
+import { DocSectionText } from '../../common/docsectiontext';
+
+export function StyledDoc() {
+    return (
+        <>
+            <DocSectionText id="style" label="Style">
+                <p>
+                    Following is the list of structural style classes, for theming classes visit <Link href="/theming">theming</Link> page.
+                </p>
+            </DocSectionText>
+            <div className="doc-tablewrapper">
+                <table className="doc-table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Element</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>p-mention</td>
+                            <td>Container element</td>
+                        </tr>
+                        <tr>
+                            <td>p-mention-panel</td>
+                            <td>Overlay panel of suggestions.</td>
+                        </tr>
+                        <tr>
+                            <td>p-mention-items</td>
+                            <td>List container of suggestions.</td>
+                        </tr>
+                        <tr>
+                            <td>p-mention-item</td>
+                            <td>List item of a suggestion.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+}
+
