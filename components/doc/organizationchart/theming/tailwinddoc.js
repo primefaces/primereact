27208eeeda7538170ed18ba84ec4bb9d30a5dfa5
gucal <gucalaliriza@outlook.com>
2023-10-24 17:25:07 +0300

    orgchart theming demo

--- components/doc/organizationchart/theming/tailwinddoc.js
+++ components/doc/organizationchart/theming/tailwinddoc.js
@@ -59,62 +59,65 @@ import React, { useState } from 'react';
 import { OrganizationChart } from 'primereact/organizationchart';
 
 export default function UnstyledDemo() {
-    const [data] = useState([
+  const [data] = useState([
+    {
+      label: 'Argentina',
+      expanded: true,
+      data: 'ar',
+      children: [
         {
-            label: 'Argentina',
-            expanded: true,
-            data: 'ar',
-            children: [
-                {
-                    label: 'Argentina',
-                    expanded: true,
-                    data: 'ar',
-                    children: [
-                        {
-                            label: 'Argentina',
-                            data: 'ar'
-                        },
-                        {
-                            label: 'Croatia',
-                            data: 'hr'
-                        }
-                    ]
-                },
-                {
-                    label: 'France',
-                    expanded: true,
-                    data: 'fr',
-                    children: [
-                        {
-                            label: 'France',
-                            data: 'fr'
-                        },
-                        {
-                            label: 'Morocco',
-                            data: 'ma'
-                        }
-                    ]
-                }
-            ]
-        }
-    ]);
+          label: 'Argentina',
+          expanded: true,
+          data: 'ar',
+          children: [
+            {
+              label: 'Argentina',
+              data: 'ar',
+            },
+            {
+              label: 'Croatia',
+              data: 'hr',
+            },
+          ],
+        },
+        {
+          label: 'France',
+          expanded: true,
+          data: 'fr',
+          children: [
+            {
+              label: 'France',
+              data: 'fr',
+            },
+            {
+              label: 'Morocco',
+              data: 'ma',
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-    const nodeTemplate = (node) => {
-        return (
-            <div className="flex flex-column align-items-center">
-                <img alt={node.label} src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png" className={\`w-2rem shadow-2 flag flag-\${node.data}\`} />
-                <div className="mt-3 font-medium text-lg">{node.label}</div>
-            </div>
-        );
-    };
-
+  const nodeTemplate = (node) => {
     return (
-        <div className="card overflow-x-auto">
-            <OrganizationChart value={data} nodeTemplate={nodeTemplate} />
-        </div>
-    )
-}
-    `
+      <div className="flex flex-col items-center">
+        <img
+          alt={node.label}
+          src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png"
+          className={\`w-8 shadow-md flag flag-\${node.data}\`}
+        />
+        <div className="mt-3 font-medium text-lg">{node.label}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="card overflow-x-auto">
+      <OrganizationChart value={data} nodeTemplate={nodeTemplate} />
+    </div>
+  );
+}`
     };
 
     return (
