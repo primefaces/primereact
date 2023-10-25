f4a7d88455194bc0702d345d2c5936bd783055f0
gucal <gucalaliriza@outlook.com>
2023-10-25 12:12:06 +0300

    Datascroller: Add Tailwind demo

--- components/doc/datascroller/theming/tailwinddoc.js
+++ components/doc/datascroller/theming/tailwinddoc.js
@@ -47,28 +47,26 @@ export default function UnstyledDemo() {
 
     const itemTemplate = (data) => {
         return (
-            <div className="col-12">
-                <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-                    <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={\`https://primefaces.org/cdn/primereact/images/product/\${data.image}\`} alt={data.name} />
-                    <div className="flex flex-column lg:flex-row justify-content-between align-items-center xl:align-items-start lg:flex-1 gap-4">
-                        <div className="flex flex-column align-items-center lg:align-items-start gap-3">
-                            <div className="flex flex-column gap-1">
-                                <div className="text-2xl font-bold text-900">{data.name}</div>
-                                <div className="text-700">{data.description}</div>
-                            </div>
-                            <div className="flex flex-column gap-2">
-                                <Rating value={data.rating} readOnly cancel={false}></Rating>
-                                <span className="flex align-items-center gap-2">
-                                    <i className="pi pi-tag product-category-icon"></i>
-                                    <span className="font-semibold">{data.category}</span>
-                                </span>
-                            </div>
+            <div className="flex flex-col xl:flex-row xl:items-start p-4 gap-4">
+                <img className="w-3/4 sm:w-64 xl:w-40 shadow-2 block xl:block mx-auto border-round" src={\`https://primefaces.org/cdn/primereact/images/product/\${data.image}\`} alt={data.name} />
+                <div className="flex flex-col  lg:flex-row justify-between items-center xl:items-start lg:flex-1 gap-4">
+                    <div className="flex flex-col  items-center lg:items-start gap-3">
+                        <div className="flex flex-col  gap-1">
+                            <div className="text-2xl font-bold text-900">{data.name}</div>
+                            <div className="text-700">{data.description}</div>
                         </div>
-                        <div className="flex flex-row lg:flex-column align-items-center lg:align-items-end gap-4 lg:gap-2">
-                            <span className="text-2xl font-semibold">\${data.price}</span>
-                            <Button icon="pi pi-shopping-cart" label="Add to Cart" disabled={data.inventoryStatus === 'OUTOFSTOCK'}></Button>
-                            <Tag value={data.inventoryStatus} severity={getSeverity(data)}></Tag>
+                        <div className="flex flex-col  gap-2">
+                            <Rating value={data.rating} readOnly cancel={false}></Rating>
+                            <span className="flex items-center gap-2">
+                                <i className="pi pi-tag product-category-icon"></i>
+                                <span className="font-semibold">{data.category}</span>
+                            </span>
                         </div>
+                    </div>
+                    <div className="flex flex-row lg:flex-col  items-center lg:items-end gap-4 lg:gap-2">
+                        <span className="text-2xl font-semibold">\${data.price}</span>
+                        <Button icon="pi pi-shopping-cart" label="Add to Cart" disabled={data.inventoryStatus === 'OUTOFSTOCK'}></Button>
+                        <Tag value={data.inventoryStatus} severity={getSeverity(data)}></Tag>
                     </div>
                 </div>
             </div>
