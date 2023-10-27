51adeb484942909496cd7c3536231f301d8d6582
habubey <habubey@outlook.com>
2023-10-27 12:13:04 +0300

    Refactor Wirefreame links necessary doc pages

--- components/doc/editor/pt/wireframe.js
+++ components/doc/editor/pt/wireframe.js
@@ -6,7 +6,7 @@ export const Wireframe = (props) => {
         <>
             <DocSectionText {...props} />
             <div>
-                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/editor.jpg" alt="editor" />
+                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/wireframe-placeholder.jpg" alt="editor" />
             </div>
         </>
     );
--- components/doc/toast/pt/wireframe.js
+++ components/doc/toast/pt/wireframe.js
@@ -6,7 +6,7 @@ export const Wireframe = (props) => {
         <>
             <DocSectionText {...props} />
             <div>
-                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/toast.jpg" alt="toast" />
+                <img className="w-full" src="https://primefaces.org/cdn/primereact/images/pt/wireframe-placeholder.jpg" alt="toast" />
             </div>
         </>
     );
