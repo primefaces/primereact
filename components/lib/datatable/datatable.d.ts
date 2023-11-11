763584647c78052176e4aa3460efe9ac9a33fc13
Dalorzo <fdalorzo@msn.com>
2023-11-11 17:11:10 -0600

    Adding frozenRow to DataTableProps #5288

--- components/lib/datatable/datatable.d.ts
+++ components/lib/datatable/datatable.d.ts
@@ -1142,6 +1142,10 @@ interface DataTableBaseProps<TValue extends DataTableValueArray> extends Omit<Re
      */
     footerColumnGroup?: React.ReactNode | undefined;
     /**
+     * Whether the row is frozen or not.
+     */
+    frozenRow?: boolean;
+    /**
      * Items of the frozen part in scrollable DataTable.
      */
     frozenValue?: DataTableRowDataArray<TValue>;
