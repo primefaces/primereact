b6fc3df6b35bcaa3a417f58a8f6bfe446cf9e00d
navedqb <109583873+navedqb@users.noreply.github.com>
2023-11-15 19:39:20 +0530

    fix: strength meter popup does not close on escape (#5322)
    * Strength meter closes on escape key press
    
    * Update keyCode comparison to escape

--- components/lib/password/Password.js
+++ components/lib/password/Password.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import PrimeReact, { PrimeReactContext, localeOption } from '../api/Api';
 import { useHandleStyle } from '../componentbase/ComponentBase';
 import { CSSTransition } from '../csstransition/CSSTransition';
-import { useMountEffect, useOverlayListener, useUnmountEffect } from '../hooks/Hooks';
+import { useMountEffect, useOverlayListener, useUnmountEffect, useOnEscapeKey } from '../hooks/Hooks';
 import { EyeIcon } from '../icons/eye';
 import { EyeSlashIcon } from '../icons/eyeslash';
 import { InputText } from '../inputtext/InputText';
@@ -45,6 +45,10 @@ export const Password = React.memo(
         const { ptm, cx, isUnstyled } = PasswordBase.setMetaData(metaData);
 
         useHandleStyle(PasswordBase.css.styles, isUnstyled, { name: 'password' });
+
+        useOnEscapeKey(overlayRef, props.feedback, (event) => {
+            hide();
+        });
 
         const [bindOverlayListener, unbindOverlayListener] = useOverlayListener({
             target: elementRef,
@@ -160,7 +164,7 @@ export const Password = React.memo(
         };
 
         const onKeyup = (e) => {
-            let keyCode = e.keyCode || e.which;
+            const keyCode = e.code;
 
             if (props.feedback) {
                 let value = e.target.value;
@@ -201,7 +205,7 @@ export const Password = React.memo(
                 setMeterState(meter);
                 setInfoTextState(label);
 
-                if (!!keyCode && !overlayVisibleState) {
+                if (!!keyCode && keyCode !== 'Escape' && !overlayVisibleState) {
                     show();
                 }
             }
