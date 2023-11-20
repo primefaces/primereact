3b9986dc10c9bb7bf97bc06b3324f9423fc81bbb
Cagatay Civici <cagatay.civici@gmail.com>
2023-11-20 14:21:47 +0300

    Cleanup

--- components/doc/installation/styleddoc.js
+++ components/doc/installation/styleddoc.js
@@ -3,18 +3,9 @@ import { DocSectionText } from '@/components/doc/common/docsectiontext';
 import Link from 'next/link';
 
 export function StyledModeDoc(props) {
-    const code2 = {
+    const code = {
         basic: `
-body {
-    font-family: var(--font-family);
-}
-`
-    };
-
-    const code1 = {
-        basic: `
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 `
     };
 
@@ -25,9 +16,7 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
                     Styled mode is based on pre-skinned components with opinionated themes like Material, Bootstrap or PrimeOne themes. Theme is the required css file to be imported, visit the <Link href="/theming/#themes">Themes</Link> section for
                     the complete list of available themes to choose from.
                 </p>
-                <DocSectionCode code={code1} hideToggleCode import hideCodeSandbox hideStackBlitz />
-                <p>Each PrimeReact theme has its own font family so it is suggested to apply it to your application as well for a unified look.</p>
-                <DocSectionCode code={code2} hideToggleCode import hideCodeSandbox hideStackBlitz />
+                <DocSectionCode code={code} hideToggleCode import hideCodeSandbox hideStackBlitz />
             </DocSectionText>
         </>
     );
