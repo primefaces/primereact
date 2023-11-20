da255f8fc1611bbfac7226ec059a7495360990da
Cagatay Civici <cagatay.civici@gmail.com>
2023-11-20 14:20:34 +0300

    Fixed space

--- components/doc/installation/styleddoc.js
+++ components/doc/installation/styleddoc.js
@@ -22,8 +22,8 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
         <>
             <DocSectionText {...props}>
                 <p>
-                    Styled mode is based on pre-skinned components with opinionated themes like Material, Bootstrap or PrimeOne themes. Theme is the required css file to be imported, visit the
-                    <Link href="/theming/#themes">Themes</Link> section for the complete list of available themes to choose from.
+                    Styled mode is based on pre-skinned components with opinionated themes like Material, Bootstrap or PrimeOne themes. Theme is the required css file to be imported, visit the <Link href="/theming/#themes">Themes</Link> section for
+                    the complete list of available themes to choose from.
                 </p>
                 <DocSectionCode code={code1} hideToggleCode import hideCodeSandbox hideStackBlitz />
                 <p>Each PrimeReact theme has its own font family so it is suggested to apply it to your application as well for a unified look.</p>
