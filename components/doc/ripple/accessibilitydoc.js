19c0df709b48f98602eba6e3b14e1fcfdb777ebb
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2023-11-23 16:45:07 +0300

    Ripple accessibility #5356

--- components/doc/ripple/accessibilitydoc.js
+++ components/doc/ripple/accessibilitydoc.js
@@ -1,18 +1,15 @@
-import { DevelopmentSection } from '@/components/doc/common/developmentsection';
 import { DocSectionText } from '@/components/doc/common/docsectiontext';
 
 export function AccessibilityDoc() {
     return (
-        <DevelopmentSection>
-            <DocSectionText id="accessibility" label="Accessibility">
-                <h6>Screen Reader</h6>
-                <p>
-                    Ripple element has the <i>aria-hidden</i> attribute as true so that it gets ignored by the screen readers.
-                </p>
+        <DocSectionText id="accessibility" label="Accessibility">
+            <h6>Screen Reader</h6>
+            <p>
+                Ripple element has the <i>aria-hidden</i> attribute as true so that it gets ignored by the screen readers.
+            </p>
 
-                <h6>Keyboard Support</h6>
-                <p>Component does not include any interactive elements.</p>
-            </DocSectionText>
-        </DevelopmentSection>
+            <h6>Keyboard Support</h6>
+            <p>Component does not include any interactive elements.</p>
+        </DocSectionText>
     );
 }
