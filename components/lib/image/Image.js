2ee78b9bed16e81af70e2c7f6790b3dd472100e2
nevermore <kl.nevermore@gmail.com>
2023-12-21 17:16:21 +0800

    fix:Image preview zoom in bug

--- components/lib/image/Image.js
+++ components/lib/image/Image.js
@@ -29,6 +29,9 @@ export const Image = React.memo(
         const previewRef = React.useRef(null);
         const previewClick = React.useRef(false);
 
+        const zoomOutDisabled = scaleState <= 0.5;
+        const zoomInDisabled = scaleState >= 1.5;
+
         useGlobalOnEscapeKey({
             callback: () => {
                 if (props.closeOnEscape) {
@@ -99,12 +102,20 @@ export const Image = React.memo(
         };
 
         const zoomIn = () => {
-            setScaleState((prevScale) => prevScale + 0.1);
+            setScaleState((prevScale) => {
+                if (zoomInDisabled) return prevScale;
+
+                return prevScale + 0.1;
+            });
             previewClick.current = true;
         };
 
         const zoomOut = () => {
-            setScaleState((prevScale) => prevScale - 0.1);
+            setScaleState((prevScale) => {
+                if (zoomOutDisabled) return prevScale;
+
+                return prevScale - 0.1;
+            });
             previewClick.current = true;
         };
 
@@ -152,8 +163,6 @@ export const Image = React.memo(
 
         const createElement = () => {
             const { downloadable, alt, crossOrigin, referrerPolicy, useMap, loading } = props;
-            const zoomOutDisabled = scaleState <= 0.5;
-            const zoomInDisabled = scaleState >= 1.5;
             const downloadIconProps = mergeProps(ptm('downloadIcon'));
             const rotateRightIconProps = mergeProps(ptm('rotateRightIcon'));
             const rotateLeftIconProps = mergeProps(ptm('rotateLeftIcon'));
