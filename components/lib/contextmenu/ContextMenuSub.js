40e298976c6ca95e5b505f65514a378367323578
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2023-12-22 19:54:53 +0300

    #5639 for ContextMenu

--- components/lib/contextmenu/ContextMenuSub.js
+++ components/lib/contextmenu/ContextMenuSub.js
@@ -5,252 +5,327 @@ import { AngleRightIcon } from '../icons/angleright';
 import { Ripple } from '../ripple/Ripple';
 import { DomHandler, IconUtils, mergeProps, ObjectUtils } from '../utils/Utils';
 
-export const ContextMenuSub = React.memo((props) => {
-    const [activeItemState, setActiveItemState] = React.useState(null);
-    const submenuRef = React.useRef(null);
-    const active = props.root || !props.resetMenu;
-    const { ptm, cx } = props;
+export const ContextMenuSub = React.memo(
+    React.forwardRef((props, ref) => {
+        const submenuRef = React.useRef(null);
+        const active = props.root || !props.resetMenu;
+        const { ptm, cx } = props;
 
-    const getPTOptions = (item, key) => {
-        return ptm(key, {
-            hostName: props.hostName,
-            context: {
-                active: activeItemState === item
-            }
-        });
-    };
-
-    if (props.resetMenu === true && activeItemState !== null) {
-        setActiveItemState(null);
-    }
-
-    const onItemMouseEnter = (event, item) => {
-        if (item.disabled || props.isMobileMode) {
-            event.preventDefault();
-
-            return;
-        }
-
-        setActiveItemState(item);
-    };
-
-    const onItemClick = (event, item) => {
-        if (item.disabled) {
-            event.preventDefault();
-
-            return;
-        }
-
-        if (!item.url) {
-            event.preventDefault();
-        }
-
-        if (item.command) {
-            item.command({
-                originalEvent: event,
-                item
+        const getPTOptions = (item, key) => {
+            return ptm(key, {
+                hostName: props.hostName
             });
-        }
+        };
 
-        if (props.isMobileMode && item.items) {
-            if (activeItemState && item === activeItemState) setActiveItemState(null);
-            else setActiveItemState(item);
-        }
+        const onItemMouseEnter = (event, item) => {
+            if (item.disabled || props.isMobileMode) {
+                event.preventDefault();
 
-        if (!item.items) {
-            props.onLeafClick(event);
-        }
-    };
-
-    const position = () => {
-        if (!props.isMobileMode) {
-            const parentItem = submenuRef.current.parentElement;
-            const containerOffset = DomHandler.getOffset(parentItem);
-            const viewport = DomHandler.getViewport();
-            const sublistWidth = submenuRef.current.offsetParent ? submenuRef.current.offsetWidth : DomHandler.getHiddenElementOuterWidth(submenuRef.current);
-            const itemOuterWidth = DomHandler.getOuterWidth(parentItem.children[0]);
-            const top = parseInt(containerOffset.top, 10) + submenuRef.current.offsetHeight - DomHandler.getWindowScrollTop();
-
-            if (top > viewport.height) {
-                submenuRef.current.style.top = viewport.height - top + 'px';
-            } else {
-                submenuRef.current.style.top = '0px';
+                return;
             }
 
-            if (parseInt(containerOffset.left, 10) + itemOuterWidth + sublistWidth > viewport.width - DomHandler.calculateScrollbarWidth()) {
-                submenuRef.current.style.left = -1 * sublistWidth + 'px';
-            } else {
-                submenuRef.current.style.left = itemOuterWidth + 'px';
+            props.onItemMouseEnter({ originalEvent: event, processedItem: item });
+        };
+
+        const onItemClick = (event, processedItem) => {
+            const item = processedItem.item;
+
+            if (item.disabled) {
+                event.preventDefault();
+
+                return;
             }
-        }
-    };
 
-    const onEnter = () => {
-        position();
-    };
+            if (!item.url) {
+                event.preventDefault();
+            }
 
-    useUpdateEffect(() => {
-        active && position();
-    });
+            if (item.command) {
+                item.command({
+                    originalEvent: event,
+                    item
+                });
+            }
 
-    const createSeparator = (index) => {
-        const key = props.id + '_separator_' + index;
-        const separatorProps = mergeProps(
-            {
-                id: key,
-                key,
-                className: cx('separator'),
-                role: 'separator'
-            },
-            ptm('separator', { hostName: props.hostName })
-        );
+            props.onItemClick({ originalEvent: event, processedItem, isFocus: true });
 
-        return <li {...separatorProps}></li>;
-    };
+            if (!item.items) {
+                props.onLeafClick(event);
+            }
+        };
 
-    const createSubmenu = (item, index) => {
-        if (item.items) {
-            return (
-                <ContextMenuSub
-                    id={props.id + '_' + index}
-                    hostName={props.hostName}
-                    menuProps={props.menuProps}
-                    model={item.items}
-                    resetMenu={item !== activeItemState}
-                    onLeafClick={props.onLeafClick}
-                    isMobileMode={props.isMobileMode}
-                    submenuIcon={props.submenuIcon}
-                    ptm={ptm}
-                    cx={cx}
-                />
+        const position = () => {
+            if (!props.isMobileMode) {
+                const parentItem = submenuRef.current.parentElement;
+                const containerOffset = DomHandler.getOffset(parentItem);
+                const viewport = DomHandler.getViewport();
+                const sublistWidth = submenuRef.current.offsetParent ? submenuRef.current.offsetWidth : DomHandler.getHiddenElementOuterWidth(submenuRef.current);
+                const itemOuterWidth = DomHandler.getOuterWidth(parentItem.children[0]);
+                const top = parseInt(containerOffset.top, 10) + submenuRef.current.offsetHeight - DomHandler.getWindowScrollTop();
+
+                if (top > viewport.height) {
+                    submenuRef.current.style.top = viewport.height - top + 'px';
+                } else {
+                    submenuRef.current.style.top = '0px';
+                }
+
+                if (parseInt(containerOffset.left, 10) + itemOuterWidth + sublistWidth > viewport.width - DomHandler.calculateScrollbarWidth()) {
+                    submenuRef.current.style.left = -1 * sublistWidth + 'px';
+                } else {
+                    submenuRef.current.style.left = itemOuterWidth + 'px';
+                }
+            }
+        };
+
+        const onEnter = () => {
+            position();
+        };
+
+        useUpdateEffect(() => {
+            active && position();
+        });
+
+        const getItemId = (processedItem) => {
+            return `${props.menuId}_${processedItem.key}`;
+        };
+
+        const getItemProp = (processedItem, name, params) => {
+            return processedItem && processedItem.item ? ObjectUtils.getItemValue(processedItem.item[name], params) : undefined;
+        };
+
+        const getItemLabel = (processedItem) => {
+            return getItemProp(processedItem, 'label');
+        };
+
+        const isItemActive = (processedItem) => {
+            return props.activeItemPath && props.activeItemPath.some((path) => path.key === processedItem.key);
+        };
+
+        const isItemVisible = (processedItem) => {
+            return getItemProp(processedItem, 'visible') !== false;
+        };
+
+        const isItemDisabled = (processedItem) => {
+            return getItemProp(processedItem, 'disabled');
+        };
+
+        const isItemFocused = (processedItem) => {
+            return props.focusedItemId === getItemId(processedItem);
+        };
+
+        const isItemGroup = (processedItem) => {
+            return ObjectUtils.isNotEmpty(processedItem.items);
+        };
+
+        const getAriaSetSize = () => {
+            return props.model.filter((processedItem) => isItemVisible(processedItem) && !getItemProp(processedItem, 'separator')).length;
+        };
+
+        const getAriaPosInset = (index) => {
+            return index - props.model.slice(0, index).filter((processedItem) => isItemVisible(processedItem) && getItemProp(processedItem, 'separator')).length + 1;
+        };
+
+        React.useImperativeHandle(ref, () => ({
+            props,
+            getElement: () => submenuRef.current
+        }));
+
+        const createSeparator = (index) => {
+            const key = props.id + '_separator_' + index;
+            const separatorProps = mergeProps(
+                {
+                    id: key,
+                    key,
+                    className: cx('separator'),
+                    role: 'separator'
+                },
+                ptm('separator', { hostName: props.hostName })
             );
-        }
 
-        return null;
-    };
+            return <li {...separatorProps}></li>;
+        };
 
-    const createMenuItem = (item, index) => {
-        if (item.visible === false) {
+        const createSubmenu = (item, index) => {
+            if (isItemGroup(item)) {
+                return (
+                    <ContextMenuSub
+                        id={props.id + '_' + index}
+                        role="menu"
+                        menuId={props.menuId}
+                        focusedItemId={props.focusedItemId}
+                        activeItemPath={props.activeItemPath}
+                        level={props.level + 1}
+                        hostName={props.hostName}
+                        menuProps={props.menuProps}
+                        model={item.items}
+                        resetMenu={!isItemActive(item)}
+                        onLeafClick={props.onLeafClick}
+                        onItemClick={props.onItemClick}
+                        onItemMouseEnter={props.onItemMouseEnter}
+                        isMobileMode={props.isMobileMode}
+                        submenuIcon={props.submenuIcon}
+                        ptm={ptm}
+                        cx={cx}
+                    />
+                );
+            }
+
             return null;
-        }
+        };
 
-        const active = activeItemState === item;
-        const key = item.id || props.id + '_' + index;
-        const iconProps = mergeProps(
+        const createMenuItem = (processedItem, index) => {
+            if (!isItemVisible(processedItem)) {
+                return null;
+            }
+
+            const item = processedItem.item;
+            const active = isItemActive(processedItem);
+            const disabled = isItemDisabled(processedItem);
+            const focused = isItemFocused(processedItem);
+            const isGroup = isItemGroup(processedItem);
+            const key = getItemId(processedItem);
+            const iconProps = mergeProps(
+                {
+                    className: cx('icon')
+                },
+                getPTOptions(item, 'icon')
+            );
+            const icon = IconUtils.getJSXIcon(getItemProp(processedItem, 'icon'), { ...iconProps }, { props: props.menuProps });
+            const submenuIconProps = mergeProps(
+                {
+                    className: cx('submenuIcon')
+                },
+                getPTOptions(item, 'submenuIcon')
+            );
+
+            const labelProps = mergeProps(
+                {
+                    className: cx('label')
+                },
+                getPTOptions(item, 'label')
+            );
+
+            const items = getItemProp(processedItem, 'items');
+
+            const submenuIcon = items && IconUtils.getJSXIcon(props.submenuIcon || <AngleRightIcon {...submenuIconProps} />, { ...submenuIconProps }, { props: props.menuProps });
+            const label = item.label && <span {...labelProps}>{item.label}</span>;
+            const submenu = createSubmenu(processedItem, index);
+            const actionProps = mergeProps(
+                {
+                    href: item.url || '#',
+                    'aria-hidden': true,
+                    tabIndex: -1,
+                    className: cx('action', { item }),
+                    target: item.target
+                },
+                getPTOptions(item, 'action')
+            );
+
+            let content = (
+                <a {...actionProps}>
+                    {icon}
+                    {label}
+                    {submenuIcon}
+                    <Ripple />
+                </a>
+            );
+
+            if (item.template) {
+                const defaultContentOptions = {
+                    className: 'p-menuitem-link',
+                    labelClassName: 'p-menuitem-text',
+                    iconClassName: 'p-menuitem-icon',
+                    submenuIconClassName: cx('submenuIcon'),
+                    element: content,
+                    props,
+                    active
+                };
+
+                content = ObjectUtils.getJSXElement(item.template, item, defaultContentOptions);
+            }
+
+            const contentProps = mergeProps(
+                {
+                    onClick: (event) => onItemClick(event, processedItem, index),
+                    onMouseEnter: (event) => onItemMouseEnter(event, processedItem),
+                    className: cx('content')
+                },
+                getPTOptions(item, 'content')
+            );
+
+            const menuitemProps = mergeProps(
+                {
+                    id: key,
+                    key,
+                    role: 'menuitem',
+                    'aria-label': getItemLabel(processedItem),
+                    'aria-disabled': disabled,
+                    'aria-expanded': isGroup ? active : undefined,
+                    'aria-haspopup': isGroup && !getItemProp(item, 'url') ? 'menu' : undefined,
+                    'aria-level': props.level + 1,
+                    'aria-setsize': getAriaSetSize(),
+                    'aria-posinset': getAriaPosInset(index),
+                    'data-p-highlight': active,
+                    'data-p-focused': focused,
+                    'data-p-disabled': disabled,
+                    className: cx('menuitem', { item, active, focused, disabled: isItemDisabled(item) }),
+                    style: item.style,
+                    key
+                },
+                getPTOptions(item, 'menuitem')
+            );
+
+            return (
+                <li {...menuitemProps}>
+                    <div {...contentProps}>{content}</div>
+                    {submenu}
+                </li>
+            );
+        };
+
+        const createItem = (processedItem, index) => {
+            return processedItem.separator ? createSeparator(index) : createMenuItem(processedItem, index);
+        };
+
+        const createMenu = () => {
+            return props.model ? props.model.map(createItem) : null;
+        };
+
+        const submenu = createMenu();
+
+        const menuProps = mergeProps(
             {
-                className: cx('icon')
+                className: cx('menu', { menuProps: props }),
+                onFocus: props.onFocus,
+                onBlur: props.onBlur,
+                onKeyDown: props.onKeyDown,
+                'aria-orientation': 'vertical',
+                'aria-activedescendant': props.ariaActivedescendant,
+                tabIndex: props.tabIndex,
+                role: props.role
             },
-            getPTOptions(item, 'icon')
+            ptm('menu', { hostName: props.hostName })
         );
-        const icon = IconUtils.getJSXIcon(item.icon, { ...iconProps }, { props: props.menuProps });
-        const submenuIconProps = mergeProps(
+
+        const transitionProps = mergeProps(
             {
-                className: cx('submenuIcon')
+                classNames: cx('submenuTransition'),
+                in: active,
+                timeout: { enter: 0, exit: 0 },
+                unmountOnExit: true,
+                onEnter
             },
-            getPTOptions(item, 'submenuIcon')
-        );
-
-        const labelProps = mergeProps(
-            {
-                className: cx('label')
-            },
-            getPTOptions(item, 'label')
-        );
-        const submenuIcon = item.items && IconUtils.getJSXIcon(props.submenuIcon || <AngleRightIcon {...submenuIconProps} />, { ...submenuIconProps }, { props: props.menuProps });
-        const label = item.label && <span {...labelProps}>{item.label}</span>;
-        const submenu = createSubmenu(item, index);
-        const actionProps = mergeProps(
-            {
-                href: item.url || '#',
-                className: cx('action', { item }),
-                target: item.target,
-                onClick: (event) => onItemClick(event, item, index),
-                role: 'menuitem',
-                'aria-haspopup': item.items != null,
-                'aria-disabled': item.disabled
-            },
-            getPTOptions(item, 'action')
-        );
-
-        let content = (
-            <a {...actionProps}>
-                {icon}
-                {label}
-                {submenuIcon}
-                <Ripple />
-            </a>
-        );
-
-        if (item.template) {
-            const defaultContentOptions = {
-                onClick: (event) => onItemClick(event, item, index),
-                className: 'p-menuitem-link',
-                labelClassName: 'p-menuitem-text',
-                iconClassName: 'p-menuitem-icon',
-                submenuIconClassName: cx('submenuIcon'),
-                element: content,
-                props,
-                active
-            };
-
-            content = ObjectUtils.getJSXElement(item.template, item, defaultContentOptions);
-        }
-
-        const menuitemProps = mergeProps(
-            {
-                id: key,
-                key,
-                role: 'none',
-                className: cx('menuitem', { item, active }),
-                style: item.style,
-                key,
-                onMouseEnter: (event) => onItemMouseEnter(event, item)
-            },
-            getPTOptions(item, 'menuitem')
+            ptm('menu.transition', { hostName: props.hostName })
         );
 
         return (
-            <li {...menuitemProps}>
-                {content}
-                {submenu}
-            </li>
+            <CSSTransition nodeRef={submenuRef} {...transitionProps}>
+                <ul ref={submenuRef} {...menuProps}>
+                    {submenu}
+                </ul>
+            </CSSTransition>
         );
-    };
-
-    const createItem = (item, index) => {
-        return item.separator ? createSeparator(index) : createMenuItem(item, index);
-    };
-
-    const createMenu = () => {
-        return props.model ? props.model.map(createItem) : null;
-    };
-
-    const submenu = createMenu();
-    const menuProps = mergeProps(
-        {
-            className: cx('menu', { menuProps: props })
-        },
-        ptm('menu', { hostName: props.hostName })
-    );
-
-    const transitionProps = mergeProps(
-        {
-            classNames: cx('submenuTransition'),
-            in: active,
-            timeout: { enter: 0, exit: 0 },
-            unmountOnExit: true,
-            onEnter
-        },
-        ptm('menu.transition', { hostName: props.hostName })
-    );
-
-    return (
-        <CSSTransition nodeRef={submenuRef} {...transitionProps}>
-            <ul ref={submenuRef} {...menuProps}>
-                {submenu}
-            </ul>
-        </CSSTransition>
-    );
-});
+    })
+);
 
 ContextMenuSub.displayName = 'ContextMenuSub';
