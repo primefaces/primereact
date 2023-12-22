d98a82d63ad00cb13ce81d222b44c82a888ce183
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2023-12-22 19:23:38 +0300

    #5639 for BreadCrumb

--- components/lib/breadcrumb/BreadCrumbBase.js
+++ components/lib/breadcrumb/BreadCrumbBase.js
@@ -5,7 +5,7 @@ const classes = {
     icon: 'p-menuitem-icon',
     action: 'p-menuitem-link',
     label: 'p-menuitem-text',
-    home: ({ _className, disabled }) => classNames('p-breadcrumb-home', { 'p-disabled': disabled }, _className),
+    home: ({ _className, disabled }) => classNames('p-breadcrumb-home p-menuitem', { 'p-disabled': disabled }, _className),
     separatorIcon: 'p-breadcrumb-chevron',
     separator: 'p-menuitem-separator',
     menuitem: ({ item }) => classNames('p-menuitem', item.className, { 'p-disabled': item.disabled }),
@@ -18,8 +18,8 @@ const styles = `
     .p-breadcrumb {
         overflow-x: auto;
     }
-    
-    .p-breadcrumb ul {
+
+    .p-breadcrumb ol {
         margin: 0;
         padding: 0;
         list-style-type: none;
@@ -27,22 +27,22 @@ const styles = `
         align-items: center;
         flex-wrap: nowrap;
     }
-    
+
     .p-breadcrumb .p-menuitem-text {
         line-height: 1;
     }
-    
+
     .p-breadcrumb .p-menuitem-link {
         text-decoration: none;
         display: flex;
         align-items: center;
     }
-    
+
     .p-breadcrumb .p-menuitem-separator {
         display: flex;
         align-items: center;
     }
-    
+
     .p-breadcrumb::-webkit-scrollbar {
         display: none;
     }
