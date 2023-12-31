1277ce409cd73881860e66b73c1a67f00e8e3fd8
Melloware <mellowaredev@gmail.com>
2023-12-31 09:09:33 -0500

    Fix #5664: FileUpload progress template (#5665)

--- components/lib/fileupload/FileUpload.js
+++ components/lib/fileupload/FileUpload.js
@@ -292,8 +292,7 @@ export const FileUpload = React.memo(
         };
 
         const onKeyDown = (event) => {
-            if (event.which === 13) {
-                // enter
+            if (event.code === 'Enter') {
                 choose();
             }
         };
@@ -530,7 +529,12 @@ export const FileUpload = React.memo(
 
         const createProgressBarContent = () => {
             if (props.progressBarTemplate) {
-                return ObjectUtils.getJSXElement(props.progressBarTemplate, props);
+                const defaultProgressBarTemplateOptions = {
+                    progress: progressState,
+                    props
+                };
+
+                return ObjectUtils.getJSXElement(props.progressBarTemplate, defaultProgressBarTemplateOptions);
             }
 
             return <ProgressBar value={progressState} showValue={false} pt={ptm('progressbar')} __parentMetadata={{ parent: metaData }} />;
