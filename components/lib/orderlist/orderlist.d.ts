1db1fba5232e4c3862c6155f3348c8ff4b514293
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2024-01-10 10:32:20 +0300

    #5675 Doc update for OrderList

--- components/lib/orderlist/orderlist.d.ts
+++ components/lib/orderlist/orderlist.d.ts
@@ -189,6 +189,16 @@ export interface OrderListProps extends Omit<React.DetailedHTMLProps<React.HTMLA
      */
     ariaLabelledBy?: string | undefined;
     /**
+     * Callback to invoke when menu receives focus.
+     * @param {React.SyntheticEvent} event - Browser event.
+     */
+    onFocus?(event: React.SyntheticEvent): void;
+    /**
+     * Callback to invoke when menu loses focus.
+     * @param {React.SyntheticEvent} event - Browser event.
+     */
+    onBlur?(event: React.SyntheticEvent): void;
+    /**
      * Name of the field that uniquely identifies the a record in the data.
      */
     dataKey?: string | undefined;
