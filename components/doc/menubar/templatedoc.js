c8c4eb7831dabba613ee3e04663181bb3f4974c5
chiefdeals <126732208+chiefdeals@users.noreply.github.com>
2024-01-14 14:07:28 -0800

    corrected typo
    'importy' -> 'import'

--- components/doc/menubar/templatedoc.js
+++ components/doc/menubar/templatedoc.js
@@ -93,7 +93,7 @@ import React from 'react';
 import { Menubar } from 'primereact/menubar';
 import { InputText } from 'primereact/inputtext';
 import { Badge } from 'primereact/badge';
-importy { Avatar } from 'primereact/avatar';  
+import { Avatar } from 'primereact/avatar';  
 
 export default function TemplateDemo() {
     const itemRenderer = (item) => (
