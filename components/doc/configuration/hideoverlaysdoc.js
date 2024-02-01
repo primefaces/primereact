468d7c8f6666c0f2fd459354af29cb4aa58613ed
melloware <mellowaredev@gmail.com>
2024-02-01 13:01:29 -0500

    Fix #5872: hideOverlaysOnDocumentScrolling docs

--- components/doc/configuration/hideoverlaysdoc.js
+++ components/doc/configuration/hideoverlaysdoc.js
@@ -19,6 +19,14 @@ export default function MyApp({ Component }) {
         </PrimeReactProvider>
     );
 }
+
+//_app.css
+body {
+  margin: 0px;
+  min-height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
         `
     };
 
@@ -27,7 +35,7 @@ export default function MyApp({ Component }) {
             <DocSectionText {...props}>
                 <p>
                     Define behavior if the browser window is scrolled while displaying an overlay panel like a Dropdown or Calendar. Depending on your organization's accessibility needs some prefer panels to be closed on scrolling and some prefer the
-                    overlay follow the scroll. Default value is false.
+                    overlay follow the scroll. Default value is false. IMPORTANT: Your <i>document.body</i> must have <i>overflow</i> CSS on this to work properly.
                 </p>
             </DocSectionText>
             <DocSectionCode code={code} hideToggleCode import hideCodeSandbox hideStackBlitz />
