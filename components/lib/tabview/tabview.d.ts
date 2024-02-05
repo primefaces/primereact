1f42c8a6f4757140ab65d126dcba987833c8d5b5
melloware <mellowaredev@gmail.com>
2024-02-05 14:01:31 -0500

    Fix #5900: TabPanel add visible prop

--- components/lib/tabview/tabview.d.ts
+++ components/lib/tabview/tabview.d.ts
@@ -156,6 +156,28 @@ export interface TabPanelPassThroughOptions {
  */
 export interface TabPanelProps {
     /**
+     * Used to get the child elements of the component.
+     * @readonly
+     */
+    children?: React.ReactNode | undefined;
+    /**
+     * Defines if tab can be removed.
+     * @defaultValue false
+     */
+    closable?: boolean | undefined;
+    /**
+     * Style class of the tab header and content.
+     */
+    className?: string | undefined;
+    /**
+     * Inline style of the tab content.
+     */
+    contentStyle?: React.CSSProperties | undefined;
+    /**
+     * Style class of the tab content.
+     */
+    contentClassName?: string | undefined;
+    /**
      * Orientation of tab headers.
      */
     header?: React.ReactNode | undefined;
@@ -163,6 +185,14 @@ export interface TabPanelProps {
      * Header template of the tab to customize more.
      */
     headerTemplate?: React.ReactNode | ((options: TabPanelHeaderTemplateOptions) => React.ReactNode);
+    /**
+     * Inline style of the tab header.
+     */
+    headerStyle?: React.CSSProperties | undefined;
+    /**
+     * Style class of the tab header.
+     */
+    headerClassName?: string | undefined;
     /**
      * Icons can be placed at left of a header.
      */
@@ -189,40 +219,6 @@ export interface TabPanelProps {
      */
     disabled?: boolean | undefined;
     /**
-     * Defines if tab can be removed.
-     * @defaultValue false
-     */
-    closable?: boolean | undefined;
-    /**
-     * Inline style of the tab header and content.
-     */
-    style?: React.CSSProperties | undefined;
-    /**
-     * Style class of the tab header and content.
-     */
-    className?: string | undefined;
-    /**
-     * Inline style of the tab header.
-     */
-    headerStyle?: React.CSSProperties | undefined;
-    /**
-     * Style class of the tab header.
-     */
-    headerClassName?: string | undefined;
-    /**
-     * Inline style of the tab content.
-     */
-    contentStyle?: React.CSSProperties | undefined;
-    /**
-     * Style class of the tab content.
-     */
-    contentClassName?: string | undefined;
-    /**
-     * Used to get the child elements of the component.
-     * @readonly
-     */
-    children?: React.ReactNode | undefined;
-    /**
      * Uses to pass attributes to DOM elements inside the component.
      * @type {TabPanelPassThroughOptions}
      */
@@ -233,10 +229,19 @@ export interface TabPanelProps {
      */
     ptOptions?: PassThroughOptions;
     /**
+     * Inline style of the tab header and content.
+     */
+    style?: React.CSSProperties | undefined;
+    /**
      * When enabled, it removes component related styles in the core.
      * @defaultValue false
      */
     unstyled?: boolean;
+    /**
+     * When set as false, hides the tab panel.
+     * @defaultValue true
+     */
+    visible?: boolean | undefined;
 }
 
 /**
