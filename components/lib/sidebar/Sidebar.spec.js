97f2fb4df8c6fc9a32ad5f764c12bca4e614b07b
Hans Pragt <hansp@telestream.net>
2024-02-14 09:04:31 -0800

    Add a test for the sidebar component.

---
+++ components/lib/sidebar/Sidebar.spec.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Sidebar } from './sidebar';
+
+describe('Sidebar', () => {
+    test(
+        'can be made visible',
+        () => {
+            const hideOn = jest.fn();
+            render(<Sidebar visible={true} onHide={hideOn} />);
+
+            const sidebar = screen.queryByRole('complementary');
+            expect(sidebar).toBeVisible();
+        }
+    );
+});
