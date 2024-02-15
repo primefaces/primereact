d15cc67538fb829fd9567ec895cd9c0a4ba779ba
melloware <mellowaredev@gmail.com>
2024-02-15 09:14:40 -0500

    Fix #5973: Calendar yearNavigator defaults

--- components/lib/calendar/Calendar.js
+++ components/lib/calendar/Calendar.js
@@ -3172,18 +3172,28 @@ export const Calendar = React.memo(
         };
 
         const createTitleYearElement = (metaYear) => {
+            const viewDate = getViewDate();
+            const viewYear = viewDate.getFullYear();
+
             if (props.yearNavigator) {
                 let yearOptions = [];
-                const years = props.yearRange.split(':');
-                const yearStart = parseInt(years[0], 10);
-                const yearEnd = parseInt(years[1], 10);
 
-                for (let i = yearStart; i <= yearEnd; i++) {
-                    yearOptions.push(i);
+                if (props.yearRange) {
+                    const years = props.yearRange.split(':');
+                    const yearStart = parseInt(years[0], 10);
+                    const yearEnd = parseInt(years[1], 10);
+
+                    for (let i = yearStart; i <= yearEnd; i++) {
+                        yearOptions.push(i);
+                    }
+                } else {
+                    const base = viewYear - (viewYear % 10);
+
+                    for (let i = 0; i < 10; i++) {
+                        yearOptions.push(base + i);
+                    }
                 }
 
-                const viewDate = getViewDate();
-                const viewYear = viewDate.getFullYear();
                 const displayedYearNames = yearOptions.filter((year) => !(props.minDate && props.minDate.getFullYear() > year) && !(props.maxDate && props.maxDate.getFullYear() < year));
                 const selectProps = mergeProps(
                     {
