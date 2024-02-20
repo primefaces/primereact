5d6091459e60f8b6589c1ffe57ff9cd1431184c9
Melloware <mellowaredev@gmail.com>
2024-02-20 14:36:00 -0500

    Fix #5927: BodyCell memory leak when not editable (#6011)
    * Fix #5927: BodyCell memory leak when not editable
    
    * Fix #5927: BodyCell memory leak when not editable

--- components/lib/hooks/usePrevious.js
+++ components/lib/hooks/usePrevious.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 
 export const usePrevious = (newValue) => {
-    const ref = React.useRef(undefined);
+    const ref = React.useRef(null);
 
     React.useEffect(() => {
         ref.current = newValue;
-    });
+
+        return () => {
+            ref.current = null;
+        };
+    }, [newValue]);
 
     return ref.current;
 };
