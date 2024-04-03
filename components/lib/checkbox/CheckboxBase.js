5704c931974a414e96c62745a89d81563de417a6
gucal <gucalaliriza@outlook.com>
2024-04-03 09:27:55 +0300

    Update checkbox

--- components/lib/checkbox/CheckboxBase.js
+++ components/lib/checkbox/CheckboxBase.js
@@ -5,12 +5,12 @@ const classes = {
     box: 'p-checkbox-box',
     input: 'p-checkbox-input',
     icon: 'p-checkbox-icon',
-    root: ({ props, checked }) =>
+    root: ({ props, checked, context }) =>
         classNames('p-checkbox p-component', {
             'p-highlight': checked,
             'p-disabled': props.disabled,
             'p-invalid': props.invalid,
-            'p-variant-filled': props.variant === 'filled'
+            'p-variant-filled': props.variant ? props.variant === 'filled' : context && context.inputStyle === 'filled'
         })
 };
 
@@ -27,6 +27,7 @@ export const CheckboxBase = ComponentBase.extend({
         inputId: null,
         inputRef: null,
         invalid: false,
+        variant: null,
         name: null,
         onChange: null,
         onContextMenu: null,
