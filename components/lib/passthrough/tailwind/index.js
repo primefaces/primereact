21d04ffef5ac37c9ff12d131e6e5cb688b7d43a0
vaelu <mail@valentin.zip>
2024-04-03 11:55:23 +0200

    OrganizationChart: fix wrong case in tailwind preset

--- components/lib/passthrough/tailwind/index.js
+++ components/lib/passthrough/tailwind/index.js
@@ -2621,14 +2621,14 @@ const Tailwind = {
                 'dark:border-blue-900/40 dark:bg-gray-900 dark:text-white/80' // Dark Mode
             )
         },
-        linecell: 'text-center align-top py-0 px-3',
-        linedown: {
+        lineCell: 'text-center align-top py-0 px-3',
+        lineDown: {
             className: classNames(
                 'mx-auto my-0 w-px h-[20px] bg-gray-300',
                 'dark:bg-blue-900/40' //Dark Mode
             )
         },
-        lineleft: ({ context }) => ({
+        lineLeft: ({ context }) => ({
             className: classNames(
                 'text-center align-top py-0 px-3 rounded-none border-r border-gray-300',
                 'dark:border-blue-900/40', //Dark Mode
@@ -2637,7 +2637,7 @@ const Tailwind = {
                 }
             )
         }),
-        lineright: ({ context }) => ({
+        lineRight: ({ context }) => ({
             className: classNames(
                 'text-center align-top py-0 px-3 rounded-none',
                 'dark:border-blue-900/40', //Dark Mode
@@ -2646,14 +2646,14 @@ const Tailwind = {
                 }
             )
         }),
-        nodecell: 'text-center align-top py-0 px-3',
-        nodetoggler: {
+        nodeCell: 'text-center align-top py-0 px-3',
+        nodeToggler: {
             className: classNames(
                 'absolute bottom-[-0.75rem] left-2/4 -ml-3 w-6 h-6 bg-inherit text-inherit rounded-full z-2 cursor-pointer no-underline select-none',
                 'focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]' // Focus styles
             )
         },
-        nodetogglericon: 'relative inline-block w-4 h-4'
+        nodeTogglerIcon: 'relative inline-block w-4 h-4'
     },
     orderlist: {
         root: 'flex',
