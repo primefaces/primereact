169b1cffb783075f32cb5b9a00e2629a64ee8207
gucal <gucalaliriza@outlook.com>
2024-04-03 10:08:35 +0300

    Update inputmask

--- components/lib/inputmask/InputMaskBase.js
+++ components/lib/inputmask/InputMaskBase.js
@@ -1,4 +1,14 @@
 import { ComponentBase } from '../componentbase/ComponentBase';
+import { classNames } from '../utils/Utils';
+
+const classes = {
+    root: ({ props, context }) =>
+        classNames('p-inputmask', {
+            'p-filled': props.filled,
+            'p-invalid': props.invalid,
+            'p-variant-filled': props.variant ? props.variant === 'filled' : context && context.inputStyle === 'filled'
+        })
+};
 
 export const InputMaskBase = ComponentBase.extend({
     defaultProps: {
@@ -10,6 +20,8 @@ export const InputMaskBase = ComponentBase.extend({
         id: null,
         mask: null,
         maxLength: null,
+        invalid: false,
+        variant: null,
         name: null,
         onBlur: null,
         onChange: null,
@@ -28,5 +40,8 @@ export const InputMaskBase = ComponentBase.extend({
         unmask: false,
         value: null,
         children: undefined
+    },
+    css: {
+        classes
     }
 });
