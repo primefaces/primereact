f932f4172f86c5a6541569dfd039486579ffd5b3
melloware <mellowaredev@gmail.com>
2024-04-08 07:56:34 -0400

    FloatLabel/StepperPanel proper use of forwardRef

--- components/lib/floatlabel/FloatLabel.js
+++ components/lib/floatlabel/FloatLabel.js
@@ -2,22 +2,28 @@ import * as React from 'react';
 import { PrimeReactContext } from '../api/Api';
 import { useHandleStyle } from '../componentbase/ComponentBase';
 import { useMergeProps } from '../hooks/Hooks';
-import { classNames } from '../utils/Utils';
+import { classNames, ObjectUtils } from '../utils/Utils';
 import { FloatLabelBase } from './FloatLabelBase';
 
 export const FloatLabel = React.memo(
-    React.forwardRef((inProps) => {
+    React.forwardRef((inProps, ref) => {
         const mergeProps = useMergeProps();
         const context = React.useContext(PrimeReactContext);
         const props = FloatLabelBase.getProps(inProps, context);
+        const elementRef = React.useRef(ref);
         const { ptm, cx, isUnstyled } = FloatLabelBase.setMetaData({
             props
         });
 
         useHandleStyle(FloatLabelBase.css.styles, isUnstyled, { name: 'floatlabel' });
 
+        React.useEffect(() => {
+            ObjectUtils.combinedRefs(elementRef, ref);
+        }, [elementRef, ref]);
+
         const rootProps = mergeProps(
             {
+                ref: elementRef,
                 className: classNames(cx('root'))
             },
             FloatLabelBase.getOtherProps(props),
--- components/lib/stepperpanel/StepperPanel.js
+++ components/lib/stepperpanel/StepperPanel.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import { PrimeReactContext } from '../api/Api';
 import { useHandleStyle } from '../componentbase/ComponentBase';
+import { ObjectUtils } from '../utils/Utils';
 import { StepperPanelBase } from './StepperPanelBase';
 
 export const StepperPanel = React.memo(
-    React.forwardRef((inProps) => {
+    React.forwardRef((inProps, ref) => {
         const context = React.useContext(PrimeReactContext);
         const props = StepperPanelBase.getProps(inProps, context);
+        const elementRef = React.useRef(ref);
         const { isUnstyled } = StepperPanelBase.setMetaData({
             props
         });
 
         useHandleStyle(StepperPanelBase.css.styles, isUnstyled, { name: 'StepperPanel' });
 
-        return <>{props.children}</>;
+        React.useEffect(() => {
+            ObjectUtils.combinedRefs(elementRef, ref);
+        }, [elementRef, ref]);
+
+        return <span ref={ref}>{props.children}</span>;
     })
 );
 
