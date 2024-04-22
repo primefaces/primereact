b049ec69eb0cd82ceab361e572e91e48ace5a539
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-04-22 15:15:53 +0300

    Fix#6443

--- components/doc/tabview/scrollabledoc.js
+++ components/doc/tabview/scrollabledoc.js
@@ -21,7 +21,7 @@ import React, { useState } from 'react';
 import { TabView, TabPanel } from 'primereact/tabview';
 
 export default function ScrollableDemo() {
-    const scrollableTabs = Array.from({ length: 50 }, (_, i) => ({ title: \`Tab \${i + 1}\`, content: \`Tab \${i + 1} Content\` }))
+    const tabs = Array.from({ length: 50 }, (_, i) => ({ title: \`Tab \${i + 1}\`, content: \`Tab \${i + 1} Content\` }))
 
     return (
         <div className="card">
@@ -43,8 +43,8 @@ import React, { useState } from 'react';
 import { TabView, TabPanel } from 'primereact/tabview';
 
 export default function ScrollableDemo() {
-    const scrollableTabs = Array.from({ length: 50 }, (_, i) => ({ title: \`Tab \${i + 1}\`, content: \`Tab \${i + 1} Content\` }))
-    
+    const tabs = Array.from({ length: 50 }, (_, i) => ({ title: \`Tab \${i + 1}\`, content: \`Tab \${i + 1} Content\` }))
+
     return (
         <div className="card">
             <TabView scrollable>
