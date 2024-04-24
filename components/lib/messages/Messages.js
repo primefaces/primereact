c35496f9610a37c0651f679cc0cf1b117d359cc7
Batuhan Tomo <91488737+Rekl0w@users.noreply.github.com>
2024-04-24 15:44:15 +0300

    Fix #6456 : Toast close by id fix (#6457)
    * Fix #6456
    
    * Fix #6456 update
    
    * Fix #6456 update
    
    * Fix #6456 messages fix

--- components/lib/messages/Messages.js
+++ components/lib/messages/Messages.js
@@ -73,7 +73,7 @@ export const Messages = React.memo(
 
         const remove = (messageInfo) => {
             // allow removal by ID or by message equality
-            const removeMessage = messageInfo._pId ? messageInfo.message : messageInfo;
+            const removeMessage = messageInfo._pId ? messageInfo._pId : messageInfo.message || messageInfo;
 
             setMessagesState((prev) => prev.filter((msg) => msg._pId !== messageInfo._pId && !ObjectUtils.deepEquals(msg.message, removeMessage)));
 
