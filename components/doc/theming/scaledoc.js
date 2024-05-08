9f6063b69d04e21c0465c4e8e8f4213e558e0afd
Melloware <mellowaredev@gmail.com>
2024-05-08 11:33:07 -0400

    Fix #6594: Fix typo in Scale doc (#6596)

--- components/doc/theming/scaledoc.js
+++ components/doc/theming/scaledoc.js
@@ -15,7 +15,7 @@ html {
             <DocSectionText {...props}>
                 <p>
                     PrimeReact utilizes <i>rem</i> units to make sure the components blend in with the rest of your UI perfectly. This also enables scaling, for example changing the size of the components is easy as configuring the font size of your
-                    document. Code below sets the scale of the components based on <i>16px</i>. If you reqire bigger or smaller components, just change this variable and components will scale accordingly.
+                    document. Code below sets the scale of the components based on <i>16px</i>. If you require bigger or smaller components, just change this variable and components will scale accordingly.
                 </p>
             </DocSectionText>
             <DocSectionCode code={code} hideToggleCode import hideStackBlitz />
