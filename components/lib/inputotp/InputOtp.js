4b5f27ec82298be1d0c85261bbf0374d5c929474
Kiril Volskiy <vtkkllx2367@gmail.com>
2024-05-17 16:21:45 +0300

    update: default case condition

--- components/lib/inputotp/InputOtp.js
+++ components/lib/inputotp/InputOtp.js
@@ -116,6 +116,7 @@ export const InputOtp = React.memo(
         };
 
         const onFocus = (event) => {
+            console.log('onFocus', event);
             event.target.select();
             props?.focus?.(event);
         };
@@ -164,11 +165,9 @@ export const InputOtp = React.memo(
                 }
 
                 default: {
-                    console.log('DEF')
                     // Prevent non-numeric characters from being entered if integerOnly is true or if the length of the input is greater than the specified length
-                    if ((props?.integerOnly && !((event.code.startsWith('Digit') || event.code.startsWith('Numpad')) && Number(event.key) >= 0 && Number(event.key) <= 9)) || (tokens.join('').length >= props.length && event.code !== 'Enter' && event.code !== 'Tab' && event.code !== 'Delete')) {
-                       event.preventDefault();
-                       console.log('PREVENT')
+                    if ( (event.code !== 'Enter' && event.code !== 'Tab')  &&  ((props?.integerOnly && !((event.code.startsWith('Digit') || event.code.startsWith('Numpad')) && Number(event.key) >= 0 && Number(event.key) <= 9)) || tokens.join('').length >= props.length && event.code !== 'Delete' )) {
+                        event.preventDefault();
                     }
 
                     break;
@@ -213,7 +212,11 @@ export const InputOtp = React.memo(
                 },
                 ptm('input')
             );
-            const inputElement = props?.inputTemplate ? ObjectUtils.getJSXElement(props?.inputTemplate, { events: inputElementEvents, props: inputElementProps }) : <InputText key={remainingInputs - 1} {...inputElementProps} {...inputElementEvents} />;
+            const inputElement = props?.inputTemplate ? (
+                ObjectUtils.getJSXElement(props?.inputTemplate, { events: inputElementEvents, props: inputElementProps })
+            ) : (
+                <InputText {...inputElementProps} {...inputElementEvents} />
+            );
             const inputElements = [inputElement, ...createInputElements(remainingInputs - 1)];
 
             return inputElements;
