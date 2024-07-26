9be78f57a6b3ce22237c547711bbe91b87925960
Melloware <mellowaredev@gmail.com>
2024-07-26 08:54:56 -0400

    Fix #6929: Unstyled prop forwarded to InputText (#6930)

--- components/lib/inputnumber/InputNumber.js
+++ components/lib/inputnumber/InputNumber.js
@@ -25,7 +25,7 @@ export const InputNumber = React.memo(
         };
         const { ptm, cx, isUnstyled } = InputNumberBase.setMetaData(metaData);
 
-        useHandleStyle(InputNumberBase.css.styles, isUnstyled, { name: 'inputnumber' });
+        useHandleStyle(InputNumberBase.css.styles, isUnstyled, { name: 'inputnumber', styled: true });
         const elementRef = React.useRef(null);
         const inputRef = React.useRef(null);
         const timer = React.useRef(null);
@@ -1188,6 +1188,7 @@ export const InputNumber = React.memo(
                     {...ariaProps}
                     {...dataProps}
                     pt={ptm('input')}
+                    unstyled={props.unstyled}
                     __parentMetadata={{ parent: metaData }}
                 />
             );
