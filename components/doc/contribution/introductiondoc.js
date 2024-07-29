d681b9f587e9c836e27bc4b7e24fa1e844669825
Sven Jansen <129374760+sja-cslab@users.noreply.github.com>
2024-07-29 15:18:11 +0200

    fixrd typo VUE => React (#6947)

--- components/doc/contribution/introductiondoc.js
+++ components/doc/contribution/introductiondoc.js
@@ -31,7 +31,7 @@ npm run dev
     return (
         <DocSectionText {...props}>
             <p>
-                PrimeReact is a popular Vue UI library maintained by PrimeTek, a company renowned for its comprehensive set of UI components for various frameworks. PrimeTek is dedicated to providing high-quality, versatile, and accessible UI
+                PrimeReact is a popular React library maintained by PrimeTek, a company renowned for its comprehensive set of UI components for various frameworks. PrimeTek is dedicated to providing high-quality, versatile, and accessible UI
                 components that help developers build better applications faster.
             </p>
             <h3>Development Setup</h3>
