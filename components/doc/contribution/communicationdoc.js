0e4976f3433f3dd4d1b8cea37df26dc23262cb0a
Toprak Koç <hello@toprak.dev>
2024-07-29 15:14:25 +0300

    Typo Fix

--- components/doc/contribution/communicationdoc.js
+++ components/doc/contribution/communicationdoc.js
@@ -5,7 +5,7 @@ export function CommunicationDoc(props) {
     return (
         <DocSectionText {...props}>
             <p>
-                Join the Contributors channel on the <a href="https://discord.com/invite/gzKFYnpmCY" target="_blank" rel="noopener noreferrer">PrimeLand Discord Server</a> to connect with PrimeReact staff and fellow contributors. In this channel, you can discuss the areas you want to contribute to and receive feedback. This channel is open to everyone who'd like to contribute.
+                Join the Contributors channel on the <a href="https://discord.com/invite/gzKFYnpmCY" target="_blank" rel="noopener noreferrer">PrimeLand Discord</a> server to connect with PrimeReact staff and fellow contributors. In this channel, you can discuss the areas you want to contribute to and receive feedback. This channel is open to everyone who'd like to contribute.
             </p>
         </DocSectionText>
     );
--- components/doc/contribution/helpneededdoc.js
+++ components/doc/contribution/helpneededdoc.js
@@ -17,7 +17,7 @@ export function HelpNeededDoc(props) {
             <p>We encourage you to send pull requests, especially for issues tagged with the <i>help-needed</i> label.</p>
 
             <h3>Community Support</h3>
-            <p>Assist other users by participating in the issue tracker, <a href="https://github.com/orgs/primefaces/discussions/categories/primereact" target="_blank" rel="noopener noreferrer">GitHub Discussions</a>, and the <a href="https://discord.com/invite/gzKFYnpmCY" target="_blank" rel="noopener noreferrer">PrimeLand Discord Server</a>. Your expertise can help others solve problems and improve their experience with PrimeReact.</p>
+            <p>Assist other users by participating in the issue tracker, <a href="https://github.com/orgs/primefaces/discussions/categories/primereact" target="_blank" rel="noopener noreferrer">GitHub Discussions</a>, and the <a href="https://discord.com/invite/gzKFYnpmCY" target="_blank" rel="noopener noreferrer">PrimeLand Discord</a> server. Your expertise can help others solve problems and improve their experience with PrimeReact.</p>
         </DocSectionText>
     );
 }
