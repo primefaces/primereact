1360b4a2a300f6654a5ee46c8678842f3cfb2340
Alex <avasuro.dev@gmail.com>
2024-07-30 19:00:44 +0200

    fix: editor does not renders initial value in some cases (#6951)
    Co-authored-by: Aleksandr Vasiuro <aleksandr.vasiuro@adensio.com>

--- components/lib/editor/Editor.js
+++ components/lib/editor/Editor.js
@@ -4,6 +4,7 @@ import { useHandleStyle } from '../componentbase/ComponentBase';
 import { useMergeProps, useMountEffect, useUpdateEffect } from '../hooks/Hooks';
 import { DomHandler, classNames } from '../utils/Utils';
 import { EditorBase } from './EditorBase';
+import { useRef } from 'react';
 
 const QuillJS = (function () {
     try {
@@ -119,13 +120,17 @@ export const Editor = React.memo(
             }
         };
 
+        const initValue = useRef(props.value);
+
+        initValue.current = props.value;
+
         const initQuill = (quillInstance) => {
             quill.current = quillInstance;
 
-            if (props.value) {
+            if (initValue.current) {
                 quillInstance.setContents(
                     quillInstance.clipboard.convert({
-                        html: props.value,
+                        html: initValue.current,
                         text: ''
                     })
                 );
