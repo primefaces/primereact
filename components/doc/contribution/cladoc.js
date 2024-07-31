a4f4ee852b9652df95c3e4fbff57d1038a0952a5
Melloware <mellowaredev@gmail.com>
2024-07-31 13:59:54 -0400

    Update cladoc.js

--- components/doc/contribution/cladoc.js
+++ components/doc/contribution/cladoc.js
@@ -3,9 +3,7 @@ import { DocSectionText } from '@/components/doc/common/docsectiontext';
 export function CLADoc(props) {
     return (
         <DocSectionText {...props}>
-            <p>
-                When a community member is offered the Contributor role, they are expected to sign a Contributor License Agreement (CLA) for legal purposes. This helps protect both the contributor and PrimeTek.
-            </p>
+            <p>When a community member is offered the Contributor role, they are expected to sign a Contributor License Agreement (CLA) for legal purposes. This helps protect both the contributor and PrimeTek.</p>
         </DocSectionText>
     );
 }
