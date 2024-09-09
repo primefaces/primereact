901fcda63a39040763005e8f4f353e8ecd9e2c19
Melloware <mellowaredev@gmail.com>
2024-09-09 07:30:43 -0400

    Update floatlabeldoc.js

--- components/doc/dropdown/floatlabeldoc.js
+++ components/doc/dropdown/floatlabeldoc.js
@@ -1,7 +1,7 @@
 import { DocSectionCode } from '@/components/doc/common/docsectioncode';
 import { DocSectionText } from '@/components/doc/common/docsectiontext';
 import { Dropdown } from '@/components/lib/dropdown/Dropdown';
-import { FloatLabel } from '@/components/lib/floatlabel/Floatlabel';
+import { FloatLabel } from '@/components/lib/floatlabel/FloatLabel';
 import { useState } from 'react';
 
 export function FloatLabelDoc(props) {
