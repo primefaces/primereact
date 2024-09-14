ac07d5c8a76bee3e2442e7f2675c7ba1099e1b32
Melloware <mellowaredev@gmail.com>
2024-09-14 11:10:47 -0400

    Fix Menu templateDoc example

--- components/doc/menu/templatedoc.js
+++ components/doc/menu/templatedoc.js
@@ -126,6 +126,7 @@ import React from 'react';
 import { Menu } from 'primereact/menu';
 import { Badge } from 'primereact/badge';
 import { Avatar } from 'primereact/avatar';
+import { classNames } from 'primereact/utils';
 
 export default function TemplateDemo() {
     const itemRenderer = (item) => (
@@ -247,6 +248,7 @@ import { Menu } from 'primereact/menu';
 import { Toast } from 'primereact/toast';
 import { Badge } from 'primereact/badge';
 import { MenuItem } from 'primereact/menuitem';
+import { classNames } from 'primereact/utils';
 
 export default function TemplateDemo() {
     const itemRenderer = (item) => (
