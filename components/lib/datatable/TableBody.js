504afcc5c6da06d263bf364ef3ed2c3f174c3f67
Jared Scott <jared.scott@variable.team>
2024-09-19 21:23:46 +0800

    fix: #7200 Fix Drag Selection for Datatable (#7217)
    * fix: #7200 Fix Drag Selection for Datatable
    
    * fix: #7200
    
    Don't restructure `originalEvent` in `allowRowDrag`, just pass the full event through from `onRowDragStart`

--- components/lib/datatable/TableBody.js
+++ components/lib/datatable/TableBody.js
@@ -668,7 +668,7 @@ export const TableBody = React.memo(
         const onRowDragStart = (e) => {
             const { originalEvent: event, index } = e;
 
-            if (allowRowDrag(event)) {
+            if (allowRowDrag(e)) {
                 rowDragging.current = true;
                 draggedRowIndex.current = index;
                 event.dataTransfer.setData('text', 'b'); // For firefox
