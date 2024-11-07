daeeef6e60d9eebdf20c587b7460331418082118
Cagatay Civici <cagatay.civici@gmail.com>
2024-11-07 16:32:22 +0300

    Add uikit page

--- pages/uikit/index.js
+++ pages/uikit/index.js
@@ -21,7 +21,7 @@ const UIKitPage = (props) => {
                             <div className="w-full md:w-6">
                                 <div className="text-primary font-bold mb-2">UP-TO-DATE</div>
                                 <div className="text-900 text-5xl font-bold mb-3">Best Features of Figma</div>
-                                <p className="mb-3 text-lg">PrimeOne for Figma uses the latest powerful features like components, variants, auto layout, styles and interactive components. It'll always follow the best practices.</p>
+                                <p className="mb-3 text-lg">PrimeOne for Figma uses the latest powerful features like components, variants, auto layout, styles, variables and interactive components. It'll always follow the best practices.</p>
 
                                 <ul className="flex flex-wrap m-0 p-0 text-lg">
                                     <li className="flex align-items-center w-6 p-3">
@@ -34,7 +34,7 @@ const UIKitPage = (props) => {
                                     </li>
                                     <li className="flex align-items-center w-6 p-3">
                                         <i className="pi pi-check-circle text-green-600 mr-2" />
-                                        <span className="font-bold">Styles</span>
+                                        <span className="font-bold">Variables and Styles</span>
                                     </li>
                                     <li className="flex align-items-center w-6 p-3">
                                         <i className="pi pi-check-circle text-green-600 mr-2" />
@@ -87,16 +87,16 @@ const UIKitPage = (props) => {
                             <div className="w-full md:w-6">
                                 <div className="text-primary font-bold mb-2">DARK MODE</div>
                                 <div className="text-900 text-5xl font-bold mb-3">Two Themes</div>
-                                <p className="mb-3 text-lg">PrimeOne is designed based on Lara Blue Light and Lara Blue Dark themes. Easily change the themes of your designs using Figma's Swap Library feature.</p>
+                                <p className="mb-3 text-lg">PrimeOne is designed based on Aura Light and Aura Dark themes. Easily change the themes of your designs using Figma's Swap Library feature or Tokens Studio Sets.</p>
 
                                 <ul className="flex flex-wrap m-0 p-0 text-lg">
                                     <li className="flex align-items-center w-6 p-3">
                                         <i className="pi pi-check-circle text-green-600 mr-2" />
-                                        <span className="font-bold">Lara Light</span>
+                                        <span className="font-bold">Aura Light</span>
                                     </li>
                                     <li className="flex align-items-center w-6 p-3">
                                         <i className="pi pi-check-circle text-green-600 mr-2" />
-                                        <span className="font-bold">Lara Dark</span>
+                                        <span className="font-bold">Aura Dark</span>
                                     </li>
                                 </ul>
                             </div>
@@ -118,9 +118,13 @@ const UIKitPage = (props) => {
                                         <i className="pi pi-check-circle text-green-600 mr-2" />
                                         <span className="font-bold">Light and Dark Sets</span>
                                     </li>
-                                    <li className="flex align-items-center w-12 p-3">
+                                    <li className="flex align-items-center w-6 p-3">
                                         <i className="pi pi-check-circle text-green-600 mr-2" />
                                         <span className="font-bold">Well Documented</span>
+                                    </li>
+                                    <li className="flex align-items-center w-6 p-3">
+                                        <i className="pi pi-check-circle text-green-600 mr-2" />
+                                        <span className="font-bold">Primitive, Semantic and Component Tokens</span>
                                     </li>
                                 </ul>
                             </div>
@@ -130,17 +134,17 @@ const UIKitPage = (props) => {
                         </div>
                     </div>
 
-                    <div className="flex flex-column md:flex-row gap-4 md:gap-8 mb-5">
+                    <div className="flex flex-column md:flex-row gap-6 md:gap-6 mb-5">
                         <a
-                            href="https://www.figma.com/file/96Tql1EywmMtVlSFTkQ2MB/Preview-%7C-PrimeOne-%7C-2.2.0?type=design&node-id=806%3A36648&mode=design&t=CPR8n1g9WLc5D2Hs-1"
+                            href="https://www.figma.com/design/G855HjuSyK8viJr0a5ZjRG/Preview-%7C%C2%A0PrimeOne-%7C-3.0.1?node-id=830-41631&t=m1MbOTTqKsBSRBLS-1"
                             className="p-5 w-full md:w-6 bg-white flex flex-column align-items-center border-2 border-transparent hover:border-primary transition-colors transition-duration-300"
                             style={{ borderRadius: '50px' }}
                         >
-                            <span className="text-gray-900 text-4xl font-bold mb-5">Preview Light</span>
+                            <span className="text-gray-900 text-4xl font-bold mb-5">Preview</span>
                             <img alt="Figma Light" src="https://primefaces.org/cdn/primereact/images/uikit/logo-figma-light.svg" className="w-4rem" />
                         </a>
                         <a
-                            href="https://www.figma.com/file/ujRD6FFCWw1bE0h4wIebxl/Preview-%7C-Dark-%7C-PrimeOne-%7C-2.2.0?type=design&node-id=806%3A36648&mode=design&t=bG7aorZhOFIqX1qz-1"
+                            href="https://www.figma.com/design/XBQzDl4vDOO0pyxEGYcICt/Preview-%7C%C2%A0Dark-%7C-PrimeOne-%7C-3.0.1?node-id=806-36648&t=7AME0kw905t3PVVY-1"
                             className="p-5 w-full md:w-6 bg-gray-900 flex flex-column align-items-center border-2 border-transparent hover:border-primary transition-colors transition-duration-300"
                             style={{ borderRadius: '50px' }}
                         >
