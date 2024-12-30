6ff0153933cd5f8788e53f5e8b255f0e804c6e1e
Melloware <mellowaredev@gmail.com>
2024-12-30 10:51:38 -0500

    Fix #7436: Button tooltip prevent display order collision (#7533)

--- components/lib/tooltip/Tooltip.js
+++ components/lib/tooltip/Tooltip.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PrimeReact, { PrimeReactContext } from '../api/Api';
 import { useHandleStyle } from '../componentbase/ComponentBase';
-import { useMergeProps, useMountEffect, useOverlayScrollListener, useResizeListener, useUnmountEffect, useUpdateEffect, useGlobalOnEscapeKey, ESC_KEY_HANDLING_PRIORITIES } from '../hooks/Hooks';
+import { ESC_KEY_HANDLING_PRIORITIES, useDisplayOrder, useGlobalOnEscapeKey, useMergeProps, useMountEffect, useOverlayScrollListener, useResizeListener, useUnmountEffect, useUpdateEffect } from '../hooks/Hooks';
 import { Portal } from '../portal/Portal';
 import { DomHandler, ObjectUtils, ZIndexUtils, classNames } from '../utils/Utils';
 import { TooltipBase } from './TooltipBase';
@@ -15,6 +15,8 @@ export const Tooltip = React.memo(
         const [positionState, setPositionState] = React.useState(props.position || 'right');
         const [classNameState, setClassNameState] = React.useState('');
         const [multipleFocusEvents, setMultipleFocusEvents] = React.useState(false);
+        const isCloseOnEscape = visibleState && props.closeOnEscape;
+        const overlayDisplayOrder = useDisplayOrder('tooltip', isCloseOnEscape);
         const metaData = {
             props,
             state: {
@@ -37,8 +39,8 @@ export const Tooltip = React.memo(
             callback: () => {
                 hide();
             },
-            when: props.closeOnEscape,
-            priority: [ESC_KEY_HANDLING_PRIORITIES.TOOLTIP, 0]
+            when: isCloseOnEscape,
+            priority: [ESC_KEY_HANDLING_PRIORITIES.TOOLTIP, overlayDisplayOrder]
         });
         const elementRef = React.useRef(null);
         const textRef = React.useRef(null);
