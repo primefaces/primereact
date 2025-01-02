5a976c4c3590d54766357c649ebc8f239b9d47b7
Melloware <mellowaredev@gmail.com>
2025-01-02 16:32:14 -0500

    Fix #7180 Screenreader improve hidden accessible (#7552)

--- components/lib/componentbase/ComponentBase.js
+++ components/lib/componentbase/ComponentBase.js
@@ -4,10 +4,17 @@ import { ObjectUtils, classNames, mergeProps } from '../utils/Utils';
 
 const baseStyle = `
 .p-hidden-accessible {
-    position: absolute;
-    pointer-events: none;
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
     opacity: 0;
+    overflow: hidden;
+    padding: 0;
+    pointer-events: none;
+    position: absolute;
     white-space: nowrap;
+    width: 1px;
 }
 
 .p-overflow-hidden {
