8bb34933d25f163e6499a6bde364712897659c8f
Poonam Ghewande <prghewande55@gmail.com>
2025-03-05 10:55:47 +0530

    fix reset focus to next index on keyboard tab click after selcting value from Calender

--- components/lib/calendar/Calendar.js
+++ components/lib/calendar/Calendar.js
@@ -1670,6 +1670,7 @@ export const Calendar = React.memo(
             if (!props.inline && isSingleSelection() && (!props.showTime || props.hideOnDateTimeSelect) && !isUpdateViewDate) {
                 setTimeout(() => {
                     hide('dateselect');
+                    
                 }, 100);
 
                 if (touchUIMask.current) {
