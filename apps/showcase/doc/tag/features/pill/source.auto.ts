57bf3d11e4fc90e81166692e2c0797e7cea1ac6f
tanerengiiin <tanerengin4444@gmail.com>
2025-04-11 12:00:31 +0300

    feat: add Tag component

---
+++ apps/showcase/doc/tag/features/pill/source.auto.ts
@@ -0,0 +1,7 @@
+/****************************************************************************
+****************** PrimeReact Demo Source (Auto-Generated) ******************
+*****************************************************************************/
+
+export const source = {
+    "code": "import { Tag } from 'primereact/tag';\n\nexport default function BasicDemo() {\n    return (\n        <div className=\"card flex flex-wrap justify-center gap-2\">\n            <Tag rounded>\n                <Tag.Label>Primary</Tag.Label>\n            </Tag>\n            <Tag severity=\"secondary\" rounded>\n                <Tag.Label>Secondary</Tag.Label>\n            </Tag>\n            <Tag severity=\"success\" rounded>\n                <Tag.Label>Success</Tag.Label>\n            </Tag>\n            <Tag severity=\"info\" rounded>\n                <Tag.Label>Info</Tag.Label>\n            </Tag>\n            <Tag severity=\"warn\" rounded>\n                <Tag.Label>Warn</Tag.Label>\n            </Tag>\n            <Tag severity=\"danger\" rounded>\n                <Tag.Label>Danger</Tag.Label>\n            </Tag>\n            <Tag severity=\"contrast\" rounded>\n                <Tag.Label>Contrast</Tag.Label>\n            </Tag>\n        </div>\n    );\n}\n"
+};
