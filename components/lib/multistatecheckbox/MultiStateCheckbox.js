80e978b8de12e194d604589c82ae915dc503b5dd
timmy471 <timilehin.ayantunji@etranzact.com>
2025-05-18 18:32:45 +0100

    feat(MultiStateCheckbox): add id prop to improve accessibility

--- components/lib/multistatecheckbox/MultiStateCheckbox.js
+++ components/lib/multistatecheckbox/MultiStateCheckbox.js
@@ -170,6 +170,7 @@ export const MultiStateCheckbox = React.memo(
 
         const checkboxProps = mergeProps(
             {
+                id: props.id,
                 className: classNames(props.className),
                 style: sx('checkbox', { selectedOption }),
                 tabIndex: props.tabIndex,
