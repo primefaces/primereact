baf5bdd6c6f41607c29e0f536a54df367f4e9f69
tanerengiiin <tanerengin4444@gmail.com>
2025-05-23 15:14:29 +0300

    feat(docs): add sticky TOC ad

--- apps/showcase/components/doc/DocToc.tsx
+++ apps/showcase/components/doc/DocToc.tsx
@@ -2,6 +2,7 @@
 
 import { TableOfContents } from '@/lib/utils/getTableOfContents';
 import { cn } from '@primeuix/utils';
+import { Button } from 'primereact/button';
 import * as React from 'react';
 
 function useActiveItem(itemIds: string[]) {
@@ -132,16 +133,28 @@ export default function DocToc({ toc }: DocTocProps) {
                 } as React.CSSProperties
             }
         >
-            <div className="flex items-center gap-2 ">
-                <i className="pi pi-align-left opacity-50 !text-sm !leading-none"></i>
-                <span className="font-medium uppercase text-sm tracking-wide">On this page</span>
-            </div>
-            <div
-                className="relative mt-4 pl-4
+            <div className="max-h-[calc(100vh-300px)] overflow-y-auto">
+                <div className="flex items-center gap-2 ">
+                    <i className="pi pi-align-left opacity-50 !text-sm !leading-none"></i>
+                    <span className="font-medium uppercase text-sm tracking-wide">On this page</span>
+                </div>
+                <div
+                    className="relative mt-4 pl-4
             after:content-[''] after:absolute after:rounded-full after:left-0 after:transition-all after:duration-200 after:top-(--top) after:h-(--height) after:bg-primary after:w-px
             before:content-[''] before:absolute before:rounded-full before:left-0 before:top-0 before:h-full before:bg-surface-200 dark:before:bg-surface-800 before:w-px"
-            >
-                <Tree tree={toc} activeId={activeId} onItemClick={onItemClick} />
+                >
+                    <Tree tree={toc} activeId={activeId} onItemClick={onItemClick} />
+                </div>
+            </div>
+            <div className="mt-8 rounded-lg border border-surface-200 dark:border-surface-800 px-4 py-6 bg-surface-0 dark:bg-surface-900">
+                <div className="text-2xl font-semibold flex flex-col gap-2 text-center">
+                    <span className="leading-none">Build Faster </span>
+                    <span className="leading-none  text-primary">Design Better</span>
+                </div>
+                <div className="text-center text-[14px] mt-4 text-surface-500">490+ ready to use UI blocks to build spectacular applications in no time</div>
+                <Button as={'a'} href="https://primeblocks.org/" target="_blank" rounded variant="outlined" className="!mx-auto mt-4 !flex !w-fit">
+                    Browse Components
+                </Button>
             </div>
         </div>
     );
