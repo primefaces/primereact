a425af097c80033922a7c9e35c9a6466c46559b9
Mert Sincan <sincan.mert@gmail.com>
2025-05-26 13:54:11 +0100

    feat(withBaseIcon): enhance rootProps by merging style and className from props

--- packages/icons/src/base/index.ts
+++ packages/icons/src/base/index.ts
@@ -1,6 +1,6 @@
 import { defaultProps, styles, withIcon } from '@primereact/core/icon';
 import type { withBaseIconOptions } from '@primereact/types/core';
-import { mergeProps } from '@primeuix/utils';
+import { cn, mergeProps, resolve } from '@primeuix/utils';
 
 export const withBaseIcon = <IProps, Exposes extends Record<PropertyKey, unknown>>({ name, render }: withBaseIconOptions<IProps, Exposes, typeof styles>) => {
     return withIcon({
@@ -8,13 +8,13 @@ export const withBaseIcon = <IProps, Exposes extends Record<PropertyKey, unknown
         styles,
         defaultProps,
         render(instance) {
-            const { id, ptmi, pti, cx, sx } = instance;
+            const { id, ptmi, pti, cx, sx, props } = instance;
 
             const rootProps = mergeProps(
                 {
                     id,
-                    style: sx('root'),
-                    className: cx('root')
+                    style: { ...sx('root'), ...resolve(props.style, instance) },
+                    className: cn(cx('root'), resolve(props.className, instance))
                 },
                 pti(),
                 ptmi('root')
