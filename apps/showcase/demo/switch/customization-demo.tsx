c999e2cfca9322fc6b801e81478defa87958912c
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-05-29 14:45:54 +0300

    refactor: customization demo update

--- apps/showcase/demo/switch/customization-demo.tsx
+++ apps/showcase/demo/switch/customization-demo.tsx
@@ -7,18 +7,18 @@ export default function CustomizationDemo() {
 
     return (
         <div className="card flex justify-center items-center gap-2">
-            <div className="flex items-center gap-2 bg-surface-50 hover:bg-surface-100 dark:bg-slate-700 hover:dark:bg-slate-800 p-4 rounded-md">
+            <label htmlFor="custom" className="flex items-center gap-2 bg-surface-50 hover:bg-surface-100 dark:bg-slate-700 hover:dark:bg-slate-800 p-4 rounded-md">
                 <div className="flex flex-col gap-1">
                     <p className="!m-0 !text-medium">Try Beta Features</p>
                     <p className="!m-0 !text-sm !text-slate-400">Experience upcoming features before they&apos;re officially released.</p>
                 </div>
 
-                <Switch checked={checked} onCheckedChange={(event: SwitchChangeEvent) => setChecked(event.checked)}>
+                <Switch inputId="custom" checked={checked} onCheckedChange={(event: SwitchChangeEvent) => setChecked(event.checked)}>
                     <Switch.Control className={`${checked ? '!bg-blue-300' : '!bg-surface-300 dark:!bg-surface-500'} !rounded-md`}>
                         <Switch.Thumb className="!bg-blue-900" />
                     </Switch.Control>
                 </Switch>
-            </div>
+            </label>
         </div>
     );
 }
