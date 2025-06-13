8e63ff6238d7a078b577344ddae1fe9f77890a34
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-06-13 15:12:28 +0300

    feat: Add Toolbar

---
+++ packages/primereact/src/toolbar/index.ts
@@ -0,0 +1,4 @@
+'use client';
+export * from './Toolbar';
+export * from './Toolbar.context';
+export * from './Toolbar.props';
---
+++ packages/types/src/shared/toolbar/useToolbar.types.ts
@@ -0,0 +1,31 @@
+/**
+ *
+ * The useToolbar manages the state and functionality of a toolbar component.
+ *
+ * [Live Demo](https://www.primereact.org/toolbar/)
+ *
+ * @module usetoolbar
+ * @group headless
+ *
+ */
+import type { HeadlessInstance } from '@primereact/types/core';
+
+/**
+ * Defines valid properties in useToolbar.
+ */
+export interface useToolbarProps {}
+
+/**
+ * Defines valid state in useToolbar.
+ */
+export interface useToolbarState {}
+
+/**
+ * Defines the methods and properties exposed by useToolbar.
+ */
+export interface useToolbarExposes {}
+
+/**
+ * Instance of useToolbar headless.
+ */
+export type useToolbarInstance = HeadlessInstance<useToolbarProps, useToolbarState, useToolbarExposes>;
