40d4e85049bf22e57e0508029ae3fa6b67944cc9
tanerengiiin <tanerengin4444@gmail.com>
2025-07-02 11:50:45 +0300

    refactor: update DocLinkCard component to accept className prop

--- apps/showcase/components/doc/DocLinkCard.tsx
+++ apps/showcase/components/doc/DocLinkCard.tsx
@@ -1,12 +1,12 @@
 import { cn } from '@primeuix/utils';
 import React from 'react';
 
-const DocLinkCard: React.FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = ({ children, target = '_blank', rel = 'noopener noreferrer', ...props }) => {
+const DocLinkCard: React.FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = ({ children, className, target = '_blank', rel = 'noopener noreferrer', ...props }) => {
     return (
         <a
             className={cn(
                 'cursor-pointer bg-gradient-to-b from-surface-0 to-surface-50 dark:from-surface-700/75 dark:to-surface-800 flex flex-col gap-4 border dark:border-0 border-surface-200 dark:border-surface-800/0 rounded-lg p-6 pb-8 shadow-[0_-8px_0px_0_color-mix(in_srgb,var(--color-surface-900)_8%,transparent)_inset] dark:shadow-[0_-8px_0px_0_color-mix(in_srgb,var(--color-surface-950)_48%,transparent)_inset] hover:opacity-75 transition-opacity duration-150 ',
-                props.className
+                className
             )}
             target={target}
             rel={rel}
