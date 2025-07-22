37d8dc61d0624fc8caea009c3ed780da33b7b257
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-07-22 13:20:57 +0300

    fix: warnings

--- apps/showcase/demo/confirmpopup/confirmpopup-pt.tsx
+++ apps/showcase/demo/confirmpopup/confirmpopup-pt.tsx
@@ -18,38 +18,6 @@ export default function ConfirmPopupPT() {
                     </ConfirmPopup.Footer>
                 </ConfirmPopup.Portal>
             </ConfirmPopup>
-            {/* <ConfirmPopup />
-            <div
-                id="pr_id_r4a"
-                role="alertdialog"
-                aria-modal="true"
-                data-pc-name="motion"
-                data-pc-section="root"
-                className="p-confirmpopup p-component !relative"
-                style={{
-                    '--pui-motion-height': '82px',
-                    zIndex: 1107,
-                    insetInlineStart: '1648.12px',
-                    top: '874.953px',
-                    transformOrigin: 'center top',
-                    '--p-confirmpopup-arrow-left': '0px'
-                }}
-            >
-                <div className="p-confirmpopup-content" data-pc-section="root" data-pc-name="confirmpopupcontent">
-                    <span className="p-confirmpopup-icon pi pi-exclamation-triangle" data-pc-section="root" data-pc-name="confirmpopupicon"></span>
-                    <span className="p-confirmpopup-message" data-pc-section="root" data-pc-name="confirmpopupmessage">
-                        Are you sure you want to proceed?
-                    </span>
-                </div>
-                <div className="p-confirmpopup-footer" data-pc-section="root" data-pc-name="confirmpopupfooter">
-                    <button id="pr_id_r5s" className="p-button p-component p-button-secondary p-button-outlined p-button-sm p-confirmpopup-reject-button" data-pc-name="reject" data-pc-section="root" data-pc-extend="button">
-                        Cancel
-                    </button>
-                    <button id="pr_id_r62" className="p-button p-component p-button-sm p-confirmpopup-accept-button" data-pc-name="accept" data-pc-section="root" data-pc-extend="button">
-                        Save
-                    </button>
-                </div>
-            </div> */}
         </>
     );
 }
