cf33741f0b3ce30d05ec439c81564c6ec9148f29
tanerengiiin <tanerengin4444@gmail.com>
2025-07-30 14:55:11 +0300

    feat: add rating demos and docs

--- apps/showcase/demo/rating/template-demo.tsx
+++ apps/showcase/demo/rating/template-demo.tsx
@@ -3,11 +3,20 @@ import { Rating } from 'primereact/rating';
 function TemplateDemo() {
     return (
         <div className="card flex justify-center">
-            <Rating
-                modelValue={3}
-                onIcon={<img src="https://primefaces.org/cdn/primevue/images/rating/custom-onicon.png" height="24" width="24" />}
-                offIcon={<img src="https://primefaces.org/cdn/primevue/images/rating/custom-officon.png" height="24" width="24" />}
-            />
+            <Rating modelValue={3} allowHalf={false}>
+                <Rating.Option
+                    onIcon={
+                        <span className="w-6 h-6">
+                            <img src="https://primefaces.org/cdn/primevue/images/rating/custom-onicon.png" className="w-6 h-6" />
+                        </span>
+                    }
+                    offIcon={
+                        <span className="w-6 h-6">
+                            <img src="https://primefaces.org/cdn/primevue/images/rating/custom-officon.png" />
+                        </span>
+                    }
+                />
+            </Rating>
         </div>
     );
 }
