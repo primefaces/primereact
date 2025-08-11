cf23a015d8a126a15988695f88187804d0ab62c9
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-08-11 16:21:25 +0300

    feat: InputGroup docs added

---
+++ apps/showcase/demo/inputgroup/basic-demo.tsx
@@ -0,0 +1,20 @@
+import { InputGroup } from 'primereact/inputgroup';
+import { InputText } from 'primereact/inputtext';
+
+export default function BasicDemo() {
+    return (
+        <div className="card grid grid-cols-1 md:grid-cols-2 gap-4">
+            <InputGroup>
+                <InputGroup.Addon>
+                    <i className="pi pi-user"></i>
+                </InputGroup.Addon>
+                <InputText placeholder="Username" />
+            </InputGroup>
+
+            <InputGroup>
+                <InputGroup.Addon>www</InputGroup.Addon>
+                <InputText placeholder="Website" />
+            </InputGroup>
+        </div>
+    );
+}
