e994b2aa267cf9f6d6cdba05486b8667264010b4
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-08-26 15:08:53 +0300

    chore: Fluid docs added

---
+++ apps/showcase/demo/fluid/fluid-pt.tsx
@@ -0,0 +1,10 @@
+import { Fluid } from 'primereact/fluid';
+import { InputText } from 'primereact/inputtext';
+
+export default function FluidPTDemo() {
+    return (
+        <Fluid>
+            <InputText />
+        </Fluid>
+    );
+}
