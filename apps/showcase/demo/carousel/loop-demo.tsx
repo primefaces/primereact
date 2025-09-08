3dfab7b3f9610c02cc9df0cef02722a66362ba36
tanerengiiin <tanerengin4444@gmail.com>
2025-09-08 12:35:44 +0300

    feat: add carousel demo & doc

---
+++ apps/showcase/demo/carousel/loop-demo.tsx
@@ -0,0 +1,34 @@
+import { Carousel } from 'primereact/carousel';
+
+function LoopDemo() {
+    return (
+        <div className="mt-8 mb-16">
+            <Carousel className="max-w-3xl mx-auto" loop align="center">
+                <Carousel.Content className="h-[200px]">
+                    {Array.from({ length: 5 }).map((_, i) => (
+                        <Carousel.Item key={i} size={60}>
+                            <div className="p-1 h-full">
+                                <div className="h-full bg-surface-0 dark:bg-surface-900 text-surface-950 dark:text-surface-0 flex flex-col items-center justify-center gap-6 rounded-xl border border-surface shadow-sm">
+                                    <div className="flex items-center justify-center text-5xl font-semibold">{i + 1}</div>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel.Content>
+                <div className="flex items-center justify-between mt-4">
+                    <Carousel.Indicators />
+                    <div className="flex items-center justify-end gap-4">
+                        <Carousel.Prev className="w-10 h-10 flex items-center justify-center rounded-full border border-surface bg-surface-0 dark:bg-surface-800 text-surface-500 dark:text-surface-400 hover:opacity-75 cursor-pointer transition-opacity">
+                            <i className="pi pi-chevron-left text-lg"></i>
+                        </Carousel.Prev>
+                        <Carousel.Next className="w-10 h-10 flex items-center justify-center rounded-full border border-surface bg-surface-0 dark:bg-surface-800 text-surface-500 dark:text-surface-400 hover:opacity-75 cursor-pointer transition-opacity">
+                            <i className="pi pi-chevron-right text-lg"></i>
+                        </Carousel.Next>
+                    </div>
+                </div>
+            </Carousel>
+        </div>
+    );
+}
+
+export default LoopDemo;
