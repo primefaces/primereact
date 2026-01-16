c897dbced521eab4791e5e420faf6d341b321caf
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2026-01-16 09:06:53 +0300

    fix: add input to ui password

--- packages/@primereact/ui/src/password/UIPassword.parts.ts
+++ packages/@primereact/ui/src/password/UIPassword.parts.ts
@@ -1,2 +1,3 @@
-export { PasswordClearIcon as ClearIcon, PasswordInput as Input, PasswordMeter as Meter, PasswordPortal as Portal, PasswordStrength as Strength } from 'primereact/password';
+export { PasswordClearIcon as ClearIcon, PasswordMeter as Meter, PasswordPortal as Portal, PasswordStrength as Strength } from 'primereact/password';
 export { UIPasswordRoot as Root } from './root';
+export { UIPasswordInput as Input } from './input';
