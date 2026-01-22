b8b0cb4ecca36f4bd6e1a56b44054d76b053c093
tanerengiiin <tanerengin4444@gmail.com>
2026-01-22 13:55:46 +0300

    chore: update switch demo & docs

---
+++ apps/showcase/demo/tailwind/switch/basic-demo.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Switch } from '@/components/ui/switch';
+
+export default function BasicDemo() {
+    return (
+        <label className="flex justify-center items-center gap-2">
+            Off
+            <Switch />
+            On
+        </label>
+    );
+}
--- apps/showcase/demo/tailwind/switch/template-demo.tsx
+++ apps/showcase/demo/tailwind/switch/template-demo.tsx
@@ -5,8 +5,9 @@ import { Switch } from '@/components/ui/switch';
 export default function TemplateDemo() {
     return (
         <div className="flex justify-center">
-            <Switch>
-                <i className="pi pi-star-fill text-[11px] text-surface-400 dark:text-surface-500 group-data-[p-checked=true]:text-primary"></i>
+            <Switch className="group">
+                <i className="pi pi-times text-[10px] absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-surface-400 dark:text-surface-600 group-data-checked:opacity-0 opacity-100 transition-opacity duration-200" />
+                <i className="pi pi-check text-[10px] absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-primary group-data-checked:opacity-100 opacity-0 transition-opacity duration-200" />
             </Switch>
         </div>
     );
