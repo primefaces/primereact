093568a72429a0cbedaa568a430e44f987eab255
tanerengiiin <tanerengin4444@gmail.com>
2026-01-22 13:51:03 +0300

    feat: add Button tailwind version

--- packages/tailwind/src/button.tsx
+++ packages/tailwind/src/button.tsx
@@ -5,7 +5,7 @@ import { Button as PRButton } from 'primereact/button';
 import * as React from 'react';
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg font-medium text-sm transition-[background-color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 focus-visible:outline-2 focus-visible:outline-offset-2",
+    'inline-flex gap-2 cursor-pointer select-none items-center justify-center overflow-hidden relative rounded-md font-medium disabled:pointer-events-none disabled:opacity-60 transition-[background-color,color] duration-200 focus-visible:outline-1 focus-visible:outline-offset-2',
     {
         variants: {
             variant: {
@@ -15,17 +15,17 @@ const buttonVariants = cva(
                 outlined: ''
             },
             severity: {
-                default: 'focus-visible:outline-primary focus-visible:outline-primary-500/20 dark:focus-visible:outline-primary-500/30',
-                secondary: 'focus-visible:outline-surface-500/20 dark:focus-visible:outline-surface-500/30',
-                info: ' focus-visible:outline-blue-500/50',
-                success: 'focus-visible:outline-green-500/50',
-                warn: 'focus-visible:outline-amber-500/50',
-                danger: 'focus-visible:outline-red-500/50',
-                contrast: 'focus-visible:outline-surface-500/50',
-                help: 'focus-visible:outline-purple-500/50'
+                default: 'focus-visible:outline-primary',
+                secondary: 'focus-visible:outline-surface-600 dark:focus-visible:outline-surface-300',
+                info: ' focus-visible:outline-blue-500',
+                success: 'focus-visible:outline-green-500',
+                warn: 'focus-visible:outline-amber-500',
+                danger: 'focus-visible:outline-red-500',
+                contrast: 'focus-visible:outline-surface-950 dark:focus-visible:outline-surface-0',
+                help: 'focus-visible:outline-violet-500'
             },
             raised: {
-                true: 'shadow'
+                true: 'shadow-sm'
             },
             rounded: {
                 true: 'rounded-full!'
@@ -34,9 +34,9 @@ const buttonVariants = cva(
                 true: 'p-0'
             },
             size: {
-                small: 'h-8 gap-1.5 px-3 has-[>svg]:px-2.5',
-                normal: 'h-9 px-4 py-2 has-[>svg]:px-3',
-                large: 'h-10 px-6 has-[>svg]:px-4'
+                small: "py-1 px-2 text-xs [&_svg:not([class*='size-'])]:size-3",
+                normal: "py-1.5 px-2.5 text-sm [&_svg:not([class*='size-'])]:size-3.5",
+                large: "py-2 px-3 [&_svg:not([class*='size-'])]:size-4"
             }
         },
         defaultVariants: {
@@ -50,137 +50,138 @@ const buttonVariants = cva(
             {
                 variant: 'default',
                 severity: 'default',
-                className: 'bg-primary hover:bg-primary-emphasis text-primary-contrast '
+                className: 'bg-primary hover:bg-primary-emphasis active:bg-primary-emphasis-alt text-primary-contrast'
             },
             {
                 variant: 'default',
                 severity: 'secondary',
-                className: 'bg-surface-100 hover:bg-surface-200 dark:bg-surface-800 dark:hover:bg-surface-700 '
+                className:
+                    'bg-surface-100 hover:bg-surface-200 active:bg-surface-300 text-surface-600 hover:text-surface-700 active:text-surface-800 dark:bg-surface-800 dark:hover:bg-surface-700 dark:active:bg-surface-600 dark:text-surface-300 dark:hover:text-surface-200 dark:active:text-surface-100'
             },
             {
                 variant: 'default',
                 severity: 'info',
-                className: 'bg-blue-500 text-surface-0 hover:bg-blue-600'
+                className: 'bg-blue-500 text-surface-0 hover:bg-blue-600 active:bg-blue-700'
             },
             {
                 variant: 'default',
                 severity: 'success',
-                className: 'bg-green-500 text-surface-0 hover:bg-green-600'
+                className: 'bg-green-500 text-surface-0 hover:bg-green-600 active:bg-green-700'
             },
             {
                 variant: 'default',
                 severity: 'warn',
-                className: 'bg-amber-500 text-surface-0 hover:bg-amber-600'
+                className: 'bg-amber-500 text-surface-0 hover:bg-amber-600 active:bg-amber-700'
             },
             {
                 variant: 'default',
                 severity: 'danger',
-                className: 'bg-red-500 text-surface-0 hover:bg-red-600'
+                className: 'bg-red-500 text-surface-0 hover:bg-red-600 active:bg-red-700'
             },
             {
                 variant: 'default',
                 severity: 'help',
-                className: 'bg-purple-500 text-surface-0 hover:bg-purple-600'
+                className: 'bg-violet-500 text-surface-0 hover:bg-violet-600 active:bg-violet-700'
             },
             {
                 variant: 'default',
                 severity: 'contrast',
-                className: 'bg-surface-950 text-surface-0 hover:bg-surface-800 dark:bg-surface-0 dark:hover:bg-surface-200 dark:text-surface-950'
+                className: 'bg-surface-950 hover:bg-surface-900 active:bg-surface-800 text-white dark:bg-surface-0 dark:hover:bg-surface-100 dark:active:bg-surface-200 dark:text-surface-950'
             },
             {
                 variant: 'outlined',
                 severity: 'default',
-                className: 'border border-primary hover:bg-primary/10 text-primary'
+                className: 'border border-primary-200 dark:border-primary-700 hover:bg-primary-50 active:bg-primary-100 dark:hover:bg-primary/5 dark:active:bg-primary/15 text-primary'
             },
             {
                 variant: 'outlined',
                 severity: 'secondary',
-                className: 'border border-surface-200 dark:border-surface-800 hover:bg-surface-100 dark:hover:bg-surface-800 text-surface-500'
+                className: 'border border-surface-200 dark:border-surface-700 hover:bg-surface-50 active:bg-surface-100 dark:hover:bg-white/5 dark:active:bg-white/15 text-surface-500 dark:text-surface-400'
             },
             {
                 variant: 'outlined',
                 severity: 'info',
-                className: 'border border-blue-500 hover:bg-blue-500/10 text-blue-500'
+                className: 'border border-blue-200 dark:border-blue-700 hover:bg-blue-500/10 active:bg-blue-500/20 text-blue-500'
             },
             {
                 variant: 'outlined',
                 severity: 'success',
-                className: 'border border-green-500 hover:bg-green-500/10 text-green-500'
+                className: 'border border-green-200 dark:border-green-700 hover:bg-green-500/10 active:bg-green-500/20 text-green-500'
             },
             {
                 variant: 'outlined',
                 severity: 'warn',
-                className: 'border border-amber-500 hover:bg-amber-500/10 text-amber-500'
+                className: 'border border-amber-200 dark:border-amber-700 hover:bg-amber-500/10 active:bg-amber-500/20 text-amber-500'
             },
             {
                 variant: 'outlined',
                 severity: 'danger',
-                className: 'border border-red-500 hover:bg-red-500/10 text-red-500'
-            },
-            {
-                variant: 'outlined',
-                severity: 'contrast',
-                className: 'border border-gray-500 hover:bg-gray-500/10 text-gray-500'
+                className: 'border border-red-200 dark:border-red-700 hover:bg-red-500/10 active:bg-red-500/20 text-red-500'
             },
             {
                 variant: 'outlined',
                 severity: 'help',
-                className: 'border border-purple-500 hover:bg-purple-500/10 text-purple-500'
+                className: 'border border-violet-200 dark:border-violet-700 hover:bg-violet-500/10 active:bg-violet-500/20 text-violet-500'
+            },
+            {
+                variant: 'outlined',
+                severity: 'contrast',
+                className: 'border border-surface-700 dark:border-surface-500 hover:bg-surface-50 active:bg-surface-100 text-surface-950 dark:text-surface-0 dark:hover:bg-surface-800 dark:active:bg-surface-700'
             },
             {
                 variant: 'text',
                 severity: 'default',
-                className: 'text-primary hover:bg-primary/10'
+                className: 'hover:bg-primary-50 active:bg-primary-100 dark:hover:bg-primary/5 dark:active:bg-primary/15 text-primary'
             },
             {
                 variant: 'text',
                 severity: 'secondary',
-                className: 'text-surface-500 hover:bg-surface-100 dark:text-surface-400 dark:hover:bg-surface-800'
+                className: 'hover:bg-surface-50 active:bg-surface-100 dark:hover:bg-surface-800 dark:active:bg-surface-700 text-surface-500 dark:text-surface-400'
             },
             {
                 variant: 'text',
                 severity: 'info',
-                className: 'text-blue-500 hover:bg-blue-500/10'
+                className: 'text-blue-500 hover:bg-blue-500/10 active:bg-blue-500/20'
             },
             {
                 variant: 'text',
                 severity: 'success',
-                className: 'text-green-500 hover:bg-green-500/10'
+                className: 'text-green-500 hover:bg-green-500/10 active:bg-green-500/20'
             },
             {
                 variant: 'text',
                 severity: 'warn',
-                className: 'text-amber-500 hover:bg-amber-500/10'
+                className: 'text-amber-500 hover:bg-amber-500/10 active:bg-amber-500/20'
             },
             {
                 variant: 'text',
                 severity: 'danger',
-                className: 'text-red-500 hover:bg-red-500/10'
-            },
-            {
-                variant: 'text',
-                severity: 'contrast',
-                className: 'text-surface-950 hover:bg-surface-100 dark:text-surface-0 dark:hover:bg-surface-800'
+                className: 'text-red-500 hover:bg-red-500/10 active:bg-red-500/20'
             },
             {
                 variant: 'text',
                 severity: 'help',
-                className: 'text-purple-500 hover:bg-purple-500/10'
+                className: 'text-violet-500 hover:bg-violet-500/10 active:bg-violet-500/20'
+            },
+            {
+                variant: 'text',
+                severity: 'contrast',
+                className: 'hover:bg-surface-50 active:bg-surface-100 dark:hover:bg-surface-800 dark:active:bg-surface-700 text-surface-950 dark:text-surface-0'
             },
             {
                 iconOnly: true,
                 size: 'small',
-                className: 'size-8'
+                className: 'size-7'
             },
             {
                 iconOnly: true,
                 size: 'normal',
-                className: 'size-9'
+                className: 'size-[35px]'
             },
             {
                 iconOnly: true,
                 size: 'large',
-                className: 'size-10.5'
+                className: 'size-[42px]'
             }
         ]
     }
