18d72d908b647b1e885665c15b52f500c93036d0
tanerengiiin <tanerengin4444@gmail.com>
2026-01-22 13:52:08 +0300

    chore: update button demo

--- apps/showcase/demo/tailwind/button/group-demo.tsx
+++ apps/showcase/demo/tailwind/button/group-demo.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@/components/ui/button';
-import { ButtonGroup } from 'primereact/buttongroup';
+import { ButtonGroup } from '@/components/ui/buttongroup';
 
 export default function GroupDemo() {
     return (
-        <div className="flex justify-center">
+        <div className="flex flex-col items-center justify-center gap-8">
             <ButtonGroup>
                 <Button>
                     <i className="pi pi-check" />
@@ -14,6 +14,20 @@ export default function GroupDemo() {
                     Delete
                 </Button>
                 <Button>
+                    <i className="pi pi-times" />
+                    Cancel
+                </Button>
+            </ButtonGroup>
+            <ButtonGroup>
+                <Button variant="outlined">
+                    <i className="pi pi-check" />
+                    Save
+                </Button>
+                <Button variant="outlined">
+                    <i className="pi pi-trash" />
+                    Delete
+                </Button>
+                <Button variant="outlined">
                     <i className="pi pi-times" />
                     Cancel
                 </Button>
