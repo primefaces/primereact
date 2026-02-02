1d3d5eacc8f66b4ed43694f5abf6876e98b59ab9
Taner Engin <tanerengin4444@gmail.com>
2026-02-02 12:43:35 +0300

    refactor: update Compare demo & doc

---
+++ apps/showcase/demo/styled/compare/basic-demo.tsx
@@ -0,0 +1,19 @@
+import { Compare } from '@primereact/ui/compare';
+
+export default function BasicDemo() {
+    return (
+        <Compare.Root className="aspect-video max-w-lg mx-auto">
+            <Compare.Item position="before">
+                <img src="https://primefaces.org/cdn/primevue/images/compare/island2.jpg" />
+            </Compare.Item>
+            <Compare.Item position="after">
+                <img src="https://primefaces.org/cdn/primevue/images/compare/island1.jpg" />
+            </Compare.Item>
+            <Compare.Slider>
+                <Compare.Thumb className="group flex items-center justify-center">
+                    <i className="pi pi-code group-data-[orientation=vertical]:rotate-90" />
+                </Compare.Thumb>
+            </Compare.Slider>
+        </Compare.Root>
+    );
+}
