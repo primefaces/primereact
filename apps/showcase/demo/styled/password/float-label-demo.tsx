6037e0d4eb1e1c955bfff8b2f5ba576d375177ff
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2026-02-09 15:46:07 +0300

    chore: Password popover demo updates

--- apps/showcase/demo/styled/password/float-label-demo.tsx
+++ apps/showcase/demo/styled/password/float-label-demo.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { PasswordChangeEvent, usePasswordProps } from '@primereact/types/shared/password';
+import { PasswordValueChangeEvent, usePasswordProps } from '@primereact/types/shared/password';
 import { FloatLabel } from '@primereact/ui/floatlabel';
 import { Label } from '@primereact/ui/label';
 import { Password } from '@primereact/ui/password';
@@ -13,17 +13,17 @@ export default function FloatLabelDemo() {
     return (
         <div className="flex flex-wrap justify-center items-end gap-4">
             <FloatLabel>
-                <Password value={value} onValueChange={(e: PasswordChangeEvent) => setValue(e.value as string)} />
+                <Password value={value} onValueChange={(e: PasswordValueChangeEvent) => setValue(e.value as string)} />
                 <Label htmlFor="over_label">Over Label</Label>
             </FloatLabel>
 
             <FloatLabel variant="in">
-                <Password value={value2} onValueChange={(e: PasswordChangeEvent) => setValue2(e.value as string)} />
+                <Password value={value2} onValueChange={(e: PasswordValueChangeEvent) => setValue2(e.value as string)} />
                 <Label htmlFor="in_label">In Label</Label>
             </FloatLabel>
 
             <FloatLabel variant="on">
-                <Password value={value3} onValueChange={(e: PasswordChangeEvent) => setValue3(e.value as string)} />
+                <Password value={value3} onValueChange={(e: PasswordValueChangeEvent) => setValue3(e.value as string)} />
                 <Label htmlFor="on_label">On Label</Label>
             </FloatLabel>
         </div>
