e85011c32ec570c5cc14252ec54ebd557c35ac10
Mert Sincan <sincan.mert@gmail.com>
2026-02-11 09:34:19 +0000

    fix: contextmenu style

--- packages/@primereact/styles/src/contextmenu/ContextMenu.style.ts
+++ packages/@primereact/styles/src/contextmenu/ContextMenu.style.ts
@@ -1,8 +1,10 @@
 import { createStyles } from '@primereact/styles/utils';
 import type { ContextMenuRootInstance } from '@primereact/types/shared/contextmenu';
+import { style } from '@primeuix/styles/contextmenu';
 
 export const styles = createStyles<ContextMenuRootInstance>({
     name: 'contextmenu',
+    style,
     classes: {
         root: 'p-contextmenu',
         list: 'p-contextmenu-list',
--- packages/@primereact/ui/src/contextmenu/root/UIContextMenuRoot.tsx
+++ packages/@primereact/ui/src/contextmenu/root/UIContextMenuRoot.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Component } from '@primereact/core/component';
-import { styles } from '@primereact/styles/contextmenu';
+import { styles } from '@primereact/styles/menu';
 import { withComponent } from '@primereact/ui/base';
 import { mergeDefaultProps } from '@primeuix/utils';
 import { ContextMenuRoot, defaultRootProps } from 'primereact/contextmenu';
